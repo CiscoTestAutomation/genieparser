f79ceb470f76ec6d08373cf2aa584bd0d94d0775
karmoham <karmoham@cisco.com>
2017-05-04 13:34:30 -0700

    Fixing parser README file to point to projects/parser instead of xbu_shared/parser

--- iosxe/tests/test_show_ospf.py
+++ iosxe/tests/test_show_ospf.py
@@ -3,7 +3,6 @@ import unittest
 from unittest.mock import Mock
 
 from ats.topology import Device
-from xbu_shared.genie.ops.ops.base import Base
 from parser.iosxe.show_ospf import ShowIpOspfNeighborDetail,\
                                               ShowIpOspf, \
                                               ShowIpOspfDatabase, \
