47184e6cb2e424c4302bb47929d41c0492a8ebde
Lubna Rasheed (lrasheed) <lrasheed@cisco.com>
2017-06-01 08:36:03 -0700

    nxos: show_bgp fixed UT for ipv6

--- nxos/show_bgp.py
+++ nxos/show_bgp.py
@@ -1388,7 +1388,7 @@ class ShowBgpVrfAllNeighbors(ShowBgpVrfAllNeighborsSchema):
 
             # BGP neighbor is 2.2.2.2,  remote AS 100, ibgp link,  Peer index 1
             # BGP neighbor is 2.2.2.5,  remote AS 200, local AS 333, ebgp link,  Peer index 2
-            p1 = re.compile(r'^\s*BGP +neighbor +is +(?P<neighbor_id>[0-9a-z\.:]+),'
+            p1 = re.compile(r'^\s*BGP +neighbor +is +(?P<neighbor_id>[a-zA-Z0-9\.\:]+),'
                              ' +remote +AS +(?P<remote_as>[0-9]+),'
                              '(?: +local +AS +(?P<local_as>[0-9]+),)?'
                              ' +(?P<link>[a-zA-Z]+) +link, +Peer +index'
@@ -1397,7 +1397,6 @@ class ShowBgpVrfAllNeighbors(ShowBgpVrfAllNeighborsSchema):
             if m:
                 if 'neighbor' not in parsed_dict:
                     parsed_dict['neighbor'] = {}
-
                 neighbor_id = str(m.groupdict()['neighbor_id'])
                 if neighbor_id not in parsed_dict['neighbor']:
                     parsed_dict['neighbor'][neighbor_id] = {}
--- nxos/tests/test_show_bgp.py
+++ nxos/tests/test_show_bgp.py
@@ -2330,70 +2330,51 @@ class test_show_bgp_vrf_all_neighbors(unittest.TestCase):
                 {'address_family':
                     {'ipv4 multicast':
                         {'bgp_table_version': 55,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
                             'memory_usage': 0},
-                        'neighbor_version': 0,
                         'send_community': True},
                     'ipv4 unicast':
                         {'bgp_table_version': 6765004,
-                        'next_hop_self': False,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
                             'memory_usage': 0},
-                        'route_reflector_client': True,
-                        'neighbor_version': 0,
                         'send_community': True},
                     'ipv6 multicast':
                         {'bgp_table_version': 2,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
                             'memory_usage': 0},
-                        'neighbor_version': 0,
                         'send_community': True},
                     'ipv6 unicast':
                         {'bgp_table_version': 2,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
                             'memory_usage': 0},
-                        'neighbor_version': 0,
                         'send_community': True},
                     'vpnv4 unicast':
                         {'bgp_table_version': 12863408,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
                             'memory_usage': 0},
-                        'neighbor_version': 0,
                         'send_community': True},
                     'vpnv6 unicast':
                         {'bgp_table_version': 2,
+                        'neighbor_version': 0,
                         'path':
                             {'accepted_paths': 0,
-                            'memory_usage': 0},
-                        'neighbor_version': 0,
+                           'memory_usage': 0},
                         'send_community': True}},
-                'bgp_negotiated_capabilities':
-                    {'dynamic_capability': 'advertised '
-                                           '(mp, '
-                                           'refresh, '
-                                            'gr) '
-                                            'received '
-                                            '(mp, '
-                                            'refresh, '
-                                            'gr)',
-                    'dynamic_capability_old': 'advertised '
-                    'received',
-                    'graceful_restart': 'advertised '
-                    'received',
-                    'route_refresh': 'advertised '
-                    'received',
-                    'route_refresh_old': 'advertised '
-                    'received'},
                 'bgp_negotiated_keepalive_timers':
                     {'hold_time': '180',
                     'keepalive_interval': '60',
                     'keepalive_timer': 'not '
-                    'running',
+                                    'running',
                     'last_read': 'never',
                     'last_written': 'never'},
                 'bgp_neighbor_counters':
@@ -2425,14 +2406,8 @@ class test_show_bgp_vrf_all_neighbors(unittest.TestCase):
                         'last_reset': 'never',
                         'reset_by': 'peer',
                         'reset_reason': 'no '
-                        'error'},
-                    'transport':
-                        {'fd': '88'}},
+                                        'error'}},
                 'bgp_version': 4,
-                'graceful_restart_paramters':
-                    {'restart_time_advertised_by_peer_seconds': 120,
-                    'restart_time_advertised_to_peer_seconds': 120,
-                    'stale_time_advertised_by_peer_seconds': 300},
                 'link': 'ebgp',
                 'local_as': 'None',
                 'peer_index': 8,
@@ -2440,11 +2415,11 @@ class test_show_bgp_vrf_all_neighbors(unittest.TestCase):
                 'received_messages': 0,
                 'received_notifications': 0,
                 'remote_as': '888',
-                'retry_time': '00:00:29',
+                'retry_time': '00:01:12',
                 'router_id': '0.0.0.0',
                 'session_state': 'Idle',
                 'shutdown': False,
-                'up_time': '01:27:55'},
+                'up_time': '01:27:52'},
             '4.4.4.4':
                 {'bgp_negotiated_keepalive_timers':
                     {'hold_time': '180',
@@ -2495,7 +2470,165 @@ class test_show_bgp_vrf_all_neighbors(unittest.TestCase):
                 'router_id': '0.0.0.0',
                 'session_state': 'Idle',
                 'shutdown': False,
-                'up_time': '01:27:54'}}}
+                'up_time': '01:27:54'},
+            'fec1::1002':
+                {'address_family':
+                    {'ipv4 unicast':
+                        {'bgp_table_version': 6765004,
+                        'neighbor_version': 6765004,
+                        'next_hop_self': False,
+                        'path':
+                            {'accepted_paths': 5,
+                            'memory_usage': 660},
+                        'route_reflector_client': True,
+                        'send_community': True}},
+                'bgp_negotiated_capabilities':
+                    {'dynamic_capability': 'advertised '
+                                            '(mp, '
+                                            'refresh, '
+                                            'gr) '
+                                            'received '
+                                            '(mp, '
+                                            'refresh, '
+                                            'gr)',
+                    'dynamic_capability_old': 'advertised '
+                                              'received',
+                    'graceful_restart': 'advertised '
+                                        'received',
+                    'route_refresh': 'advertised '
+                                     'received',
+                    'route_refresh_old': 'advertised '
+                                         'received'},
+                'bgp_negotiated_keepalive_timers':
+                    {'hold_time': '180',
+                    'keepalive_interval': '60',
+                    'keepalive_timer': 'expiry '
+                                       'due '
+                                       '00:00:20',
+                    'last_read': '00:00:39',
+                    'last_written': '00:00:39'},
+                'bgp_neighbor_counters':
+                    {'messages':
+                        {'received':
+                            {'bytes_in_queue': 0,
+                            'capability': 6,
+                            'keepalives': 17,
+                            'notifications': 0,
+                            'opens': 5,
+                            'route_refresh': 0,
+                            'total': 43,
+                            'total_bytes': 1420,
+                            'updates': 15},
+                        'sent':
+                            {'bytes_in_queue': 0,
+                            'capability': 6,
+                            'keepalives': 17,
+                            'notifications': 0,
+                            'opens': 5,
+                            'route_refresh': 0,
+                            'total': 33,
+                            'total_bytes': 1739,
+                            'updates': 20}}},
+                'bgp_session_transport':
+                    {'connection':
+                        {'dropped': 4,
+                        'established': 5,
+                        'last_reset': 'never',
+                        'reset_by': 'us',
+                        'reset_reason': 'no '
+                        'error'},
+                    'transport':
+                        {'fd': '88'}},
+                'bgp_version': 4,
+                'graceful_restart_paramters':
+                    {'restart_time_advertised_by_peer_seconds': 120,
+                    'restart_time_advertised_to_peer_seconds': 120,
+                    'stale_time_advertised_by_peer_seconds': 300},
+                'link': 'ibgp',
+                'local_as': 'None',
+                'peer_index': 3,
+                'received_bytes_queue': 0,
+                'received_messages': 43,
+                'received_notifications': 0,
+                'remote_as': '333',
+                'retry_time': 'None',
+                'router_id': '21.0.101.1',
+                'session_state': 'Established',
+                'shutdown': False,
+                'up_time': '00:07:40'},
+            'fec1::2002':
+                {'address_family':
+                    {'ipv4 unicast':
+                        {'bgp_table_version': 6765004,
+                        'neighbor_version': 0,
+                        'path':
+                            {'accepted_paths': 0,
+                            'memory_usage': 0},
+                        'send_community': True},
+                    'ipv6 multicast':
+                        {'bgp_table_version': 2,
+                        'neighbor_version': 0,
+                        'path':
+                            {'accepted_paths': 0,
+                            'memory_usage': 0},
+                        'send_community': True},
+                    'ipv6 unicast':
+                        {'bgp_table_version': 2,
+                        'neighbor_version': 0,
+                        'path':
+                            {'accepted_paths': 0,
+                            'memory_usage': 0},
+                        'send_community': True}},
+                'bgp_negotiated_keepalive_timers':
+                    {'hold_time': '180',
+                    'keepalive_interval': '60',
+                    'keepalive_timer': 'not '
+                    'running',
+                    'last_read': 'never',
+                    'last_written': 'never'},
+                'bgp_neighbor_counters':
+                    {'messages':
+                        {'received':
+                            {'bytes_in_queue': 0,
+                            'capability': 0,
+                            'keepalives': 0,
+                            'notifications': 0,
+                            'opens': 0,
+                            'route_refresh': 0,
+                            'total': 0,
+                            'total_bytes': 0,
+                            'updates': 0},
+                        'sent':
+                            {'bytes_in_queue': 0,
+                            'capability': 0,
+                            'keepalives': 0,
+                            'notifications': 0,
+                            'opens': 0,
+                            'route_refresh': 0,
+                            'total': 0,
+                            'total_bytes': 0,
+                            'updates': 0}}},
+                'bgp_session_transport':
+                    {'connection':
+                        {'dropped': 0,
+                        'established': 0,
+                        'last_reset': 'never',
+                        'reset_by': 'peer',
+                        'reset_reason': 'no '
+                        'error'}},
+                'bgp_version': 4,
+                'link': 'ebgp',
+                'local_as': 'None',
+                'peer_index': 4,
+                'received_bytes_queue': 0,
+                'received_messages': 0,
+                'received_notifications': 0,
+                'remote_as': '888',
+                'retry_time': '00:00:29',
+                'router_id': '0.0.0.0',
+                'session_state': 'Idle',
+                'shutdown': False,
+                'up_time': '01:27:55'}}}
 
     golden_output3 = {'execute.return_value': '''
         BGP neighbor is 4.4.4.4, remote AS 0, unknown link, Peer index 5
@@ -2930,14 +3063,14 @@ class test_show_bgp_vrf_all_neighbors(unittest.TestCase):
     def test_show_bgp_vrf_default_all_neighbors_golden2(self):
         self.maxDiff = None
         self.device = Mock(**self.golden_output2)
-        obj = ShowBgpVrfAllNeighbors(device=self.device, )
+        obj = ShowBgpVrfAllNeighbors(device=self.device)
         parsed_output = obj.parse(vrf='default')
         self.assertEqual(parsed_output,self.golden_parsed_output2)
 
     def test_show_bgp_vrf_default_all_neighbors_golden3(self):
         self.maxDiff = None
         self.device = Mock(**self.golden_output3)
-        obj = ShowBgpVrfAllNeighbors(device=self.device, )
+        obj = ShowBgpVrfAllNeighbors(device=self.device)
         parsed_output = obj.parse(vrf='default')
         self.assertEqual(parsed_output,self.golden_parsed_output3)
 
