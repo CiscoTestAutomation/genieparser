1fb0da0bb01aa80a2b2ad1be130b0b465913c540
Zhi Li <zhil3@cisco.com>
2017-06-01 10:00:07 -0700

    workaround for import error when import iptools and cnetconf

--- nxos/show_version.py
+++ nxos/show_version.py
@@ -7,7 +7,12 @@ import xmltodict
 from ats import tcl
 from metaparser import MetaParser
 from metaparser.util.schemaengine import Any
-from cnetconf import testmodel
+try:
+    import iptools
+    from cnetconf import testmodel
+except ImportError:
+    pass
+
 
 class ShowVersion(MetaParser):
     """ parser class - implements detail parsing mechanisms for cli, xml, and 
