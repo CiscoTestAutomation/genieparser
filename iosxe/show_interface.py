68cf6bbf8406edf99857dca918c1df46de52d821
Zhi Li <zhil3@cisco.com>
2017-09-08 08:45:45 -0700

    fixes

--- iosxe/show_interface.py
+++ iosxe/show_interface.py
@@ -187,8 +187,6 @@ class ShowInterfaces(ShowInterfacesSchema):
         out = self.device.execute('show interfaces')
         interface_dict = {}
         unnumbered_dict = {}
-        rx = False
-        tx = False
         for line in out.splitlines():
             line = line.strip()
 
@@ -383,7 +381,8 @@ class ShowInterfaces(ShowInterfacesSchema):
 
             # Auto-duplex, 1000Mb/s, media type is 10/100/1000BaseTX
             # Full-duplex, 1000Mb/s, link type is auto, media type is
-            p11 = re.compile(r'^(?P<duplex_mode>\w+)[\-\s]+duplex, +'
+            # Full Duplex, 1000Mbps, link type is auto, media type is RJ45
+            p11 = re.compile(r'^(?P<duplex_mode>\w+)[\-\s]+[d|D]uplex, +'
                               '(?P<port_speed>[0-9]+)(?: *Mb/s)?,'
                               '( *link +type +is +(?P<link_type>\w+),)?'
                               ' *media +type +is *(?P<media_type>[\w\/]+)?$')
@@ -521,7 +520,6 @@ class ShowInterfaces(ShowInterfacesSchema):
                 in_rate = int(m.groupdict()['in_rate'])
                 in_rate_pkts = int(m.groupdict()['in_rate_pkts'])
                 unit = m.groupdict()['unit']
-
                 # covert minutes to seconds
                 if 'minute' in unit:
                     load_interval = load_interval * 60
@@ -556,12 +554,9 @@ class ShowInterfaces(ShowInterfacesSchema):
                               ' *(?P<out_rate_pkts>[0-9]+) *packets/sec$')
             m = p21.match(line)
             if m:
-                load_interval = int(m.groupdict()['load_interval'])
                 out_rate = int(m.groupdict()['out_rate'])
                 out_rate_pkts = int(m.groupdict()['out_rate_pkts'])
 
-                interface_dict[interface]['counters']['rate']\
-                    ['load_interval'] = load_interval
                 interface_dict[interface]['counters']['rate']\
                     ['out_rate'] = out_rate
                 interface_dict[interface]['counters']['rate']\
@@ -784,86 +779,6 @@ class ShowInterfaces(ShowInterfacesSchema):
         return(interface_dict)
 
 
-    def yang(self):
-        ''' parsing mechanism: yang
-
-        Function yang() defines the yang type output parsing mechanism which
-        typically contains 3 steps: executing, transforming, returning
-        '''
-
-        base = testmodel.BaseTest()
-        logger.info(banner('Connecting Netconf Server {ip} {port} ...'.format(
-                ip = os.environ['YTOOL_NETCONF_HOST'], 
-                port = os.environ['YTOOL_NETCONF_PORT'])))
-        base.connect_netconf()
-
-        netconf_request = """
-            <rpc message-id="101" xmlns="urn:ietf:params:xml:ns:netconf:base:1.0"> 
-                <get> 
-                    <filter>
-                        <native xmlns="urn:ios"><interface></interface></native>
-                    </filter>
-                </get>
-            </rpc>"""
-
-        logger.info('NETCONF REQUEST: %s' % netconf_request)
-        
-        # netconf_request += "\n##\n"
-        ncout = base.netconf.send_config(netconf_request)
-
-        logger.info('NETCONF RETURN: %s' % ncout)
-
-        filtered_result = xmltodict.parse(ncout, process_namespaces=True,
-            namespaces={'urn:ietf:params:xml:ns:netconf:base:1.0':None,
-                        'urn:ios':None,'urn:iosxr':None,'urn:nxos':None,})
-        partial_result = dict(
-                            filtered_result['rpc-reply']['data'].get('native'))
-        logger.info('NETCONF DICT: %s' % pprint.pformat(partial_result))
-
-        # to satisfy the schema, we need to rerange thestructure for yang output
-        recursivedict = lambda: defaultdict(recursivedict)
-        transfered_dict = recursivedict()
-        
-        for intf in partial_result['interface']:
-            if type(partial_result['interface'][intf]) is list:
-                for item in partial_result['interface'][intf]:
-                    ip = item['ip']['address']['primary']['address'] \
-                        if 'address' in item['ip'] else ''
-                    mask = item['ip']['address']['primary']['mask'] \
-                        if 'address' in item['ip'] else ''
-                    ip_mask = ip + \
-                              '/' + \
-                              str(iptools.ipv4.netmask2prefix(mask)) \
-                              if ip and mask else ''
-                    admin_state = 'administratively down' \
-                        if 'shutdown' in item else 'up'
-                    interface = (intf+item['name']).lower()
-                    transfered_dict['interface'][interface]['ip_address'] = \
-                        ip_mask
-                    transfered_dict['interface'][interface]['admin_state'] = \
-                        admin_state
-            else:
-                item = partial_result['interface'][intf]
-                ip = item['ip']['address']['primary']['address'] \
-                    if 'address' in item['ip'] else ''
-                mask = item['ip']['address']['primary']['mask'] \
-                    if 'address' in item['ip'] else ''
-                ip_mask = ip + '/' + str(iptools.ipv4.netmask2prefix(mask)) \
-                    if ip and mask else ''
-                admin_state = 'administratively down' \
-                    if 'shutdown' in item else 'up'
-                interface  = (intf + 
-                              partial_result['interface'][intf]['name']).lower()
-                transfered_dict['interface'][interface]\
-                               ['ip_address'] = ip_mask
-                transfered_dict['interface'][interface]\
-                               ['admin_state'] = admin_state
-
-        # to satisfy the schema, need to complete the dict by merging cli output
-        result = self.cli()
-        result = merge_dict(result, transfered_dict, update=True)
-        return result
-
 # parser using parsergen
 # ----------------------
 class ShowIpInterfaceBriefSchema(MetaParser):
@@ -1028,199 +943,3 @@ class ShowIpInterfaceBriefPipeVlan(ShowIpInterfaceBrief):
     def yang_cli(self):
         super(ShowIpInterfaceBriefPipeVlan, self).yang_cli()
 
-
-# switchport administrative mode is what's configured on the switch port while operational mode is what is actually functioning at the moment.
-class ShowInterfacesSwitchportSchema(MetaParser):
-    schema = {'interface':
-                {Any():
-                    {Optional('switchport_mode'):
-                        {Optional(Any()):
-                            {Optional('vlan_id'):
-                                {Optional(Any()):
-                                    {Optional('admin_trunking_encapsulation'): str,
-                                     Optional('allowed_vlans'): str}
-                                },
-                            }
-                        },
-                    },
-                },
-            }
-
-class ShowInterfacesSwitchport(ShowInterfacesSwitchportSchema, MetaParser):
-    """ parser class - implements detail parsing mechanisms for cli, xml, and
-    yang output.
-    """
-    #*************************
-    # schema - class variable
-    #
-    # Purpose is to make sure the parser always return the output
-    # (nested dict) that has the same data structure across all supported
-    # parsing mechanisms (cli(), yang(), xml()).
-
-    def cli(self):
-        ''' parsing mechanism: cli
-
-        Function cli() defines the cli type output parsing mechanism which
-        typically contains 3 steps: exe
-        cuting, transforming, returning
-        '''
-        cmd = 'show interfaces switchport'.format()
-        out = self.device.execute(cmd)
-        intf_dict = {}
-        trunk_section = False
-        access_section = False
-        trunk_encapsulation = ''
-        for line in out.splitlines():
-            line = line.rstrip()
-            p1 = re.compile(r'^\s*Name:\s*(?P<interface_name>[a-zA-Z0-9\/]+)$')
-            m = p1.match(line)
-            if m:
-                interface_name = m.groupdict()['interface_name']
-                if 'interface' not in intf_dict:
-                    intf_dict['interface'] = {}
-                if interface_name not in intf_dict['interface']:
-                    intf_dict['interface'][interface_name] = {}
-                continue
-
-            p2 = re.compile(r'^\s*Administrative Mode:\s*(?P<admin_mode>[a-z\s*]+)$')
-            m = p2.match(line)
-            if m:
-                admin_mode = m.groupdict()['admin_mode']
-                if any(word in admin_mode for word in ['trunk', 'access']):
-                    if 'switchport_mode' not in intf_dict['interface']:
-                        intf_dict['interface'][interface_name]['switchport_mode'] = {}
-                    if admin_mode not in intf_dict['interface'][interface_name]['switchport_mode']:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode] = {}
-
-                if 'trunk' in admin_mode:
-                    trunk_section = True
-                    access_section = False
-                elif 'access' in admin_mode:
-                    access_section = True
-                    trunk_section = False
-                continue
-
-            p3 = re.compile(r'^\s*Trunking Native Mode VLAN:\s*(?P<trunking_native_vlan>[0-9]+)( \([a-zA-Z]+\))*$')
-            m = p3.match(line)
-            if m:
-                vlan_id = m.groupdict()['trunking_native_vlan']
-                if any(word in admin_mode for word in ['trunk', 'access']):
-                    if 'vlan_id' not in intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'] = {}
-                    if vlan_id not in intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id']:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'][vlan_id] = {}
-                    if trunk_encapsulation:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]\
-                            ['vlan_id'][vlan_id]['admin_trunking_encapsulation'] = trunk_encapsulation
-                continue
-
-            p4 = re.compile(r'^\s*Access Mode VLAN:\s*(?P<access_mode_vlan_id>[a-z0-9]+)( \([a-zA-Z]+\))*$')
-            m = p4.match(line)
-            if m:
-                vlan_id = m.groupdict()['access_mode_vlan_id']
-                if any(word in admin_mode for word in ['trunk', 'access']):
-                    if 'vlan_id' not in intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'] = {}
-                    if vlan_id not in intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id']:
-                        intf_dict['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'][vlan_id] = {}
-                continue
-
-            if trunk_section:
-                p5 = re.compile(r'^\s*Administrative Trunking Encapsulation:\s*(?P<admin_trunking_encapsulation>[a-z0-9]+)$')
-                m = p5.match(line)
-                if m:
-                    trunk_encapsulation = m.groupdict()['admin_trunking_encapsulation']
-                    continue
-
-        return intf_dict
-
-    def yang(self):
-        ''' parsing mechanism: yang
-
-        Function yang() defines the yang type output parsing mechanism which
-        typically contains 3 steps: executing, transforming, returning
-        '''
-        ret = {}
-        cmd = '''<native><interface><GigabitEthernet/></interface></native>'''
-        output = self.device.get(('subtree', cmd))
-
-        for data in output.data:
-            for native in data:
-                for interface in native:
-                    gig_number = None
-                    interface_name = None
-                    admin_mode = None
-                    vlan_id = None
-                    allowed_vlans = None
-                    for gigabitethernet in interface:
-                        # Remove the namespace
-                        text = gigabitethernet.tag[gigabitethernet.tag.find('}')+1:]
-                        if text == 'name':
-                            gig_number = gigabitethernet.text
-                            interface_name = 'Gigabitethernet' + str(gig_number)
-                            continue
-                        if text == 'switchport':
-                            for switchport in gigabitethernet:
-                                # admin_mode = None
-                                text = switchport.tag[switchport.tag.find('}')+1:]
-                                #ydk.models.ned_edison.ned.Native.Interface.Gigabitethernet.Switchport.Trunk
-                                #ydk.models.xe_recent_edison.Cisco_IOS_XE_native.Native.Interface.Gigabitethernet.Switchport.Trunk
-                                if text == 'trunk':
-                                    admin_mode = 'trunk'
-                                    for vlan in switchport:
-                                        # vlan_id = None
-                                        text = vlan.tag[vlan.tag.find('}')+1:]
-                                        #ydk.models.ned_edison.ned.Native.Interface.Gigabitethernet.Switchport.Trunk.Native_
-                                        #ydk.models.xe_recent_edison.Cisco_IOS_XE_native.Native.Interface.Gigabitethernet.Switchport.Trunk.Native_
-                                        if text == 'native':
-                                            for item in vlan:
-                                                text = item.tag[item.tag.find('}')+1:]
-                                                vlan_id = item.text
-                                        #ydk.models.ned_edison.ned.Native.Interface.Gigabitethernet.Switchport.Trunk.Allowed
-                                        #ydk.models.xe_recent_edison.Cisco_IOS_XE_native.Native.Interface.Gigabitethernet.Switchport.Trunk.Allowed
-                                        # allowed_vlans = None
-                                        if text == 'allowed':
-                                            #ydk.models.ned_edison.ned.Native.Interface.Gigabitethernet.Switchport.Trunk.Allowed.Vlan
-                                            #ydk.models.xe_recent_edison.Cisco_IOS_XE_native.Native.Interface.Gigabitethernet.Switchport.Trunk.Allowed.Vlan
-                                            for item in vlan:
-                                                # Remove the namespace
-                                                text = item.tag[item.tag.find('}')+1:]
-                                                if text == 'vlan':
-                                                    for stuff in item:
-                                                        text = stuff.tag[stuff.tag.find('}')+1:]
-                                                        if text == 'vlans':
-                                                            allowed_vlans = stuff.text
-                                                        continue
-                                #ydk.models.ned_edison.ned.Native.Interface.Gigabitethernet.Switchport.Access
-                                #ydk.models.xe_recent_edison.Cisco_IOS_XE_native.Native.Interface.Gigabitethernet.Switchport.Access
-                                if text == 'access':
-                                    admin_mode = 'access'
-                                    for vlan in switchport:
-                                        vlan_id = None
-                                        for item in vlan:
-                                            text = item.tag[item.tag.find('}')+1:]
-                                            vlan_id = item.text
-                                            continue
-                    # Let's build it now
-                    if 'interface' not in ret:
-                        ret['interface'] = {}
-                    if interface_name is not None:
-                        ret['interface'][interface_name] = {}
-                        if admin_mode is not None:
-                            if 'switchport_mode' not in ret['interface'][interface_name]:
-                                ret['interface'][interface_name]['switchport_mode'] = {}
-                            ret['interface'][interface_name]['switchport_mode'][admin_mode] = {}
-                            if vlan_id is not None:
-                                if 'vlan_id' not in ret['interface'][interface_name]['switchport_mode'][admin_mode]:
-                                    ret['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'] = {}
-                                ret['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'][vlan_id] = {}
-                                if allowed_vlans is not None:
-                                    ret['interface'][interface_name]['switchport_mode'][admin_mode]['vlan_id'][vlan_id]['allowed_vlans'] = allowed_vlans
-
-        return ret
-
-    def yang_cli(self):
-        cli_output = self.cli()
-        yang_output = self.yang()
-        merged_output = _merge_dict(yang_output,cli_output)
-        return merged_output
