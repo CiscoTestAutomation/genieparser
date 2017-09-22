bef90cb266a985254f19169d4f5ee982d19ea40f
Takashi Higashimura <tahigash@cisco.com>
2017-09-22 00:18:08 -0400

    NX HSRP Parser incl. unittest updated

--- nxos/tests/test_show_hsrp.py
+++ nxos/tests/test_show_hsrp.py
@@ -93,42 +93,53 @@ class test_show_hsrp_all(unittest.TestCase):
     device = Device(name='aDevice')
     empty_output = {'execute.return_value': ''}
     
-    golden_parsed_output = {
-        "Ethernet4/1": {
-            "address_family": {
-                "ipv4": {
-                    "version": {
-                        2: {
-                            "groups": {
-                                0: {
-                                    "active_router": "unknown",
-                                    "authentication": "cisco123",
-                                    "configured_priority": 110,
-                                    "hsrp_router_state": "initial(interface down)",
-                                    "session_name": "hsrp-Eth4/1-0",
-                                    "last_state_change": "never",
-                                    "lower_fwd_threshold": 1,
-                                    "num_state_changes": 0,
-                                    "preempt": True,
-                                    "priority": 110,
-                                    "standby_router": "unknown",
-                                    "timers": {
-                                        "hello_sec": 1,
-                                        "hold_sec": 3
-                                    },
-                                    "upper_fwd_threshold": 110,
-                                    'primary_ipv4_address': {
-                                        'address': "192.168.1.254",
-                                    },
-                                    "virtual_mac_addr": "0000.0c9f.f000"
-                                }
-                            }
-                        }
-                    }
+    golden_parsed_output = \
+    {
+      'Ethernet4/1': {
+        'address_family': {
+          'ipv4': {
+            'version': {
+              2: {
+                'groups': {
+                  0: {
+                  'active_router': 'unknown',
+                  'authentication': 'cisco123',
+                  'configured_priority': 110,
+                  'group_number': 0,
+                  'hsrp_router_state': 'initial',
+                  'hsrp_router_state_reason': 'interface '
+                  'down',
+                  'last_state_change': 'never',
+                  'lower_fwd_threshold': 1,
+                  'num_state_changes': 0,
+                  'preempt': True,
+                    'primary_ipv4_address': {
+                    'address': '192.168.1.254',
+                    'virtual_ip_learn': False
+                    },
+                  'priority': 110,
+                  'session_name': 'hsrp-Eth4/1-0',
+                  'standby_router': 'unknown',
+                    'timers': {
+                    'hello_msec_flag': False,
+                    'hello_sec': 1,
+                    'hold_msec_flag': False,
+                    'hold_sec': 3
+                    },
+                  'upper_fwd_threshold': 110,
+                  'virtual_mac_address': '0000.0c9f.f000',
+                  'virtual_mac_address_status': 'default'
+                  }
                 }
+              }
             }
-        }
+          }
+        },
+      'interface': 'Ethernet4/1',
+      'use_bia': False
+      }
     }
+
 
 
     golden_output = {'execute.return_value': '''
@@ -147,97 +158,130 @@ class test_show_hsrp_all(unittest.TestCase):
 
 
 
-    golden_parsed_output_1 = {
-        "Ethernet1/3": {
-            "address_family": {
-                "ipv4": {
-                    "version": {
-                        2: {
-                            "groups": {
-                                0: {
-                                    "active_priority": 110,
-                                    "active_router": "local",
-                                    "authentication": "cisco123",
-                                    "configured_priority": 110,
-                                    "hsrp_router_state": "active",
-                                    "session_name": "hsrp-Eth1/3-0",
-                                    "last_state_change": "00:01:43",
-                                    "lower_fwd_threshold": 0,
-                                    "num_state_changes": 10,
-                                    "preempt": True,
-                                    "priority": 110,
-                                    "standby_expire": 2.429,
-                                    "standby_priority": 90,
-                                    "standby_router": "192.168.1.2",
-                                    "standby_ip_address": "192.168.1.2",
-                                    "timers": {
-                                        "hello_sec": 1,
-                                        "hold_sec": 3
-                                    },
-                                    "upper_fwd_threshold": 110,
-                                    'primary_ipv4_address': {
-                                        'address': "192.168.1.254",
-                                    },
-                                    "virtual_mac_addr": "0000.0c9f.f000"
-                                },
-                                2: {
-                                    "active_router": "unknown",
-                                    "authentication": "cisco",
-                                    "configured_priority": 1,
-                                    "hsrp_router_state": "disabled(virtual ip not cfged)",
-                                    "session_name": "hsrp-Eth1/3-2",
-                                    "last_state_change": "never",
-                                    "lower_fwd_threshold": 0,
-                                    "num_state_changes": 0,
-                                    "priority": 1,
-                                    "standby_router": "unknown",
-                                    "timers": {
-                                        "hello_sec": 3,
-                                        "hold_sec": 10
-                                    },
-                                    "upper_fwd_threshold": 1,
-                                    "virtual_mac_addr": "0000.0c9f.f002"
-                                }
-                            }
-                        }
-                    }
-                },
-                "ipv6": {
-                    "version": {
-                        2: {
-                            "groups": {
-                                2: {
-                                    "active_priority": 100,
-                                    "active_router": "local",
-                                    "authentication": "cisco",
-                                    "configured_priority": 100,
-                                    "hsrp_router_state": "active",
-                                    "session_name": "hsrp-Eth1/3-2-V6",
-                                    "last_state_change": "02:43:40",
-                                    "lower_fwd_threshold": 0,
-                                    "num_state_changes": 2,
-                                    "priority": 100,
-                                    "secondary_vips": "192:168::1",
-                                    "standby_expire": 8.96,
-                                    "standby_priority": 90,
-                                    "standby_router": "fe80::20c:29ff:fe69:14bb",
-                                    "standby_ipv6_address": "fe80::20c:29ff:fe69:14bb",
-                                    "timers": {
-                                        "hello_sec": 3,
-                                        "hold_sec": 10
-                                    },
-                                    "upper_fwd_threshold": 100,
-                                    'link_local_ipv6_address': {
-                                        'address': "fe80::5:73ff:fea0:2",
-                                    },
-                                    "virtual_mac_addr": "0005.73a0.0002"
-                                }
-                            }
-                        }
-                    }
+    golden_parsed_output_1 = \
+    {
+      'Ethernet1/3': {
+        'address_family': {
+          'ipv4': {
+            'version': {
+              2: {
+                'groups': {
+                  0: {
+                  'active_priority': 110,
+                  'active_router': 'local',
+                  'authentication': 'cisco123',
+                  'configured_priority': 110,
+                  'group_number': 0,
+                  'hsrp_router_state': 'active',
+                  'last_state_change': '00:01:43',
+                  'lower_fwd_threshold': 0,
+                  'num_state_changes': 10,
+                  'preempt': True,
+                    'primary_ipv4_address': {
+                    'address': '192.168.1.254',
+                    'virtual_ip_learn': False
+                    },
+                  'priority': 110,
+                  'session_name': 'hsrp-Eth1/3-0',
+                  'standby_expire': 2.429,
+                  'standby_ip_address': '192.168.1.2',
+                  'standby_priority': 90,
+                  'standby_router': '192.168.1.2',
+                    'timers': {
+                    'hello_msec_flag': False,
+                    'hello_sec': 1,
+                    'hold_msec_flag': False,
+                    'hold_sec': 3
+                    },
+                    'tracked_objects': {
+                      1: {
+                      'object_name': 1,
+                      'priority_decrement': 22,
+                      'status': 'UP'
+                      }
+                    },
+                  'upper_fwd_threshold': 110,
+                  'virtual_mac_address': '0000.0c9f.f000',
+                  'virtual_mac_address_status': 'default'
+                  },
+                  2: {
+                  'active_router': 'unknown',
+                  'authentication': 'cisco',
+                  'configured_priority': 1,
+                  'group_number': 2,
+                  'hsrp_router_state': 'disabled',
+                  'hsrp_router_state_reason': 'virtual '
+                  'ip '
+                  'not '
+                  'cfged',
+                  'last_state_change': 'never',
+                  'lower_fwd_threshold': 0,
+                  'num_state_changes': 0,
+                  'priority': 1,
+                  'session_name': 'hsrp-Eth1/3-2',
+                  'standby_router': 'unknown',
+                    'timers': {
+                    'hello_msec_flag': False,
+                    'hello_sec': 3,
+                    'hold_msec_flag': False,
+                    'hold_sec': 10
+                    },
+                  'upper_fwd_threshold': 1,
+                  'virtual_mac_address': '0000.0c9f.f002',
+                  'virtual_mac_address_status': 'default'
+                  }
                 }
+              }
             }
-        }
+          },
+          'ipv6': {
+            'version': {
+              2: {
+                'groups': {
+                  2: {
+                  'active_priority': 100,
+                  'active_router': 'local',
+                  'authentication': 'cisco',
+                  'configured_priority': 100,
+                    'global_ipv6_addresses': {
+                      '192:168::1': {
+                      'address': '192:168::1'
+                      }
+                    },
+                  'group_number': 2,
+                  'hsrp_router_state': 'active',
+                  'last_state_change': '02:43:40',
+                    'link_local_ipv6_address': {
+                    'address': 'fe80::5:73ff:fea0:2',
+                    'auto_configure': True
+                    },
+                  'lower_fwd_threshold': 0,
+                  'num_state_changes': 2,
+                  'priority': 100,
+                  'secondary_vips': ['192:168::1'],
+                  'session_name': 'hsrp-Eth1/3-2-V6',
+                  'standby_expire': 8.96,
+                  'standby_ipv6_address': 'fe80::20c:29ff:fe69:14bb',
+                  'standby_priority': 90,
+                  'standby_router': 'fe80::20c:29ff:fe69:14bb',
+                    'timers': {
+                    'hello_msec_flag': False,
+                    'hello_sec': 3,
+                    'hold_msec_flag': False,
+                    'hold_sec': 10
+                    },
+                  'upper_fwd_threshold': 100,
+                  'virtual_mac_address': '0005.73a0.0002',
+                  'virtual_mac_address_status': 'default'
+                  }
+                }
+              }
+            }
+          }
+        },
+      'interface': 'Ethernet1/3',
+      'use_bia': False
+      }
     }
 
 
@@ -253,6 +297,7 @@ class test_show_hsrp_all(unittest.TestCase):
         Authentication MD5, key-string "cisco123"
         Virtual mac address is 0000.0c9f.f000 (Default MAC)
         10 state changes, last state change 00:01:43
+          Track object 1 state UP decrement 22        
         IP redundancy name is hsrp-Eth1/3-0 (default)
 
       Ethernet1/3 - Group 2 (HSRP-V2) (IPv4)
