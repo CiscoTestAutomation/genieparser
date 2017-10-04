d977ef07febdfd81d7cd61121eacb2ab5e11e838
Zahra Vakhideh <zvakhide@cisco.com>
2017-10-04 15:29:43 -0400

    adding pim bsr election for iosXE

--- iosxe/tests/test_show_pim.py
+++ iosxe/tests/test_show_pim.py
@@ -10,7 +10,8 @@ from metaparser.util.exceptions import SchemaEmptyParserError, \
                                        SchemaMissingKeyError
 
 # Parser
-from parser.iosxe.show_pim import ShowIpv6PimInterface
+from parser.iosxe.show_pim import ShowIpv6PimInterface, \
+                                  ShowIpv6PimBsrElection
 
 
 # ============================================
@@ -203,6 +204,127 @@ class test_show_ipv6_pim_interface(unittest.TestCase):
         obj = ShowIpv6PimInterface(device=self.device)
         parsed_output = obj.parse(vrf='VRF1')
         self.assertEqual(parsed_output,self.golden_parsed_output2)
+
+# ============================================
+# Parser for 'show ip pim bsr election'
+# Parser for 'show ip pim vrf xxx bsr election'
+# ============================================
+class test_show_ip_pim_bsr_election(unittest.TestCase):
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_parsed_output_elec_2 = {
+        'vrf':
+            {'default':
+                {
+                'address_family':
+                    {'ipv6':
+                        {'rp':
+                            {'bsr':
+                                {'bsr_candidate': {
+                                    'address': '2001:1:1:1::1',
+                                    'priority': 0,
+                                    'hash_mask_length': 126,
+                                },
+                                'bsr': {
+                                    'address': '2001:1:1:1::1',
+                                    'hash_mask_length': 126,
+                                    'priority': 0,
+                                    'up_time': '00:00:07',
+                                    'scope_range_list': 'ff00::/8',
+                                    'reverse_path_forwarding_address': 'FE80::21E:F6FF:FE2D:3600',
+                                    'reverse_path_forwarding_interface': 'Loopback0',
+                                    'bs_timer': '00:00:52',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        }
+    golden_output_bsr_elec_2 = {'execute.return_value': '''
+    R1_xe#show ipv6 pim bsr election
+        PIMv2 BSR information
+
+        BSR Election Information
+          Scope Range List: ff00::/8
+          This system is the Bootstrap Router (BSR)
+             BSR Address: 2001:1:1:1::1
+             Uptime: 00:00:07, BSR Priority: 0, Hash mask length: 126
+             RPF: FE80::21E:F6FF:FE2D:3600,Loopback0
+             BS Timer: 00:00:52
+          This system is candidate BSR
+              Candidate BSR address: 2001:1:1:1::1, priority: 0, hash mask length: 126
+    '''}
+
+    golden_parsed_output_bsr_elec_1 = {
+        'vrf':
+            {'VRF1':
+                {
+                'address_family':
+                    {'ipv6':
+                        {'rp':
+                            {'bsr':
+                                {'bsr_candidate': {
+                                    'address': '2001:DB8:1:5::1',
+                                    'priority': 0,
+                                    'hash_mask_length': 126,
+                                    },
+                                'bsr': {
+                                    'address': '2001:DB8:1:5::1',
+                                    'hash_mask_length': 126,
+                                    'priority': 0,
+                                    'up_time': '00:08:39',
+                                    'scope_range_list': 'ff00::/8',
+                                    'reverse_path_forwarding_address': 'FE80::5054:FF:FEC3:D71C',
+                                    'reverse_path_forwarding_interface': 'GigabitEthernet3',
+                                    'bs_timer': '00:00:22',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+    }
+    golden_output_bsr_elec_1 = {'execute.return_value': '''
+        R1_xe#show ipv6 pim vrf VRF1 bsr election
+        PIMv2 BSR information
+
+        BSR Election Information
+          Scope Range List: ff00::/8
+          This system is the Bootstrap Router (BSR)
+             BSR Address: 2001:DB8:1:5::1
+             Uptime: 00:08:39, BSR Priority: 0, Hash mask length: 126
+             RPF: FE80::5054:FF:FEC3:D71C,GigabitEthernet3
+             BS Timer: 00:00:22
+          This system is candidate BSR
+              Candidate BSR address: 2001:DB8:1:5::1, priority: 0, hash mask length: 126
+
+        '''}
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowIpv6PimBsrElection(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden_mapping_1(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_bsr_elec_1)
+        obj = ShowIpv6PimBsrElection(device=self.device)
+        parsed_output = obj.parse(vrf='VRF1')
+        self.assertEqual(parsed_output, self.golden_parsed_output_bsr_elec_1)
+
+
+    def test_golden_mapping_2(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_bsr_elec_2)
+        obj = ShowIpv6PimBsrElection(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output_elec_2)
+
 
 if __name__ == '__main__':
     unittest.main()
