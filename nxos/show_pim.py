52dab5bbdf9c2ca286845a652fd04bbd93668515
Zahra Vakhideh <zvakhide@cisco.com>
2017-10-24 10:10:07 -0400

    precommit for interface schema

--- nxos/show_pim.py
+++ nxos/show_pim.py
@@ -2,10 +2,116 @@ import re
 from metaparser import MetaParser
 from metaparser.util.schemaengine import Schema, Any, Optional
 
+# ====================================================
+# schema Parser for 'show ip pim interface vrf all'
+# ====================================================
+class ShowIpPimInterfaceVrfAllSchema(MetaParser):
 
+    '''Schema for show ip pim interface vrf all'''
 
+    schema = {
+        'vrf':{
+            Any():{
+                'interfaces':{
+                    Any():{
+                        'address_family': {
+                            Any(): {
+                                Optional('interface_status'): str,
+                                Optional('ip_address'): str,
+                                Optional('ip_subnet'): str,
+                                Optional('dr_address'): str,
+                                Optional('dr_priority'): str,
+                                Optional('neighbor_count'): str,
+                                Optional('hello_interval'): str,
+                                Optional('hello_expiration'): str,
+                                Optional('neighbor_holdtime'): int,
+                                Optional('dr_delay'): int,
+                                Optional('border_interface'): bool,
+                                Optional('genid'): str,
+                                Optional('hello_md5_ah_authentication'): str,
+                                Optional('neighbor_policy'): str,
+                                Optional('jp_inbound_policy'): str,
+                                Optional('jp_outbound_policy'): str,
+                                Optional('jp_interval'): int,
+                                Optional('jp_next_sending'): int,
+                                Optional('bfd'):{
+                                    Optional('enable'): bool,
+                                    },
+                                Optional('passive_interface'): bool,
+                                Optional('vpc_svi'): bool,
+                                Optional('auto_enabled'): bool,
+                                Optional('statistics'):{
+                                    Optional('general'):{
+                                        Optional('hellos'): str,
+                                        Optional('jps'): str,
+                                        Optional('grafts'): str,
+                                        Optional('grafts_acks'): str,
+                                        Optional('df_offers'): str,
+                                        Optional('df_winners'): str,
+                                        Optional('df_backoffs'): str,
+                                        Optional('df_passes'): str,
+                                    },
+                                    Optional('error'):{
+                                        Optional('checksum'): int,
+                                        Optional('invalid_packet_types'): int,
+                                        Optional('invalid_df_subtypes'): int,
+                                        Optional('authentication_failed'): int,
+                                        Optional('packet_length'): int,
+                                        Optional('bad_version_packets'): int,
+                                        Optional('packets_from_self'): int,
+                                        Optional('packets_from_non_neighbors'): int,
+                                        Optional('packets_ received_on_passiveinterface'): int,
+                                        Optional('jpPs_received_on_rpf_interface'): int,
+                                        Optional('joins_received_with_no_rp'): int,
+                                        Optional('joins_received_with_wrong_rp'): int,
+                                        Optional('joins_received_with_ssm_groups'): int,
+                                        Optional('joins_received_with_bidir_groups'): int,
+                                        Optional('jps_filtered_by_inbound_policy'): int,
+                                        Optional('jps_filtered_by_outbound_policy'): int,
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
 
+# ==========================================================
+#  parser for show ip pim interface vrf all
+#
+# ==========================================================
+class ShowIpPimInterfaceVrfAll(ShowIpPimInterfaceVrfAllSchema):
+    '''Parser for show ip pim interface vrf all'''
 
+    def cli(self):
+
+        cmd = 'show ip pim interface vrf all'
+        out = self.device.execute(cmd)
+        af_name = 'ipv4'
+
+        # Init dictionary
+        parsed_dict = dict()
+
+        for line in out.splitlines():
+            line = line.rstrip()
+
+            #PIM Interface Status for VRF "VRF1"
+            p1 = re.compile(r'^\s*PIM +Interface +Status +for +VRF+ \"(?P<vrf>[\w]+)\"$')
+            m = p1.match(line)
+            if m:
+                vrf = m.groupdict()['vrf']
+
+            # Ethernet2/2, Interface status: protocol-up/link-up/admin-up
+            p2 = re.compile(r'^\s*(?P<interface_name>[\w\/]+),'
+                            ' +Interface +status: +(?P<interface_status>[\w\-\/]+)$')
+            m = p2.match(line)
+            if m:
+                interface_name = m.groupdict()['interface_name']
+                interface_status = m.groupdict()['interface_status']
+
+        return parsed_dict
 # ================================
 # schema Parser for 'show ipv6 pim vrf all detail'
 # ================================
