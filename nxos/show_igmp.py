11792acc03d9bea3c3b4e8b7ba169aab4f4fc74a
Zahra Vakhideh <zvakhide@cisco.com>
2017-11-03 12:27:56 -0400

    adding parser for show ip igmp snooping

--- nxos/show_igmp.py
+++ nxos/show_igmp.py
@@ -11,6 +11,7 @@ NXOS parsers for the following show commands:
     * show ip igmp local-groups
     * show ip igmp local-groups vrf all
     * show ip igmp local-groups vrf <WORD>
+    * show ip igmp snooping
 
 '''
 
@@ -24,6 +25,314 @@ from metaparser.util.schemaengine import Schema, Any, Optional
 # import parser utils
 from parser.utils.common import Common
 
+
+# ==============================================
+#  Schema for show ip igmp snooping
+# ==============================================
+class ShowIpIgmpSnoopingSchema(MetaParser):
+    schema = {
+        Optional('global_configuration'): {
+            Optional('enabled'): str,
+            Optional('v1v2_report_suppression'): str,
+            Optional('v3_report_suppression'): str,
+            Optional('link_local_groups_suppression'): str,
+            Optional('vpc_multicast_optimization'): str,
+        },
+        'vlans': {
+            Any(): { # configuration_vlan_id
+                Optional('ip_igmp_snooping'): str,
+                Optional('lookup_mode'): str,
+                Optional('igmp_querier'): {
+                    Optional('address'): str,
+                    Optional('version'): int,
+                    Optional('interval'): int,
+                    Optional('last_member_query_interval'): int,
+                    Optional('robustness'): int,
+                    },
+                Optional('switch_querier'): str,
+                Optional('igmp_explicit_tracking'): str,
+                Optional('v2_fast_leave'): str,
+                Optional('router_ports_count'): int,
+                Optional('groups_count'): int,
+                Optional('vlan_vpc_function'): str,
+                Optional('active_ports'): list,
+                Optional('report_flooding'): str,
+                Optional('report_flooding_interfaces'): str,
+                Optional('group_address_for_proxy_leaves'): str,
+
+            },
+        },
+    }
+
+# ==============================================
+#  Parser for show ip igmp snooping
+# ==============================================
+class ShowIpIgmpSnooping(ShowIpIgmpSnoopingSchema):
+
+    def cli(self, vlan=''):
+        if vlan:
+            cmd = 'show ip igmp snooping vlan {}'.format(vlan)
+        else:
+            cmd = 'show ip igmp snooping'
+
+        out = self.device.execute(cmd)
+
+        # initial variables
+        ret_dict = {}
+        global_configuration_enabled = v1v2_report_suppression = v3_report_suppression =\
+            link_local_group_suppression_enabled = vpc_multicast_optimization = configuration_vlan_id = \
+            ip_igmp_snooping = lookup_mode = igmp_querier = querier_interval =\
+            querier_last_member_query_interval = querier_robustness = switch_querier = igmp_explicit_tracking = \
+            v2_fast_leave = router_ports_count = groups_count = vlan_vpc_function = report_flooding = \
+            report_flooding_interfaces = group_address_for_proxy_leaves = address = version = ""
+
+        active_ports = []
+
+
+        for line in out.splitlines():
+            if line:
+                line = line.rstrip()
+            else:
+                continue
+
+            # Global IGMP Snooping Information:
+            #  IGMP Snooping enabled
+            p1 = re.compile(r'^\s*IGMP +Snooping +(?P<enabled>\w+)$')
+            m = p1.match(line)
+            if m:
+                global_configuration_enabled = m.groupdict()['enabled']
+
+            #  IGMPv1/v2 Report Suppression enabled
+            p2 = re.compile(r'^\s*IGMPv1\/v2 +Report +Suppression +(?P<v1v2_report_suppression_enabled>\w+)$')
+            m = p2.match(line)
+            if m:
+                v1v2_report_suppression = m.groupdict()['v1v2_report_suppression_enabled']
+
+            #  IGMPv3 Report Suppression disabled
+            p3 = re.compile(r'^\s*IGMPv3 +Report +Suppression +(?P<v3_report_suppression_enabled>\w+)$')
+            m = p3.match(line)
+            if m:
+                v3_report_suppression = m.groupdict()['v3_report_suppression_enabled']
+
+            #  Link Local Groups Suppression enabled
+            p4 = re.compile(r'^\s*Link +Local +Groups +Suppression +(?P<link_local_group_suppression_enabled>\w+)$')
+            m = p4.match(line)
+            if m:
+                link_local_group_suppression_enabled = m.groupdict()['link_local_group_suppression_enabled']
+
+            #  VPC Multicast optimization disabled
+            p5 = re.compile(r'^\s*VPC +Multicast +optimization +(?P<vpc_multicast_optimization_enabled>\w+)$')
+            m = p5.match(line)
+            if m:
+                vpc_multicast_optimization = m.groupdict()['vpc_multicast_optimization_enabled']
+
+            # IGMP Snooping information for vlan 1
+            p6 = re.compile(r'^\s*IGMP +Snooping +information +for +vlan +(?P<vlan_id>\d+)$')
+            m = p6.match(line)
+            if m:
+                configuration_vlan_id = m.groupdict()['vlan_id']
+                address = version = querier_robustness = querier_interval =\
+                    querier_last_member_query_interval = igmp_querier = ""
+
+            #  IGMP snooping enabled
+            p7 = re.compile(r'^\s*IGMP +snooping +(?P<ip_igmp_snooping>\w+)$')
+            m = p7.match(line)
+            if m:
+                ip_igmp_snooping = m.groupdict()['ip_igmp_snooping']
+
+            #  Lookup mode: IP
+            p8 = re.compile(r'^\s*Lookup +mode: +(?P<lookup_mode>\w+)$')
+            m = p8.match(line)
+            if m:
+                lookup_mode = m.groupdict()['lookup_mode'].lower()
+
+            #  IGMP querier none
+            #  IGMP querier present, address: 50.1.1.1, version: 2, i/f Vlan100
+            p9 = re.compile(r'^\s*IGMP +querier +(?P<igmp_querier>\S+)'
+                            '(, +address: +(?P<address>\S+))?'
+                            '(, +version: +(?P<version>\d))?'
+                            '(, +i\/f +(?P<vlan>\w+))?$')
+            m = p9.match(line)
+            if m:
+                igmp_querier = m.groupdict()['igmp_querier']
+                if m.groupdict()['address']:
+                    address = m.groupdict()['address']
+                if m.groupdict()['version']:
+                    version = m.groupdict()['version']
+
+            # Querier interval: 125 secs
+            p9_1 = re.compile(r'^\s*Querier +interval: +(?P<querier_interval>\d+) +secs$')
+            m = p9_1.match(line)
+            if m:
+                querier_interval = m.groupdict()['querier_interval']
+
+
+            # Querier last member query interval: 1 secs
+            p9_2 = re.compile(r'^\s*Querier +last +member +query +interval:'
+                              ' +(?P<querier_last_member_query_interval>\d+) +secs$')
+            m = p9_2.match(line)
+            if m:
+                querier_last_member_query_interval = m.groupdict()['querier_last_member_query_interval']
+
+            # Querier robustness: 2
+            p9_3 = re.compile(r'^\s*Querier +robustness: +(?P<querier_robustness>\d+)$')
+            m = p9_3.match(line)
+            if m:
+                querier_robustness = m.groupdict()['querier_robustness']
+
+            #  Switch-querier disabled
+            p10 = re.compile(r'^\s*Switch\-querier +(?P<switch_querier>\S+)$')
+            m = p10.match(line)
+            if m:
+                switch_querier = m.groupdict()['switch_querier']
+
+            #  IGMP Explicit tracking enabled
+            p11 = re.compile(r'^\s*IGMP +Explicit +tracking +(?P<igmp_explicit_tracking>\w+)$')
+            m = p11.match(line)
+            if m:
+                igmp_explicit_tracking = m.groupdict()['igmp_explicit_tracking']
+
+            #  IGMPv2 Fast leave disabled
+            p12 = re.compile(r'^\s*IGMPv2 +Fast +leave +(?P<v2_fast_leave>\w+)$')
+            m = p12.match(line)
+            if m:
+                v2_fast_leave = m.groupdict()['v2_fast_leave']
+
+            #  Number of router-ports: 1
+            p13 = re.compile(r'^\s*Number +of +router\-ports: +(?P<router_ports_count>\d+)$')
+            m = p13.match(line)
+            if m:
+                router_ports_count = m.groupdict()['router_ports_count']
+
+            #  Number of groups: 0
+            p14 = re.compile(r'^\s*Number +of +groups: +(?P<groups_count>\d+)$')
+            m = p14.match(line)
+            if m:
+                groups_count = m.groupdict()['groups_count']
+
+            #  VLAN vPC function enabled
+            p14_1 = re.compile(r'^\s*VLAN +vPC +function +(?P<vlan_vpc_function>\w+)$')
+            m = p14_1.match(line)
+            if m:
+                vlan_vpc_function = m.groupdict()['vlan_vpc_function']
+
+            #  Active ports:
+            #    Po20        Po30
+            p15_1 = re.compile(r'^(?P<space>\s{4})(?P<active_ports>[\w\s]+)$')
+            m = p15_1.match(line)
+            if m:
+                active_ports = m.groupdict()['active_ports'].split()
+
+            #  Report Flooding: Disabled
+            p16 = re.compile(r'^\s*Report +Flooding: +(?P<report_flooding>\w+)$')
+            m = p16.match(line)
+            if m:
+                report_flooding = m.groupdict()['report_flooding'].lower()
+
+            #  Interfaces for Report Flooding: n/a
+            p17 = re.compile(r'^\s*Interfaces +for +Report +Flooding: +(?P<report_flooding_interfaces>\S+)$')
+            m = p17.match(line)
+            if m:
+                report_flooding_interfaces = m.groupdict()['report_flooding_interfaces']
+
+            #  Use Group Address for Proxy Leaves: no
+            p18 = re.compile(r'^\s*Use +Group +Address +for +Proxy +Leaves: +(?P<group_address_for_proxy_leaves>\S+)$')
+            m = p18.match(line)
+            if m:
+                group_address_for_proxy_leaves = m.groupdict()['group_address_for_proxy_leaves']
+
+
+            if global_configuration_enabled:
+                if 'global_configuration' not in ret_dict:
+                    ret_dict['global_configuration'] = {}
+                ret_dict['global_configuration']['enabled'] = global_configuration_enabled
+
+            if v1v2_report_suppression:
+                if 'global_configuration' not in ret_dict:
+                    ret_dict['global_configuration'] = {}
+                ret_dict['global_configuration']['v1v2_report_suppression'] = v1v2_report_suppression
+
+            if v3_report_suppression:
+                if 'global_configuration' not in ret_dict:
+                    ret_dict['global_configuration'] = {}
+                ret_dict['global_configuration']['v3_report_suppression'] = v3_report_suppression
+
+            if link_local_group_suppression_enabled:
+                if 'global_configuration' not in ret_dict:
+                    ret_dict['global_configuration'] = {}
+                ret_dict['global_configuration']['link_local_groups_suppression'] \
+                    = link_local_group_suppression_enabled
+
+            if vpc_multicast_optimization:
+                if 'global_configuration' not in ret_dict:
+                    ret_dict['global_configuration'] = {}
+                ret_dict['global_configuration']['vpc_multicast_optimization'] \
+                    = vpc_multicast_optimization
+
+            if configuration_vlan_id:
+                if 'vlans' not in ret_dict:
+                    ret_dict['vlans'] = {}
+                if configuration_vlan_id not in ret_dict['vlans']:
+                    ret_dict['vlans'][configuration_vlan_id] = {}
+
+            if ip_igmp_snooping:
+                ret_dict['vlans'][configuration_vlan_id]['ip_igmp_snooping'] = ip_igmp_snooping
+
+            if lookup_mode:
+                ret_dict['vlans'][configuration_vlan_id]['lookup_mode'] = lookup_mode
+
+            if igmp_querier and 'none' not in igmp_querier.lower():
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier'] = {}
+
+            if igmp_querier and 'none' not in igmp_querier.lower() and address:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier']['address'] = address
+
+            if igmp_querier and 'none' not in igmp_querier.lower() and version:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier']['version'] = int(version)
+
+            if igmp_querier and 'none' not in igmp_querier.lower() and querier_interval:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier']['interval'] = int(querier_interval)
+
+            if igmp_querier and 'none' not in igmp_querier.lower() and querier_last_member_query_interval:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier']['last_member_query_interval'] = \
+                    int(querier_last_member_query_interval)
+
+            if igmp_querier and 'none' not in igmp_querier.lower() and querier_robustness:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_querier']['robustness'] = int(querier_robustness)
+
+            if switch_querier:
+                ret_dict['vlans'][configuration_vlan_id]['switch_querier'] = switch_querier
+
+            if igmp_explicit_tracking:
+                ret_dict['vlans'][configuration_vlan_id]['igmp_explicit_tracking'] = igmp_explicit_tracking
+
+            if v2_fast_leave:
+                ret_dict['vlans'][configuration_vlan_id]['v2_fast_leave'] = v2_fast_leave
+
+            if router_ports_count:
+                ret_dict['vlans'][configuration_vlan_id]['router_ports_count'] = int(router_ports_count)
+
+            if groups_count:
+                ret_dict['vlans'][configuration_vlan_id]['groups_count'] = int(groups_count)
+
+            if vlan_vpc_function:
+                ret_dict['vlans'][configuration_vlan_id]['vlan_vpc_function'] = vlan_vpc_function
+
+            if active_ports:
+                ret_dict['vlans'][configuration_vlan_id]['active_ports'] = active_ports
+
+            if report_flooding:
+                ret_dict['vlans'][configuration_vlan_id]['report_flooding'] = report_flooding
+
+            if report_flooding_interfaces:
+                ret_dict['vlans'][configuration_vlan_id]['report_flooding_interfaces'] = report_flooding_interfaces
+
+            if group_address_for_proxy_leaves:
+                ret_dict['vlans'][configuration_vlan_id]['group_address_for_proxy_leaves'] = \
+                    group_address_for_proxy_leaves
+            continue
+
+        return ret_dict
 # ==============================================
 # Parser for 'show ip igmp interface'
 # Parser for 'show ip igmp interface vrf all'
