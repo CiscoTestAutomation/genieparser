99467582569e254e89a7178d18f187511a29dc95
Lubna Rasheed <lrasheed@cisco.com>
2018-01-24 15:59:06 -0800

    iosxe: show_bgp fix

--- iosxe/show_bgp.py
+++ iosxe/show_bgp.py
@@ -143,14 +143,17 @@ class ShowBgpAllDetail(ShowBgpAllDetailSchema):
             # Paths: (1 available, best #1, no table)
             # Paths: (1 available, best #1, table default, RIB-failure(17))
             p2 = re.compile(r'^\s*Paths: +(?P<paths>\((?P<available_path>[0-9]'
-                             '+) +available\, +best +\#(?P<best_path>[0-9]+)\,'
-                             ' +((table +(?P<vrf_id>[a-zA-Z0-9\-]+)(?:\,|\)?)?'
-                             '|(no table\)))?))')
+                             '+) +available\, +(no +best +path|best +\#(?P<best_path>[0-9]+))\,?'
+                             '(?: +((table +(?P<vrf_id>[a-zA-Z0-9\-]+)(?:\,|\)?)?'
+                             '|(no table)?\)))?))')
             m = p2.match(line)
             if m:
                 paths = m.groupdict()['paths']
                 available_path = m.groupdict()['available_path']
-                best_path = m.groupdict()['best_path']
+                if m.groupdict()['best_path']:  
+                    best_path = m.groupdict()['best_path']
+                else: 
+                    best_path = None 
                 if m.groupdict()['vrf_id']:
                     vrf = m.groupdict()['vrf_id']
                 else:
