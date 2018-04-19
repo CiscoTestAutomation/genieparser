43d7b3b0e40ceb6fbde79af445f03683d63f3a8b
jeaubin <jeaubin@cisco.com>
2018-04-19 11:20:38 -0400

    version number

--- src/genie/libs/parser/__init__.py
+++ src/genie/libs/parser/__init__.py
@@ -1,3 +1,18 @@
+'''
+    Module:
+        genie.libs.parser
+
+    Description:
+        This is the library sub-component of Genie for `genie.parser`.
+
+'''
+
+# metadata
+__version__ = '3.0.0'
+__author__ = 'Cisco Systems Inc.'
+__contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
+__copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
+
 __import__('abstract').declare_package(__name__)
 from .base import *
 
