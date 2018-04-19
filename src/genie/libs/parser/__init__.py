152cda9a07ab46c22ec6eff479cd307610a3223c
jeaubin <jeaubin@cisco.com>
2018-04-19 10:12:03 -0400

    Fix import *

--- src/genie/libs/parser/__init__.py
+++ src/genie/libs/parser/__init__.py
@@ -13,6 +13,10 @@ __author__ = 'Cisco Systems Inc.'
 __contact__ = ['pyats-support@cisco.com', 'pyats-support-ext@cisco.com']
 __copyright__ = 'Copyright (c) 2018, Cisco Systems Inc.'
 
-from genie.base import *
+from .base import tcl_invoke_ats_cmd,\
+                  tcl_package_require_caas,\
+                  tcl_package_require_caas_parsers,\
+                  tcl_invoke_caas_abstract_parser,\
+                  CaasMetaParser
 from genie import abstract
 abstract.declare_package(__name__)
