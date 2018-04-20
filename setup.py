7623055985d8e2b7f1ca174a09f0700c29dbfd01
jeaubin <jeaubin@cisco.com>
2018-04-20 07:47:25 -0400

    Fix warning for namespace

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
     keywords = 'genie pyats test automation',
 
     # uses namespace package
-    namespace_packages = ['genie.libs'],
+    namespace_packages = ['genie', 'genie.libs'],
 
     # project packages
     packages = find_packages(where = 'src'),
