20d8c66effc58beedab6e3a3c66534cbeaa1b39d
Zahra Vakhideh <zvakhide@cisco.com>
2019-02-26 10:36:47 -0500

    memory iios

---
+++ src/genie/libs/parser/ios/show_memory.py
@@ -0,0 +1,10 @@
+"""show_memory.py
+
+"""
+
+from genie.libs.parser.iosxe.show_memory import ShowMemoryStatistics as ShowMemoryStatistics_iosxe
+
+class ShowMemoryStatistics(ShowMemoryStatistics_iosxe):
+    """Parser for show memory statistics"""
+    pass
+        
