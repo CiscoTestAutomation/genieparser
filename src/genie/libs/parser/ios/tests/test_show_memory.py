527f490eb42818fd84c696d6f230af7d9d794afa
Zahra Vakhideh <zvakhide@cisco.com>
2019-02-27 16:57:41 -0500

    fixed ios unittests

--- src/genie/libs/parser/ios/tests/test_show_memory.py
+++ src/genie/libs/parser/ios/tests/test_show_memory.py
@@ -7,41 +7,9 @@ from genie.metaparser.util.exceptions import SchemaEmptyParserError,\
                                        SchemaMissingKeyError
 from genie.libs.parser.ios.show_memory import ShowMemoryStatistics
 
+from genie.libs.parser.iosxe.tests.test_show_memory import test_show_memory_statistics as test_show_memory_statistics_iosxe
 
-class test_show_memory_statistics(unittest.TestCase):
-
-    dev = Device(name='c3850')
-    empty_output = {'execute.return_value': ''}
-
-    golden_parsed_output = {
-        "tracekey": "1#f8e3c2db7822c04e58ce2bd2fc7e476a",
-        "name": {
-            "processor": {
-               "total": 856541768,
-               "free": 501425640,
-               "largest": 501041348,
-               "head": "FF86F21010",
-               "lowest": 499097976,
-               "used": 355116128
-            },
-            "lsmpi_io": {
-               "total": 6295128,
-               "free": 824,
-               "largest": 412,
-               "head": "FF867C51A8",
-               "lowest": 824,
-               "used": 6294304
-            }
-        }
-    }
-
-    golden_output = {'execute.return_value': '''\
-        Tracekey : 1#f8e3c2db7822c04e58ce2bd2fc7e476a   
-
-                        Head    Total(b)     Used(b)     Free(b)   Lowest(b)  Largest(b)
-        Processor  FF86F21010   856541768   355116128   501425640   499097976   501041348
-         lsmpi_io  FF867C51A8     6295128     6294304         824         824         412'''
-    }
+class test_show_memory_statistics(test_show_memory_statistics_iosxe):
 
     def test_empty(self):
         self.dev = Mock(**self.empty_output)
