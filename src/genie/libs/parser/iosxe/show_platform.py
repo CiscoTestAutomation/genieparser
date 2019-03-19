bfb187876f2d4d94fb7ad44dc70d89cc0d91b4f1
Lunzhi Dong <lundong@cisco.com>
2019-03-19 11:16:55 -0400

    address comment for ShowProcessesCpuHistory

--- src/genie/libs/parser/iosxe/show_platform.py
+++ src/genie/libs/parser/iosxe/show_platform.py
@@ -4249,3 +4249,131 @@ class ShowPlatformHardwareQfpStatisticsDrop(ShowPlatformHardwareQfpStatisticsDro
                 continue
 
         return ret_dict
+
+
+class ShowProcessesCpuHistorySchema(MetaParser):
+    """Schema for show processes cpu history"""
+
+    schema = {
+        '60s':{
+            Any(): {
+                'maximum': int,
+                Optional('average'): int,
+            },
+        },
+        '60m':{
+            Any(): {
+                'maximum': int,
+                Optional('average'): int,
+            },
+        },
+        '72h':{
+            Any(): {
+                'maximum': int,
+                Optional('average'): int,
+            },
+        },
+    }
+
+
+class ShowProcessesCpuHistory(ShowProcessesCpuHistorySchema):
+    """Parser for show processes cpu history"""
+    
+    cli_command = 'show processes cpu history'
+
+    def cli(self, output=None):
+
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        #           888886666611111                    11111          
+        # 777775555599999666664444466666333335555544444666667777777777
+        p1 = re.compile(r'^ *\d+( +\d+)* *$')
+
+        #          0    5    0    5    0    5    0    5    0    5    0
+        p2 = re.compile(r'^ *0( {4}5 {4}0){5,6} *$')
+
+        # 80     * **#*#**   * *       *                               
+        # 70  *  * **#*#**   * *       *           *                   
+        p3 = re.compile(r'^ *(?P<num>[\d]+)(?P<line>.*#.*$)')
+
+        # CPU% per second (last 60 seconds)
+        p4 = re.compile(r'^ *CPU%.*$')
+
+        # initialize max list & average list & return dictionary
+        max_list = []
+        average_list = []
+        ret_dict = {}
+
+        for line in out.splitlines():
+            strip_line = line[6:]
+            m = p1.match(strip_line)
+            if m:
+                max_list.append(strip_line)
+                continue
+
+            m1 = p3.match(line)
+            m2 = p4.match(line)
+            if m1 or m2:
+                average_list.append(line)
+                continue
+
+        # parser max value
+        tmp = [''] * 72
+        count = 0
+        for line in max_list:
+            m = p2.match(line)
+            if not m:
+                for i, v in enumerate(line):
+                    if v is ' ':
+                        pass
+                    else:
+                        tmp[i] += v
+            else:
+                if count == 0:
+                    sub_dict = ret_dict.setdefault('60s', {})
+                    for i in range(60):
+                        sub_dict.setdefault(i + 1, {}).update({'maximum': int(tmp[i]) if tmp[i] is not '' else 0})
+
+                elif count == 1:
+                    sub_dict = ret_dict.setdefault('60m', {})
+                    for i in range(60):
+                        sub_dict.setdefault(i + 1, {}).update({'maximum': int(tmp[i]) if tmp[i] is not '' else 0})
+
+                else:
+                    sub_dict = ret_dict.setdefault('72h', {})
+                    for i in range(72):
+                        sub_dict.setdefault(i + 1, {}).update({'maximum': int(tmp[i]) if tmp[i] is not '' else 0})
+                tmp = [''] * 72
+                count += 1
+
+        # parser average value
+        count = 0
+        for line in average_list:
+            m = p3.match(line)
+            if count == 0:
+                sub_dict = ret_dict.setdefault('60s', {})
+            elif count == 1:
+                sub_dict = ret_dict.setdefault('60m', {})
+            else:
+                sub_dict = ret_dict.setdefault('72h', {})
+
+            if m:
+                num = int(m.groupdict()['num'])
+                line = m.groupdict()['line']
+                for i, char in enumerate(line):
+                    if char == '#':
+                        t = sub_dict.setdefault(i, {})
+                        if 'average' not in t:
+                            t.update({'average': num})
+
+            else:
+                for value in sub_dict.values():
+                    if 'average' not in value: 
+                        value.update({'average': 0})
+                        
+                count += 1
+
+        return ret_dict
