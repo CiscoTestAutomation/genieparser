96377276ae45ef492c5c893d79aa5b748b373cd3
karmoham <karmoham@cisco.com>
2019-03-20 13:36:18 -0400

    Addressing comments

--- src/genie/libs/parser/iosxe/show_l2vpn.py
+++ src/genie/libs/parser/iosxe/show_l2vpn.py
@@ -41,11 +41,12 @@ class ShowBridgeDomainSchema(MetaParser):
                 'state': str,
                 'mac_learning_state': str,
                 'member_ports': list,
-                'vpls': str,
-                'neighbor': str,
-                'no_of_ports_belongs': str,
-                'port_belonging_group': str,
-                'group_number': str,
+                Any(): {
+                    Any(): {
+                        'num_of_ports': str,
+                        'interfaces': list,
+                    },
+                },
                 'mac_table': {
                     Any(): {
                         'pseudoport': str,
@@ -70,15 +71,17 @@ class ShowBridgeDomain(ShowBridgeDomainSchema):
                   show bridge-domain <WORD>
                   show bridge-domain | count <WORD>"""
 
-    cli_command = 'show bridge-domain'
+    cli_command = ['show bridge-domain', 'show bridge-domain {bd_id}', 'show bridge-domain | count {word}']
 
-    def cli(self, keyword=None, count=False, output=None):
+    def cli(self, bd_id=None, word=None, output=None):
         cli = self.cli_command
         if output is None:
-            if keyword:
-                if count:
-                    cli = cli + ' | count'
-                cli = cli + ' {keyword}'.format(keyword=keyword)
+            if bd_id:
+                cli = self.cli_command[1].format(bd_id=bd_id)
+            elif word:
+                cli = self.cli_command[2].format(word=word)
+            else:
+                cli = self.cli_command[0]
             out = self.device.execute(cli)
         else:
             out = output
@@ -96,15 +99,22 @@ class ShowBridgeDomain(ShowBridgeDomainSchema):
         # Aging-Timer: 3600 second(s)
         p3 = re.compile(r'^Aging-Timer: +(?P<aging_timer>\d+) +second\(s\)$')
 
+        # Load for five secs: 10%/1%; one minute: 11%; five minutes: 12%
+        p4 = re.compile(r'^Load +for.*$')
+
+        # Time source is NTP, 19:54:46.940 JST Wed Nov 2 2016
+        p4_1 = re.compile(r'^Time +source.*$')
+
+        # AED MAC address    Policy  Tag       Age  Pseudoport
+        p4_2 = re.compile(r'^AED +MAC +address +Policy +Tag +Age +Pseudoport$')
+
+        # 1 ports belonging to split-horizon group 0
+        p5 = re.compile(r'^(?P<num_of_ports>\d+) +ports +belonging +to +(?P<port_belonging_group>[\w\-\d]+) +group +(?P<group_number>\d+)$')
+
         #     vfi VPLS-2051 neighbor 27.93.202.64 2051
-        p4 = re.compile(r'^vfi +(?P<vpls>[\d\w\-]+) +neighbor (?P<neighbor>[\d\.]+) +(\d+)$')
-
-        # # 1 ports belonging to split-horizon group 0
-        p5 = re.compile(r'^(?P<no_of_ports_belongs>[\w\-\d]+) +ports +belonging +to +(?P<port_belonging_group>[\w\-\d]+) +group +(?P<group_number>\d+)$')
-
         #     Port-channel1 service instance 2051 (split-horizon)
         #     GigabitEthernet0/0/3 service instance 3051 (split-horizon)
-        p6 = re.compile(r'^(?P<member_port>[\w\d\-\/]+) +service +instance +(\d+) +\(.*\)$')
+        p6 = re.compile(r'^(?P<member_port>[\w\d\-\/\s\.]+)( +\(.*\))?$')
 
         #    AED MAC address    Policy  Tag       Age  Pseudoport
         #    0   0000.A000.0027 forward dynamic   3142 Port-channel1.EFP2051
@@ -116,6 +126,7 @@ class ShowBridgeDomain(ShowBridgeDomainSchema):
 
         for line in out.splitlines():
             line = line.strip()
+            # import pdb; pdb.set_trace()
 
             m = p1.match(line)
             if m:
@@ -143,41 +154,57 @@ class ShowBridgeDomain(ShowBridgeDomainSchema):
 
             m = p4.match(line)
             if m:
-                group = m.groupdict()
-                final_dict.update({k:v for k, v in group.items()})
+                continue
+
+            m = p4_1.match(line)
+            if m:
+                continue
+
+            m = p4_2.match(line)
+            if m:
                 continue
 
             m = p5.match(line)
             if m:
                 group = m.groupdict()
-                final_dict.update({k:v for k, v in group.items()})
+                num_of_ports = group['num_of_ports']
+                port_belonging_group = group['port_belonging_group']+'_group'
+                group_number = group['group_number']
+                final_dict.setdefault(port_belonging_group, {})
+                final_dict[port_belonging_group].setdefault(group_number, {})
+                final_dict[port_belonging_group][group_number]['num_of_ports'] = num_of_ports
+                ret_dict['interfaces'] = []
                 continue
 
             m = p6.match(line)
             if m:
+                m_2 = p7.match(line)
+                if m_2:
+                    group = m_2.groupdict()
+                    pseudoport = group['pseudoport']
+                    mac_address = group['mac_address']
+                    final_dict.setdefault('mac_table', {}).setdefault(
+                        pseudoport, {}).setdefault(
+                            'mac_address', {}).setdefault(mac_address, {})
+                    final_dict['mac_table'][pseudoport]['pseudoport'] = pseudoport
+                    final_dict['mac_table'][pseudoport]['mac_address']\
+                        [mac_address]['mac_address'] = mac_address
+                    final_dict['mac_table'][pseudoport]['mac_address']\
+                        [mac_address]['aed'] = int(group['aed'])
+                    final_dict['mac_table'][pseudoport]['mac_address']\
+                        [mac_address]['policy'] = group['policy']
+                    final_dict['mac_table'][pseudoport]['mac_address']\
+                        [mac_address]['tag'] = group['tag']
+                    final_dict['mac_table'][pseudoport]['mac_address']\
+                        [mac_address]['age'] = int(group['age'])
+                    continue
                 group = m.groupdict()
+                if 'interfaces' in ret_dict and port_belonging_group in final_dict:
+                    ret_dict['interfaces'].append(group['member_port'])
+                    final_dict[port_belonging_group][group_number]\
+                        ['interfaces'] = ret_dict['interfaces']
                 member_port_list.append(group['member_port'])
                 final_dict['member_ports'] = member_port_list
-                continue
-
-            m = p7.match(line)
-            if m:
-                group = m.groupdict()
-                pseudoport = group['pseudoport']
-                mac_address = group['mac_address']
-                final_dict.setdefault('mac_table', {}).setdefault(
-                    pseudoport, {}).setdefault('mac_address', {}).setdefault(mac_address, {})
-                final_dict['mac_table'][pseudoport]['pseudoport'] = pseudoport
-                final_dict['mac_table'][pseudoport]['mac_address']\
-                    [mac_address]['mac_address'] = mac_address
-                final_dict['mac_table'][pseudoport]['mac_address']\
-                    [mac_address]['aed'] = int(group['aed'])
-                final_dict['mac_table'][pseudoport]['mac_address']\
-                    [mac_address]['policy'] = group['policy']
-                final_dict['mac_table'][pseudoport]['mac_address']\
-                    [mac_address]['tag'] = group['tag']
-                final_dict['mac_table'][pseudoport]['mac_address']\
-                    [mac_address]['age'] = int(group['age'])
                 continue
 
             m = p8.match(line)
@@ -185,5 +212,8 @@ class ShowBridgeDomain(ShowBridgeDomainSchema):
                 group = m.groupdict()
                 ret_dict['lines_match_regexp'] = int(group['lines_match_regexp'])
                 continue
+
+        if 'interfaces' in ret_dict:
+            del ret_dict['interfaces']
 
         return ret_dict
