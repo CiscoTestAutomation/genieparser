8d8701c163019a78daf84ef458109261264cce5c
Zahra Vakhideh <zvakhide@cisco.com>
2019-03-25 14:46:15 -0400

    Code indentation

--- src/genie/libs/parser/iosxe/tests/test_show_archive.py
+++ src/genie/libs/parser/iosxe/tests/test_show_archive.py
@@ -91,10 +91,10 @@ class test_show_archive_config_differences(unittest.TestCase):
             'ip subnet-zero', 
             'ip cef', 
             'ip name-server 10.4.4.4', 
-	    'voice dnis-map1', 
+            'voice dnis-map1', 
             'dnis 111', 
             'interface FastEthernet1/0',
-	    'no ip address', 
+            'no ip address', 
             'no ip route-cache', 
             'no ip mroute-cache',
             'shutdown', 
@@ -102,8 +102,8 @@ class test_show_archive_config_differences(unittest.TestCase):
             'ip default-gateway 10.5.5.5',
             'ip classless', 
             'access-list 110 deny	ip any host 10.1.1.1',
-	    'access-list 110 deny	ip any host 10.1.1.2',
-	    'access-list 110 deny	ip any host 10.1.1.3',
+            'access-list 110 deny	ip any host 10.1.1.2',
+            'access-list 110 deny	ip any host 10.1.1.3',
 	    'snmp-server community private RW'
         ]
     }
@@ -126,20 +126,20 @@ class test_show_archive_config_differences(unittest.TestCase):
         ip subnet-zero
         ip cef
         ip name-server 10.4.4.4
-	voice dnis-map1
-	 dnis 111
-	interface FastEthernet1/0
-         no ip address
-         no ip route-cache
-         no ip mroute-cache
-         shutdown
-	 duplex half
+        voice dnis-map1
+            dnis 111
+        interface FastEthernet1/0
+            no ip address
+            no ip route-cache
+            no ip mroute-cache
+            shutdown
+            duplex half
 	ip default-gateway 10.5.5.5
         ip classless
-	access-list 110 deny	ip any host 10.1.1.1
+        access-list 110 deny	ip any host 10.1.1.1
         access-list 110 deny	ip any host 10.1.1.2
-	access-list 110 deny	ip any host 10.1.1.3
-	snmp-server community private RW
+        access-list 110 deny	ip any host 10.1.1.3
+        snmp-server community private RW
         '''
     }
 
