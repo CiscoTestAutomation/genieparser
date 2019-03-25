a0b3629e46ca05de19a44060a20f81e38001121c
Zahra Vakhideh <zvakhide@cisco.com>
2019-03-25 14:23:11 -0400

    Code indentation

--- src/genie/libs/parser/iosxe/tests/test_show_archive.py
+++ src/genie/libs/parser/iosxe/tests/test_show_archive.py
@@ -88,17 +88,24 @@ class test_show_archive_config_differences(unittest.TestCase):
 
     golden_parsed_output_incremental_diff = {
         'list_of_commands': [
-            'ip subnet-zero', 'ip cef', 
+            'ip subnet-zero', 
+            'ip cef', 
             'ip name-server 10.4.4.4', 
-	    'voice dnis-map1', 'dnis 111', 
+	    'voice dnis-map1', 
+            'dnis 111', 
             'interface FastEthernet1/0',
-	    'no ip address', 'no ip route-cache', 
-            'no ip mroute-cache','shutdown', 'duplex half',
-            'ip default-gateway 10.5.5.5','ip classless', 
+	    'no ip address', 
+            'no ip route-cache', 
+            'no ip mroute-cache',
+            'shutdown', 
+            'duplex half',
+            'ip default-gateway 10.5.5.5',
+            'ip classless', 
             'access-list 110 deny	ip any host 10.1.1.1',
 	    'access-list 110 deny	ip any host 10.1.1.2',
 	    'access-list 110 deny	ip any host 10.1.1.3',
-	    'snmp-server community private RW']
+	    'snmp-server community private RW'
+        ]
     }
     
     golden_output = {'execute.return_value': '''\
