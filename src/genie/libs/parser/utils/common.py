852a14df2d760f09b7c336e35a772ad8db3d9da2
Takashi Higashimura <tahigash@cisco.com>
2019-03-25 20:21:04 -0400

    fix in _find_command

--- src/genie/libs/parser/utils/common.py
+++ src/genie/libs/parser/utils/common.py
@@ -79,7 +79,8 @@ def _find_command(command, data, device):
         for pattern in patterns:
             word = pattern.replace('{', '').replace('}', '')
             new_pattern = '(?P<{p}>.*)'.format(p=word)
-            reg = re.sub(pattern, new_pattern, reg)+'$'
+            reg = re.sub(pattern, new_pattern, reg)
+        reg += '$'
 
         match = re.match(reg, command)
         if match:
