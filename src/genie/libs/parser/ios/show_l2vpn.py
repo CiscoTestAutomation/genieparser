25842e86fc2113c20d09361726840919b7ad0a4a
karmoham <karmoham@cisco.com>
2019-03-28 14:10:47 -0400

    Adding IOS unittest for ShowEthernetServiceInstanceStats

--- src/genie/libs/parser/ios/show_l2vpn.py
+++ src/genie/libs/parser/ios/show_l2vpn.py
@@ -21,7 +21,8 @@ from genie.metaparser.util.schemaengine import Schema, \
 
 # import iosxe parser
 from genie.libs.parser.iosxe.show_l2vpn import ShowL2vpnVfi as ShowL2vpnVfi_iosxe,\
-                                               ShowL2vpnServiceAll as ShowL2vpnServiceAll_iosxe
+                                               ShowL2vpnServiceAll as ShowL2vpnServiceAll_iosxe, \
+                                               ShowEthernetServiceInstanceStats as ShowEthernetServiceInstanceStats_iosxe
 
 # ===========================
 # Parser for 'show l2vpn vfi'
@@ -31,7 +32,21 @@ class ShowL2vpnVfi(ShowL2vpnVfi_iosxe):
     """
     pass
 
+
+# ===================================
+# Parser for 'show l2vpn service all'
+# ===================================
 class ShowL2vpnServiceAll(ShowL2vpnServiceAll_iosxe):
     """Parser for show l2vpn service all
+    """
+    pass
+
+
+# =================================================
+# Parser for 'show ethernet service instance stats'
+# =================================================
+class ShowEthernetServiceInstanceStats(ShowEthernetServiceInstanceStats_iosxe):
+    """Parser for show ethernet service instance stats
+                  show ethernet service instance interface <interface> stats
     """
     pass
