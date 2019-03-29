00e709d445008ce661a8d7de972d93e707136cb2
Lunzhi Dong <lundong@cisco.com>
2019-03-29 10:50:55 -0400

    fix run parsers errors

--- src/genie/libs/parser/ios/tests/test_show_lag.py
+++ src/genie/libs/parser/ios/tests/test_show_lag.py
@@ -54,8 +54,76 @@ class test_show_lacp_sysid(test_show_lacp_sysid_iosxe):
 ###################################################
 # unit test for show lacp counter
 ####################################################
-class test_show_lacp_counters(test_show_lacp_counters_iosxe):
+class test_show_lacp_counters(unittest.TestCase):
     """unit test for show lacp counters """
+
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+        Device# show lacp 1 counters
+                      LACPDUs         Marker       LACPDUs
+        Port       Sent   Recv     Sent   Recv     Pkts Err
+        ---------------------------------------------------
+        Channel group: 1
+          Fa4/1    8      15       0      0         3    0
+          Fa4/2    14     18       0      0         3    0
+          Fa4/3    14     18       0      0         0
+          Fa4/4    13     18       0      0         0
+    '''}
+
+    golden_parsed_output = {
+        "interfaces": {
+            "Port-channel1": {
+                "name": "Port-channel1",
+                "protocol": "lacp",
+                "members": {
+                    "FastEthernet4/1": {
+                        "interface": "FastEthernet4/1",
+                        "counters": {
+                            "lacp_in_pkts": 15,
+                            "lacp_out_pkts": 8,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_pkts": 3,
+                            "lacp_errors": 0
+                        }
+                    },
+                    "FastEthernet4/2": {
+                        "interface": "FastEthernet4/2",
+                        "counters": {
+                            "lacp_in_pkts": 18,
+                            "lacp_out_pkts": 14,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_pkts": 3,
+                            "lacp_errors": 0
+                        }
+                    },
+                    "FastEthernet4/3": {
+                        "interface": "FastEthernet4/3",
+                        "counters": {
+                            "lacp_in_pkts": 18,
+                            "lacp_out_pkts": 14,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_pkts": 0
+                        }
+                    },
+                    "FastEthernet4/4": {
+                        "interface": "FastEthernet4/4",
+                        "counters": {
+                            "lacp_in_pkts": 18,
+                            "lacp_out_pkts": 13,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_pkts": 0
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     def test_empty(self):
         self.device = Mock(**self.empty_output)
@@ -74,8 +142,57 @@ class test_show_lacp_counters(test_show_lacp_counters_iosxe):
 ###################################################
 # unit test for show lacp internal
 ####################################################
-class test_show_lacp_internal(test_show_lacp_internal_iosxe):
+class test_show_lacp_internal(unittest.TestCase):
     """unit test for show lacp internal """
+
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+        Device# show lacp 1 internal
+        Channel group 1
+                                    LACPDUs     LACP Port    Admin   Oper    Port     Port
+        Port      Flags    State    Interval    Priority     Key     Key     Number   State
+        Fa4/1     saC      bndl     30s         32768        100     100     0xc1     0x75
+        Fa4/2     saC      bndl     30s         32768        100     100     0xc2     0x75
+    '''}
+
+    golden_parsed_output = {
+        "interfaces": {
+            "Port-channel1": {
+                "name": "Port-channel1",
+                "protocol": "lacp",
+                "members": {
+                    "FastEthernet4/1": {
+                        "interface": "FastEthernet4/1",
+                        "activity": "auto",
+                        "flags": "saC",
+                        "state": "bndl",
+                        "bundled": True,
+                        "lacp_port_priority": 32768,
+                        "admin_key": 100,
+                        "oper_key": 100,
+                        "port_num": 193,
+                        "port_state": 117,
+                        "lacp_interval": "30s"
+                    },
+                    "FastEthernet4/2": {
+                        "interface": "FastEthernet4/2",
+                        "activity": "auto",
+                        "flags": "saC",
+                        "state": "bndl",
+                        "bundled": True,
+                        "lacp_port_priority": 32768,
+                        "admin_key": 100,
+                        "oper_key": 100,
+                        "port_num": 194,
+                        "port_state": 117,
+                        "lacp_interval": "30s"
+                    }
+                }
+            }
+        }
+    }
 
     def test_empty(self):
         self.device = Mock(**self.empty_output)
