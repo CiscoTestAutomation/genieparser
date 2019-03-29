6bbaae4c6742a96b8000a6d7953d2b209e05b1ba
Lunzhi Dong <lundong@cisco.com>
2019-03-29 17:16:42 -0400

    add ios show session parsers

---
+++ src/genie/libs/parser/ios/show_session.py
@@ -0,0 +1,27 @@
+"""show_session.py
+
+"""
+import re
+
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Schema, \
+                                               Any, \
+                                               Optional, \
+                                               Or, \
+                                               And, \
+                                               Default, \
+                                               Use
+
+# import iosxe parser
+from genie.libs.parser.iosxe.show_session import ShowLine as ShowLine_iosxe,\
+                                                 ShowUsers as ShowUsers_iosxe
+
+
+class ShowLine(ShowLine_iosxe):
+    """Parser for show line"""
+    pass
+
+
+class ShowUsers(ShowUsers_iosxe):
+    """Parser for show users"""
+    pass
