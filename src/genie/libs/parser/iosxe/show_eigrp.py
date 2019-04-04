1635712747370348b74a43f8e437eabb19864be3
Felipe Sasso <fcoralsa@cisco.com>
2019-04-04 18:12:51 -0400

    Schema for show ip eigrp neighbors detail. Modified Schema for show ip eigrp neighbors. Tests for show ip eigrp neighbors

--- src/genie/libs/parser/iosxe/show_eigrp.py
+++ src/genie/libs/parser/iosxe/show_eigrp.py
@@ -20,22 +20,64 @@ class ShowEigrpNeighborsSchema(MetaParser):
         * 'show ip eigrp neighbors'
     '''
 
-    schema = { 
-        'eigrp': {
-            Optional('index'):
-                {Any():
-                    {'peer_handle': int,
-                     'nbr_address': str,
-                     'interface': str,
-                     'hold_time': int,
-                     'uptime': str,
-                     'q_cnt': int,
-                     'last_seq_number': int,
-                     'srtt': int,
-                     'rto': float, },
+    schema = {
+        'eigrp_interface': {
+            Any(): {
+                'index': {
+                    Any(): {
+                        Any(): {
+                            'peer_handle': int,
+                            'hold_time': int,
+                            'uptime': str,
+                            'q_cnt': int,
+                            'last_seq_number': int,
+                            'srtt': float,
+                            'rto': int, }, },
+                },
             },
-        },
+        }
     }
+
+
+class ShowEigrpNeighborsDetailSchema(MetaParser):
+    ''' Schema for
+        * 'show ip eigrp neighbors detail'
+    '''
+
+    schema = {
+        'eigrp_instance': {
+            Any(): { 
+            'address_family': {
+                Any(): {                    
+                    'eigrp_interface': {
+                        Any(): {
+                            'index': {
+                                Any(): {
+                                    Any(): {
+                                        'retransmit_count': int,
+                                        'retry_count': int,
+                                        'last_seq_number': int,
+                                        'srtt': float,
+                                        'rto': int,
+                                        'q_cnt': int,
+                                        'nbr_sw_ver': {                                        
+                                            'os_majorver': int,
+                                            'os_minorver': int,
+                                            'tlv_majorrev': int,
+                                            'tlv_minorrev': int, },
+                                        'hold': int,
+                                        'uptime': str,
+                                        'prefixes': int,
+                                        'topology_ids_from_peer': int, }, }, 
+                                    }, 
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        }
+
 
 # ====================================
 # Parser for 'show ip eigrp neighbors'
@@ -51,7 +93,7 @@ class ShowEigrpNeighbors(ShowEigrpNeighborsSchema):
         else:
             out = output
 
-        parsed_dict = {'eirgp': {}}
+        parsed_dict = {}
 
         # H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
         #                             (sec)           (ms)          Cnt Num
@@ -60,35 +102,186 @@ class ShowEigrpNeighbors(ShowEigrpNeighborsSchema):
         # 1   10.1.2.3     Gi0/1      11    00:20:39  2202   5000   0   5
         r1 = re.compile(r'^(?P<peer_handle>\d+) +'
                          '(?P<nbr_address>\S+) +'
-                         '(?P<interface>[A-Za-z]+\s*[\d\/]+) +'
+                         '(?P<eigrp_interface>[A-Za-z]+\s*[\d\/]+) +'
                          '(?P<hold_time>\d+) +(?P<uptime>\S+) +'
                          '(?P<srtt>\d+) +'
                          '(?P<rto>\d+) +'
                          '(?P<q_cnt>\d+) +'
-                         '(?P<last_seq_num>\d+)$')
+                         '(?P<last_seq_number>\d+)$')
+
+        parsed_dict = {'eigrp_interface': {}}
 
         for line in out.splitlines():
             line = line.strip()
 
-            result = r1.match()
+            result = r1.match(line)
 
+            if result:
+                group = result.groupdict()                
+
+                eigrp_interface = group['eigrp_interface']
+                if eigrp_interface in parsed_dict['eigrp_interface']:
+                    index = max(parsed_dict['eigrp_interface'][eigrp_interface]['index'].keys()) + 1                    
+                else:
+                    index = 1                    
+
+                interface_dict = parsed_dict.setdefault('eigrp_interface', {}).setdefault(eigrp_interface, {})\
+                    .setdefault('index', {}).setdefault(index, {})
+            
+                nbr_address = group['nbr_address']
+
+                ip_dict = {}
+                
+                ip_dict['peer_handle'] = int(group['peer_handle'])
+                ip_dict['hold_time'] = int(group['hold_time'])
+                ip_dict['uptime'] = group['uptime']
+                ip_dict['srtt'] = float(group['srtt'])
+                ip_dict['rto'] = int(group['rto'])
+                ip_dict['q_cnt'] = int(group['q_cnt'])
+                ip_dict['last_seq_number'] = int(group['last_seq_number'])
+                interface_dict[nbr_address] = ip_dict
+
+        return parsed_dict
+
+# ===========================================
+# Parser for 'show ip eigrp neighbors detail'
+# ===========================================
+class ShowEigrpNeighborsDetail(ShowEigrpNeighborsDetailSchema):
+
+    cli_command = 'show ip eigrp neighbors detail'
+
+    def cli(self, output=None):
+
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        
+
+        # EIGRP-IPv4 VR(foo) Address-Family Neighbors for AS(1)
+        r1 = re.compile(r'(?P<address_family>[\w \-\(\)]+) +'
+                        'Address-Family\s*Neighbors\s*for '
+                        '\w*\(\s*(?P<as_num>[\S]+)\)')
+
+        # EIGRP-IPv4 Neighbors for AS(100)
+        r2 = re.compile(r'(?P<address_family>[\w \-\(\)]+) +'
+                        'Neighbors\s*for \w*\(\s*(?P<as_num>[\S]+)\)')
+
+        # H     Address     Interface   Hold    Uptime   SRTT   RTO     Q       Seq
+        #                               (sec)                   (ms)    Cnt     Num
+        # 0     10.1.2.1    Et1/0       11      00:02:31 12     200     0       6
+        r3 = re.compile(r'^(?P<peer_handle>\d+) +'
+                         '(?P<nbr_address>\S+) +'
+                         '(?P<eigrp_interface>[A-Za-z]+\s*[\d\/]+) +'
+                         '(?P<hold_time>\d+) +(?P<uptime>\S+) +'
+                         '(?P<srtt>\d+) +'
+                         '(?P<rto>\d+) +'
+                         '(?P<q_cnt>\d+) +'
+                         '(?P<last_seq_number>\d+)$')
+
+        # Version 8.0/2.0, Retrans: 0, Retries: 0, Prefixes: 1
+        r4 = re.compile(r'Version\s*'
+                         '(?P<os_majorver>\d+)\.(?P<os_minorver>\d+)\/'
+                         '(?P<tlv_majorrev>\d+)\.(?P<tlv_minorrev>\d+), +'
+                         'Retrans\s*:\s*(?P<retrans>\d+)\, +'
+                         'Retries\s*:\s*(?P<retries>\d+)')
+
+        # Topology-ids from peer - 0
+        r5 = re.compile(r'Topology\-ids\s+from\s+peer\s+\-\s+'
+                        '(?P<topology_ids_from_peer>\d+)')
+
+        parsed_dict = {}
+        interfaces_dict = {'eigrp_interface': {}}
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            result = r1.match(line)
+            if result:
+
+                group = result.groupdict()
+
+                address_family = group['address_family']
+                as_num = group['as_num']
+
+                instance_dict = parsed_dict.setdefault('eigrp_instace', {})\
+                    .setdefault(as_num, {}).setdefault('address_family', {address_family: {}})
+
+                continue
+
+            result = r2.match(line)
             if result:
                 group = result.groupdict()
 
-                device_dict = devices_dict_info.setdelfault(device_id_index, {})
+                address_family = group['address_family']
+                as_num = group['as_num']
 
-                device_dict['peer_handle'] = group['peer_handle']
-                device_dict['nbr_address'] = group['nbr_address']
-                device_dict['interface'] = group['interface']
-                device_dict['hold_time'] = group['hold_time']
-                device_dict['uptime'] = group['uptime']
-                device_dict['srtt'] = group['srtt']
-                device_dict['rto'] = group['rto']
-                device_dict['q_cnt'] = group['q_cnt']
-                device_dict['last_seq_num'] = group['last_seq_num']
+                # Init dicts
 
-        if device_id_index:
-             parsed_dict.setdefault('cdp', {}).\
-                setdefault('index', devices_dict_info)
+                if as_num not in parsed_dict:
+                    parsed_dict['eigrp_instace'] = {as_num: {}}
+                    parsed_dict['eigrp_instace'][as_num]['address_family'] = {address_family: {}}
 
+                continue
+
+            result = r3.match(line)
+            if result:
+
+                group = result.groupdict()
+                eigrp_interface = group['eigrp_interface']
+
+
+                if eigrp_interface in interfaces_dict:
+                    index = max(interfaces_dict['eigrp_interface'][eigrp_interface]['index'].keys()) + 1
+
+                else:
+                    index = 1
+
+                interface = interfaces_dict.setdefault('eigrp_interface', {})\
+                    .setdefault(eigrp_interface, {}).setdefault('index', {}).setdefault(index, {})
+            
+                nbr_address = group['nbr_address']
+
+                ip_dict = {}
+                
+                ip_dict['peer_handle'] = int(group['peer_handle'])
+                ip_dict['hold_time'] = int(group['hold_time'])
+                ip_dict['uptime'] = group['uptime']
+                ip_dict['srtt'] = float(group['srtt'])
+                ip_dict['rto'] = int(group['rto'])
+                ip_dict['q_cnt'] = int(group['q_cnt'])
+                ip_dict['last_seq_number'] = int(group['last_seq_number'])
+                interface[nbr_address] = ip_dict                
+
+                import pdb; pdb.set_trace()
+
+
+                continue
+
+            result = r4.match(line)
+            if result:
+                group = result.groupdict()
+
+                sw_ver_dict = interface[nbr_address].setdefault('nbr_sw_ver', {})               
+
+                sw_ver_dict['os_majorver'] = group['os_majorver']
+                sw_ver_dict['os_minorver'] = group['os_minorver']
+                sw_ver_dict['tlv_majorrev'] = group['tlv_majorrev']
+                sw_ver_dict['tlv_minorrev'] = group['tlv_minorrev']
+                interface[nbr_address]['retrans'] = group['retrans']
+                interface[nbr_address]['retries'] = group['retries']        
+
+                continue
+
+            result = r5.match(line)
+            if result:
+                group = result.groupdict()
+
+                interface[nbr_address]['topology_ids_from_peer'] = \
+                    group['topology_ids_from_peer']
+
+                continue
+
+        import pdb; pdb.set_trace()
         return parsed_dict
--- src/genie/libs/parser/iosxe/tests/test_show_eigrp.py
+++ src/genie/libs/parser/iosxe/tests/test_show_eigrp.py
@@ -5,18 +5,83 @@ from ats.topology import Device
 
 from genie.metaparser.util.exceptions import SchemaEmptyParserError
 
-from genie.libs.parser.iosxe.show_eigrp import ShowEigrpNeighbors
+from genie.libs.parser.iosxe.show_eigrp import ShowEigrpNeighbors, ShowEigrpNeighborsDetail
 
 class test_show_eigrp_neighbors(unittest.TestCase):
 
     device = Device(name='aDevice')
 
-    expected_parsed_output_1 = {}
-    expected_parsed_output_2 = {}
-    expected_parsed_output_3 = {}
+    expected_parsed_output_1 = {
+        'eigrp_interface': {
+            'Gi0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.2': {
+                            'hold_time': 13,
+                            'last_seq_number': 5,
+                            'peer_handle': 0,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 1996.0,
+                            'uptime': '00:00:03'},},},
+            },
+        },
+    }
+
+    expected_parsed_output_2 = {
+        'eigrp_interface': {
+            'Gi0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.9': {
+                            'hold_time': 14,
+                            'last_seq_number': 5,
+                            'peer_handle': 2,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 206.0,
+                            'uptime': '00:02:24'}},
+                    2: {
+                        '10.1.1.2': {
+                            'hold_time': 13,
+                            'last_seq_number': 5,
+                            'peer_handle': 0,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 1996.0,
+                            'uptime': '00:00:03'}}}}}}
+
+    expected_parsed_output_3 = {
+        'eigrp_interface': {
+            'Gi0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.9': {
+                            'hold_time': 14,
+                            'last_seq_number': 5,
+                            'peer_handle': 2,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 206.0,
+                            'uptime': '00:02:24'}}}},
+            'Gi0/1': {
+                'index': {
+                    1: {
+                        '10.1.2.3': {
+                            'hold_time': 11,
+                            'last_seq_number': 5,
+                            'peer_handle': 1,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 2202.0,
+                            'uptime': '00:20:39'}}}}}}
+
+    expected_parsed_output_empty = {
+        'eigrp_interface': {}
+    }
+
 
     device_output_1 = {'execute.return_value': '''
-
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num
         0   10.1.1.2     Gi0/0      13    00:00:03  1996   5000   0   5
@@ -26,19 +91,82 @@ class test_show_eigrp_neighbors(unittest.TestCase):
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num        
         2   10.1.1.9     Gi0/0      14    00:02:24  206    5000   0   5        
+        0   10.1.1.2     Gi0/0      13    00:00:03  1996   5000   0   5
     '''}
 
     device_output_3 = {'execute.return_value': '''
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num
+        2   10.1.1.9     Gi0/0      14    00:02:24  206    5000   0   5
         1   10.1.2.3     Gi0/1      11    00:20:39  2202   5000   0   5
     '''}
+
+
+    device_output_empty = {'execute.return_value': ''}
+
 
 
     def test_show_eigrp_neighbors_1(self):
         self.maxDiff = None
         self.device = Mock(**self.device_output_1)
-        obj = ShowCdpNeighbors(device=self.device)        
-        parsed_output = obj.parse()        
+        obj = ShowEigrpNeighbors(device=self.device)        
+        parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.expected_parsed_output_1)
+
+    def test_show_eigrp_neighbors_2(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_2)
+        obj = ShowEigrpNeighbors(device=self.device)        
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_2)
+
+    def test_show_eigrp_neighbors_3(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_3)
+        obj = ShowEigrpNeighbors(device=self.device)        
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_3)
+
+    def test_show_eigrp_neighbors_empty(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_empty)
+        obj = ShowEigrpNeighbors(device=self.device)        
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_empty)
+
+class test_show_eigrp_neighbors_detail(unittest.TestCase):
+
+    device = Device(name='aDevice')
+
+    expected_parsed_output_1 = {}
+
+    device_output_1 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors detail
+
+        EIGRP-IPv4 Neighbors for AS(100)
+        H   Address                 Interface       Hold Uptime   SRTT   RTO  Q  Seq
+                                            (sec)         (ms)       Cnt Num
+        0   10.1.2.1                 Et1/0             11 00:00:25   10   200  0  5
+        Version 5.1/3.0, Retrans: 2, Retries: 0, Prefixes: 1
+        Topology-ids from peer - 0
+
+        EIGRP-IPv4 Neighbors for AS(100)
+        H   Address                 Interface       Hold Uptime   SRTT   RTO  Q  Seq
+                                            (sec)         (ms)       Cnt Num
+        0   10.1.2.1                 Et1/0             11 00:02:31   12   200  0  6
+    
+        Time since Restart 00:01:34
+        Version 5.1/3.0, Retrans: 2, Retries: 0, Prefixes: 1
+        Topology-ids from peer - 0
+    '''}
+
+    def test_show_eigrp_neighbors_detail_1(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_1)
+        obj = ShowEigrpNeighborsDetail(device=self.device)        
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_1)
+
+if __name__ == '__main__':
+    unittest.main()
     
