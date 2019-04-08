8051deecaf3fd221e186475357af15f751a86dc8
Felipe Sasso <fcoralsa@cisco.com>
2019-04-08 16:45:40 -0400

    Added more tests

--- src/genie/libs/parser/iosxe/tests/test_show_eigrp.py
+++ src/genie/libs/parser/iosxe/tests/test_show_eigrp.py
@@ -1,8 +1,14 @@
+# Python
 import unittest
 from unittest.mock import Mock
 
+# ATS
 from ats.topology import Device
 
+# Metaparser
+from genie.metaparser.util.exceptions import SchemaEmptyParserError
+
+# iosxe show ip eigrp
 from genie.libs.parser.iosxe.show_eigrp import ShowEigrpNeighbors, ShowEigrpNeighborsDetail
 
 
@@ -75,17 +81,132 @@ class test_show_eigrp_neighbors(unittest.TestCase):
                             'srtt': 2202.0,
                             'uptime': '00:20:39'}}}}}}
 
-    expected_parsed_output_empty = {
-        'eigrp_interface': {}
-    }
+    expected_parsed_output_4 = {
+        'eigrp_interface': {
+            'GigabitEthernet0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.2': {
+                            'hold': 13,
+                            'last_seq_number': 5,
+                            'peer_handle': 0,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 1996.0,
+                            'uptime': '00:00:03'}},
+                    2: {
+                        '10.1.1.9': {
+                            'hold': 14,
+                            'last_seq_number': 5,
+                            'peer_handle': 2,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 206.0,
+                            'uptime': '00:02:24'}}}},
+            'GigabitEthernet0/1': {
+                'index': {
+                    1: {
+                        '10.1.2.3': {
+                            'hold': 11,
+                            'last_seq_number': 5,
+                            'peer_handle': 1,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 2202.0,
+                            'uptime': '00:20:39'},
+                        },
+                    },
+                },
+            },
+        }
+
+    expected_parsed_output_5 = {
+        'eigrp_interface': {
+            'GigabitEthernet0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.2': {
+                            'hold': 13,
+                            'last_seq_number': 5,
+                            'peer_handle': 0,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 1996.0,
+                            'uptime': '00:00:03'}},
+                    2: {
+                        '10.1.1.9': {
+                            'hold': 14,
+                            'last_seq_number': 5,
+                            'peer_handle': 2,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 206.0,
+                            'uptime': '00:02:24'}, }, }, },
+            'GigabitEthernet0/1': {
+                'index': {
+                    1: {
+                        '10.1.2.3': {
+                            'hold': 11,
+                            'last_seq_number': 5,
+                            'peer_handle': 1,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 2202.0,
+                            'uptime': '00:20:39'},
+                        },
+                    },
+                },
+            },
+        }
+
+    expected_parsed_output_6 = {
+        'eigrp_interface': {
+            'Ethernet0/0': {
+                'index': {
+                    1: {
+                        '10.1.1.2': {
+                            'hold': 13,
+                            'last_seq_number': 5,
+                            'peer_handle': 0,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 1996.0,
+                            'uptime': '00:00:03'}},
+                    2: {
+                        '10.1.1.9': {
+                            'hold': 14,
+                            'last_seq_number': 5,
+                            'peer_handle': 2,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 206.0,
+                            'uptime': '00:02:24'}}}},
+            'Ethernet0/1': {
+                'index': {
+                    1: {
+                        '10.1.2.3': {
+                            'hold': 11,
+                            'last_seq_number': 5,
+                            'peer_handle': 1,
+                            'q_cnt': 0,
+                            'rto': 5000,
+                            'srtt': 2202.0,
+                            'uptime': '00:20:39'},
+                        },
+                    },
+                },
+            },
+        }
 
     device_output_1 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num
         0   10.1.1.2     Gi0/0      13    00:00:03  1996   5000   0   5
     '''}
 
     device_output_2 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num
         2   10.1.1.9     Gi0/0      14    00:02:24  206    5000   0   5
@@ -93,41 +214,96 @@ class test_show_eigrp_neighbors(unittest.TestCase):
     '''}
 
     device_output_3 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors
         H   Address      Interface  Hold  Uptime    SRTT   RTO    Q   Seq
                                     (sec)           (ms)          Cnt Num
         2   10.1.1.9     Gi0/0      14    00:02:24  206    5000   0   5
         1   10.1.2.3     Gi0/1      11    00:20:39  2202   5000   0   5
     '''}
 
+    device_output_4 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors
+
+        H   Address     Interface       Hold Uptime   SRTT   RTO  Q  Seq
+                                        (sec)         (ms)       Cnt Num
+        0   10.1.1.2     Gi0/0           13 00:00:03  1996   5000  0  5
+        2   10.1.1.9     Gi0/0           14 00:02:24   206   5000  0  5
+        1   10.1.2.3     Gi0/1           11 00:20:39  2202   5000  0  5
+
+    '''}
+
+    device_output_5 = {'execute.return_value': '''
+        Device# show ip eigrp neighbors
+
+        H   Address     Interface       Hold Uptime   SRTT   RTO  Q  Seq
+                                        (sec)         (ms)       Cnt Num
+        0   10.1.1.2     Gi0/0           13 00:00:03  1996   5000  0  5
+        2   10.1.1.9     Gi0/0           14 00:02:24   206   5000  0  5
+        1   10.1.2.3     Gi0/1           11 00:20:39  2202   5000  0  5
+
+    '''}
+
+    device_output_6 = {'execute.return_value': '''
+        Router# show ip eigrp neighbors
+
+        H   Address     Interface       Hold Uptime   SRTT   RTO  Q  Seq
+                                        (sec)         (ms)       Cnt Num
+        0   10.1.1.2     Et0/0             13 00:00:03 1996  5000  0  5
+        2   10.1.1.9     Et0/0             14 00:02:24 206   5000  0  5
+        1   10.1.2.3     Et0/1             11 00:20:39 2202  5000  0  5
+
+    '''}
+
     device_output_empty = {'execute.return_value': ''}
 
-    def test_show_eigrp_neighbors_1(self):
-        self.maxDiff = None
-        self.device = Mock(**self.device_output_1)
-        obj = ShowEigrpNeighbors(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_1)
+    # def test_show_eigrp_neighbors_1(self):
+    #     self.maxDiff = None
+    #     self.device = Mock(**self.device_output_1)
+    #     obj = ShowEigrpNeighbors(device=self.device)
+    #     parsed_output = obj.parse()
+    #     self.assertEqual(parsed_output, self.expected_parsed_output_1)
 
-    def test_show_eigrp_neighbors_2(self):
-        self.maxDiff = None
-        self.device = Mock(**self.device_output_2)
-        obj = ShowEigrpNeighbors(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_2)
+    # def test_show_eigrp_neighbors_2(self):
+    #     self.maxDiff = None
+    #     self.device = Mock(**self.device_output_2)
+    #     obj = ShowEigrpNeighbors(device=self.device)
+    #     parsed_output = obj.parse()
+    #     self.assertEqual(parsed_output, self.expected_parsed_output_2)
 
-    def test_show_eigrp_neighbors_3(self):
+    # def test_show_eigrp_neighbors_3(self):
+    #     self.maxDiff = None
+    #     self.device = Mock(**self.device_output_3)
+    #     obj = ShowEigrpNeighbors(device=self.device)
+    #     parsed_output = obj.parse()
+    #     self.assertEqual(parsed_output, self.expected_parsed_output_3)
+
+    def test_show_eigrp_neighbors_4(self):
         self.maxDiff = None
-        self.device = Mock(**self.device_output_3)
+        self.device = Mock(**self.device_output_4)
         obj = ShowEigrpNeighbors(device=self.device)
         parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_3)
+        self.assertEqual(parsed_output, self.expected_parsed_output_4)
+
+    def test_show_eigrp_neighbors_5(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_5)
+        obj = ShowEigrpNeighbors(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_5)
+
+    def test_show_eigrp_neighbors_6(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output_6)
+        obj = ShowEigrpNeighbors(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.expected_parsed_output_6)
 
     def test_show_eigrp_neighbors_empty(self):
         self.maxDiff = None
         self.device = Mock(**self.device_output_empty)
         obj = ShowEigrpNeighbors(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_empty)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
 
 
 class test_show_eigrp_neighbors_detail(unittest.TestCase):
@@ -224,8 +400,6 @@ class test_show_eigrp_neighbors_detail(unittest.TestCase):
             },
         }
 
-    expected_parsed_output_empty = {'eigrp_instance': {}}
-
     device_output_1 = {'execute.return_value': '''
         Device# show ip eigrp neighbors detail
 
@@ -277,8 +451,8 @@ class test_show_eigrp_neighbors_detail(unittest.TestCase):
         self.maxDiff = None
         self.device = Mock(**self.device_output_empty)
         obj = ShowEigrpNeighborsDetail(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_empty)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
 
 
 if __name__ == '__main__':
