3a35d79d263fd031769bbf69be0f27032b32fa4f
Badal Soyantar <bsoyanta@cisco.com>
2019-04-08 10:00:54 -0400

    show protocols removed method

--- src/genie/libs/parser/ios/tests/test_show_protocols.py
+++ src/genie/libs/parser/ios/tests/test_show_protocols.py
@@ -61,66 +61,8 @@ class test_show_ip_protocols(test_show_ip_protocols_iosxe):
         with self.assertRaises(SchemaEmptyParserError):
             parsed_output = obj.parse()
 
-class test_show_ip_protocols_section_rip(unittest.TestCase):
-    device = Device(name='aDevice')
-    empty_output = {'execute.return_value': ''}
-    golden_output = {'execute.return_value': '''
-    Routing Protocol is "bgp 100"
-      Sending updates every 60 seconds, next due in 0 sec
-      Outgoing update filter list for all interfaces is 
-      Incoming update filter list for all interfaces is 
-      IGP synchronization is disabled
-      Automatic route summarization is disabled
-      Redistributing:connected, static
-      Routing for Networks:
-      Routing Information Sources:
-        Gateway         Distance      Last Update
-        10.13.13.13          200      02:20:54
-        10.18.18.18          200      03:26:15
-      Distance:external 20 internal 200 local 200
-    '''
-    }
-    golden_parsed_output = {
-        'protocols': {
-            'bgp': {
-                'instance': {
-                    'default': {
-                        'bgp_id': 100,
-                        'vrf': {
-                            'default': {
-                                'address_family': {
-                                    'ipv4': {
-                                        'igp_sync': False,
-                                        'automatic_route_summarization': False,
-                                        'neighbor': {
-                                            '10.13.13.13': {
-                                                'neighbor_id': '10.13.13.13',
-                                                'distance': 200,
-                                                'last_update': '02:20:54',
-                                                },
-                                            '10.18.18.18': {
-                                                'neighbor_id': '10.18.18.18',
-                                                'distance': 200,
-                                                'last_update': '03:26:15',
-                                                },
-                                            },
-                                        'timers': {
-                                            'update_interval': 60,
-                                            'next_update': 0,
-                                        },
-                                        'redistribute': {
-                                            'connected': {},
-                                            'static': {},
-                                        },
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                    },
-                },
-            },
-    }
+class test_show_ip_protocols_section_rip(test_show_ip_protocols_section_rip_iosxe):
+    
     def test_empty(self):
         self.device1 = Mock(**self.empty_output)
         obj = ShowIpProtocolsSectionRip(device=self.device1)
@@ -133,6 +75,13 @@ class test_show_ip_protocols_section_rip(unittest.TestCase):
         obj = ShowIpProtocolsSectionRip(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
+
+    def test_golden_vrf_vrf1(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_2)
+        obj = ShowIpProtocolsSectionRip(device=self.device)
+        parsed_output = obj.parse(vrf="VRF1")
+        self.assertEqual(parsed_output, self.golden_parsed_output_2)
 
 
 # ============================================
