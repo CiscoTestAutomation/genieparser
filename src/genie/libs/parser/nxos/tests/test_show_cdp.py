41819297e8f15a5330b9cfb5485eb79fee42e67e
Felipe Sasso <fcoralsa@cisco.com>
2019-04-09 14:35:02 -0400

    Changed test for when device output is empty. Fixed some parsed outputs that were wrong

--- src/genie/libs/parser/nxos/tests/test_show_cdp.py
+++ src/genie/libs/parser/nxos/tests/test_show_cdp.py
@@ -10,7 +10,7 @@ from genie.libs.parser.nxos.show_cdp import ShowCdpNeighbors, ShowCdpNeighborsDe
 
 class test_show_cdp_neighbors(unittest.TestCase):
 
-    device = Device(name='aDevice')    
+    device = Device(name='aDevice')
 
     expected_parsed_output_1 = {
         'cdp': {
@@ -33,6 +33,16 @@ class test_show_cdp_neighbors(unittest.TestCase):
                 }
             }
 
+    device_output_1 = {'execute.return_value': '''
+        switchA# show cdp neighbors
+        Capability Codes: R - Router, T - Trans-Bridge, B - Source-Route-Bridge
+            S - Switch, H - Host, I - IGMP, r - Repeater,
+            V - VoIP-Phone, D - Remotely-Managed-Device,
+            s - Supports-STP-Dispute
+            Device ID              Local Intrfce   Hldtme  Capability  Platform      Port ID
+            switchB                Ethernet2/3     177     R S I    WS-C2960-24TC Ethernet1/4
+            switchA                Ethernet1/4     120     R S I    WS-C2960-24TC Ethernet2/3
+    '''}
 
     expected_parsed_output_2 = {
         'cdp': {
@@ -43,46 +53,21 @@ class test_show_cdp_neighbors(unittest.TestCase):
                     'hold_time': 163,
                     'local_interface': 'mgmt0',
                     'platform': 'WS-C2960-24TC',
-                    'port_id': 'Fas0/21'},
+                    'port_id': 'FastEthernet0/21'},
                 2: {
                     'capability': 'R S I',
                     'device_id': 'swordfish-6k-2',
                     'hold_time': 149,
                     'local_interface': 'Ethernet3/2',
                     'platform': 'WS-C6506-E',
-                    'port_id': 'Gig1/38'}
+                    'port_id': 'GigabitEthernet1/38'}
                 }
             }
         }
 
-    expected_parsed_empty_output = {
-        'cdp': {}
-    }
-
-    empty_device_output = {'execute.return_value': '''
-        Device# show cdp neighbors
-        Capability Codes: 
-                        R - Router, T - Trans-Bridge, B - Source-Route-Bridge
-                        S - Switch, H - Host, I - IGMP, r - Repeater,
-                        V - VoIP-Phone, D - Remotely-Managed-Device,
-                        s - Supports-STP-Dispute
-        Device ID              Local Intrfce   Hldtme  Capability  Platform      Port ID
-      '''}
-
-    device_output_1 = {'execute.return_value': '''
-        switchA# show cdp neighbors
-        Capability Codes: R - Router, T - Trans-Bridge, B - Source-Route-Bridge
-            S - Switch, H - Host, I - IGMP, r - Repeater,
-            V - VoIP-Phone, D - Remotely-Managed-Device,
-            s - Supports-STP-Dispute
-            Device ID              Local Intrfce   Hldtme  Capability  Platform      Port ID
-            switchB                Ethernet2/3     177     R S I    WS-C2960-24TC Ethernet1/4
-            switchA                Ethernet1/4     120     R S I    WS-C2960-24TC Ethernet2/3      
-    '''}
-
     device_output_2 = {'execute.return_value': '''
         switchA# show cdp neighbors
-        Capability Codes: 
+        Capability Codes:
                 R - Router, T - Trans-Bridge, B - Source-Route-Bridge
                 S - Switch, H - Host, I - IGMP, r - Repeater,
                 V - VoIP-Phone, D - Remotely-Managed-Device,
@@ -92,35 +77,40 @@ class test_show_cdp_neighbors(unittest.TestCase):
         swordfish-6k-2 Eth3/2 149 R S I WS-C6506-E Gig1/38
     '''}
 
+    empty_device_output = {'execute.return_value': '''
+        Device# show cdp neighbors
+        Capability Codes:
+                        R - Router, T - Trans-Bridge, B - Source-Route-Bridge
+                        S - Switch, H - Host, I - IGMP, r - Repeater,
+                        V - VoIP-Phone, D - Remotely-Managed-Device,
+                        s - Supports-STP-Dispute
+        Device ID              Local Intrfce   Hldtme  Capability  Platform      Port ID
+      '''}
+
     def test_show_cdp_neighbors_1(self):
         self.maxDiff = None
         self.device = Mock(**self.device_output_1)
         obj = ShowCdpNeighbors(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.expected_parsed_output_1)
-    
+
     def test_show_cdp_neighbors_2(self):
         self.maxDiff = None
         self.device = Mock(**self.device_output_2)
         obj = ShowCdpNeighbors(device=self.device)
-        parsed_output = obj.parse()    
+        parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.expected_parsed_output_2)
 
     def test_show_cdp_neighbors_empty_output(self):
         self.maxDiff = None
         self.device = Mock(**self.empty_device_output)
         obj = ShowCdpNeighbors(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_empty_output)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
 
 
 class test_show_cdp_neighbors_detail(unittest.TestCase):
     device = Device(name='aDevice')
-
-
-    expected_parsed_output_empty = {
-        'total_entries_displayed': 0
-    }
 
     expected_parsed_output_1 = {
         'index': {
@@ -141,107 +131,9 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
                                     '(C3750E-UNIVERSAL-M), Version 12.2(35)SE5, '
                                     'RELEASE SOFTWARE (fc1)',
                 'system_name': '',
-                'vtp_mng_domain': ''}},
+                'advertisement_ver': 2,
+                'vtp_management_domain': ''}},
         'total_entries_displayed': 1}
-
-    expected_parsed_output_2 = {
-        'index': {
-            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Disput',
-                'device_id': 'swor96(SSI13110AAQ)',
-                'duplex_mode': 'full',
-                'hold_time': 175,
-                'interface_addresses': {
-                    '192.168.0.1': {}},
-                'local_interface': 'Ethernet1/17',
-                'management_addresses': {
-                    '192.168.0.96': {}},
-                'native_vlan': '1',
-                'physical_location': 'snmplocation',
-                'platform': 'N5K-C5010P-BF',
-                'port_id': 'Ethernet1',
-                'software_version': 'Cisco Nexus Operating System (NX-OS) '
-                                   'Software, Version 5.0(3)N2(1)',
-               'system_name': 'swor96',
-               'vtp_mng_domain': ''}},
-        'total_entries_displayed': 1
-    }
-
-    expected_parsed_output_3 = {
-        'index': {
-            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Disput',
-                'device_id': 'swor96(SSI13110AAQ)',
-                'duplex_mode': 'full',
-                'hold_time': 175,
-                'interface_addresses': {'192.168.0.1': {}},
-                'local_interface': 'Ethernet1/18',
-                'management_addresses': {'192.168.0.96': {}},
-                'native_vlan': '1',
-                'physical_location': 'snmplocation',
-                'platform': 'N5K-C5010P-BF',
-                'port_id': 'Ethernet1',
-                'software_version': 'Cisco Nexus Operating System (NX-OS) '
-                                    'Software, Version 5.0(3)N2(1)',
-                'system_name': 'swor96',
-                'vtp_mng_domain': ''}},
-        'total_entries_displayed': 1
-    }
-
-    expected_parsed_output_4 = {
-        'index': {
-            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Dispute',
-                'device_id': 'swor95(SSI13110AAS)',
-                'duplex_mode': 'full',
-                'hold_time': 121,
-                'interface_addresses': {'192.168.0.95': {}},
-                'local_interface': 'Ethernet1/29',
-                'management_addresses': {'192.168.0.95': {}},
-                'native_vlan': '1',
-                'physical_location': 'snmplocation',
-                'platform': 'N5K-C5010P-BF',
-                'port_id': 'Ethernet1',
-                'software_version': 'Cisco Nexus Operating System (NX-OS) '
-                                    'Software, Version 5.0(3)N2(1)',
-                'system_name': 'swor95',
-                'vtp_mng_domain': ''}},
-        'total_entries_displayed': 1}
-
-    expected_parsed_output_5 = {
-        'index': {
-            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Dispute',
-                'device_id': 'swor95(SSI13110AAS)',
-                'duplex_mode': 'full',
-                'hold_time': 121,
-                'interface_addresses': {
-                    '192.168.0.95': {},
-                    '192.168.0.96': {},
-                    '192.168.0.97': {},
-                    '4000::BC:0:C0A8B:BC06': {
-                        'type': 'global unicats'},
-                    '5000::CB:0:C0B8B:BCC6': {
-                        'type': 'link-local'}},
-                'local_interface': 'Ethernet1/29',
-                'management_addresses': {
-                    '192.168.0.95': {},
-                    '192.168.0.96': {},
-                    '192.168.0.97': {},
-                    '4000::BC:0:C0A8B:BC06': {
-                        'type': 'global unicats'},
-                    '5000::CB:0:C0B8B:BCC6': {
-                        'type': 'link-local'}},
-                'native_vlan': '1',
-                'physical_location': 'snmplocation',
-                'platform': 'N5K-C5010P-BF',
-                'port_id': 'Ethernet1',
-                'software_version': 'Cisco Nexus Operating System (NX-OS) '
-                                    'Software, Version 5.0(3)N2(1)',
-                'system_name': 'swor95',
-                'vtp_mng_domain': ''}},
-        'total_entries_displayed': 1
-    }
-
-    device_output_empty = {'execute.return_value': """
-    """}
-
 
     device_output_1 = {'execute.return_value': """
         Device# show cdp neighbors detail
@@ -268,6 +160,29 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
 
     """}
 
+    expected_parsed_output_2 = {
+        'index': {
+            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Disput',
+                'device_id': 'swor96(SSI13110AAQ)',
+                'duplex_mode': 'full',
+                'hold_time': 175,
+                'interface_addresses': {
+                    '192.168.0.1': {}},
+                'local_interface': 'Ethernet1/17',
+                'management_addresses': {
+                    '192.168.0.96': {}},
+                'native_vlan': '1',
+                'physical_location': 'snmplocation',
+                'platform': 'N5K-C5010P-BF',
+                'port_id': 'Ethernet1',
+                'software_version': 'Cisco Nexus Operating System (NX-OS) '
+                                    'Software, Version 5.0(3)N2(1)',
+                'system_name': 'swor96',
+                'advertisement_ver': 2,
+                'vtp_management_domain': ''}},
+        'total_entries_displayed': 1
+    }
+
     device_output_2 = {'execute.return_value': """
         Device# show cdp neighbors detail
 
@@ -290,7 +205,28 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         Mgmt address(es):
         IPv4 Address: 192.168.0.96
     """}
- 
+
+    expected_parsed_output_3 = {
+        'index': {
+            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Disput',
+                'device_id': 'swor96(SSI13110AAQ)',
+                'duplex_mode': 'full',
+                'hold_time': 175,
+                'interface_addresses': {'192.168.0.1': {}},
+                'local_interface': 'Ethernet1/18',
+                'management_addresses': {'192.168.0.96': {}},
+                'native_vlan': '1',
+                'physical_location': 'snmplocation',
+                'platform': 'N5K-C5010P-BF',
+                'port_id': 'Ethernet1',
+                'software_version': 'Cisco Nexus Operating System (NX-OS) '
+                                    'Software, Version 5.0(3)N2(1)',
+                'system_name': 'swor96',
+                'advertisement_ver': 2,
+                'vtp_management_domain': ''}},
+        'total_entries_displayed': 1
+    }
+
     device_output_3 = {'execute.return_value': """
         Device# show cdp neighbors detail
 
@@ -313,7 +249,27 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         Mgmt address(es):
         IPv4 Address: 192.168.0.96
     """}
- 
+
+    expected_parsed_output_4 = {
+        'index': {
+            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Dispute',
+                'device_id': 'swor95(SSI13110AAS)',
+                'duplex_mode': 'full',
+                'hold_time': 121,
+                'interface_addresses': {'192.168.0.95': {}},
+                'local_interface': 'Ethernet1/29',
+                'management_addresses': {'192.168.0.95': {}},
+                'native_vlan': '1',
+                'physical_location': 'snmplocation',
+                'platform': 'N5K-C5010P-BF',
+                'port_id': 'Ethernet1',
+                'software_version': 'Cisco Nexus Operating System (NX-OS) '
+                                    'Software, Version 5.0(3)N2(1)',
+                'system_name': 'swor95',
+                'advertisement_ver': 2,
+                'vtp_management_domain': ''}},
+        'total_entries_displayed': 1}
+
     device_output_4 = {'execute.return_value': """
         Device# show cdp neighbors detail
 
@@ -333,17 +289,52 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         Duplex: full
         Physical Location: snmplocation
         Mgmt address(es):
-        IPv4 Address: 192.168.0.95 
+        IPv4 Address: 192.168.0.95
     """}
+
+    expected_parsed_output_5 = {
+        'index': {
+            1: {'capabilities': 'Switch IGMP Filtering Supports-STP-Dispute',
+                'device_id': 'swor95(SSI13110AAS)',
+                'duplex_mode': 'full',
+                'hold_time': 121,
+                'interface_addresses': {
+                    '192.168.0.95': {},
+                    '192.168.0.96': {},
+                    '192.168.0.97': {},
+                    '4000::BC:0:C0A8B:BC06': {
+                        'type': 'global unicast'},
+                    '5000::CB:0:C0B8B:BCC6': {
+                        'type': 'link-local'}},
+                'local_interface': 'Ethernet1/29',
+                'management_addresses': {
+                    '192.168.0.95': {},
+                    '192.168.0.96': {},
+                    '192.168.0.97': {},
+                    '4000::BC:0:C0A8B:BC06': {
+                        'type': 'global unicast'},
+                    '5000::CB:0:C0B8B:BCC6': {
+                        'type': 'link-local'}},
+                'native_vlan': '1',
+                'physical_location': 'snmplocation',
+                'platform': 'N5K-C5010P-BF',
+                'port_id': 'Ethernet1',
+                'software_version': 'Cisco Nexus Operating System (NX-OS) '
+                                    'Software, Version 5.0(3)N2(1)',
+                'system_name': 'swor95',
+                'advertisement_ver': 2,
+                'vtp_management_domain': ''}},
+        'total_entries_displayed': 1
+    }
 
     device_output_5 = {'execute.return_value': """
         Device# show cdp neighbors detail
-        
+
         Device ID:swor95(SSI13110AAS)
         System Name:swor95
         Interface address(es):
         IPv4 Address: 192.168.0.95
-        IPv6 Address: 4000::BC:0:C0A8B:BC06 (global unicats)
+        IPv6 Address: 4000::BC:0:C0A8B:BC06 (global unicast)
         IPv6 Address: 5000::CB:0:C0B8B:BCC6 (link-local)
         IPv4 Address: 192.168.0.96
         IPv4 Address: 192.168.0.97
@@ -360,10 +351,13 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         Physical Location: snmplocation
         Mgmt address(es):
         IPv4 Address: 192.168.0.95
-        IPv6 Address: 4000::BC:0:C0A8B:BC06 (global unicats)
+        IPv6 Address: 4000::BC:0:C0A8B:BC06 (global unicast)
         IPv6 Address: 5000::CB:0:C0B8B:BCC6 (link-local)
         IPv4 Address: 192.168.0.96
         IPv4 Address: 192.168.0.97
+    """}
+
+    device_output_empty = {'execute.return_value': """
     """}
 
     def test_show_cdp_neighbors_detail_1(self):
@@ -373,7 +367,7 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.expected_parsed_output_1)
 
-    def test_show_cdp_neighbors_detail_2(self ):
+    def test_show_cdp_neighbors_detail_2(self):
         self.maxDiff = None
         self.device = Mock(**self.device_output_2)
         obj = ShowCdpNeighborsDetail(device=self.device)
@@ -405,8 +399,9 @@ class test_show_cdp_neighbors_detail(unittest.TestCase):
         self.maxDiff = None
         self.device = Mock(**self.device_output_empty)
         obj = ShowCdpNeighborsDetail(device=self.device)
-        parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.expected_parsed_output_empty)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
 
 if __name__ == '__main__':
     unittest.main()
