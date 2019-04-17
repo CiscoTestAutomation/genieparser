e9d98d2a3ea75723090cc55cc8884482753d0596
Badal Soyantar <bsoyanta@cisco.com>
2019-04-17 15:30:36 -0400

    Code updated to new style

--- src/genie/libs/parser/nxos/show_l2route.py
+++ src/genie/libs/parser/nxos/show_l2route.py
@@ -209,33 +209,22 @@ class ShowL2routeEvpnMacIpEvi(ShowL2routeEvpnMacIpEviSchema):
 
             # 101         fa16.3e04.e54a BGP    --            0          100.101.8.3    66.66.66.66
             # 101         fa16.3ec5.fcab HMM    --            0          100.101.1.4    Local
-            p1 = re.compile(r'^\s*(?P<topology>[0-9]+) +(?P<mac_address>[a-z0-9\.]+) +(?P<prod>[a-zA-Z]+) +(?P<flags>[a-zA-Z\,\-]+) +(?P<seq_no>[0-9]+) +(?P<host_ip>[\d\.]+) +(?P<next_hops>[a-zA-Z0-9\/\.]+)')
+            p1 = re.compile(r'^\s*(?P<topology>[0-9]+) +(?P<mac_address>'
+                '[a-z0-9\.]+) +(?P<prod>[a-zA-Z]+) +(?P<flags>[a-zA-Z\,\-]+)'
+                ' +(?P<seq_no>[0-9]+) +(?P<host_ip>[\d\.]+) +(?P<next_hops>'
+                '[a-zA-Z0-9\/\.]+)')
             m = p1.match(line)
             if m:
-
-                topology = str(m.groupdict()['topology'])
-                mac_address = str(m.groupdict()['mac_address'])
-
-                if 'topology' not in ret_dict:
-                    ret_dict['topology'] = {}
-                if topology not in ret_dict['topology']:
-                    ret_dict['topology'][topology] = {}
-                if 'mac_address' not in ret_dict['topology'][topology]:
-                    ret_dict['topology'][topology]['mac_address'] = {}
-                if mac_address not in ret_dict['topology'][topology]['mac_address']:
-                    ret_dict['topology'][topology]['mac_address'][mac_address] = {}
-
-                ret_dict['topology'][topology]['mac_address'][mac_address]['prod'] = \
-                    str(m.groupdict()['prod'])
-                ret_dict['topology'][topology]['mac_address'][mac_address]['flags'] = \
-                    str(m.groupdict()['flags'])
-                ret_dict['topology'][topology]['mac_address'][mac_address]['seq_no'] = \
-                    str(m.groupdict()['seq_no'])
-                ret_dict['topology'][topology]['mac_address'][mac_address]['host_ip'] = \
-                    str(m.groupdict()['host_ip'])
-                ret_dict['topology'][topology]['mac_address'][mac_address]['next_hops'] = \
-                    str(m.groupdict()['next_hops'])
-
+                group = m.groupdict()
+                topology = ret_dict.setdefault('topology', {}).\
+                    setdefault(group['topology'], {}). \
+                    setdefault('mac_address', {}).\
+                    setdefault(group['mac_address'], {})
+                topology.update({'prod' : group['prod']})
+                topology.update({'flags' : group['flags']})
+                topology.update({'seq_no' : group['seq_no']})
+                topology.update({'host_ip' : group['host_ip']})
+                topology.update({'next_hops' : group['next_hops']})
                 continue
 
         return ret_dict
