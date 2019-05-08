74c4b436e11791a8ce0ae286babb4f5026842b63
hanyyu <hanyyu@cisco.com>
2019-05-08 16:50:04 -0700

    show lldp all and show lldp timers done

---
+++ src/genie/libs/parser/nxos/tests/test_show_lldp.py
@@ -0,0 +1,14 @@
+# Python
+import unittest
+from unittest.mock import Mock
+
+# ATS
+from ats.topology import Device
+from ats.topology import loader
+
+# Metaparser
+from genie.metaparser.util.exceptions import SchemaEmptyParserError, \
+    SchemaMissingKeyError
+
+# iosxe show_lisp
+from genie.libs.parser.iosxe.show_lisp import ShowLispSession
