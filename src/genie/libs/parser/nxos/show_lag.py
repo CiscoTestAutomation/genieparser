d238c27283fd116eb2257573875de26926dbfba4
hanyyu <hanyyu@cisco.com>
2019-05-10 15:41:25 -0700

    show lacp counters, show system identifier

--- src/genie/libs/parser/nxos/show_lag.py
+++ src/genie/libs/parser/nxos/show_lag.py
@@ -68,12 +68,150 @@ class ShowFeature(ShowFeatureSchema):
                 state = True if group['state'] == 'enabled' else False
                 sub_dict = parsed_dict.setdefault('features', {}).setdefault(
                     group['feature_name'], {}).setdefault('instances', {})
-                sub_dict[group['instance']]=state
+                sub_dict[group['instance']] = state
 
                 continue
         return parsed_dict
 
 
+# ============================
+# parser for show lacp system-identifier
+# ============================
+class ShowLacpSystemIdentifierSchema(MetaParser):
+    """Schema for show lacp system-identifier"""
+    schema = {
+        'system_id_mac': str,
+        'system_priority': int,
+    }
 
 
+class ShowLacpSystemIdentifier(ShowLacpSystemIdentifierSchema):
+    """Parser for :
+       show lacp sys-id"""
 
+    cli_command = 'show lacp sys-id'
+
+    def cli(self, output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        parsed_dict = {}
+        # 32768,5e-2-0-1-0-7
+        p1 = re.compile(r'^\s*(?P<system_priority>[\d]+), *(?P<system_id_mac>[\w\.\-]+)$')
+
+        for line in out.splitlines():
+            if line:
+                line = line.rstrip()
+            else:
+                continue
+
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                parsed_dict.update({'system_priority': int(group['system_priority'])})
+                parsed_dict.update({'system_id_mac': group['system_id_mac']})
+                continue
+
+        return parsed_dict
+
+
+# ============================
+# schema for show lacp counters
+# ============================
+class ShowLacpCountersSchema(MetaParser):
+    """schema for: show lcap counters"""
+    schema = {
+        'interfaces': {
+            Any(): {
+                'members': {
+                    Any(): {
+                        'interface': str,
+                        'counters': {
+                            'lacp_in_pkts': int,
+                            'lacp_out_pkts': int,
+                            'lacp_errors': int,
+                            'marker_in_pkts': int,
+                            'marker_out_pkts': int
+                        },
+                    },
+                }
+            },
+        },
+    }
+
+
+# =============================
+# parser for lacp counters
+# ============================
+class ShowLacpCounters(ShowLacpCountersSchema):
+    """Parser for: show lacp counters"""
+
+    cli_command = 'show lacp counters'
+
+    def cli(self, output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        # init return dict
+        parsed_dict = {}
+        intf_dict = {}
+        # port-channel1
+        p1 = re.compile(r'^port-channel\d+$')
+        # ------------------------------------------------------------------------------
+        #                              LACPDUs                      Markers / Resp
+        # LACPDUs
+        # Port              Sent                Recv                  Recv
+        # Sent  Pkts
+        # Err
+        # ------------------------------------------------------------------------------
+        # port - channel1
+        # Ethernet1 / 1        92                   85                     0      0   
+        # 0      
+        # Ethernet1 / 2        79                   87                     0      0   
+        # 0      
+        #
+        # port - channel2
+        # Ethernet1 / 3        136                  112                    0      0   
+        # 0      
+        # Ethernet1 / 4        95                   90                     0      0   
+        # 0      
+        # Ethernet1 / 5        118                  146                    0      0   
+        # 0      
+        p2 = re.compile(
+            r'^(?P<interface>[\w\/]+) +(?P<lacp_out_pkts>[\d]+) +(?P<lacp_in_pkts>[\d]+)'
+            ' +(?P<marker_in_pkts>[\d]+) +(?P<marker_out_pkts>[\d]+) +( +('
+            '?P<lacp_pkts_errors>[\d]+))?$')
+
+        for line in out.splitlines():
+            if line:
+                line = line.strip().replace('\xa0', ' ')
+            else:
+                continue
+
+            # port-channel1
+            m = p1.match(line)
+            if m:
+                intf_dict = parsed_dict.setdefault('interfaces', {}).setdefault(m.group(),
+                                                                                {})
+                continue
+
+            m = p2.match(line)
+            if m:
+                group = m.groupdict()
+                interface = Common.convert_intf_name(group["interface"])
+                member_dict = intf_dict.setdefault('members', {}).setdefault(interface,
+                                                                             {})
+                member_dict.update({'interface': interface})
+                counter_dict = member_dict.setdefault('counters', {})
+                counter_dict.update({'lacp_in_pkts': int(group['lacp_in_pkts'])})
+                counter_dict.update({'lacp_out_pkts': int(group['lacp_out_pkts'])})
+                counter_dict.update({'marker_in_pkts': int(group['marker_in_pkts'])})
+                counter_dict.update({'marker_out_pkts': int(group['marker_out_pkts'])})
+                counter_dict.update({'lacp_errors': int(group['lacp_pkts_errors'])})
+                continue
+
+        return parsed_dict
--- src/genie/libs/parser/nxos/tests/test_show_lag.py
+++ src/genie/libs/parser/nxos/tests/test_show_lag.py
@@ -11,7 +11,8 @@ from genie.metaparser.util.exceptions import SchemaEmptyParserError, \
     SchemaMissingKeyError
 
 # iosxe show_lisp
-from genie.libs.parser.nxos.show_lag import ShowFeature
+from genie.libs.parser.nxos.show_lag import ShowFeature, ShowLacpSystemIdentifier, \
+    ShowLacpCounters
 
 
 # =================================
@@ -66,6 +67,138 @@ class test_show_feature(unittest.TestCase):
         parsed_output = obj.parse()
 
         self.assertEqual(parsed_output, self.golden_parsed_output)
+
+
+# =================================
+# Unit test for 'show system-identifier'
+# =================================
+class test_show_system_identifier(unittest.TestCase):
+    device = Device(name='aDevice')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+    32768,5e-2-0-1-0-7
+    '''}
+    golden_parsed_output = {
+        'system_id_mac': '5e-2-0-1-0-7',
+        'system_priority': 32768,
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowLacpSystemIdentifier(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output)
+        obj = ShowLacpSystemIdentifier(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+# =================================
+# Unit test for 'show lacp counters'
+# =================================
+class test_show_lacp_counters(unittest.TestCase):
+    """unit test for show lacp counters """
+
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+        NOTE: Clear lacp counters to get accurate statistics
+    
+        ------------------------------------------------------------------------------
+                                     LACPDUs                      Markers/Resp LACPDUs
+        Port              Sent                Recv                  Recv Sent  Pkts Err
+        ------------------------------------------------------------------------------
+        port-channel1
+        Ethernet1/1        92                   85                     0      0    0      
+        Ethernet1/2        79                   87                     0      0    0      
+        
+        port-channel2
+        Ethernet1/3        136                  112                    0      0    0      
+        Ethernet1/4        95                   90                     0      0    0      
+        Ethernet1/5        118                  146                    0      0    0  
+    '''}
+
+    golden_parsed_output = {
+        "interfaces": {
+            "port-channel1": {
+                "members": {
+                    "Ethernet1/1": {
+                        "interface": "Ethernet1/1",
+                        "counters": {
+                            "lacp_in_pkts": 85,
+                            "lacp_out_pkts": 92,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_errors": 0
+                        }
+                    },
+                    "Ethernet1/2": {
+                        "interface": "Ethernet1/2",
+                        "counters": {
+                            "lacp_in_pkts": 87,
+                            "lacp_out_pkts": 79,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_errors": 0,
+                        }
+                    }
+                }
+            },
+            "port-channel2": {
+                "members": {
+                    "Ethernet1/3": {
+                        "interface": "Ethernet1/3",
+                        "counters": {
+                            "lacp_in_pkts": 112,
+                            "lacp_out_pkts": 136,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_errors": 0,
+                        }
+                    },
+                    "Ethernet1/4": {
+                        "interface": "Ethernet1/4",
+                        "counters": {
+                            "lacp_in_pkts": 90,
+                            "lacp_out_pkts": 95,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_errors": 0,
+                        }
+                    },
+                    "Ethernet1/5": {
+                        "interface": "Ethernet1/5",
+                        "counters": {
+                            "lacp_in_pkts": 146,
+                            "lacp_out_pkts": 118,
+                            "marker_in_pkts": 0,
+                            "marker_out_pkts": 0,
+                            "lacp_errors": 0,
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowLacpCounters(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output)
+        obj = ShowLacpCounters(device=self.device)
+        parsed_output = obj.parse()
+        self.assertDictEqual(parsed_output, self.golden_parsed_output)
 
 
 if __name__ == '__main__':
