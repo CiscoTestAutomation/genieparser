74fa145e739e92103f821b21186994e4df1dcf00
hanyyu <hanyyu@cisco.com>
2019-05-13 10:38:05 -0700

    show lacp meighbor, show port-channel summary

--- src/genie/libs/parser/nxos/show_lag.py
+++ src/genie/libs/parser/nxos/show_lag.py
@@ -99,7 +99,7 @@ class ShowLacpSystemIdentifier(ShowLacpSystemIdentifierSchema):
 
         parsed_dict = {}
         # 32768,5e-2-0-1-0-7
-        p1 = re.compile(r'^\s*(?P<system_priority>[\d]+), *(?P<system_id_mac>[\w\.\-]+)$')
+        p1 = re.compile(r'^\s*(?P<system_priority>[\d]+), *(?P<system_id_mac>[\w.\-]+)$')
 
         for line in out.splitlines():
             if line:
@@ -143,7 +143,7 @@ class ShowLacpCountersSchema(MetaParser):
 
 
 # =============================
-# parser for lacp counters
+# parser for show lacp counters
 # ============================
 class ShowLacpCounters(ShowLacpCountersSchema):
     """Parser for: show lacp counters"""
@@ -215,3 +215,198 @@ class ShowLacpCounters(ShowLacpCountersSchema):
                 continue
 
         return parsed_dict
+
+
+# =============================
+# schema for show lacp neighbor
+# ============================
+class ShowLacpNeighborSchema(MetaParser):
+    """schema for: show lacp neighbor"""
+    schema = {
+        'interfaces': {
+            Any(): {
+                'members': {
+                    Any(): {
+                        'interface': str,
+                        'activity': str,
+                        'oper_key': int,
+                        'port_num': int,
+                        'partner_id': str,
+                        'age': int,
+                        'interval': str,
+                        'lacp_port_priority': int,
+                        'port_state': int,
+                    },
+                }
+            },
+        },
+    }
+
+
+# =============================
+# parser for show lacp neighbor
+# ============================
+class ShowLacpNeighbor(ShowLacpNeighborSchema):
+    """parser for: show lacp neighbor"""
+    cli_command = 'show lacp neighbor'
+
+    def cli(self, output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        # init dictionary
+        parsed_dict = {}
+        intf_dict = {}
+        member_dict = {}
+        # port-channel1 neighbors
+        p1 = re.compile(r'^(?P<port_channel>[\w-]+)[\xa0 ]+neighbors$')
+        #             Partner                Partner                     Partner
+        # Port        System ID              Port Number     Age         Flags
+        # Eth1/1      32768,5e-2-0-1-0-7     0x101           1140        SA
+        p2 = re.compile(
+            r'^(?P<interface>[\w/]+)[\xa0 ]+\d+,[\xa0 ]*(?P<sys_id>[\w.\-]+)[\xa0 ]+('
+            r'?P<port_num>0x[a-fA-F0-9]+)[\xa0 ]+(?P<age>\d+)[\xa0 ]+(?P<flags>[\w]+)$')
+        #             LACP Partner           Partner                     Partner
+        #             Port Priority          Oper Key                    Port State
+        #             32768                  0x8000                      0x3d
+        p3 = re.compile(
+            r'^(?P<lacp_port_priority>\d+)[\xa0 ]+(?P<oper_key>0x[a-fA-F0-9]+)[\xa0 ]+('
+            r'?P<port_state>0x[a-fA-F0-9]+)$')
+
+        for line in out.splitlines():
+            if line:
+                line = line.strip()
+            else:
+                continue
+
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                intf_dict = parsed_dict.setdefault('interfaces', {}).setdefault(
+                    group['port_channel'], {})
+                continue
+            m = p2.match(line)
+            if m:
+                group = m.groupdict()
+                interface = Common.convert_intf_name(group.pop("interface"))
+                member_dict = intf_dict.setdefault('members', {}).setdefault(interface,
+                                                                             {})
+                member_dict.update({'interface': interface})
+                flags = group['flags'].lower()
+                if 'a' in flags:
+                    activity = 'active'
+                else:
+                    activity = 'passive'
+                if 's' in flags:
+                    interval = 'slow'
+                else:
+                    interval = 'fast'
+
+                member_dict.update({'interval': interval})
+                member_dict.update({'activity': activity})
+
+                member_dict.update({'port_num': int(group['port_num'], 0)})
+                member_dict.update({'partner_id': group['sys_id']})
+                member_dict.update({'age': int(group['age'])})
+                continue
+            m = p3.match(line)
+            if m:
+                group = m.groupdict()
+                member_dict.update(
+                    {'lacp_port_priority': int(group['lacp_port_priority'])})
+                member_dict.update({'oper_key': int(group['oper_key'], 0)})
+                member_dict.update({'port_state': int(group['port_state'], 0)})
+                continue
+
+        return parsed_dict
+
+
+# =============================
+# schema for show port-channel summary
+# ============================
+class ShowPortChannelSummarySchema(MetaParser):
+    """schema for: show show port-channel summary"""
+    schema = {
+        'interfaces': {
+            Any(): {
+                'bundle_id': int,
+                'oper_status': str,
+                'layer': str,  # routed vs switched
+                'protocol': str,
+                'type': str,
+                'members': {
+                    Any(): {
+                        'flags': str,
+                    }
+                },
+            },
+        }
+    }
+
+
+# =============================
+# parser for show port-channel summary
+# ============================
+class ShowPortChannelSummary(ShowPortChannelSummarySchema):
+    """parser for: show port-channel summary"""
+
+    cli_command = 'show port-channel summary'
+
+    def cli(self, output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        parsed_dict = {}
+        inft_dict = {}
+
+        # --------------------------------------------------------------------------------
+        # Group Port-       Type     Protocol  Member Ports
+        #       Channel
+        # --------------------------------------------------------------------------------
+        # 1     Po1(RU)     Eth      LACP      Eth1/1(P)    Eth1/2(P)
+        # 2     Po2(SU)     Eth      LACP      Eth1/3(P)    Eth1/4(P)    Eth1/5(H)
+        p1 = re.compile(
+            r'(?P<bundle_id>[\d]+)[\xa0 ]+(?P<name>[\w\-]+)\((?P<flags>[\w]+)\)?[\xa0 '
+            r']+(?P<type>\w+)[\xa0 ]+(?P<protocol>[\w\-]+)?[\xa0 ]+(?P<ports>[\w\-/() '
+            r'\xa0]+ *)?$')
+        for line in out.splitlines():
+            if line:
+                line = line.strip()
+            else:
+                continue
+            # 1     Po1(RU)     Eth      LACP      Eth1/1(P)    Eth1/2(P)
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                name = Common.convert_intf_name(group["name"])
+                intf_dict = parsed_dict.setdefault('interfaces', {}).setdefault(name, {})
+                intf_dict.update({'bundle_id': int(group["bundle_id"])})
+                intf_dict.update({'type': group['type'].lower()})
+                intf_dict.update({'protocol': group['protocol'].lower()})
+                flags = group['flags'].lower()
+                intf_dict['layer'] = 'switched' if 's' in flags else 'routed'
+                intf_dict['oper_status'] = 'up' if 'u' in flags else 'down'
+                port_dict = intf_dict.setdefault('members', {})
+                port_list = re.findall(r'([\w/]+)\((\w+)\)', group['ports'])
+                for port in port_list:
+                    intf = Common.convert_intf_name(port[0])
+                    port_sub_dict = port_dict.setdefault(intf, {})
+                    port_sub_dict['flags'] = port[1]
+
+                continue
+
+        return parsed_dict
+
+
+# =====================================
+# schema for show port-channel database
+# =====================================
+class ShowPortChannelDatabaseSchema(MetaParser):
+    """show post-channel database"""
+    schema = {
+
+    }
--- src/genie/libs/parser/nxos/tests/test_show_lag.py
+++ src/genie/libs/parser/nxos/tests/test_show_lag.py
@@ -12,7 +12,7 @@ from genie.metaparser.util.exceptions import SchemaEmptyParserError, \
 
 # iosxe show_lisp
 from genie.libs.parser.nxos.show_lag import ShowFeature, ShowLacpSystemIdentifier, \
-    ShowLacpCounters
+    ShowLacpCounters, ShowLacpNeighbor, ShowPortChannelSummary
 
 
 # =================================
@@ -97,6 +97,7 @@ class test_show_system_identifier(unittest.TestCase):
         obj = ShowLacpSystemIdentifier(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
+
 
 # =================================
 # Unit test for 'show lacp counters'
@@ -199,6 +200,230 @@ class test_show_lacp_counters(unittest.TestCase):
         obj = ShowLacpCounters(device=self.device)
         parsed_output = obj.parse()
         self.assertDictEqual(parsed_output, self.golden_parsed_output)
+
+
+# =================================
+# Unit test for 'show lacp neighbors'
+# =================================
+class test_show_lacp_neighbors(unittest.TestCase):
+    """unit test for show lacp counters """
+
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+Flags:  S - Device is sending Slow LACPDUs F - Device is sending Fast LACPDUs
+        A - Device is in Active mode       P - Device is in Passive mode
+port-channel1 neighbors
+Partner's information
+            Partner                Partner                     Partner
+Port        System ID              Port Number     Age         Flags
+Eth1/1      32768,5e-2-0-1-0-7     0x101           1140        SA
+
+            LACP Partner           Partner                     Partner
+            Port Priority          Oper Key                    Port State
+            32768                  0x8000                      0x3d
+
+Partner's information
+            Partner                Partner                     Partner
+Port        System ID              Port Number     Age         Flags
+Eth1/2      32768,5e-2-0-1-0-7     0x102           1140        SA
+
+            LACP Partner           Partner                     Partner
+            Port Priority          Oper Key                    Port State
+            32768                  0x8000                      0x3d
+
+
+Po2 neighbors
+Partner's information
+            Partner                Partner                     Partner
+Port        System ID              Port Number     Age         Flags
+Eth1/3      32768,5e-2-0-1-0-7     0x103           625         SA
+
+            LACP Partner           Partner                     Partner
+            Port Priority          Oper Key                    Port State
+            32768                  0x1                         0x3d
+
+Partner's information
+            Partner                Partner                     Partner
+Port        System ID              Port Number     Age         Flags
+Eth1/4      32768,5e-2-0-1-0-7     0x104           638         SA
+
+            LACP Partner           Partner                     Partner
+            Port Priority          Oper Key                    Port State
+            32768                  0x1                         0x3d
+
+Partner's information
+            Partner                Partner                     Partner
+Port        System ID              Port Number     Age         Flags
+Eth1/5      32768,5e-2-0-1-0-7     0x105           834         SA
+
+            LACP Partner           Partner                     Partner
+            Port Priority          Oper Key                    Port State
+            32768                  0x1                         0xd
+'''}
+    golden_parsed_output = {
+        'interfaces': {
+            'port-channel1': {
+                'members': {
+                    'Ethernet1/1': {
+                        'interface': 'Ethernet1/1',
+                        'activity': 'active',
+                        'oper_key': 0x8000,
+                        'port_num': 0x101,
+                        'partner_id': '5e-2-0-1-0-7',
+                        'age': 1140,
+                        'interval': 'slow',
+                        'lacp_port_priority': 32768,
+                        'port_state': 0x3d,
+                    },
+                    'Ethernet1/2': {
+                        'interface': 'Ethernet1/2',
+                        'activity': 'active',
+                        'oper_key': 0x8000,
+                        'port_num': 0x102,
+                        'partner_id': '5e-2-0-1-0-7',
+                        'age': 1140,
+                        'interval': 'slow',
+                        'lacp_port_priority': 32768,
+                        'port_state': 0x3d,
+                    },
+                }
+            },
+            'Po2': {
+                'members': {
+                    'Ethernet1/3': {
+                        'interface': 'Ethernet1/3',
+                        'activity': 'active',
+                        'oper_key': 0x1,
+                        'port_num': 0x103,
+                        'partner_id': '5e-2-0-1-0-7',
+                        'age': 625,
+                        'interval': 'slow',
+                        'lacp_port_priority': 32768,
+                        'port_state': 0x3d,
+                    },
+                    'Ethernet1/4': {
+                        'interface': 'Ethernet1/4',
+                        'activity': 'active',
+                        'oper_key': 0x1,
+                        'port_num': 0x104,
+                        'partner_id': '5e-2-0-1-0-7',
+                        'age': 638,
+                        'interval': 'slow',
+                        'lacp_port_priority': 32768,
+                        'port_state': 0x3d,
+                    },
+                    'Ethernet1/5': {
+                        'interface': 'Ethernet1/5',
+                        'activity': 'active',
+                        'oper_key': 0x1,
+                        'port_num': 0x105,
+                        'partner_id': '5e-2-0-1-0-7',
+                        'age': 834,
+                        'interval': 'slow',
+                        'lacp_port_priority': 32768,
+                        'port_state': 0xd,
+                    }
+                }
+            }
+        }
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowLacpNeighbor(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output)
+        obj = ShowLacpNeighbor(device=self.device)
+        parsed_output = obj.parse()
+        self.assertDictEqual(parsed_output, self.golden_parsed_output)
+
+
+# =================================
+# Unit test for 'show port-channel summary'
+# =================================
+class test_show_port_channel_summary(unittest.TestCase):
+    """unittest for show port-channel summary"""
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+Flags:  D - Down        P - Up in port-channel (members)
+        I - Individual  H - Hot-standby (LACP only)
+        s - Suspended   r - Module-removed
+        b - BFD Session Wait
+        S - Switched    R - Routed
+        U - Up (port-channel)
+        p - Up in delay-lacp mode (member)
+        M - Not in use. Min-links not met
+--------------------------------------------------------------------------------
+Group Port-       Type     Protocol  Member Ports
+      Channel
+--------------------------------------------------------------------------------
+1     Po1(RU)     Eth      LACP      Eth1/1(P)    Eth1/2(P)
+2     Po2(SU)     Eth      LACP      Eth1/3(P)    Eth1/4(P)    Eth1/5(H)
+'''}
+
+
+    golden_parsed_output = {
+        'interfaces': {
+            'Port-channel1': {
+                'bundle_id': 1,
+                'oper_status': 'up',
+                'layer': 'routed',
+                'protocol': 'lacp',
+                'type': 'eth',
+                'members': {
+                    'Ethernet1/1': {
+                        'flags': 'P',
+                    },
+                    'Ethernet1/2': {
+                        'flags': 'P'
+                    }
+                },
+            },
+            'Port-channel2': {
+                'bundle_id': 2,
+                'oper_status': 'up',
+                'layer': 'switched',
+                'protocol': 'lacp',
+                'type': 'eth',
+                'members': {
+                    'Ethernet1/3': {
+                        'flags': 'P',
+                    },
+                    'Ethernet1/4': {
+                        'flags': 'P'
+                    },
+                    'Ethernet1/5': {
+                        'flags': 'H'
+                    }
+                },
+
+            }
+        }
+    }
+
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowPortChannelSummary(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output)
+        obj = ShowPortChannelSummary(device=self.device)
+        parsed_output = obj.parse()
+        self.assertDictEqual(parsed_output, self.golden_parsed_output)
+
 
 
 if __name__ == '__main__':
