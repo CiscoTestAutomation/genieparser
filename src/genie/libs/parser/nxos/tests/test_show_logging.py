24103fd47fde350835a506a80818762f1830dd7e
Lubna Rasheed (lrasheed) <lrasheed@cisco.com>
2019-05-23 08:44:19 -0700

    make unittest pretty

--- src/genie/libs/parser/nxos/tests/test_show_logging.py
+++ src/genie/libs/parser/nxos/tests/test_show_logging.py
@@ -32,7 +32,8 @@ class test_show_logging(unittest.TestCase):
         'acl_names': ['match-ef-acl'],
         'logs': [
             '2019 May 22 16:20:45 ha01-n7010-01 %ACLLOG-5-ACLLOG_FLOW_INTERVAL: Src IP: 172.30.10.100, Dst IP: 15.15.15.2, Src Port: 0, Dst Port: 0, Src Intf: Ethernet3/3, Protocol: "IP"(253), ACL Name: match-ef-acl, ACE Action: Permit, Appl Intf: Vlan10, Hit-count: 600',
-            '2019 May 22 16:20:50 ha01-n7010-01 %ACLLOG-5-ACLLOG_FLOW_INTERVAL: Src IP: 172.30.10.100, Dst IP: 15.15.15.2, Src Port: 0, Dst Port: 0, Src Intf: Ethernet3/3, Protocol: "IP"(253), ACL Name: match-ef-acl, ACE Action: Permit, Appl Intf: Vlan10, Hit-count: 500'],
+            '2019 May 22 16:20:50 ha01-n7010-01 %ACLLOG-5-ACLLOG_FLOW_INTERVAL: Src IP: 172.30.10.100, Dst IP: 15.15.15.2, Src Port: 0, Dst Port: 0, Src Intf: Ethernet3/3, Protocol: "IP"(253), ACL Name: match-ef-acl, ACE Action: Permit, Appl Intf: Vlan10, Hit-count: 500',
+            ],
         }
 
     golden_output_1 = {'execute.return_value': '''
