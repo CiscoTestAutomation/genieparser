e6c15ddfcd8e95388dae638cf08e98c0abb29cd5
Lubna Rasheed (lrasheed) <lrasheed@cisco.com>
2019-05-23 08:41:13 -0700

    addressed code review comments

--- src/genie/libs/parser/nxos/show_logging.py
+++ src/genie/libs/parser/nxos/show_logging.py
@@ -1,7 +1,8 @@
 ''' show_logging.py
 
 NXOS parsers for the following show commands:
-    * show logging logfile | inc ACL
+    * show logging logfile
+    * show logging logfile | include {include}
 '''
 
 # Python
@@ -12,40 +13,56 @@ from genie.metaparser import MetaParser
 from genie.metaparser.util.schemaengine import Schema, Any, Optional
 
 
-# ===========================================
-# Schema for 'show logging logfile | inc ACL'
-# ===========================================
-class ShowLoggingACLSchema(MetaParser):
+# ==============================================
+# Schema for:
+#   * 'show logging logfile'
+#   * 'show logging logfile | include {include}'
+# ==============================================
+class ShowLoggingLogfileSchema(MetaParser):
     
     '''Schema for:
-        * 'show logging logfile | inc ACL'
+        * 'show logging logfile'
+        * 'show logging logfile | include {include}'
     '''
 
     schema = {
-        'acl_names': list,
+        'logs': list,
+        Optional('acl_names'): list,
         }
 
 
-# ===========================================
-# Parser for 'show logging logfile | inc ACL'
-# ===========================================
-class ShowLoggingACL(ShowLoggingACLSchema):
+# ==============================================
+# Parser for:
+#   * 'show logging logfile'
+#   * 'show logging logfile | include {include}'
+# ==============================================
+class ShowLoggingLogfile(ShowLoggingLogfileSchema):
 
-    '''Parser for:
-        * 'show logging logfile | inc ACL'
+    '''Schema for:
+        * 'show logging logfile'
+        * 'show logging logfile | include {include}'
     '''
 
-    cli_command = ['show logging logfile | inc ACL']
+    cli_command = ['show logging logfile | include {include}',
+                   'show logging logfile',
+                   ]
 
-    def cli(self, output=None):
+    def cli(self, include='', output=None):
 
         if output is None:
-            out = self.device.execute(self.cli_command[0])
+            # Build the command
+            if include:
+                cmd = self.cli_command[0].format(include=include)
+            else:
+                cmd = self.cli_command[1]
+            # Execute the command
+            out = self.device.execute(cmd)
         else:
             out = output
 
         # Init vars
         parsed_dict = {}
+        log_lines = []
         acl_names = []
 
         # 2019 May 22 16:20:45 ha01-n7010-01 %ACLLOG-5-ACLLOG_FLOW_INTERVAL: Src IP: 172.30.10.100, Dst IP: 15.15.15.2, Src Port: 0, Dst Port: 0, Src Intf: Ethernet3/3, Protocol: "IP"(253), ACL Name: match-ef-acl, ACE Action: Permit, Appl Intf: Vlan10, Hit-count: 600
@@ -55,6 +72,12 @@ class ShowLoggingACL(ShowLoggingACLSchema):
         for line in out.splitlines():
             line = line.strip()
 
+            # Add line to 'logs'
+            if line and 'show logging logfile' not in line:
+                log_lines.append(line)
+                parsed_dict['logs'] = log_lines
+
+            # ... ACL Name: match-ef-acl ...
             m = p1.match(line)
             if m:
                 acl_name = m.groupdict()['acl_name']
