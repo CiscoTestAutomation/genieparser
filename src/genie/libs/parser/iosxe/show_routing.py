2b37fc5bd23787f06cd2d4271bb2d5eb33660169
hanyyu <hanyyu@cisco.com>
2019-06-03 12:06:00 -0700

    added output in comment

--- src/genie/libs/parser/iosxe/show_routing.py
+++ src/genie/libs/parser/iosxe/show_routing.py
@@ -1097,6 +1097,7 @@ class ShowIpRouteWord(ShowIpRouteWordSchema):
         p1 = re.compile(r'^Routing +entry +for +'
                          '(?P<entry>(?P<ip>[\w\:\.]+)\/(?P<mask>\d+))'
                          '(, +(?P<net>[\w\s]+))?$')
+        # Known via "connected", distance 0, metric 0 (connected)
         p2 = re.compile(r'^Known +via +\"(?P<known_via>[\w\s]+)\", +distance +(?P<distance>\d+), +metric +(?P<metric>\d+),? +(?P<type>[\w\- ]+)?(\((connected)\))?$')
         p3 = re.compile(r'^Redistributing +via +(?P<redist_via>\w+) *'
                          '(?P<redist_via_tag>\d+)?$')
@@ -1140,6 +1141,7 @@ class ShowIpRouteWord(ShowIpRouteWordSchema):
             # Known via "static", distance 1, metric 0, candidate default path
             # Known via "eigrp 1", distance 130, metric 10880, type internal
             # Known via "rip", distance 120, metric 2
+            # Known via "connected", distance 0, metric 0 (connected)
             m = p2.match(line)
             if m:
                 group = m.groupdict()
