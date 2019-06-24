cf4911e5ee395375507a7024fcefe9a4817214a6
Parsa Yadollahi <payadoll@cisco.com>
2019-06-24 16:42:24 -0400

    fixed merge conflicts

--- src/genie/libs/parser/iosxe/show_ospf.py
+++ src/genie/libs/parser/iosxe/show_ospf.py
@@ -6295,9 +6295,14 @@ class ShowIpOspfNeighbor(ShowIpOspfNeighborSchema):
         * 'show ip ospf neighbor {interface}'
     '''
 
+<<<<<<< Updated upstream
     cli_command = [
         'show ip ospf neighbor {interface}',
         'show ip ospf neighbor']
+=======
+    cli_command = 'show ip ospf neighbor'
+    exclude = ['dead_time']
+>>>>>>> Stashed changes
 
     def cli(self, interface='', output=None):
 
