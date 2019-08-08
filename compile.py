187a3f5d95d27df80e6036db70be9babb697b249
Parsa Yadollahi <payadoll@cisco.com>
2019-08-08 13:48:06 -0400

    addressed comment

--- compile.py
+++ compile.py
@@ -35,18 +35,8 @@ def generate_cython_modules(src_path, ignore_patterns = []):
                     # pattern match!
                     break
             else:
-                module_temp = module_file.split('/')
-                module_name = module_temp[:-1] + module_temp[-1].split('.')
-
-                if 'ats' in module_name:
-                    index = module_name.index('ats')
-                else:
-                    index = module_name.index(src) + 1
-
-                module_name = '.'.join(module_name[index:])
-
+                module_name = os.path.splitext(os.path.basename(module_file))[0] + '.py'
                 modules.append(Extension(module_name, [module_file,]))
-
     return modules
 
 
