a112dd6d012528fba39a1b8874b84a28c8551610
Felipe Sasso <fcoralsa@cisco.com>
2019-08-13 09:29:59 -0400

    Fixed schema

--- src/genie/libs/parser/junos/show_route.py
+++ src/genie/libs/parser/junos/show_route.py
@@ -13,10 +13,6 @@ import re
 from genie.metaparser import MetaParser
 from genie.metaparser.util.schemaengine import Any, Optional
 
-# Libs 
-from genie.libs.parser.utils.common import Common
-
-
 '''
 Schema for:
     * show route table {table}
@@ -34,27 +30,30 @@ class ShowRouteTableSchema(MetaParser):
                         'active_route_count': int,
                         'holddown_route_count': int,
                         'hidden_route_count': int,
-                        'rt': {
-                            'rt_destination': {
+                        'routes': {                            
                                 Any(): {
                                     'active_tag': str,
                                     'protocol_name': str,
                                     'preference': str,
                                     'age': str,
                                     'metric': str,
-                                    'nh': {
-                                        'to': str,
-                                        'via': str,
-                                        Optional('mpls_label'): str,
-                                    }
-                                }   
+                                    'next_hop': {
+                                        'next_hop_list': {
+                                            Any(): {
+                                                'to': str,
+                                                'via': str,
+                                                Optional('mpls_label'): str,
+                                                Optional('best_route'): str
+                                            }
+                                        }
+                                    }   
+                                }
                             }
                         }
                     }
                 }
             }
         }
-    }
 
 '''
 Parser for:
@@ -93,8 +92,8 @@ class ShowRouteTable(ShowRouteTableSchema):
 
         # > to 200.0.0.6 via ge-0/0/1.0
         # > to 200.0.0.6 via ge-0/0/1.0, Push 305550
-        r3 = re.compile(r'\>\s+to\s+(?P<to>\S+)\s+via\s+(?P<via>[\w\d\/\-\.]+)'
-                         '\,*\s*(?:(?P<mpls_label>\S+\s+\d+))?')
+        r3 = re.compile(r'(?:(?P<best_route>\>*))?\s*to\s+(?P<to>\S+)\s+via\s+'
+                         '(?P<via>[\w\d\/\-\.]+)\,*\s*(?:(?P<mpls_label>\S+\s+\d+))?')
 
         parsed_output = {}
 
@@ -126,8 +125,7 @@ class ShowRouteTable(ShowRouteTableSchema):
 
                 rt_destination = group['rt_destination']
 
-                route_dict = table_dict.setdefault('rt', {})\
-                .setdefault('rt_destination', {})\
+                route_dict = table_dict.setdefault('routes', {})\
                 .setdefault(rt_destination, {})
                 
                 route_dict['active_tag'] = group['active_tag']
@@ -140,20 +138,32 @@ class ShowRouteTable(ShowRouteTableSchema):
 
             # > to 200.0.0.6 via ge-0/0/1.0
             # > to 200.0.0.6 via ge-0/0/1.0, Push 305550
+            # to 10.2.94.2 via lt-1/2/0.49
             result = r3.match(line)
             if result:
-                group = result.groupdict()
-                nh_dict = route_dict.setdefault('nh', {})
-                nh_dict['to'] = group['to']
 
-                interface = group['via'].strip()
-                interface = Common.convert_intf_name(intf=interface)
-                nh_dict['via'] = interface
+                max_index = table_dict.get('routes', {}).get(rt_destination, {}).get('next_hop', {}).get('next_hop_list', {}).keys()
+
+                if not max_index:
+                    max_index = 1
+                else:
+                    max_index = max(max_index) + 1
+
+                group = result.groupdict()
+
+                nh_dict = route_dict.setdefault('next_hop', {}).setdefault('next_hop_list', {}).setdefault(max_index, {})
+
+                nh_dict['to'] = group['to']
+                nh_dict['via'] = group['via']
 
                 mpls_label = group['mpls_label']
                 if mpls_label:
                     nh_dict['mpls_label'] = mpls_label
 
-                continue
+                best_route = group['best_route']
+                if best_route:
+                    nh_dict['best_route'] = best_route
 
+                continue
+        
         return parsed_output
