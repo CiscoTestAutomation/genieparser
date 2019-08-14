1baa2f2c4f5219b8748b107f1cf165ccde588cf8
Thai Nguyen <thaing@cisco.com>
2019-08-14 19:36:47 -0400

    Add empty test for new parser

--- src/genie/libs/parser/iosxe/tests/test_show_authentication_sessions.py
+++ src/genie/libs/parser/iosxe/tests/test_show_authentication_sessions.py
@@ -76,32 +76,32 @@ class test_show_authentication_sessions(unittest.TestCase):
     }
 
     golden_parsed_output_2 = {
-		'interfaces': {
-			'GigabitEthernet1/7/35': {
-				'interface': 'GigabitEthernet1/7/35',
-				'client': {
-					'0000.0022.2222': {
-						'client': '0000.0022.2222',
-						'method': 'dot1x',
-						'domain': 'UNKNOWN',
-						'status': 'Auth',
-						'session': {
-							'141927640000000E0B40EDB0': {
-								'session_id': '141927640000000E0B40EDB0',
-	                        }
-	                    }
-	                }
-	            }
-	        }
-	    }
-	}
+        'interfaces': {
+            'GigabitEthernet1/7/35': {
+                'interface': 'GigabitEthernet1/7/35',
+                'client': {
+                    '0000.0022.2222': {
+                        'client': '0000.0022.2222',
+                        'method': 'dot1x',
+                        'domain': 'UNKNOWN',
+                        'status': 'Auth',
+                        'session': {
+                            '141927640000000E0B40EDB0': {
+                                'session_id': '141927640000000E0B40EDB0',
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     golden_output_2 = {'execute.return_value': '''\
-    	show authentication sessions interface GigabitEthernet1/7/35
+        show authentication sessions interface GigabitEthernet1/7/35
 
-		Interface Identifier     Method Domain  Status Fg Session ID
-		-----------------------------------------------------------------------------
-		Gi1/7/35  0000.0022.2222 dot1x  UNKNOWN Auth      141927640000000E0B40EDB0
+        Interface Identifier     Method Domain  Status Fg Session ID
+        -----------------------------------------------------------------------------
+        Gi1/7/35  0000.0022.2222 dot1x  UNKNOWN Auth      141927640000000E0B40EDB0
     '''
     }
 
@@ -126,72 +126,75 @@ class test_show_authentication_sessions(unittest.TestCase):
         self.assertEqual(parsed_output,self.golden_parsed_output_2)
 
 class test_show_authentication_sessions_interface_details(unittest.TestCase):
+    dev1 = Device(name='empty')
     dev_c3850 = Device(name='c3850')
+    empty_output = {'execute.return_value': '      '}
+
     golden_parsed_output_3 = {
-    	'interfaces': {
-	        'GigabitEthernet3/0/2': {
-	            'interface': 'GigabitEthernet3/0/2',
-	            'iif_id': '0x1055240000001F6',
-	            'mac_address': '0010.0010.0001', 
-	            'ipv6_address': 'Unknown',
-	            'ipv4_address': '192.0.2.1',
-	            'user_name': 'auto601',
-	            'status': 'Authorized',
-	            'domain': 'DATA',
-	            'current_policy': 'dot1x_dvlan_reauth_hm', 
-	            'oper_host_mode': 'single-host',
-	            'oper_control_dir': 'both',
-	            'session_timeout': 'N/A',
-	            'common_session_id': 'AC14FC0A0000101200E28D62',
-	            'acct_session_id': 'Unknown',
-	            'handle': '0xDB003227',
-	            'local_policies': {
-	            	'CRITICAL_VLAN': {
-		                'template': 'CRITICAL_VLAN',
-		                'priority': 150,
-	            	},
-	            	'Vlan': {
-	                	'vlan_name': 'Vlan',
+        'interfaces': {
+            'GigabitEthernet3/0/2': {
+                'interface': 'GigabitEthernet3/0/2',
+                'iif_id': '0x1055240000001F6',
+                'mac_address': '0010.0010.0001', 
+                'ipv6_address': 'Unknown',
+                'ipv4_address': '192.0.2.1',
+                'user_name': 'auto601',
+                'status': 'Authorized',
+                'domain': 'DATA',
+                'current_policy': 'dot1x_dvlan_reauth_hm', 
+                'oper_host_mode': 'single-host',
+                'oper_control_dir': 'both',
+                'session_timeout': 'N/A',
+                'common_session_id': 'AC14FC0A0000101200E28D62',
+                'acct_session_id': 'Unknown',
+                'handle': '0xDB003227',
+                'local_policies': {
+                    'CRITICAL_VLAN': {
+                        'template': 'CRITICAL_VLAN',
+                        'priority': 150,
+                    },
+                    'Vlan': {
+                        'vlan_name': 'Vlan',
                         'vlan_value': 130
-	            	}
+                    }
                 },
-	            'method_status': {
+                'method_status': {
                     'dot1x': {
                         'method': 'dot1x',
                         'state': 'Authc Failed',
                     }
-	            },
+                },
             }
         }
     }
 
     golden_output_3 = {'execute.return_value': '''\
-    	show authentication sessions interface GigabitEthernet3/0/2 details
+        show authentication sessions interface GigabitEthernet3/0/2 details
 
-		        Interface:  GigabitEthernet3/0/2
-		           IIF-ID:  0x1055240000001F6 
-		      MAC Address:  0010.0010.0001
-		     IPv6 Address:  Unknown
-		     IPv4 Address:  192.0.2.1
-		        User-Name:  auto601
-		           Status:  Authorized
-		           Domain:  DATA
-		   Oper host mode:  single-host
-		 Oper control dir:  both
-		  Session timeout:  N/A
-		Common Session ID:  AC14FC0A0000101200E28D62
-		  Acct Session ID:  Unknown
-		           Handle:  0xDB003227
-		   Current Policy:  dot1x_dvlan_reauth_hm
+                Interface:  GigabitEthernet3/0/2
+                   IIF-ID:  0x1055240000001F6 
+              MAC Address:  0010.0010.0001
+             IPv6 Address:  Unknown
+             IPv4 Address:  192.0.2.1
+                User-Name:  auto601
+                   Status:  Authorized
+                   Domain:  DATA
+           Oper host mode:  single-host
+         Oper control dir:  both
+          Session timeout:  N/A
+        Common Session ID:  AC14FC0A0000101200E28D62
+          Acct Session ID:  Unknown
+                   Handle:  0xDB003227
+           Current Policy:  dot1x_dvlan_reauth_hm
 
-		Local Policies:
-		         Template: CRITICAL_VLAN (priority 150)
-		       Vlan Group:  Vlan: 130
+        Local Policies:
+                 Template: CRITICAL_VLAN (priority 150)
+               Vlan Group:  Vlan: 130
 
-		Method status list:
-		   Method           State
-		   dot1x            Authc Failed
-		'''
+        Method status list:
+           Method           State
+           dot1x            Authc Failed
+        '''
     }
 
     golden_parsed_output_4 = {
@@ -260,15 +263,20 @@ class test_show_authentication_sessions_interface_details(unittest.TestCase):
            mab              Running
         '''
     }
+    def test_empty_3(self):
+        self.dev1 = Mock(**self.empty_output)
+        obj = ShowAuthenticationSessions(device=self.dev1)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
 
-    def test_golden_3(self):
+    def test_golden_4(self):
         self.maxDiff = None
         self.dev_c3850 = Mock(**self.golden_output_3)
         obj = ShowAuthenticationSessionsInterfaceDetails(device=self.dev_c3850)
         parsed_output = obj.parse(intf='GigabitEthernet3/0/2')
         self.assertEqual(parsed_output,self.golden_parsed_output_3)
 
-    def test_golden_4(self):
+    def test_golden_5(self):
         self.maxDiff = None
         self.dev_c3850 = Mock(**self.golden_output_4)
         obj = ShowAuthenticationSessionsInterfaceDetails(device=self.dev_c3850)
