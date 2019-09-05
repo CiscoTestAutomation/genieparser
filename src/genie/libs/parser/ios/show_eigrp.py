0cbe96ad9a93df0b1bbcf43668eaca1dddf922a8
Felipe Sasso <fcoralsa@cisco.com>
2019-09-05 07:27:21 -0400

    Added inheritance

--- src/genie/libs/parser/ios/show_eigrp.py
+++ src/genie/libs/parser/ios/show_eigrp.py
@@ -18,37 +18,25 @@ from genie.libs.parser.iosxe.show_eigrp import (
     ShowIpv6EigrpNeighborsDetail as ShowIpv6EigrpNeighborsDetail_iosxe,
 )
 
-
 class ShowIpEigrpNeighbors(ShowIpEigrpNeighbors_iosxe):
     # Parser for:
     #   * 'show ip eigrp vrf {vrf} neighbors'
     #   * 'show ip eigrp neighbors'
-
-    def cli(self, vrf="", output=None):
-        return super().cli(vrf=vrf, output=output)
-
+    pass
 
 class ShowIpv6EigrpNeighbors(ShowIpv6EigrpNeighbors_iosxe):
     # Parser for:
     #   * 'show ipv6 eigrp vrf {vrf} neighbors'
     #   * 'show ipv6 eigrp neighbors'
-
-    def cli(self, vrf="", output=None):
-        return super().cli(vrf=vrf, output=output)
-
+    pass
 
 class ShowIpEigrpNeighborsDetail(ShowIpEigrpNeighborsDetail_iosxe):
     # Parser for:
     #   'show ip eigrp neighbors detail'
     #   'show ip eigrp vrf {vrf} neighbors detail'
-
-    def cli(self, vrf="", output=None):
-        return super().cli(vrf=vrf, output=output)
-
+    pass
 
 class ShowIpv6EigrpNeighborsDetail(ShowIpv6EigrpNeighborsDetail_iosxe):
     # Parser for:
     #   'show ipv6 eigrp neighbors detail'
-
-    def cli(self, output=None):
-        return super().cli(output=output)
+    pass
