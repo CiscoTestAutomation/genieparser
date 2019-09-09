77f6d9659995e84577b2da2a2daabdfe90d8f8cf
Felipe Sasso <fcoralsa@cisco.com>
2019-09-09 05:05:00 -0400

    Removed command from test

--- src/genie/libs/parser/ios/tests/test_show_eigrp.py
+++ src/genie/libs/parser/ios/tests/test_show_eigrp.py
@@ -3,8 +3,7 @@ Unit tests for the following commands:
 
     * 'show ip eigrp neighbors'
     * 'show ip eigrp vrf {vrf} neighbors'
-    * 'show ipv6 eigrp neighbors'
-    * 'show ipv6 eigrp vrf {vrf} neighbors'
+    * 'show ipv6 eigrp neighbors'    
     * 'show ip eigrp neighbors detail'
     * 'show ip eigrp vrf {vrf} neighbors detail'
     * 'show ipv6 eigrp neighbors detail'
