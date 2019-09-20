7fd67f024a1ecf26036877cec14e7d8269d0549d
Thai Nguyen <thaing@cisco.com>
2019-09-20 08:54:07 -0700

    continue working on parser

--- src/genie/libs/parser/iosxe/show_ip_nat.py
+++ src/genie/libs/parser/iosxe/show_ip_nat.py
@@ -50,7 +50,7 @@ class ShowIpNatTranslationsSchema(MetaParser):
                     }
                 }
             },
-            number_of_translations: int
+            Optional('number_of_translations'): int
         }
     }
 
@@ -75,25 +75,50 @@ class ShowIpNatTranslations(ShowIpNatTranslationsSchema):
         else:
             out = output
 
-        p1 = re.compile(r'^(?P<protocol>-+|udp|tcp) +(?P<inside_global>\S+) '
-                     '+(?P<inside_local>\S+) +(?P<outside_local>\S+) +(?P<outside_global>\S+)$')
-
-        # initialize variables
-        ret_dict = {}
-        index = 0
-
-        for line in out.splitlines():
-            line = line.strip()
-        
-        m = p1.match(line)
-        if m:
-            protoca
         # udp  10.5.5.1:1025          192.0.2.1:4000 --- ---
         # udp  10.5.5.1:1024          192.0.2.3:4000 --- ---
         # udp  10.5.5.1:1026          192.0.2.2:4000 --- ---
         # --- 171.69.233.209     192.168.1.95 --- ---
-        # --- 171.69.233.210     192.168.1.89 --- --
+        # --- 171.69.233.210     192.168.1.89 --- ---
         # udp 171.69.233.209:1220  192.168.1.95:1220  171.69.2.132:53    171.69.2.132:53
         # tcp 171.69.233.209:11012 192.168.1.89:11012 171.69.1.220:23    171.69.1.220:23
         # tcp 171.69.233.209:1067  192.168.1.95:1067  171.69.1.161:23    171.69.1.161:23
+        p1 = re.compile(r'^(?P<protocol>-+|udp|tcp) +(?P<inside_global>\S+) '
+                         '+(?P<inside_local>\S+) +(?P<outside_local>\S+) +(?P<outside_global>\S+)$')
+
+        # initialize variables
+        ret_dict = {}
+        index = 1
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            # udp  10.5.5.1:1025          192.0.2.1:4000 --- ---
+            # udp  10.5.5.1:1024          192.0.2.3:4000 --- ---
+            # udp  10.5.5.1:1026          192.0.2.2:4000 --- ---
+            # --- 171.69.233.209     192.168.1.95 --- ---
+            # --- 171.69.233.210     192.168.1.89 --- ---
+            # udp 171.69.233.209:1220  192.168.1.95:1220  171.69.2.132:53    171.69.2.132:53
+            # tcp 171.69.233.209:11012 192.168.1.89:11012 171.69.1.220:23    171.69.1.220:23
+            # tcp 171.69.233.209:1067  192.168.1.95:1067  171.69.1.161:23    171.69.1.161:23
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                #import pdb;pdb.set_trace()
+                protocol_dict = ret_dict.setdefault('nat_translations', {}).setdefault('index', {}).setdefault(index, {})
+                protocol_dict.update({'protocol': group['protocol']})
+                protocol_dict.update({'inside_global': group['inside_global']})
+                protocol_dict.update({'inside_local': group['inside_local']})
+                protocol_dict.update({'outside_global': group['outside_global']})
+                protocol_dict.update({'outside_local': group['outside_local']})
+
+                index += 1
+
+                continue
+            
+        return ret_dict
+
+
+
+
 
--- src/genie/libs/parser/iosxe/tests/test_show_ip_nat.py
+++ src/genie/libs/parser/iosxe/tests/test_show_ip_nat.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 import unittest
 from unittest.mock import Mock
 
@@ -18,9 +19,6 @@ from genie.libs.parser.iosxe.show_ip_nat import ShowIpNatTranslations
 
 class TestShowIpNatTranslations(unittest.TestCase):
 
-    dev_c3850 = Device(name='c3850')
-    empty_output = {'execute.return_value': ''}
-
     golden_output = {'execute.return_value': '''\
         Device# show ip nat translations
 
@@ -31,6 +29,34 @@ class TestShowIpNatTranslations(unittest.TestCase):
     '''
     }
 
+    golden_parsed_output = {
+        'nat_translations': {
+            'index': {
+                1: {
+                    'inside_global': '10.5.5.1:1025',
+                    'inside_local': '192.0.2.1:4000',
+                    'outside_global': '---',
+                    'outside_local': '---',
+                    'protocol': 'udp'
+                },
+                2: {
+                    'inside_global': '10.5.5.1:1024',
+                    'inside_local': '192.0.2.3:4000',
+                    'outside_global': '---',
+                    'outside_local': '---',
+                    'protocol': 'udp'
+                },
+                3: {
+                    'inside_global': '10.5.5.1:1026',
+                    'inside_local': '192.0.2.2:4000',
+                    'outside_global': '---',
+                    'outside_local': '---',
+                    'protocol': 'udp'
+                }
+            }
+        }
+    }
+
     golden_output_1 = {'execute.return_value': '''\
         Router#show ip nat translations
         Pro Inside global      Inside local       Outside local      Outside global
@@ -39,6 +65,26 @@ class TestShowIpNatTranslations(unittest.TestCase):
     '''
     }
 
+    golden_parsed_output_1 = {
+        'nat_translations': {
+            'index': {
+                1: {
+                    'inside_global': '171.69.233.209',
+                    'inside_local': '192.168.1.95',
+                    'outside_global': '---',
+                    'outside_local': '---',
+                    'protocol': '---'
+                },
+                2: {
+                    'inside_global': '171.69.233.210',
+                    'inside_local': '192.168.1.89',
+                    'outside_global': '--',
+                    'outside_local': '---',
+                    'protocol': '---'
+                }
+            }
+        }
+    }
     golden_output_2 = {'execute.return_value': '''\
         Router#show ip nat translations
         Pro Inside global        Inside local       Outside local      Outside global
@@ -48,17 +94,69 @@ class TestShowIpNatTranslations(unittest.TestCase):
     '''
     }
 
+    golden_parsed_output_2 = {
+        'nat_translations': {
+            'index': {
+                1: {
+                    'inside_global': '171.69.233.209:1220',
+                    'inside_local': '192.168.1.95:1220',
+                    'outside_global': '171.69.2.132:53',
+                    'outside_local': '171.69.2.132:53',
+                    'protocol': 'udp'
+                },
+                2: {
+                    'inside_global': '171.69.233.209:11012',
+                    'inside_local': '192.168.1.89:11012',
+                    'outside_global': '171.69.1.220:23',
+                    'outside_local': '171.69.1.220:23',
+                    'protocol': 'tcp'
+                },
+                3: {'inside_global': '171.69.233.209:1067',
+                    'inside_local': '192.168.1.95:1067',
+                    'outside_global': '171.69.1.161:23',
+                    'outside_local': '171.69.1.161:23',
+                    'protocol': 'tcp'
+                }
+            }
+        }
+    }
+
+    device = Device(name='c3850')
+    dev_empty = Device(name='empty')
+    empty_output = {'execute.return_value': '  '}
+
     def test_empty(self):
-        self.device = Mock(**self.empty_output)
-        obj = ShowIpNatTranslations(device=self.device)
+        self.dev_empty = Mock(**self.empty_output)
+        obj = ShowIpNatTranslations(device=self.dev_empty)
         with self.assertRaises(SchemaEmptyParserError):
             parsed_output = obj.parse()
     
-    def test_golden_translations(self):
+    def test_golden(self):
         self.maxDiff = None
         self.device = Mock(**self.golden_output)
-        obj = ShowIpPimDf(device=self.device)
+        obj = ShowIpNatTranslations(device=self.device)
         parsed_output = obj.parse()
         import pprint
         pprint.pprint(parsed_output)
-        #self.assertEqual(parsed_output, self.golden_parsed_output)
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+    def test_golden_1(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_1)
+        obj = ShowIpNatTranslations(device=self.device)
+        parsed_output = obj.parse()
+        import pprint
+        pprint.pprint(parsed_output)
+        self.assertEqual(parsed_output, self.golden_parsed_output_1)
+
+    def test_golden_2(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_2)
+        obj = ShowIpNatTranslations(device=self.device)
+        parsed_output = obj.parse()
+        import pprint
+        pprint.pprint(parsed_output)
+        self.assertEqual(parsed_output, self.golden_parsed_output_2)
+
+if __name__ == '__main__':
+    unittest.main()
