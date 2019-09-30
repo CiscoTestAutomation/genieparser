de7653240692bb98a996ab864cb37fbea780825f
Xiaoxin Zhou <xiaoxinz@cisco.com>
2019-09-30 11:31:04 -0700

    update show_l2route_topology for xr

--- src/genie/libs/parser/iosxr/show_l2route.py
+++ src/genie/libs/parser/iosxr/show_l2route.py
@@ -5,18 +5,32 @@ show l2route parser class
 """
 
 import re
-from ipaddress import ip_address
 
 from genie.metaparser import MetaParser
-from genie.metaparser.util.schemaengine import Any
-
-from genie.libs.parser.base import *
+from genie.metaparser.util.schemaengine import Any, Optional
 
 
-class ShowL2routeTopology(MetaParser):
+class ShowL2routeTopologySchema(MetaParser):
+    """Schema for:
+        * 'show l2route topology'
+    """
+    schema = {
+        Any(): {
+            'topo_id': {
+                Any(): {
+                    'topo_name': {
+                        Any(): {
+                            Optional('topo_type'): str
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+class ShowL2routeTopology(ShowL2routeTopologySchema):
     """Parser class for show l2route topology """
-
-    # TODO schema
 
     cli_command = 'show l2route topology'
 
@@ -26,122 +40,51 @@ class ShowL2routeTopology(MetaParser):
         else:
             out = output
 
-        result = {
-            'topologies': [],
-        }
+        # Topology ID   Topology Name    Type
+        # -----------   -------------    ----
+        # 51             bd2              L2VRF
+        # 4294967294     GLOBAL           N/A
+        # 4294967295     ALL              N/A
 
+        p = re.compile(
+            r'^\s*(?P<topo_id>\d+)\s* +'
+            r'(?P<topo_name>\S+)\s* +'
+            r'(?:N/A|(?P<topo_type>\S+))')
 
-        for line in out.splitlines():
-            line = line.rstrip()
-
-            # Topology ID   Topology Name    Type
-            # -----------   -------------    ----
-
-            # 51             bd2              L2VRF
-            # 4294967294     GLOBAL           N/A
-            # 4294967295     ALL              N/A
-            m = re.match(r'^(?P<topo_id>\d+)'
-                         r' +(?P<name>\S+)'
-                         r' +(?:N/A|(?P<type>\S+))$', line)
-            if m:
-                topo_id = eval(m.group('topo_id'))
-                entry = {
-                    'topo_id': topo_id,
-                    'name': m.group('name'),
-                    'type': m.group('type'),
-                }
-                result['topologies'].append(entry)
-                continue
-
-        return result
-
-
-class ShowL2routeEvpnMac(MetaParser):
-    """Parser for show l2route evpn mac all"""
-
-    # TODO schema
-
-    cli_command = 'show l2route evpn mac all'
-
-    def cli(self, output=None):
-        if output is None:
-            out = self.device.execute(self.cli_command)
-        else:
-            out = output
-
-        result = {
-            'entries': [],
-        }
-
+        parsed_dict = {}
+        counts = 0
 
         for line in out.splitlines():
             line = line.rstrip()
 
-            # Topo ID  Mac Address    Prod   Next Hop(s)
-            # -------- -------------- ------ ----------------------------------------
+            result = p.match(line)
 
-            # 1        7777.7777.0002 LOCAL  Bundle-Ether1.7
-            # 50       fc00.0001.0000 L2VPN  192.0.0.0/28270/ME
-            m = re.match(r'^(?P<topo_id>\d+)'
-                         r' +(?P<mac>[A-Za-z0-9]+\.[A-Za-z0-9]+\.[A-Za-z0-9]+)'
-                         r' +(?P<producer>\S+)'
-                         r' +(?:none|(?P<next_hop>\S+))$', line)
-            if m:
-                entry = {
-                    'topo_id': eval(m.group('topo_id')),
-                    'mac': m.group('mac'),
-                    'producer': m.group('producer'),
-                    'next_hop': m.group('next_hop'),
-                }
-                result['entries'].append(entry)
+            if result:
+                counts += 1
+                group_dict = result.groupdict()
+
+                str_topology = 'topology %d' % counts
+                str_id = group_dict['topo_id']
+                str_name = group_dict['topo_name']
+                str_type = group_dict['topo_type']
+
+                if str_type is None:
+                    str_type = 'N/A'
+
+                id_dict = {}
+                name_dict = {}
+                type_dict = {}
+
+                id_dict['topo_id'] = {}
+                name_dict['topo_name'] = {}
+                type_dict['topo_type'] = {}
+
+                type_dict['topo_type'] = str_type
+                name_dict['topo_name'].setdefault(str_name, type_dict)
+                id_dict['topo_id'].setdefault(str_id, name_dict)
+
+                parsed_dict.update({str_topology: id_dict})
+
                 continue
 
-        return result
-
-
-class ShowL2routeEvpnMacIp(MetaParser):
-    """Parser for show l2route evpn mac-ip all"""
-
-    # TODO schema
-
-    cli_command = 'show l2route evpn mac-ip all'
-
-    def cli(self, output=None):
-        if output is None:
-            out = self.device.execute(self.cli_command)
-        else:
-            out = output
-
-        result = {
-            'entries': [],
-        }
-
-
-        for line in out.splitlines():
-            line = line.rstrip()
-
-            # Topo ID  Mac Address    IP Address      Prod   Next Hop(s)
-            # -------- -------------- --------------- ------ ----------------------------------------
-
-            # 0        fc00.0001.0006 192.168.166.3       LOCAL  Bundle-Ether1.0
-            # 0        fc00.0001.0006 192.168.166.3       L2VPN  Bundle-Ether1.0
-            # 0        fc00.0001.0008 192.168.49.3       L2VPN  68101/I/ME
-            m = re.match(r'^(?P<topo_id>\d+)'
-                         r' +(?P<mac>[A-Za-z0-9]+\.[A-Za-z0-9]+\.[A-Za-z0-9]+)'
-                         r' +(?P<ip>\d+\.\d+\.\d+\.\d+|[A-Fa-f0-9:]+)'
-                         r' +(?P<producer>\S+)'
-                         r' +(?:none|(?P<next_hop>\S+))$', line)
-            if m:
-                entry = {
-                    'topo_id': eval(m.group('topo_id')),
-                    'mac': m.group('mac'),
-                    'ip': ip_address(m.group('ip')),
-                    'producer': m.group('producer'),
-                    'next_hop': m.group('next_hop'),
-                }
-                result['entries'].append(entry)
-                continue
-
-        return result
-
-# vim: ft=python ts=8 sw=4 et
+        return parsed_dict
