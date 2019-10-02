4e8e1388fa64953f19f86fd2e250ec22df3e6f78
Felipe Sasso <fcoralsa@cisco.com>
2019-10-02 03:34:46 -0400

    Changed system_id key to Optional

--- src/genie/libs/parser/iosxe/show_clns.py
+++ src/genie/libs/parser/iosxe/show_clns.py
@@ -651,7 +651,7 @@ class ShowClnsIsNeighborsDetailSchema(MetaParser):
     schema = {
         'tag': {
             Any(): {
-                'system_id': {
+                Optional('system_id'): {
                     Any(): {
                        'type': {
                            Any(): {
