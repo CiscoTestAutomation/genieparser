c7450223b3a4e8487c28c348853c955f52e6392c
Badal Soyantar <bsoyanta@cisco.com>
2019-10-02 07:31:47 -0700

    Comments addressed

--- src/genie/libs/parser/iosxr/tests/test_show_evpn.py
+++ src/genie/libs/parser/iosxr/tests/test_show_evpn.py
@@ -519,5 +519,184 @@ class test_show_evpn_ethernet_segment_detail(unittest.TestCase):
         parsed_output = obj.parse()
         self.assertEqual(parsed_output,self.golden_parsed_output1)
 
+# ============================================================
+#  Unit test for 'show evpn ethernet-segment esi {esi} detail'
+# ============================================================
+
+class TestShowEvpnEthernetSegmentEsiDetail(unittest.TestCase):
+
+    '''Unit test for 'show evpn ethernet-segment esi {esi} detail'''
+    
+    device = Device(name='aDevice')
+    empty_output = {'execute.return_value': ''}
+
+    golden_parsed_output1 = {
+        'segment_id': {
+            '0036.3700.0000.0000.1100': {
+                'interface': {
+                    'BE100': {
+                        'next_hops': ['3.3.3.36', '3.3.3.37'],
+                        'es_to_bgp_gates': 'Ready',
+                        'es_to_l2fib_gates': 'Ready',
+                        'main_port': {
+                            'interface': 'Bundle-Ether100',
+                            'interface_mac': '008a.9644.d8dd',
+                            'if_handle': '0x0800001c',
+                            'state': 'Up',
+                            'redundancy': 'Not Defined',
+                        },
+                        'esi_type': 0,
+                        'value': '36.3700.0000.0000.1100',
+                        'es_import_rt': '3637.0000.0000 (from ESI)',
+                        'source_mac': '0000.0000.0000 (N/A)',
+                        'topology': {
+                            'operational': 'MH, All-active',
+                            'configured': 'All-active (AApF) (default)',
+                        },
+                        'service_carving': 'Auto-selection',
+                        'peering_details': '3.3.3.36[MOD:P:00] 3.3.3.37[MOD:P:00]',
+                        'service_carving_results': {
+                            'forwarders': 1,
+                            'permanent': 0,
+                            'elected': {
+                                'num_of_total': 1,
+                            },
+                            'not_elected': {
+                                'num_of_total': 0,
+                            },
+                        },
+                        'mac_flushing_mode': 'STP-TCN',
+                        'peering_timer': '3 sec [not running]',
+                        'recovery_timer': '30 sec [not running]',
+                        'carving_timer': '0 sec [not running]',
+                        'local_shg_label': 64005,
+                        'remote_shg_label': 1,
+                        'shg_label': {
+                            64005: {
+                                'next_hop': '3.3.3.37',
+                            },
+                            'num_of_label': 1,
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+    golden_output1 = {'execute.return_value': '''
+        show evpn ethernet-segment esi 0036.3700.0000.0000.1100 detail
+
+        Legend:
+
+        B   - No Forwarders EVPN-enabled,
+
+        C   - Backbone Source MAC missing (PBB-EVPN),
+
+        RT  - ES-Import Route Target missing,
+
+        E   - ESI missing,
+
+        H   - Interface handle missing,
+
+        I   - Name (Interface or Virtual Access) missing,
+
+        M   - Interface in Down state,
+
+        O   - BGP End of Download missing,
+
+        P   - Interface already Access Protected,
+
+        Pf  - Interface forced single-homed,
+
+        R   - BGP RID not received,
+
+        S   - Interface in redundancy standby state,
+
+        X   - ESI-extracted MAC Conflict
+
+        SHG - No local split-horizon-group label allocated
+
+        
+
+        Ethernet Segment Id      Interface                          Nexthops
+
+        ------------------------ ---------------------------------- --------------------
+
+        0036.3700.0000.0000.1100 BE100                              3.3.3.36
+
+                                                                    3.3.3.37
+
+        ES to BGP Gates   : Ready
+
+        ES to L2FIB Gates : Ready
+
+        Main port         :
+
+            Interface name : Bundle-Ether100
+
+            Interface MAC  : 008a.9644.d8dd
+
+            IfHandle       : 0x0800001c
+
+            State          : Up
+
+            Redundancy     : Not Defined
+
+        ESI type          : 0
+
+            Value          : 36.3700.0000.0000.1100
+
+        ES Import RT      : 3637.0000.0000 (from ESI)
+
+        Source MAC        : 0000.0000.0000 (N/A)
+
+        Topology          :
+
+            Operational    : MH, All-active
+
+            Configured     : All-active (AApF) (default)
+
+        Service Carving   : Auto-selection
+
+        Peering Details   : 3.3.3.36[MOD:P:00] 3.3.3.37[MOD:P:00]
+
+        Service Carving Results:
+
+            Forwarders     : 1
+
+            Permanent      : 0
+
+            Elected        : 1
+
+            Not Elected    : 0
+
+        MAC Flushing mode : STP-TCN
+
+        Peering timer     : 3 sec [not running]
+
+        Recovery timer    : 30 sec [not running]
+
+        Carving timer     : 0 sec [not running]
+
+        Local SHG label   : 64005
+
+        Remote SHG labels : 1
+
+                    64005 : nexthop 3.3.3.37
+        '''}
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowEvpnEthernetSegmentEsiDetail(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse(esi='0036.3700.0000.0000.1100')
+
+    def test_golden1(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output1)
+        obj = ShowEvpnEthernetSegmentEsiDetail(device=self.device)
+        parsed_output = obj.parse(esi='0036.3700.0000.0000.1100')
+        self.assertEqual(parsed_output,self.golden_parsed_output1)
+
 if __name__ == '__main__':
     unittest.main()
