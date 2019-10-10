a255c7dcc72041cb14e166ec88eb3473db9211f7
Thai Nguyen <thaing@cisco.com>
2019-10-10 14:34:53 -0700

    continue with another command

--- src/genie/libs/parser/iosxe/show_ip_nat.py
+++ src/genie/libs/parser/iosxe/show_ip_nat.py
@@ -180,6 +180,146 @@ class ShowIpNatTranslations(ShowIpNatTranslationsSchema):
         return ret_dict
 
 
+class ShowIpNatStatisticsSchema(MetaParser):
+    """ Schema for command:
+            * show ip nat statistics
+    """
 
+    schema = {
+        'total_active_translations': {
+            'total_translations': int,
+            'static': int,
+            'dynamic': int,
+            'extended': int,
+        },
+        'outside_interfaces': [],
+        'inside_interfaces': [],
+        'hits': int,
+        'misses': int,
+        Optional('cef_translated_pkts'): int,
+        Optional('cef_punted_pkts'): int,
+        Optional('expired_translations'): int,
+        Optional('dynamic_mappings'): {
+            Any(): {  # 'Inside source'
+                'id': int,
+                'access_list': str,
+                'pool': str,
+                'refcount': int,
+                Optional('pool'): {
+                    Any(): {  # mypool serial0/0 FastEthernet1/0
+                    'netmask': str,
+                    'start': str,
+                    'end': str,
+                    'type': str,
+                    'total': str,
+                    'allocated': str,
+                    'missed': str
+                    }
+                }
+            }
+        },
+        Optional('nat_limit_statistics'): {
+            'max_entry': str,
+            'used': int,
+            'missed': int
+        },
+        Optional('pool_stats_drop'): int,
+        Optional('mapping_stats_drop'): int,
+        Optional('port_block_alloc_fail'): int,
+        Optional('ip_alias_add_fail'): int,
+        Optional('limit_entry_add_fail'): int,
+        Optional('queued_packets'): int
+    }
 
+class ShowIpNatStatistics(ShowIpNatStatisticsSchema):
+    """
+        * show ip nat statistics
+    """
+
+    cli_command = ['show ip nat statistics']
+
+    def cli(self, output=None):
+
+        cmd = self.cli_command
+
+        if output is None:
+            out = self.device.execute(cmd)
+        else:
+            out = output
+
+        # Total active translations: 0 (0 static, 0 dynamic 0 extended)
+        p1 = re.compile(r'^Total +active +translations: +(?P<total_translations>\d+) '
+                        '+\((?P<static>\d+) +static\, +(?P<dynamic>\d+) '
+                        '+dynamic\; +(?P<extended>\d+) +extended\)$')
+
+        # Outside interfaces
+        # Inside interfaces
+        p2 = re.compile(r'^(?P<in_out_interfaces>Outside|Inside) +interfaces\:$')
+
+        # TenGigabitEthernet1/0/0, TenGigabitEthernet1/1/0, TenGigabitEthernet1/2/0, TenGigabitEthernet1/3/0
+        # FastEthernet0/0
+        p3 = re.compile(r'^(?P<direction_interfaces>[\w\d\/\d\/\d\,\s]+)$')
+
+        parsed_dict = {}
+        on_the_outside = False
+        on_the_inside = False
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            # Total active translations: 0 (0 static, 0 dynamic 0 extended)
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                total_active_dict = parsed_dict.setdefault(
+                    'total_active_translations', {})
+                total_active_dict['total_translations'] = group['total_translations']
+                total_active_dict['static'] = int(group['static'])
+                total_active_dict['dynamic'] = int(group['static'])
+                total_active_dict['extended'] = int(group['extended'])
+            
+                continue
+            # Outside interfaces
+            # Inside interfaces
+            m = p2.match(line)
+
+            # TenGigabitEthernet1/0/0, TenGigabitEthernet1/1/0, TenGigabitEthernet1/2/0, TenGigabitEthernet1/3/0
+            # FastEthernet0/0
+            m_1 = p3.match(line)
+            if m or m_1:
+                if m:
+                    group = m.groupdict()
+                    if group['in_out_interfaces'] == 'Outside' or on_the_outside:
+                        outside_interface = group['in_out_interfaces']
+                        on_the_outside = True
+                        on_the_inside = False
+                    else:
+                        inside_interface = group['in_out_interfaces']
+                        on_the_inside = True  
+                        on_the_outside = False       
+                else:
+                    group_m1 = m_1.groupdict()
+                    # import pdb;pdb.set_trace()
+                    if on_the_outside:
+                        outside_list = group_m1['direction_interfaces'].split()
+                        for item in len(outside_list):
+                            parsed_dict['outside_interfaces'] += group_m1['direction_interfaces']
+                        import pprint
+                        pprint.pprint (outside_list)
+
+                    elif on_the_inside:
+                        inside_list = group_m1['direction_interfaces'].split()
+                        for item in len(outside_list):
+                            parsed_dict['inside_interfaces'] += group_m1['direction_interfaces']
+                            print(item)
+                        import pprint
+                        pprint.pprint (inside_list)
+
+                continue
+                    
+                
+
+                    
+
+                    
 
--- src/genie/libs/parser/iosxe/tests/test_show_ip_nat.py
+++ src/genie/libs/parser/iosxe/tests/test_show_ip_nat.py
@@ -8,7 +8,8 @@ from ats.topology import Device
 from genie.metaparser.util.exceptions import SchemaEmptyParserError, \
                                              SchemaMissingKeyError
 
-from genie.libs.parser.iosxe.show_ip_nat import ShowIpNatTranslations
+from genie.libs.parser.iosxe.show_ip_nat import ShowIpNatTranslations, \
+                                                ShowIpNatStatistics
 
 
 ####################################################
@@ -250,6 +251,55 @@ class TestShowIpNatTranslations(unittest.TestCase):
         obj = ShowIpNatTranslations(device=self.device)
         parsed_output = obj.parse(option='verbose')
         self.assertEqual(parsed_output, self.golden_parsed_output_3)
+
+class TestShowIpNatStatistics(unittest.TestCase):
+
+    device = Device(name='c3850')
+    dev_empty = Device(name='empty')
+    empty_output = {'execute.return_value': '  '}
+
+    golden_output = {'execute.return_value': '''
+        Device# show ip nat statistics
+
+        Total active translations: 3 (0 static, 3 dynamic; 3 extended)
+        Outside interfaces:
+        TenGigabitEthernet2/0/0, TenGigabitEthernet2/1/0, TenGigabitEthernet2/2/0
+        TenGigabitEthernet2/3/0
+        Inside interfaces: 
+        TenGigabitEthernet1/0/0, TenGigabitEthernet1/1/0, TenGigabitEthernet1/2/0
+        TenGigabitEthernet1/3/0
+        Hits: 59230465  Misses: 3
+        CEF Translated packets: 0, CEF Punted packets: 0
+        Expired translations: 0
+        Dynamic mappings:
+        -- Inside Source
+        [Id: 1] access-list 102 pool mypool refcount 3
+        pool mypool: netmask 255.255.255.0
+                start 10.5.5.1 end 10.5.5.5
+                type generic, total addresses 5, allocated 1 (20%), misses 0
+        nat-limit statistics:
+        max entry: max allowed 2147483647, used 3, missed 0
+        Pool stats drop: 0  Mapping stats drop: 0
+        Port block alloc fail: 0
+        IP alias add fail: 0
+        Limit entry add fail: 0
+    '''
+    }
+
+    def test_empty(self):
+        self.dev_empty = Mock(**self.empty_output)
+        obj = ShowIpNatStatistics(device=self.dev_empty)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output)
+        obj = ShowIpNatStatistics(device=self.device)
+        parsed_output = obj.parse()
+        import pprint
+        pprint.pprint(parsed_output)
+        #self.assertEqual(parsed_output, self.golden_parsed_output)
 
 if __name__ == '__main__':
     unittest.main()
