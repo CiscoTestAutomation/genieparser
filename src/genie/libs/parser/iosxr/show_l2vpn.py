b9f8445daecd9000dd1c113f672ef41359614997
Badal Soyantar <bsoyanta@cisco.com>
2019-10-21 13:48:29 -0700

    Addressed comments

--- src/genie/libs/parser/iosxr/show_l2vpn.py
+++ src/genie/libs/parser/iosxr/show_l2vpn.py
@@ -388,6 +388,7 @@ class ShowL2vpnBridgeDomainSchema(MetaParser):
                         Optional('vfi'): {
                             'num_vfi': int,
                             Any(): {
+                                Optional('state'): str,
                                 'neighbor': {
                                     Any(): {
                                         'pw_id': {
@@ -433,7 +434,6 @@ class ShowL2vpnBridgeDomain(ShowL2vpnBridgeDomainSchema):
         else:
             out = output        
         ret_dict = {}
-        
         # Bridge group: g1, bridge-domain: bd1, id: 0, state: up, ShgId: 0, MSTi: 0
         # Bridge group: EVPN-Multicast, bridge-domain: EVPN-Multicast-BTV, id: 0, state: up, ShgId: 0, MSTi: 0
         p1 = re.compile(r'^Bridge +group: +(?P<bridge_group>\S+), +bridge\-domain: +'
@@ -465,7 +465,7 @@ class ShowL2vpnBridgeDomain(ShowL2vpnBridgeDomainSchema):
 
         # VFI 1
         # VFI vfi60 (up)
-        p6 = re.compile(r'^VFI +(?P<vfi>[\S ]+)$')
+        p6 = re.compile(r'^VFI +(?P<vfi>\S+)( +\((?P<state>\w+)\))?$')
 
         # Neighbor 10.1.1.1 pw-id 1, state: up, Static MAC addresses: 0
         p7 = re.compile(r'Neighbor +(?P<neighbor>\S+) +pw-id +(?P<pw_id>\d+), +state: +'
@@ -598,6 +598,9 @@ class ShowL2vpnBridgeDomain(ShowL2vpnBridgeDomainSchema):
                 vfi = group['vfi']
                 vfi_dict = bridge_domain_dict.setdefault('vfi', {}). \
                     setdefault(vfi, {})
+                state = group['state']
+                if state:
+                    vfi_dict.update({'state': state})
                 continue
 
             # Neighbor 10.1.1.1 pw-id 1, state: up, Static MAC addresses: 0
