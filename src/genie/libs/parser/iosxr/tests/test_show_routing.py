694116c4ee8982dff74d6d304cd610833e178a38
Xiaoxin Zhou <xiaoxinz@cisco.com>
2019-10-23 08:14:10 -0700

    fix show_route_ipv6 xr

--- src/genie/libs/parser/iosxr/tests/test_show_routing.py
+++ src/genie/libs/parser/iosxr/tests/test_show_routing.py
@@ -1379,6 +1379,76 @@ VRF: VRF505
         },
     }
 
+    device_output = {'execute.return_value': '''
+    7.70.219.128, 2d04h
+    B    182.18.240.0/22 [200/0] via 67.70.219.128, 1w3d
+    B    182.18.244.0/22 [200/0] via 67.70.219.128, 1w3d
+    B    182.18.246.0/24 [200/0] via 67.70.219.128, 1w3d
+    '''}
+
+    device_parsed_output = {
+        'vrf': {
+            'default': {
+                'address_family': {
+                    'ipv6': {
+                        'routes': {
+                            '182.18.240.0/22': {
+                                'active': True,
+                                'metric': 0,
+                                'next_hop': {
+                                    'next_hop_list': {
+                                        1: {
+                                            'index': 1,
+                                            'next_hop': '67.70.219.128',
+                                            'updated': '1w3d',
+                                        },
+                                    },
+                                },
+                                'route': '182.18.240.0/22',
+                                'route_preference': 200,
+                                'source_protocol': 'bgp',
+                                'source_protocol_codes': 'B',
+                            },
+                            '182.18.244.0/22': {
+                                'active': True,
+                                'metric': 0,
+                                'next_hop': {
+                                    'next_hop_list': {
+                                        1: {
+                                            'index': 1,
+                                            'next_hop': '67.70.219.128',
+                                            'updated': '1w3d',
+                                        },
+                                    },
+                                },
+                                'route': '182.18.244.0/22',
+                                'route_preference': 200,
+                                'source_protocol': 'bgp',
+                                'source_protocol_codes': 'B',
+                            },
+                            '182.18.246.0/24': {
+                                'active': True,
+                                'metric': 0,
+                                'next_hop': {
+                                    'next_hop_list': {
+                                        1: {
+                                            'index': 1,
+                                            'next_hop': '67.70.219.128',
+                                            'updated': '1w3d',
+                                        },
+                                    },
+                                },
+                                'route': '182.18.246.0/24',
+                                'route_preference': 200,
+                                'source_protocol': 'bgp',
+                                'source_protocol_codes': 'B',
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
     def test_empty_1(self):
         self.device = Mock(**self.empty_output)
         obj = ShowRouteIpv6(device=self.device)
@@ -1398,6 +1468,13 @@ VRF: VRF505
         obj = ShowRouteIpv6(device=self.device)
         parsed_output = obj.parse(vrf='all')
         self.assertEqual(parsed_output, self.golden_parsed_output_2)
+
+    def test_3(self):
+        self.maxDiff = None
+        self.device = Mock(**self.device_output)
+        obj = ShowRouteIpv6(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.device_parsed_output)
 
 
 if __name__ == '__main__':
