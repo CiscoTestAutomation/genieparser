95c841ceb21b2c8dbe777c179e18e8512a86c88c
SANJANA BHUTANI <sanbhuta@cisco.com>
2019-11-19 16:49:36 +0530

    committing changes for cust issue - show vtp status

--- src/genie/libs/parser/ios/tests/test_show_vtp.py
+++ src/genie/libs/parser/ios/tests/test_show_vtp.py
@@ -176,6 +176,62 @@ class test_show_vtp_status(unittest.TestCase):
 
     '''}
 
+    golden_output_4 = {'execute.return_value': '''\
+        VTP Version capable             : 1 to 3
+        VTP version running             : 3
+        VTP Domain Name                 : MIGRA
+        VTP Pruning Mode                : Disabled
+        VTP Traps Generation            : Disabled
+        Device ID                       : 885a.92d1.aac0
+        
+        Feature VLAN:
+        --------------
+        VTP Operating Mode                : Client
+        Number of existing VLANs          : 47
+        Number of existing extended VLANs : 0
+        Maximum VLANs supported locally   : 2048
+        Configuration Revision            : 15
+        Primary ID                        : 501c.bf9d.0c80
+        Primary Description               : sw001
+        MD5 digest                        : 0xD9 0xAA 0x42 0x1D 0xD7 0xD6 0xA7 0x23
+                                            0xE8 0xBE 0xA0 0xB3 0x33 0xB1 0x7A 0x62
+        
+        
+        Feature MST:
+        --------------
+        VTP Operating Mode                : Transparent
+
+    '''}
+
+    golden_parsed_output_4 = {
+        'vtp': {
+            'version_capable': [1, 2, 3],
+            'version': '3',
+            'feature': {
+                'vlan': {
+                    'operating_mode': 'client',
+                    'enabled': True,
+                    'existing_vlans': 47,
+                    'existing_extended_vlans': 0,
+                    'maximum_vlans': 2048,
+                    'configuration_revision': 15,
+                    'primary_id': '501c.bf9d.0c80',
+                    'primary_description': 'sw001',
+                    'md5_digest': '0x1D 0x23 0x33 0x42 0x62 0x7A 0xA0 0xA7 0xAA 0xB1 0xB3 0xBE 0xD6 0xD7 0xD9 0xE8'
+                },
+                'mst': {
+                    'operating_mode': 'transparent',
+                    'enabled': False
+                }
+            },
+                'domain_name': 'MIGRA',
+                'pruning_mode': False,
+                'traps_generation': False,
+                'device_id': '885a.92d1.aac0'
+            }
+        }
+    
+    
     def test_empty(self):
         self.device1 = Mock(**self.empty_output)
         obj = ShowVtpStatus(device=self.device1)
@@ -198,6 +254,12 @@ class test_show_vtp_status(unittest.TestCase):
         self.maxDiff = None
         self.assertEqual(parsed_output,self.golden_parsed_output_3)
 
+    def test_golden_4(self):
+        self.device = Mock(**self.golden_output_4)
+        obj = ShowVtpStatus(device=self.device)
+        parsed_output = obj.parse()
+        self.maxDiff = None
+        self.assertEqual(parsed_output,self.golden_parsed_output_4)
 
 if __name__ == '__main__':
     unittest.main()
