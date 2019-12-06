9a53bd2fe452a341365f84e7fb3e7e043a8908a9
Xiaoxin Zhou <xiaoxinz@cisco.com>
2019-12-06 08:28:35 -0800

    remove comments

--- src/genie/libs/parser/ios/show_platform.py
+++ src/genie/libs/parser/ios/show_platform.py
@@ -193,24 +193,6 @@ class ShowInventory(ShowInventorySchema_iosxe):
                 vid = group.get('vid', '')
                 sn = group['sn']
 
-                # # NAME: "IOSv"
-                # result = r1_6.match(name)
-                # if result:
-                #     slot = '1'
-                #     slot_dict = parsed_output \
-                #         .setdefault('slot', {}) \
-                #         .setdefault(slot, {}) \
-                #         .setdefault('rp', {}) \
-                #         .setdefault(pid, {})
-                #
-                #     slot_dict['name'] = name
-                #     slot_dict['descr'] = descr
-                #     slot_dict['pid'] = pid
-                #     slot_dict['vid'] = vid
-                #     slot_dict['sn'] = sn
-                #
-                #     continue
-
                 # CISCO3945-CHASSIS
                 # Cisco Systems Cisco 6500 4-slot Chassis
                 if r1_0.match(descr):
