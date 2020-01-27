85abe21e85fdf292d7465a2a94bde84a0904ef82
Xiaoxin Zhou <xiaoxinz@cisco.com>
2020-01-27 12:32:12 -0800

    create show isis parser for sros

---
+++ src/genie/libs/parser/sros/show_isis.py
@@ -0,0 +1,91 @@
+""" show_isis.py
+    supports commands:
+        * show router isis adjacency
+        * show router isis adjacency detail
+"""
+
+# Python
+import re
+
+# Metaparser
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Schema, Any, Optional
+
+# parser utils
+from genie.libs.parser.utils.common import Common
+
+# =============================================
+# Parser for 'show router isis adjacency'
+# =============================================
+
+
+class ShowRouterIsisAdjacencySchema(MetaParser):
+    """Schema for show router isis adjacency"""
+    schema = {
+        'system_id': {
+            Any(): {
+                'usage': str,
+                'state': str,
+                'hold': int,
+                'interface': str,
+                'mt_id': int,
+            },
+        },
+        'adjacencies': int,
+    }
+
+
+class ShowRouterIsisAdjacency(ShowRouterIsisAdjacencySchema):
+    """ Parser for show router isis adjacency"""
+
+    cli_command = 'show router isis adjacency'
+
+    def cli(self, output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        parsed_dict = {}
+
+        # System ID                Usage State Hold Interface                     MT-ID
+        # -------------------------------------------------------------------------------
+        # COPQON05R07              L2    Up    24   To-COPQON05R07-LAG-7          0
+        # COTKON04XR1              L2    Up    24   To-COTKON04XR1-LAG-4          0
+        # COTKPQ03R07              L2    Up    24   To-COTKPQ03R07-LAG-9          0
+        # -------------------------------------------------------------------------------
+        p1 = re.compile(r'^(?P<system_id>\S+) +(?P<usage>\S+) +(?P<state>\S+) '
+                        r'+(?P<hold>\d+) +(?P<interface>\S+) +(?P<mt_id>\d+)$')
+
+        # Adjacencies : 3
+        p2 = re.compile(r'^Adjacencies : (?P<adjacencies>\d+)$')
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+
+                system_id_dict = parsed_dict.setdefault('system_id', {}).\
+                                setdefault(group['system_id'], {})
+                for k in ['usage', 'state', 'interface']:
+                    system_id_dict[k] = group[k]
+                for int_k in ['hold', 'mt_id']:
+                    system_id_dict[int_k] = int(group[int_k])
+                continue
+
+            # Adjacencies: 3
+            m = p2.match(line)
+            if m:
+                parsed_dict['adjacencies'] = int(m.groupdict()['adjacencies'])
+                continue
+
+        return parsed_dict
+
+
+# =============================================
+# Parser for 'show router isis adjacency detail'
+# =============================================
+# class ShowRouterIsisAdjacencyDetailSchema(MetaParser):
+    """Schema for show router isis adjacency detail"""
---
+++ src/genie/libs/parser/sros/tests/test_show_isis.py
@@ -0,0 +1,146 @@
+import unittest
+from unittest.mock import Mock
+from pyats.topology import Device
+
+from genie.metaparser.util.exceptions import SchemaEmptyParserError,\
+                                       SchemaMissingKeyError
+from genie.libs.parser.sros.show_isis import ShowRouterIsisAdjacency
+
+
+
+class TestShowRouterIsisAdjacency(unittest.TestCase):
+    dev = Device(name='device')
+    empty_output = {'execute.return_value': ''}
+
+    sample_output = {'execute.return_value': '''
+    A:admin@COTKON04XR2# show router  isis adjacency 
+    ===============================================================================
+    Rtr Base ISIS Instance 0 Adjacency 
+    ===============================================================================
+    System ID                Usage State Hold Interface                     MT-ID
+    -------------------------------------------------------------------------------
+    COPQON05R07              L2    Up    24   To-COPQON05R07-LAG-7          0
+    COTKON04XR1              L2    Up    24   To-COTKON04XR1-LAG-4          0
+    COTKPQ03R07              L2    Up    24   To-COTKPQ03R07-LAG-9          0
+    -------------------------------------------------------------------------------
+    Adjacencies : 3
+    ===============================================================================
+    []
+    '''}
+
+    sample_parsed_output = {
+        'adjacencies': 3,
+        'system_id': {
+            'COPQON05R07': {
+                'hold': 24,
+                'interface': 'To-COPQON05R07-LAG-7',
+                'mt_id': 0,
+                'state': 'Up',
+                'usage': 'L2',
+            },
+            'COTKON04XR1': {
+                'hold': 24,
+                'interface': 'To-COTKON04XR1-LAG-4',
+                'mt_id': 0,
+                'state': 'Up',
+                'usage': 'L2',
+            },
+            'COTKPQ03R07': {
+                'hold': 24,
+                'interface': 'To-COTKPQ03R07-LAG-9',
+                'mt_id': 0,
+                'state': 'Up',
+                'usage': 'L2',
+            },
+        },
+    }
+
+    def test_empty(self):
+        self.dev = Mock(**self.empty_output)
+        obj = ShowRouterIsisAdjacency(device=self.dev)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.dev = Mock(**self.sample_output)
+        obj = ShowRouterIsisAdjacency(device=self.dev)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output,self.sample_parsed_output)
+
+
+class TestShowRouterIsisAdjacencyDetail(unittest.TestCase):
+    dev = Device(name='device')
+    empty_output = {'execute.return_value': ''}
+
+    sample_output = {'execute.return_value': '''
+    A:admin@COTKON04XR2# show router  isis adjacency detail 
+    ===============================================================================
+    Rtr Base ISIS Instance 0 Adjacency (detail)
+    ===============================================================================
+    Hostname    : COPQON05R07
+    SystemID    : 0691.5819.6089                   SNPA        : 00:23:3e:8f:17:97
+    Interface   : To-COPQON05R07-LAG-7             Up Time     : 58d 03:24:48
+    State       : Up                               Priority    : 0
+    Nbr Sys Typ : L2                               L. Circ Typ : L2
+    Hold Time   : 22                               Max Hold    : 30
+    Adj Level   : L2                               MT Enabled  : No
+    Topology    : Unicast
+    IPv6 Neighbor     : ::
+    IPv4 Neighbor     : 10.11.97.22
+    IPv4 Adj SID      : Label 524213
+    Restart Support   : Disabled
+    Restart Status    : Not currently being helped
+    Restart Supressed : Disabled
+    Number of Restarts: 0
+    Last Restart at   : Never
+    Hostname    : COTKON04XR1
+    SystemID    : 0670.7021.9137                   SNPA        : 84:26:2b:bc:2d:e1
+    Interface   : To-COTKON04XR1-LAG-4             Up Time     : 36d 23:21:57
+    State       : Up                               Priority    : 0
+    Nbr Sys Typ : L2                               L. Circ Typ : L2
+    Hold Time   : 23                               Max Hold    : 30
+    Adj Level   : L2                               MT Enabled  : No
+    Topology    : Unicast
+    IPv6 Neighbor     : ::
+    IPv4 Neighbor     : 10.11.79.245
+    IPv4 Adj SID      : Label 524127
+    Restart Support   : Disabled
+    Restart Status    : Not currently being helped
+    Restart Supressed : Disabled
+    Number of Restarts: 0
+    Last Restart at   : Never
+    Hostname    : COTKPQ03R07
+    SystemID    : 0691.5819.6091                   SNPA        : 00:23:3e:8d:2f:99
+    Interface   : To-COTKPQ03R07-LAG-9             Up Time     : 58d 03:24:48
+    State       : Up                               Priority    : 0
+    Nbr Sys Typ : L2                               L. Circ Typ : L2
+    Hold Time   : 22                               Max Hold    : 30
+    Adj Level   : L2                               MT Enabled  : No
+    Topology    : Unicast
+    IPv6 Neighbor     : ::
+    IPv4 Neighbor     : 10.11.79.242
+    IPv4 Adj SID      : Label 524214
+    Restart Support   : Disabled
+    Restart Status    : Not currently being helped
+    Restart Supressed : Disabled
+    Number of Restarts: 0
+    Last Restart at   : Never
+    []
+    '''}
+
+    def test_empty(self):
+        self.dev = Mock(**self.empty_output)
+        obj = ShowRouterIsisAdjacencyDetail(device=self.dev)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.maxDiff = None
+        self.dev = Mock(**self.sample_output)
+        obj = ShowRouterIsisAdjacencyDetail(device=self.dev)
+        parsed_output = obj.parse()
+        # self.assertEqual(parsed_output,self.sample_parsed_output)
+
+
+
