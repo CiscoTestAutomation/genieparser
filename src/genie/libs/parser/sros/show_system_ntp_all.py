12a870647292fbc9efd871231b07bf28de420c0f
Xiaoxin Zhou <xiaoxinz@cisco.com>
2020-01-31 07:33:00 -0800

    finish creating parser show system ntp all

--- src/genie/libs/parser/sros/show_system_ntp_all.py
+++ src/genie/libs/parser/sros/show_system_ntp_all.py
@@ -41,10 +41,10 @@ class ShowSystemNtpAllSchema(MetaParser):
                     Any(): { # == 'client'
                         'state': str,
                         'refid': str,
-                        Optional('stratum'): int, # values: '-' and 4
+                        'stratum': str,
                         'type': str,
                         'poll': int,
-                        'reach': str, # TBD
+                        'reach': str,
                         'offset': float,
                         'a': str,
                         'router': str,
@@ -69,7 +69,123 @@ class ShowSystemNtpAll(ShowSystemNtpAllSchema):
 
         parsed_dict = {}
 
+        # ==============================================================
+        # NTP Status
+        # ==============================================================
+        # Configured         : Yes                Stratum              : 3
+        p0 = re.compile(r'Configured +: +(?P<configured>\S+) '
+                        r'+Stratum +: +(?P<stratum>\d+)')
+        # Admin Status       : up                 Oper Status          : up
+        p1 = re.compile(r'Admin Status +: +(?P<admin_status>\S+) '
+                        r'+Oper Status +: +(?P<oper_status>\S+)')
+        # Server Enabled     : No                 Server Authenticate  : No
+        p2 = re.compile(r'Server Enabled +: +(?P<server_enabled>\S+) '
+                        r'+Server Authenticate +: +(?P<server_authenticate>\S+)')
+        # Clock Source       : 192.168.132.170
+        p3 = re.compile(r'Clock Source +: +(?P<clock_source>\S+)')
+        # Auth Check         : No
+        p4 = re.compile(r'Auth Check +: +(?P<auth_check>\S+)')
+        # Current Date & Time: 2020/01/17 17:24:12 UTC
+        p5 = re.compile(r'Current Date & Time: +(?P<current_date_time>[\s\S]+)')
 
+        # ==============================================================
+        # NTP Active Associations
+        # ==============================================================
+        # reject                    STEP            -  srvr  -  64   ........  0.000
+        p6 = re.compile(r'(?P<state>\S+) +(?P<refid>\S+) '
+                        r'+(?P<stratum>[-\d]) +(?P<type>\S+) '
+                        r'+(?P<a>[-]) +(?P<poll>\d+) +(?P<reach>\S+) '
+                        r'+(?P<offset>[-.\d]+)')
+        # Base           172.16.189.64
+        p7 = re.compile(r'(?P<router>\S+) +(?P<remote>[.\d]+)')
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            # ==============================================================
+            # NTP Status
+            # ==============================================================
+            # Configured         : Yes                Stratum              : 3
+            m = p0.match(line)
+            if m:
+                group = m.groupdict()
+                system_status_dict = parsed_dict.setdefault('clock_state', {}).\
+                                                 setdefault('system_status', {})
+                system_status_dict['configured'] = group['configured']
+                system_status_dict['stratum'] = int(group['stratum'])
+                continue
+
+            # Admin Status       : up                 Oper Status          : up
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                system_status_dict['admin_status'] = group['admin_status']
+                system_status_dict['oper_status'] = group['oper_status']
+                continue
+
+            # Server Enabled     : No                 Server Authenticate  : No
+            m = p2.match(line)
+            if m:
+                group = m.groupdict()
+                system_status_dict['server_enabled'] = group['server_enabled']
+                system_status_dict['server_authenticate'] = group['server_authenticate']
+                continue
+
+            # Clock Source       : 192.168.132.170
+            m = p3.match(line)
+            if m:
+                system_status_dict['clock_source'] = m.groupdict()['clock_source']
+                continue
+
+            # Auth Check         : No
+            m = p4.match(line)
+            if m:
+                system_status_dict['auth_check'] = m.groupdict()['auth_check']
+                continue
+
+            # Current Date & Time: 2020/01/17 17:24:12 UTC
+            m = p5.match(line)
+            if m:
+                k = 'current_date_time'
+                system_status_dict[k] = m.groupdict()[k]
+                continue
+
+            # ==============================================================
+            # NTP Active Associations
+            # ==============================================================
+            # reject                    STEP            -  srvr  -  64   ........  0.000
+            # candidate                 172.16.25.201  4  srvr  -  64   YYYYYYYY  -0.541
+            m = p6.match(line)
+            if m:
+
+                group = m.groupdict()
+                local_mode_dict = {'local_mode': {}}
+                client_dict = local_mode_dict['local_mode'].setdefault('client', {})
+
+                str_keys = ['state', 'refid', 'stratum',
+                            'type', 'a', 'reach']
+                for k in str_keys:
+                    client_dict[k] = group[k]
+
+                client_dict['poll'] = int(group['poll'])
+                client_dict['offset'] = float(group['offset'])
+                continue
+
+            # Base           172.16.189.64
+            m = p7.match(line)
+            if m:
+                group = m.groupdict()
+                remote = group['remote']
+                router = group['router']
+
+                client_dict['router'] = router
+                client_dict['remote'] = remote
+
+                parsed_dict.setdefault('peer', {}).\
+                            setdefault(remote, local_mode_dict)
+                continue
+
+        return parsed_dict
 
 
 
--- src/genie/libs/parser/sros/tests/test_show_system_ntp_all.py
+++ src/genie/libs/parser/sros/tests/test_show_system_ntp_all.py
@@ -31,18 +31,18 @@ class TestShowSystemNtpAll(unittest.TestCase):
         Router         Remote                                            
     -------------------------------------------------------------------------------
     reject                    STEP            -  srvr  -  64   ........  0.000
-        Base           138.120.105.64
+        Base           172.16.189.64
     reject                    STEP            -  srvr  -  64   ........  0.000
-        Base           138.120.105.74
-    candidate                 142.113.80.201  4  srvr  -  64   YYYYYYYY  -0.541
-        Base           142.113.80.140
-    candidate                 142.113.80.201  4  srvr  -  64   YYYYYYYY  -0.335
-        Base           142.113.80.141
+        Base           172.16.189.74
+    candidate                 172.16.25.201  4  srvr  -  64   YYYYYYYY  -0.541
+        Base           172.16.25.140
+    candidate                 172.16.25.201  4  srvr  -  64   YYYYYYYY  -0.335
+        Base           172.16.25.141
     reject                    STEP            -  srvr  -  64   ........  0.000
-        Base           142.122.12.22
+        Base           172.16.186.22
     reject                    STEP            -  srvr  -  64   ........  0.000
         Base           172.24.107.142
-    chosen                    142.117.81.97   2  srvr  -  64   YYYYYYYY  0.259
+    chosen                    172.16.85.97   2  srvr  -  64   YYYYYYYY  0.259
         Base           192.168.132.170
     ===============================================================================
     ===============================================================================
@@ -54,7 +54,135 @@ class TestShowSystemNtpAll(unittest.TestCase):
     ===============================================================================
     '''}
 
-    # sample_parsed_output
+    sample_parsed_output = {
+    'clock_state': {
+        'system_status': {
+            'admin_status': 'up',
+            'auth_check': 'No',
+            'clock_source': '192.168.132.170',
+            'configured': 'Yes',
+            'current_date_time': '2020/01/17 17:24:12 UTC',
+            'oper_status': 'up',
+            'server_authenticate': 'No',
+            'server_enabled': 'No',
+            'stratum': 3,
+        },
+    },
+    'peer': {
+        '172.16.189.64': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': 0.0,
+                    'poll': 64,
+                    'reach': '........',
+                    'refid': 'STEP',
+                    'remote': '172.16.189.64',
+                    'router': 'Base',
+                    'state': 'reject',
+                    'stratum': '-',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '172.16.189.74': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': 0.0,
+                    'poll': 64,
+                    'reach': '........',
+                    'refid': 'STEP',
+                    'remote': '172.16.189.74',
+                    'router': 'Base',
+                    'state': 'reject',
+                    'stratum': '-',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '172.16.25.140': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': -0.541,
+                    'poll': 64,
+                    'reach': 'YYYYYYYY',
+                    'refid': '172.16.25.201',
+                    'remote': '172.16.25.140',
+                    'router': 'Base',
+                    'state': 'candidate',
+                    'stratum': '4',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '172.16.25.141': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': -0.335,
+                    'poll': 64,
+                    'reach': 'YYYYYYYY',
+                    'refid': '172.16.25.201',
+                    'remote': '172.16.25.141',
+                    'router': 'Base',
+                    'state': 'candidate',
+                    'stratum': '4',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '172.16.186.22': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': 0.0,
+                    'poll': 64,
+                    'reach': '........',
+                    'refid': 'STEP',
+                    'remote': '172.16.186.22',
+                    'router': 'Base',
+                    'state': 'reject',
+                    'stratum': '-',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '172.24.107.142': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': 0.0,
+                    'poll': 64,
+                    'reach': '........',
+                    'refid': 'STEP',
+                    'remote': '172.24.107.142',
+                    'router': 'Base',
+                    'state': 'reject',
+                    'stratum': '-',
+                    'type': 'srvr',
+                },
+            },
+        },
+        '192.168.132.170': {
+            'local_mode': {
+                'client': {
+                    'a': '-',
+                    'offset': 0.259,
+                    'poll': 64,
+                    'reach': 'YYYYYYYY',
+                    'refid': '172.16.85.97',
+                    'remote': '192.168.132.170',
+                    'router': 'Base',
+                    'state': 'chosen',
+                    'stratum': '2',
+                    'type': 'srvr',
+                },
+            },
+        },
+    },
+}
 
     def test_empty(self):
         self.dev = Mock(**self.empty_output)
@@ -67,4 +195,9 @@ class TestShowSystemNtpAll(unittest.TestCase):
         self.dev = Mock(**self.sample_output)
         obj = ShowSystemNtpAll(device=self.dev)
         parsed_output = obj.parse()
+        import pprint
+        pprint.pprint(parsed_output)
+        import pdb
+        pdb.set_trace()
+
         self.assertEqual(parsed_output,self.sample_parsed_output)
