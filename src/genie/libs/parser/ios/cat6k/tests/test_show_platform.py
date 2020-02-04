403f0069e8062220c64071b8b34b4fbfab6499a5
Xiaoxin Zhou <xiaoxinz@cisco.com>
2020-02-03 16:57:42 -0800

    move sample_output_10 to ios/cat6k

--- src/genie/libs/parser/ios/cat6k/tests/test_show_platform.py
+++ src/genie/libs/parser/ios/cat6k/tests/test_show_platform.py
@@ -380,17 +380,368 @@ class TestShowInventory(unittest.TestCase):
             },
         },
     }
+
+    golden_output_10 = {'execute.return_value': '''
+        NAME: "WS-C6506-E", DESCR: "Cisco Systems, Inc. Catalyst 6500 6-slot Chassis System"
+        PID: WS-C6506-E        ,                     VID: V05, SN: SAL123456ET
+
+        NAME: "WS-C6K-VTT-E 1", DESCR: "VTT-E FRU 1"
+        PID: WS-C6K-VTT-E      ,                     VID:    , SN: SMT6789A123
+
+        NAME: "WS-C6K-VTT-E 2", DESCR: "VTT-E FRU 2"
+        PID: WS-C6K-VTT-E      ,                     VID:    , SN: SMT6789A876
+
+        NAME: "WS-C6K-VTT-E 3", DESCR: "VTT-E FRU 3"
+        PID: WS-C6K-VTT-E      ,                     VID:    , SN: SMT6789A456
+
+        NAME: "CLK-7600 1", DESCR: "OSR-7600 Clock FRU 1"
+        PID: CLK-7600          ,                     VID:    , SN: SMT8760A198
+
+        NAME: "CLK-7600 2", DESCR: "OSR-7600 Clock FRU 2"
+        PID: CLK-7600          ,                     VID:    , SN: SMT8760A198
+
+        NAME: "1", DESCR: "C6800-16P10G-XL DCEF2T 4 port 40GE / 16 port 10GE Rev. 2.1"
+        PID: C6800-16P10G-XL   ,                     VID: V03, SN: JAE88776PMK
+
+        NAME: "C6800-DFC-XL Distributed Forwarding Card 4 EARL 1 sub-module of 1", DESCR: "C6800-DFC-XL Distributed Forwarding Card 4 Rev. 2.1"
+        PID: C6800-DFC-XL      ,                     VID:    , SN: JAE88776PMK
+
+        NAME: "Transceiver Te1/1", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/1L
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD656567YT     
+
+        NAME: "Transceiver Te1/3", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/3"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD989078Z2     
+
+        NAME: "Transceiver Te1/5", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/5"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345MB     
+
+        NAME: "Transceiver Te1/7", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/7"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345LL     
+
+        NAME: "Transceiver Te1/9", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/9"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345LF     
+
+        NAME: "Transceiver Te1/11", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/11"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345L4     
+
+        NAME: "Transceiver Te1/13", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/13"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345MN     
+
+        NAME: "Transceiver Te1/15", DESCR: "SFP+ Transceiver 10Gbase-SR Te1/15"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345LE     
+
+        NAME: "2", DESCR: "C6800-16P10G-XL DCEF2T 4 port 40GE / 16 port 10GE Rev. 2.1"
+        PID: C6800-16P10G-XL   ,                     VID: V03, SN: JAE65432PMG
+
+        NAME: "C6800-DFC-XL Distributed Forwarding Card 4 EARL 1 sub-module of 2", DESCR: "C6800-DFC-XL Distributed Forwarding Card 4 Rev. 2.1"
+        PID: C6800-DFC-XL      ,                     VID:    , SN: JAE65432PM
+
+        NAME: "Transceiver Te2/1", DESCR: "SFP+ Transceiver 10Gbase-SR Te2/1"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345RB     
+
+        NAME: "Transceiver Te2/3", DESCR: "SFP+ Transceiver 10Gbase-SR Te2/3"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD012345RF     
+
+        NAME: "Transceiver Te2/5", DESCR: "SFP+ Transceiver 10Gbase-SR Te2/5"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD0908070T     
+
+        NAME: "Transceiver Te2/7", DESCR: "SFP+ Transceiver 10Gbase-SR Te2/7"
+        PID: SFP-10G-SR         ,                     VID: V03 , SN: AVD097896CU     
+
+        NAME: "3", DESCR: "C6800-48P-TX-XL 48 ports DCEF-XL 48P 10/100/1000MB Ethernet Rev. 1.0"
+        PID: C6800-48P-TX-XL   ,                     VID: V01, SN: JAE264857DC
+
+        NAME: "WS-F6K-DFC4-AXL Distributed Forwarding Card 4 EARL 1 sub-module of 3", DESCR: "WS-F6K-DFC4-AXL Distributed Forwarding Card 4 Rev. 1.0"
+        PID: WS-F6K-DFC4-AXL   ,                     VID:    , SN: JAE264857DO
+
+        NAME: "5", DESCR: "VS-SUP2T-10G 5 ports Supervisor Engine 2T 10GE w/ CTS Rev. 2.1"
+        PID: VS-SUP2T-10G      ,                     VID: V06, SN: JAE264233CQ
+
+        NAME: "msfc sub-module of 5", DESCR: "VS-F6K-MSFC5 CPU Daughterboard Rev. 3.1"
+        PID: VS-F6K-MSFC5      ,                     VID:    , SN: JAE66880PAV
+
+        NAME: "VS-F6K-PFC4 Policy Feature Card 4 EARL 1 sub-module of 5", DESCR: "VS-F6K-PFC4 Policy Feature Card 4 Rev. 3.0"
+        PID: VS-F6K-PFC4       ,                     VID: V04, SN: JAE66880GWE
+
+        NAME: "Transceiver Te5/4", DESCR: "X2 Transceiver 10Gbase-SR Te5/4"
+        PID: X2-10GB-SR        ,                     VID: V06 , SN: FNS00002Q8Q
+
+        NAME: "6", DESCR: "VS-SUP2T-10G 5 ports Supervisor Engine 2T 10GE w/ CTS Rev. 2.1"
+        PID: VS-SUP2T-10G      ,                     VID: V06, SN: JAE77720W0N
+
+        NAME: "msfc sub-module of 6", DESCR: "VS-F6K-MSFC5 CPU Daughterboard Rev. 3.1"
+        PID: VS-F6K-MSFC5      ,                     VID:    , SN: JAE66880PAO
+
+        NAME: "VS-F6K-PFC4 Policy Feature Card 4 EARL 1 sub-module of 6", DESCR: "VS-F6K-PFC4 Policy Feature Card 4 Rev. 3.0"
+        PID: VS-F6K-PFC4       ,                     VID: V04, SN: JAE66880GWE
+
+        NAME: "Transceiver Te6/4", DESCR: "X2 Transceiver 10Gbase-SR Te6/4"
+        PID: X2-10GB-SR        ,                     VID: V06 , SN: FNS00002Q9B
+
+        NAME: "WS-C6506-E-FAN 1", DESCR: "Enhanced 6-slot Fan Tray 1"
+        PID: WS-C6506-E-FAN    ,                     VID: V03, SN: DCH200022UZ
+
+        NAME: "PS 1 WS-CAC-6000W", DESCR: "AC power supply, 6000 watt 1"
+        PID: WS-CAC-6000W      ,                     VID: V06, SN: ART1999F5B5
+
+        NAME: "PS 2 WS-CAC-6000W", DESCR: "AC power supply, 6000 watt 2"
+        PID: WS-CAC-6000W      ,                     VID: V06, SN: ART1999F5B8
+
+
+        '''}
+
+    golden_parsed_output_10 = {
+    'index': {
+        1: {
+            'descr': 'Cisco Systems, Inc. Catalyst 6500 6-slot Chassis System',
+            'name': 'WS-C6506-E',
+            'pid': 'WS-C6506-E',
+            'sn': 'SAL123456ET',
+            'vid': 'V05',
+        },
+        2: {
+            'descr': 'VTT-E FRU 1',
+            'name': 'WS-C6K-VTT-E 1',
+            'pid': 'WS-C6K-VTT-E',
+            'sn': 'SMT6789A123',
+        },
+        3: {
+            'descr': 'VTT-E FRU 2',
+            'name': 'WS-C6K-VTT-E 2',
+            'pid': 'WS-C6K-VTT-E',
+            'sn': 'SMT6789A876',
+        },
+        4: {
+            'descr': 'VTT-E FRU 3',
+            'name': 'WS-C6K-VTT-E 3',
+            'pid': 'WS-C6K-VTT-E',
+            'sn': 'SMT6789A456',
+        },
+        5: {
+            'descr': 'OSR-7600 Clock FRU 1',
+            'name': 'CLK-7600 1',
+            'pid': 'CLK-7600',
+            'sn': 'SMT8760A198',
+        },
+        6: {
+            'descr': 'OSR-7600 Clock FRU 2',
+            'name': 'CLK-7600 2',
+            'pid': 'CLK-7600',
+            'sn': 'SMT8760A198',
+        },
+        7: {
+            'descr': 'C6800-16P10G-XL DCEF2T 4 port 40GE / 16 port 10GE Rev. 2.1',
+            'name': '1',
+            'pid': 'C6800-16P10G-XL',
+            'sn': 'JAE88776PMK',
+            'vid': 'V03',
+        },
+        8: {
+            'descr': 'C6800-DFC-XL Distributed Forwarding Card 4 Rev. 2.1',
+            'name': 'C6800-DFC-XL Distributed Forwarding Card 4 EARL 1 sub-module of 1',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD656567YT',
+            'vid': 'V03',
+        },
+        9: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/3',
+            'name': 'Transceiver Te1/3',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD989078Z2',
+            'vid': 'V03',
+        },
+        10: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/5',
+            'name': 'Transceiver Te1/5',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345MB',
+            'vid': 'V03',
+        },
+        11: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/7',
+            'name': 'Transceiver Te1/7',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345LL',
+            'vid': 'V03',
+        },
+        12: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/9',
+            'name': 'Transceiver Te1/9',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345LF',
+            'vid': 'V03',
+        },
+        13: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/11',
+            'name': 'Transceiver Te1/11',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345L4',
+            'vid': 'V03',
+        },
+        14: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/13',
+            'name': 'Transceiver Te1/13',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345MN',
+            'vid': 'V03',
+        },
+        15: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te1/15',
+            'name': 'Transceiver Te1/15',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345LE',
+            'vid': 'V03',
+        },
+        16: {
+            'descr': 'C6800-16P10G-XL DCEF2T 4 port 40GE / 16 port 10GE Rev. 2.1',
+            'name': '2',
+            'pid': 'C6800-16P10G-XL',
+            'sn': 'JAE65432PMG',
+            'vid': 'V03',
+        },
+        17: {
+            'descr': 'C6800-DFC-XL Distributed Forwarding Card 4 Rev. 2.1',
+            'name': 'C6800-DFC-XL Distributed Forwarding Card 4 EARL 1 sub-module of 2',
+            'pid': 'C6800-DFC-XL',
+            'sn': 'JAE65432PM',
+        },
+        18: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te2/1',
+            'name': 'Transceiver Te2/1',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345RB',
+            'vid': 'V03',
+        },
+        19: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te2/3',
+            'name': 'Transceiver Te2/3',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD012345RF',
+            'vid': 'V03',
+        },
+        20: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te2/5',
+            'name': 'Transceiver Te2/5',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD0908070T',
+            'vid': 'V03',
+        },
+        21: {
+            'descr': 'SFP+ Transceiver 10Gbase-SR Te2/7',
+            'name': 'Transceiver Te2/7',
+            'pid': 'SFP-10G-SR',
+            'sn': 'AVD097896CU',
+            'vid': 'V03',
+        },
+        22: {
+            'descr': 'C6800-48P-TX-XL 48 ports DCEF-XL 48P 10/100/1000MB Ethernet Rev. 1.0',
+            'name': '3',
+            'pid': 'C6800-48P-TX-XL',
+            'sn': 'JAE264857DC',
+            'vid': 'V01',
+        },
+        23: {
+            'descr': 'WS-F6K-DFC4-AXL Distributed Forwarding Card 4 Rev. 1.0',
+            'name': 'WS-F6K-DFC4-AXL Distributed Forwarding Card 4 EARL 1 sub-module of 3',
+            'pid': 'WS-F6K-DFC4-AXL',
+            'sn': 'JAE264857DO',
+        },
+        24: {
+            'descr': 'VS-SUP2T-10G 5 ports Supervisor Engine 2T 10GE w/ CTS Rev. 2.1',
+            'name': '5',
+            'pid': 'VS-SUP2T-10G',
+            'sn': 'JAE264233CQ',
+            'vid': 'V06',
+        },
+        25: {
+            'descr': 'VS-F6K-MSFC5 CPU Daughterboard Rev. 3.1',
+            'name': 'msfc sub-module of 5',
+            'pid': 'VS-F6K-MSFC5',
+            'sn': 'JAE66880PAV',
+        },
+        26: {
+            'descr': 'VS-F6K-PFC4 Policy Feature Card 4 Rev. 3.0',
+            'name': 'VS-F6K-PFC4 Policy Feature Card 4 EARL 1 sub-module of 5',
+            'pid': 'VS-F6K-PFC4',
+            'sn': 'JAE66880GWE',
+            'vid': 'V04',
+        },
+        27: {
+            'descr': 'X2 Transceiver 10Gbase-SR Te5/4',
+            'name': 'Transceiver Te5/4',
+            'pid': 'X2-10GB-SR',
+            'sn': 'FNS00002Q8Q',
+            'vid': 'V06',
+        },
+        28: {
+            'descr': 'VS-SUP2T-10G 5 ports Supervisor Engine 2T 10GE w/ CTS Rev. 2.1',
+            'name': '6',
+            'pid': 'VS-SUP2T-10G',
+            'sn': 'JAE77720W0N',
+            'vid': 'V06',
+        },
+        29: {
+            'descr': 'VS-F6K-MSFC5 CPU Daughterboard Rev. 3.1',
+            'name': 'msfc sub-module of 6',
+            'pid': 'VS-F6K-MSFC5',
+            'sn': 'JAE66880PAO',
+        },
+        30: {
+            'descr': 'VS-F6K-PFC4 Policy Feature Card 4 Rev. 3.0',
+            'name': 'VS-F6K-PFC4 Policy Feature Card 4 EARL 1 sub-module of 6',
+            'pid': 'VS-F6K-PFC4',
+            'sn': 'JAE66880GWE',
+            'vid': 'V04',
+        },
+        31: {
+            'descr': 'X2 Transceiver 10Gbase-SR Te6/4',
+            'name': 'Transceiver Te6/4',
+            'pid': 'X2-10GB-SR',
+            'sn': 'FNS00002Q9B',
+            'vid': 'V06',
+        },
+        32: {
+            'descr': 'Enhanced 6-slot Fan Tray 1',
+            'name': 'WS-C6506-E-FAN 1',
+            'pid': 'WS-C6506-E-FAN',
+            'sn': 'DCH200022UZ',
+            'vid': 'V03',
+        },
+        33: {
+            'descr': 'AC power supply, 6000 watt 1',
+            'name': 'PS 1 WS-CAC-6000W',
+            'pid': 'WS-CAC-6000W',
+            'sn': 'ART1999F5B5',
+            'vid': 'V06',
+        },
+        34: {
+            'descr': 'AC power supply, 6000 watt 2',
+            'name': 'PS 2 WS-CAC-6000W',
+            'pid': 'WS-CAC-6000W',
+            'sn': 'ART1999F5B8',
+            'vid': 'V06',
+        },
+    },
+}
+
     def test_empty(self):
         self.device = Mock(**self.empty_output)
         obj = ShowInventory(device=self.device)
         with self.assertRaises(SchemaEmptyParserError):
             parsed_output = obj.parse()
 
-    def test_show_redundancy_1(self):
+    def test_1(self):
         self.device = Mock(**self.golden_output)
         obj = ShowInventory(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
+
+    def test_golden_output_10(self):
+        self.maxDiff = None
+        self.device = Mock(**self.golden_output_10)
+        obj = ShowInventory(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output_10)
 
 class TestShowModule(unittest.TestCase):
     device = Device(name="aDevice")
