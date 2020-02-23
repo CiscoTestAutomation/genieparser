4687255cb04342e308b80c9860a3f4bba71a9649
Badal Soyantar <bsoyanta@cisco.com>
2020-02-22 20:03:00 -0800

    TC 1-7 issue

--- src/genie/libs/parser/iosxe/show_mpls.py
+++ src/genie/libs/parser/iosxe/show_mpls.py
@@ -1686,7 +1686,7 @@ class ShowMplsForwardingTable(ShowMplsForwardingTableSchema):
         #            No Label   10.23.120.0/24[V]   \
         # None       No Label   10.0.0.16/30     0             Gi3        10.0.0.9
         # 39    [M]  16052      10.169.14.241/32   \
-        p1 = re.compile(r'^((?P<local_label>\d+|[Nn]one) +)?(?:\[(?P<t>(?:T|M)+)\] +)?(?P<outgoing_label>[\w\s]+) +(?P<prefix_or_tunnel_id>[\S]+) +\\$')
+        p1 = re.compile(r'((?P<local_label>\d+|[Nn]one) +)?(?:\[(?P<t>(?:T|M)+)\] +)?(?P<outgoing_label>(\w+|No +Label)) +(?P<prefix_or_tunnel_id>[\S]+) +\\$')
 
         p2 = re.compile(r'^(?P<bytes_label_switched>\d+)( +(?P<interface>\S+))?( +(?P<next_hop>[\w\.]+))?$')
 
