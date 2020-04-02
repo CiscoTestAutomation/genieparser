72117a4339c4e8cc0960091d49b129e12e9ba080
Petter Fors <karl.petter.andersson@gmail.com>
2020-04-02 16:32:54 +0200

    LINUX parser route

---
+++ src/genie/libs/parser/linux/route.py
@@ -0,0 +1,84 @@
+"""route.py
+
+Linux parsers for the following commands:
+    * route
+"""
+
+# python
+import re
+
+# metaparser
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Schema, Any, Optional
+
+# =======================================================
+# Schema for 'route'
+# =======================================================
+class RouteSchema(MetaParser):
+    """Schema for route"""
+
+    # Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+    # 0.0.0.0         192.168.1.1     0.0.0.0         UG        0 0          0 wlo1
+
+    schema = {
+        Any(): {
+            'destination': str,
+            'gateway': str,
+            'mask': str,
+            'flags': str,
+            'metric': int,
+            'ref': int,
+            'use': int,
+            'interface': str
+        }
+    }
+
+# =======================================================
+# Parser for 'route'
+# =======================================================
+class Route(RouteSchema):
+    """Parser for route"""
+
+    cli_command = ['route', 'route -4 -n', 'route -4n', 'route -n4' ]
+
+    def cli(self, output=None):
+        if output is None:    
+            cmd = self.cli_command[0]
+            out = self.device.execute(cmd)
+        else:
+            out = output
+
+        result_dict = {}
+
+        # Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+        # 192.168.1.0     0.0.0.0         255.255.255.0   U     600    0        0 wlo1
+
+        p1 = re.compile(r'(?P<destination>[a-z0-9\.\:]+)'
+                        ' +(?P<gateway>[a-z0-9\.\:_]+)'
+                        ' +(?P<mask>[a-z0-9\.\:]+)'
+                        ' +(?P<flags>[a-zA-Z]+)'
+                        ' +(?P<metric>(\d+))'
+                        ' +(?P<ref>(\d+))'
+                        ' +(?P<use>(\d+))'
+                        ' +(?P<interface>[a-zA-Z0-9]+)'
+                        )
+        
+        # Initializes the Python dictionary variable
+        parsed_dict = {}
+
+        # Defines the "for" loop, to pattern match each line of output
+
+        for line in out.splitlines():
+            line = line.strip()
+
+
+            # 192.168.1.0     0.0.0.0         255.255.255.0   U     600    0        0 wlo1
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                destination = group['destination']
+                parsed_dict.setdefault(destination, {})
+                parsed_dict[destination].update({k: (int(v) if v.isdigit() else v) for k, v in group.items()})
+                continue
+
+        return parsed_dict
---
+++ src/genie/libs/parser/linux/tests/test_route.py
@@ -0,0 +1,90 @@
+import re
+import unittest
+from unittest.mock import Mock
+
+from pyats.topology import Device
+
+from genie.metaparser.util.exceptions import (SchemaMissingKeyError,
+                                              SchemaEmptyParserError)
+
+from genie.libs.parser.linux.route import Route
+
+
+#############################################################################
+# unitest For route
+#############################################################################
+
+class test_route(unittest.TestCase):
+    device = Device(name='aDevice')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_parsed_output = {
+        '172.17.0.0': {
+            'destination': '172.17.0.0',
+            'flags': 'U',
+            'gateway': '0.0.0.0',
+            'interface': 'docker0',
+            'mask': '255.255.0.0',
+            'metric': 0,
+            'ref': 0,
+            'use': 0
+            },
+        '192.168.1.0': {
+            'destination': '192.168.1.0',
+            'flags': 'U',
+            'gateway': '0.0.0.0',
+            'interface': 'wlo1',
+            'mask': '255.255.255.0',
+            'metric': 600,
+            'ref': 0,
+            'use': 0
+            },
+        '192.168.122.0': {
+            'destination': '192.168.122.0',
+            'flags': 'U',
+            'gateway': '0.0.0.0',
+            'interface': 'virbr0',
+            'mask': '255.255.255.0',
+            'metric': 0,
+            'ref': 0,
+            'use': 0
+            },
+        'default': {
+            'destination': 'default',
+            'flags': 'UG',
+            'gateway': '_gateway',
+            'interface': 'wlo1',
+            'mask': '0.0.0.0',
+            'metric': 600,
+            'ref': 0,
+            'use': 0
+            }
+        }
+
+    golden_output = {'execute.return_value': '''
+        Kernel IP routing table
+        Destination     Gateway         Genmask         Flags Metric Ref    Use Iface
+        default         _gateway        0.0.0.0         UG    600    0        0 wlo1
+        172.17.0.0      0.0.0.0         255.255.0.0     U     0      0        0 docker0
+        192.168.1.0     0.0.0.0         255.255.255.0   U     600    0        0 wlo1
+        192.168.122.0   0.0.0.0         255.255.255.0   U     0      0        0 virbr0
+    '''}
+
+
+    def test_empty(self):
+        self.device1 = Mock(**self.empty_output)
+        obj = Route(device=self.device1)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = Route(device=self.device)
+        parsed_output = obj.parse()
+        self.maxDiff = None
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
