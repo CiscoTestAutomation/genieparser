4cd7ca6bb818d3d5f4ec53625c1ee9b894a4acfb
Andrew Delph <adelph@cisco.com>
2020-04-02 11:45:36 -0400

    create parser for show_ospf3_interface

---
+++ src/genie/libs/parser/junos/show_ospf3_interface.py
@@ -0,0 +1,81 @@
+# Python
+import re
+
+# Metaparser
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import (Any,
+        Optional, Use, SchemaTypeError, Schema)
+
+class ShowOspf3Schema(MetaParser):
+
+    # Sub Schema
+    def validate_ospf_interface_list(value):
+        # Pass ospf3-interface list as value
+        if not isinstance(value, list):
+            raise SchemaTypeError('ospf-interface is not a list')
+        entry_schema = Schema({
+                "bdr-id": str,
+                "dr-id": str,
+                "interface-name": str,
+                "neighbor-count": str,
+                "ospf-area": str,
+                "ospf-interface-state": str
+            })
+        # Validate each dictionary in list
+        for item in value:
+            entry_schema.validate(item)
+        return value
+
+    # Main Schema
+    schema = {
+        "ospf3-interface-information": {
+            "ospf3-interface": Use(validate_ospf_interface_list)
+        }
+    }
+
+class ShowOspf3(ShowOspf3Schema):
+    """ Parser for:
+    * show ospf interface
+    """
+    cli_command = 'show ospf interface'
+
+    def cli(self, output=None):
+        if not output:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        # use the pattern [0-9]{1,3}(\.[0-9]{1,3}){3} to pick up on ip addresses
+        # in area, dr_id and bdr_id
+        p1 = re.compile(r'^(?P<interface>\S+) +(?P<state>\S+) +(?P<area>[0-9]{1,3}(\.[0-9]{1,3}){3})'
+            r' +(?P<dr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<bdr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<nbrs>\S+)$')
+
+        entry_list = []
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+
+                interface_entry = {}
+                interface_entry['interface-name'] = group['interface']
+                interface_entry['ospf-interface-state'] = group['state']
+                interface_entry['ospf-area'] = group['area']
+                interface_entry['dr-id'] = group['dr_id']
+                interface_entry['bdr-id'] = group['bdr_id']
+                interface_entry['neighbor-count'] = group['nbrs']
+
+                entry_list.append(interface_entry)
+
+        if entry_list:
+            data = {
+                "ospf3-interface-information": {
+                    "ospf3-interface": entry_list
+                }
+            }
+            return data
+        else:
+            return None
+
---
+++ src/genie/libs/parser/junos/tests/test_show_ospf3_interface.py
@@ -0,0 +1,75 @@
+# Python
+import unittest
+from unittest.mock import Mock
+
+# ATS
+from pyats.topology import Device
+from pyats.topology import loader
+
+# Metaparser
+from genie.metaparser.util.exceptions import SchemaEmptyParserError
+
+# junos show_arp
+from genie.libs.parser.junos.show_ospf3_interface import (ShowOspf3)
+
+class TestShowOspf3(unittest.TestCase):
+
+    device = Device(name='aDevice')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+        show ospf3 interface | no-more
+        Interface           State   Area            DR ID           BDR ID          Nbrs
+        ge-0/0/0.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
+        ge-0/0/1.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
+        lo0.0               DR      0.0.0.8         111.87.5.252    0.0.0.0            0
+    '''}
+
+    golden_parsed_output = {
+            "ospf3-interface-information": {
+                "ospf3-interface": [
+                    {
+                        "bdr-id": "0.0.0.0",
+                        "dr-id": "0.0.0.0",
+                        "interface-name": "ge-0/0/0.0",
+                        "neighbor-count": "1",
+                        "ospf-area": "0.0.0.8",
+                        "ospf-interface-state": "PtToPt"
+                    },
+                    {
+                        "bdr-id": "0.0.0.0",
+                        "dr-id": "0.0.0.0",
+                        "interface-name": "ge-0/0/1.0",
+                        "neighbor-count": "1",
+                        "ospf-area": "0.0.0.8",
+                        "ospf-interface-state": "PtToPt"
+                    },
+                    {
+                        "bdr-id": "0.0.0.0",
+                        "dr-id": "111.87.5.252",
+                        "interface-name": "lo0.0",
+                        "neighbor-count": "0",
+                        "ospf-area": "0.0.0.8",
+                        "ospf-interface-state": "DR"
+                    }
+                ]
+            }
+        }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowOspf3(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            obj.parse()
+
+    def test_golden(self):
+        import pprint
+        self.device = Mock(**self.golden_output)
+        obj = ShowOspf3(device=self.device)
+        parsed_output = obj.parse()
+        self.maxDiff = None
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+if __name__ == '__main__':
+    unittest.main()
