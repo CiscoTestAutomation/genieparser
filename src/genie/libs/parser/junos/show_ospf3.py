eb5631c0938c2b583a216326a2170892b67e5f80
Andrew Delph <adelph@cisco.com>
2020-04-03 17:07:02 -0400

    update line length

--- src/genie/libs/parser/junos/show_ospf3.py
+++ src/genie/libs/parser/junos/show_ospf3.py
@@ -65,8 +65,9 @@ class ShowOspf3Interface(ShowOspf3InterfaceSchema):
             out = output
 
         # ge-0/0/0.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
-        p1 = re.compile(r'^(?P<interface_name>\S+) +(?P<ospf_interface_state>\S+) +(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3})'
-            r' +(?P<dr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<bdr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<neighbor_count>\S+)$')
+        p1 = re.compile(r'^(?P<interface_name>\S+) +(?P<ospf_interface_state>\S+)'
+            r' +(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<dr_id>[0-9]{1,3}'
+            r'(\.[0-9]{1,3}){3}) +(?P<bdr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<neighbor_count>\S+)$')
 
         ret_dict = {}
 
@@ -77,7 +78,8 @@ class ShowOspf3Interface(ShowOspf3InterfaceSchema):
             m = p1.match(line)
             if m:
 
-                entry_list = ret_dict.setdefault("ospf3-interface-information", {}).setdefault("ospf3-interface", [])
+                entry_list = ret_dict.setdefault("ospf3-interface-information", {})\
+                    .setdefault("ospf3-interface", [])
 
                 group = m.groupdict()
                 entry = {}
@@ -174,14 +176,18 @@ class ShowOspf3Database(ShowOspf3DatabaseSchema):
             out = output
 
         #    OSPF3 database, Area 0.0.0.8
-        p1 = re.compile(r'^( *)OSPF3( +)database,( +)Area( +)(?P<ospf_area>(\*{0,1})[0-9]{1,3}(\.[0-9]{1,3}){3})$')
+        p1 = re.compile(r'^( *)OSPF3( +)database,( +)Area( +)'
+            r'(?P<ospf_area>(\*{0,1})[0-9]{1,3}(\.[0-9]{1,3}){3})$')
 
         # Type       ID               Adv Rtr           Seq         Age  Cksum  Len
         # Router      0.0.0.0          10.34.2.250     0x800018ed  2407  0xaf2d  56
-        p2 = re.compile(r'^( *)(?P<lsa_type>\S+) +(\*{0,1})(?P<lsa_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<advertising_router>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<sequence_number>\S+) +(?P<age>\d+) +(?P<checksum>\S+) +(?P<lsa_length>\d+)$')
+        p2 = re.compile(r'^( *)(?P<lsa_type>\S+) +(\*{0,1})(?P<lsa_id>[0-9]{1,3}'
+            r'(\.[0-9]{1,3}){3}) +(?P<advertising_router>[0-9]{1,3}(\.[0-9]{1,3})'
+            r'{3}) +(?P<sequence_number>\S+) +(?P<age>\d+) +(?P<checksum>\S+) +(?P<lsa_length>\d+)$')
 
         # OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
-        p3 = re.compile(r'^( *)OSPF3( +)Link-Local( +)database,( +)interface( +)(?P<ospf_intf>\S+)( +)Area( +)(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3})$')
+        p3 = re.compile(r'^( *)OSPF3( +)Link-Local( +)database,( +)interface( +)'
+            r'(?P<ospf_intf>\S+)( +)Area( +)(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3})$')
 
         ret_dict = {}
 
@@ -191,19 +197,22 @@ class ShowOspf3Database(ShowOspf3DatabaseSchema):
             # ge-0/0/0.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
             m = p1.match(line)
             if m:
-                ospf_area = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-area-header", {}).setdefault("ospf-area", None)
+                ospf_area = ret_dict.setdefault("ospf3-database-information", {})\
+                    .setdefault("ospf3-area-header", {}).setdefault("ospf-area", None)
                 if ospf_area:
                     raise Exception("ospf-area already exists"+str(ospf_area))
 
                 group = m.groupdict()
 
-                ret_dict["ospf3-database-information"]["ospf3-area-header"]["ospf-area"] = group["ospf_area"]
+                ret_dict["ospf3-database-information"]["ospf3-area-header"]["ospf-area"]\
+                     = group["ospf_area"]
                 continue
 
             # Router      0.0.0.0          10.34.2.250     0x800018ed  2407  0xaf2d  56
             m = p2.match(line)
             if m:
-                entry_list = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-database", [])
+                entry_list = ret_dict.setdefault("ospf3-database-information", {})\
+                    .setdefault("ospf3-database", [])
 
                 group = m.groupdict()
                 entry = {}
@@ -217,7 +226,8 @@ class ShowOspf3Database(ShowOspf3DatabaseSchema):
             # OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
             m = p3.match(line)
             if m:
-                entry_list = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-intf-header", [])
+                entry_list = ret_dict.setdefault("ospf3-database-information", {})\
+                    .setdefault("ospf3-intf-header", [])
 
                 group = m.groupdict()
                 entry = {}
