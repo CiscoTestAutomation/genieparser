6305b389170c180a9105bcc591d9aee0c677c99f
Andrew Delph <adelph@cisco.com>
2020-04-03 15:50:06 -0400

    parse and test show ospf3 database

--- src/genie/libs/parser/junos/show_ospf3.py
+++ src/genie/libs/parser/junos/show_ospf3.py
@@ -57,12 +57,9 @@ class ShowOspf3Interface(ShowOspf3InterfaceSchema):
         else:
             out = output
 
-        # use the pattern r'[0-9]{1,3}(\.[0-9]{1,3}){3}' to pick up on ip addresses
-        # in 'area', 'dr_id' and 'bdr_id'
-
         # ge-0/0/0.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
-        p1 = re.compile(r'^(?P<interface>\S+) +(?P<state>\S+) +(?P<area>[0-9]{1,3}(\.[0-9]{1,3}){3})'
-            r' +(?P<dr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<bdr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<nbrs>\S+)$')
+        p1 = re.compile(r'^(?P<interface_name>\S+) +(?P<ospf_interface_state>\S+) +(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3})'
+            r' +(?P<dr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<bdr_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<neighbor_count>\S+)$')
 
         ret_dict = {}
 
@@ -76,20 +73,16 @@ class ShowOspf3Interface(ShowOspf3InterfaceSchema):
                 entry_list = ret_dict.setdefault("ospf3-interface-information", {}).setdefault("ospf3-interface", [])
 
                 group = m.groupdict()
+                entry = {}
+                for group_key, group_value in group.items():
+                    entry_key = group_key.replace('_','-')
+                    entry[entry_key] = group_value
 
-                interface_entry = {}
-                interface_entry['interface-name'] = group['interface']
-                interface_entry['ospf-interface-state'] = group['state']
-                interface_entry['ospf-area'] = group['area']
-                interface_entry['dr-id'] = group['dr_id']
-                interface_entry['bdr-id'] = group['bdr_id']
-                interface_entry['neighbor-count'] = group['nbrs']
-
-                entry_list.append(interface_entry)
+                entry_list.append(entry)
                 continue
 
         return ret_dict
-        
+
 # ==============================================
 #  Schema for show ospf3 neighbor extensive
 # ==============================================
@@ -147,7 +140,7 @@ class ShowOspf3NeighborExtensiveSchema(MetaParser):
         for item in value:
             entry_schema.validate(item)
         return value
-            
+
     # Main Schema
     schema = {
         "ospf3-neighbor-information": {
@@ -178,7 +171,7 @@ class ShowOspf3NeighborExtensive(ShowOspf3NeighborExtensiveSchema):
         #111.87.5.253     ge-0/0/0.0             Full      128     35
         p1 = re.compile(r'^(?P<neighborid>[\w\.\:\/]+) +(?P<interfacename>\S+) '
                 r'+(?P<ospfneighborstate>\S+) +(?P<pri>\S+) +(?P<dead>\d+)$')
-        
+
         #Neighbor-address fe80::250:56ff:fe8d:53c0
         p2 = re.compile(r'^Neighbor-address +(?P<neighbor_address>\S+)$')
 
@@ -206,9 +199,9 @@ class ShowOspf3NeighborExtensive(ShowOspf3NeighborExtensiveSchema):
                 ospf3_entry_dict['interface-name'] = group['interfacename']
                 ospf3_entry_dict['ospf-neighbor-state'] = group['ospfneighborstate']
                 ospf3_entry_dict['neighbor-priority'] = group['pri']
-                
+
                 continue
-            
+
             #Neighbor-address fe80::250:56ff:fe8d:53c0
             m = p2.match(line)
             if m:
@@ -241,7 +234,7 @@ class ShowOspf3NeighborExtensive(ShowOspf3NeighborExtensiveSchema):
                 ospf3_entry_dict['neighbor-up-time'] = {'#text': group['up']}
                 ospf3_entry_list.append(ospf3_entry_dict)
                 continue
-        
+
         return ret_dict
 
 # ==============================================
@@ -281,7 +274,7 @@ class ShowOspf3NeighborSchema(MetaParser):
         for item in value:
             entry_schema.validate(item)
         return value
-            
+
     # Main Schema
     schema = {
         "ospf3-neighbor-information": {
@@ -312,7 +305,7 @@ class ShowOspf3Neighbor(ShowOspf3NeighborSchema):
         #111.87.5.253     ge-0/0/0.0             Full      128     35
         p1 = re.compile(r'^(?P<id>[\d\.]+) +(?P<interface>\S+) '
                 r'+(?P<state>\S+) +(?P<pri>\S+) +(?P<dead>\d+)$')
-        
+
         #Neighbor-address fe80::250:56ff:fe8d:53c0
         p2 = re.compile(r'^Neighbor-address +(?P<neighbor_address>\S+)$')
 
@@ -360,3 +353,142 @@ class ShowOspf3NeighborDetail(ShowOspf3NeighborExtensive):
             out = output
 
         return super().cli(output=out)
+
+class ShowOspf3DatabaseSchema(MetaParser):
+    '''
+    schema = {
+        "ospf3-database-information": {
+            "ospf3-area-header": {
+                "ospf-area": str
+            },
+            "ospf3-database": [
+                {
+                    "advertising-router": str,
+                    "age": str,
+                    "checksum": str,
+                    "lsa-id": str,
+                    "lsa-length": str,
+                    "lsa-type": str,
+                    "sequence-number": str
+                }
+            ],
+            "ospf3-intf-header": [
+                {
+                    "ospf-intf": str
+                }
+            ]
+        }
+    }
+    '''
+
+    # Sub Schema ospf3-database
+    def validate_ospf3_database_list(value):
+        # Pass ospf3-database list as value
+        if not isinstance(value, list):
+            raise SchemaTypeError('ospf-interface is not a list')
+        ospf3_database_schema = Schema({
+                "advertising-router": str,
+                "age": str,
+                "checksum": str,
+                "lsa-id": str,
+                "lsa-length": str,
+                "lsa-type": str,
+                "sequence-number": str
+            })
+        # Validate each dictionary in list
+        for item in value:
+            ospf3_database_schema.validate(item)
+        return value
+
+    # Sub Schema ospf3-intf-header
+    def validate_ospf3_intf_header_list(value):
+        # Pass ospf3-intf-header list as value
+        if not isinstance(value, list):
+            raise SchemaTypeError('ospf-interface is not a list')
+        ospf3_intf_header_schema = Schema({
+                "ospf-area": str,
+                "ospf-intf": str
+            })
+        # Validate each dictionary in list
+        for item in value:
+            ospf3_intf_header_schema.validate(item)
+        return value
+
+    # Main Schema
+    schema = {
+        "ospf3-database-information": {
+            "ospf3-area-header": {
+                "ospf-area": str
+            },
+            "ospf3-database": Use(validate_ospf3_database_list),
+            "ospf3-intf-header": Use(validate_ospf3_intf_header_list),
+        }
+    }
+
+class ShowOspf3Database(ShowOspf3DatabaseSchema):
+    """ Parser for:
+    * show ospf3 database
+    """
+    cli_command = 'show ospf3 database'
+
+    def cli(self, output=None):
+        if not output:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        #    OSPF3 database, Area 0.0.0.8
+        p1 = re.compile(r'^( *)OSPF3( +)database,( +)Area( +)(?P<ospf_area>(\*{0,1})[0-9]{1,3}(\.[0-9]{1,3}){3})$')
+
+        # Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        # Router      0.0.0.0          59.128.2.250     0x800018ed  2407  0xaf2d  56
+        p2 = re.compile(r'^( *)(?P<lsa_type>\S+) +(\*{0,1})(?P<lsa_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<advertising_router>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<sequence_number>\S+) +(?P<age>\d+) +(?P<checksum>\S+) +(?P<lsa_length>\d+)$')
+
+        # OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
+        p3 = re.compile(r'^( *)OSPF3( +)Link-Local( +)database,( +)interface( +)(?P<ospf_intf>\S+)( +)Area( +)(?P<ospf_area>[0-9]{1,3}(\.[0-9]{1,3}){3})$')
+
+        ret_dict = {}
+
+        for line in out.splitlines():
+            line = line.strip()
+            # ge-0/0/0.0          PtToPt  0.0.0.8         0.0.0.0         0.0.0.0            1
+            m = p1.match(line)
+            if m:
+                ospf_area = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-area-header", {}).setdefault("ospf-area", None)
+                if ospf_area:
+                    raise Exception("ospf-area already exists"+str(ospf_area))
+
+                group = m.groupdict()
+
+                ret_dict["ospf3-database-information"]["ospf3-area-header"]["ospf-area"] = group["ospf_area"]
+                continue
+
+            # Router      0.0.0.0          59.128.2.250     0x800018ed  2407  0xaf2d  56
+            m = p2.match(line)
+            if m:
+                entry_list = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-database", [])
+
+                group = m.groupdict()
+                entry = {}
+                for group_key, group_value in group.items():
+                    entry_key = group_key.replace('_','-')
+                    entry[entry_key] = group_value
+
+                entry_list.append(entry)
+                continue
+
+            # OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
+            m = p3.match(line)
+            if m:
+                entry_list = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-intf-header", [])
+
+                group = m.groupdict()
+                entry = {}
+                for group_key, group_value in group.items():
+                    entry_key = group_key.replace('_','-')
+                    entry[entry_key] = group_value
+
+                entry_list.append(entry)
+                continue
+
+        return ret_dict
--- src/genie/libs/parser/junos/tests/test_show_ospf3.py
+++ src/genie/libs/parser/junos/tests/test_show_ospf3.py
@@ -7,11 +7,11 @@ from genie.metaparser.util.exceptions import SchemaEmptyParserError
 from genie.libs.parser.junos.show_ospf3 import ShowOspf3Interface, \
                                                ShowOspf3NeighborExtensive, \
                                                ShowOspf3NeighborDetail, \
-                                               ShowOspf3Neighbor
+                                               ShowOspf3Neighbor, ShowOspf3Database
 
 class TestShowOspf3Interface(unittest.TestCase):
 
-    device = Device(name='aDevice')
+    device = Device(name='test-device')
 
     empty_output = {'execute.return_value': ''}
 
@@ -65,7 +65,6 @@ class TestShowOspf3Interface(unittest.TestCase):
         obj = ShowOspf3Interface(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
-
 
 class TestShowOspf3NeighborExtensive(unittest.TestCase):
     """ Unit tests for:
@@ -215,11 +214,10 @@ class TestShowOspf3NeighborDetail(unittest.TestCase):
                     "ospf-area": "0.0.0.8",
                     "ospf-neighbor-state": "Full",
                     "ospf3-interface-index": "3"
-                }
+                    }
             ]
         }
     }
-
     def test_empty(self):
         self.device = Mock(**self.empty_output)
         obj = ShowOspf3NeighborDetail(device=self.device)
@@ -231,6 +229,397 @@ class TestShowOspf3NeighborDetail(unittest.TestCase):
         obj = ShowOspf3NeighborDetail(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
+class TestShowOspf3Database(unittest.TestCase):
+
+    device = Device(name='test-device')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value':
+    '''
+
+        OSPF3 database, Area 0.0.0.8
+        Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        Router      0.0.0.0          59.128.2.250     0x800018ed  2407  0xaf2d  56
+        Router      0.0.0.0          59.128.2.251     0x80001841   532  0x1d57  56
+        Router      0.0.0.0          106.187.14.240   0x80001a0b  2956  0x52ba  72
+        Router      0.0.0.0          106.187.14.241   0x800018b7  1259  0x94a3  72
+        Router     *0.0.0.0          111.87.5.252     0x80001890   913  0xae6c  56
+        Router      0.0.0.0          111.87.5.253     0x8000182a   915  0x8fdc  56
+        IntraArPfx  0.0.0.1          59.128.2.250     0x8000178c  1657  0xc4fc  76
+        IntraArPfx  0.0.0.1          59.128.2.251     0x8000178b   907  0x9e2d  76
+        IntraArPfx  0.0.0.1          106.187.14.240   0x80001808  2683  0x6948  88
+        IntraArPfx  0.0.0.1          106.187.14.241   0x800017e6   926  0xa81e  88
+        IntraArPfx *0.0.0.1          111.87.5.252     0x8000178a  1413  0x9b24  76
+        IntraArPfx  0.0.0.1          111.87.5.253     0x80001788   415  0x8820  76
+            OSPF3 AS SCOPE link state database
+        Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        Extern      0.0.0.1          59.128.2.250     0x8000178e  1282  0x3c81  28
+        Extern      0.0.0.3          59.128.2.250     0x8000178e   907  0x21bf  44
+        Extern      0.0.0.4          59.128.2.250     0x80000246  2783  0xcc71  44
+        Extern      0.0.0.1          59.128.2.251     0x80001789  1282  0x4081  28
+        Extern      0.0.0.2          59.128.2.251     0x80001788  2782  0x17d0  44
+        Extern      0.0.0.3          59.128.2.251     0x80000246   157  0xea52  44
+        Extern      0.0.0.18         106.187.14.240   0x80000349  1592  0xbddb  28
+        Extern      0.0.0.19         106.187.14.240   0x8000034d   774  0x3603  44
+        Extern      0.0.0.22         106.187.14.240   0x800002b9  2138  0xab95  44
+        Extern      0.0.0.23         106.187.14.240   0x80000247   501  0x7049  44
+        Extern      0.0.0.24         106.187.14.240   0x80000246  2410  0x4e6c  44
+        Extern      0.0.0.9          106.187.14.241   0x800002f0  2593  0xd341  44
+        Extern      0.0.0.10         106.187.14.241   0x80000246   593  0xd4f2  44
+        Extern      0.0.0.11         106.187.14.241   0x80000245  2926  0xe6df  44
+        Extern     *0.0.0.1          111.87.5.252     0x8000063f  1913  0x3ff4  44
+        Extern      0.0.0.1          111.87.5.253     0x80000e1e  1915  0x7dcd  44
+
+            OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
+        Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        Link       *0.0.0.2          111.87.5.252     0x8000178a   413  0xae5c  56
+        Link        0.0.0.2          111.87.5.253     0x80001787  2415  0x13d7  56
+
+            OSPF3 Link-Local database, interface ge-0/0/1.0 Area 0.0.0.8
+        Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        Link        0.0.0.3          106.187.14.240   0x8000179e  1047  0xbe92  56
+        Link       *0.0.0.3          111.87.5.252     0x80001789  2913  0x607c  56
+
+            OSPF3 Link-Local database, interface lo0.0 Area 0.0.0.8
+        Type       ID               Adv Rtr           Seq         Age  Cksum  Len
+        Link       *0.0.0.1          111.87.5.252     0x8000178b  2413  0xa440  44
+
+    '''}
+
+    golden_parsed_output = {
+        "ospf3-database-information": {
+            "ospf3-area-header": {
+                "ospf-area": "0.0.0.8"
+            },
+            "ospf3-database": [
+                {
+                    "advertising-router": "59.128.2.250",
+                    "age": "2407",
+                    "checksum": "0xaf2d",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "56",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x800018ed"
+                },
+                {
+                    "advertising-router": "59.128.2.251",
+                    "age": "532",
+                    "checksum": "0x1d57",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "56",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x80001841"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "2956",
+                    "checksum": "0x52ba",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "72",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x80001a0b"
+                },
+                {
+                    "advertising-router": "106.187.14.241",
+                    "age": "1259",
+                    "checksum": "0x94a3",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "72",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x800018b7"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "913",
+                    "checksum": "0xae6c",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "56",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x80001890"
+                },
+                {
+                    "advertising-router": "111.87.5.253",
+                    "age": "915",
+                    "checksum": "0x8fdc",
+                    "lsa-id": "0.0.0.0",
+                    "lsa-length": "56",
+                    "lsa-type": "Router",
+                    "sequence-number": "0x8000182a"
+                },
+                {
+                    "advertising-router": "59.128.2.250",
+                    "age": "1657",
+                    "checksum": "0xc4fc",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "76",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x8000178c"
+                },
+                {
+                    "advertising-router": "59.128.2.251",
+                    "age": "907",
+                    "checksum": "0x9e2d",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "76",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x8000178b"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "2683",
+                    "checksum": "0x6948",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "88",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x80001808"
+                },
+                {
+                    "advertising-router": "106.187.14.241",
+                    "age": "926",
+                    "checksum": "0xa81e",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "88",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x800017e6"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "1413",
+                    "checksum": "0x9b24",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "76",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x8000178a"
+                },
+                {
+                    "advertising-router": "111.87.5.253",
+                    "age": "415",
+                    "checksum": "0x8820",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "76",
+                    "lsa-type": "IntraArPfx",
+                    "sequence-number": "0x80001788"
+                },
+                {
+                    "advertising-router": "59.128.2.250",
+                    "age": "1282",
+                    "checksum": "0x3c81",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "28",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x8000178e"
+                },
+                {
+                    "advertising-router": "59.128.2.250",
+                    "age": "907",
+                    "checksum": "0x21bf",
+                    "lsa-id": "0.0.0.3",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x8000178e"
+                },
+                {
+                    "advertising-router": "59.128.2.250",
+                    "age": "2783",
+                    "checksum": "0xcc71",
+                    "lsa-id": "0.0.0.4",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000246"
+                },
+                {
+                    "advertising-router": "59.128.2.251",
+                    "age": "1282",
+                    "checksum": "0x4081",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "28",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80001789"
+                },
+                {
+                    "advertising-router": "59.128.2.251",
+                    "age": "2782",
+                    "checksum": "0x17d0",
+                    "lsa-id": "0.0.0.2",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80001788"
+                },
+                {
+                    "advertising-router": "59.128.2.251",
+                    "age": "157",
+                    "checksum": "0xea52",
+                    "lsa-id": "0.0.0.3",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000246"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "1592",
+                    "checksum": "0xbddb",
+                    "lsa-id": "0.0.0.18",
+                    "lsa-length": "28",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000349"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "774",
+                    "checksum": "0x3603",
+                    "lsa-id": "0.0.0.19",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x8000034d"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "2138",
+                    "checksum": "0xab95",
+                    "lsa-id": "0.0.0.22",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x800002b9"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "501",
+                    "checksum": "0x7049",
+                    "lsa-id": "0.0.0.23",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000247"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "2410",
+                    "checksum": "0x4e6c",
+                    "lsa-id": "0.0.0.24",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000246"
+                },
+                {
+                    "advertising-router": "106.187.14.241",
+                    "age": "2593",
+                    "checksum": "0xd341",
+                    "lsa-id": "0.0.0.9",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x800002f0"
+                },
+                {
+                    "advertising-router": "106.187.14.241",
+                    "age": "593",
+                    "checksum": "0xd4f2",
+                    "lsa-id": "0.0.0.10",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000246"
+                },
+                {
+                    "advertising-router": "106.187.14.241",
+                    "age": "2926",
+                    "checksum": "0xe6df",
+                    "lsa-id": "0.0.0.11",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000245"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "1913",
+                    "checksum": "0x3ff4",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x8000063f"
+                },
+                {
+                    "advertising-router": "111.87.5.253",
+                    "age": "1915",
+                    "checksum": "0x7dcd",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "44",
+                    "lsa-type": "Extern",
+                    "sequence-number": "0x80000e1e"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "413",
+                    "checksum": "0xae5c",
+                    "lsa-id": "0.0.0.2",
+                    "lsa-length": "56",
+                    "lsa-type": "Link",
+                    "sequence-number": "0x8000178a"
+                },
+                {
+                    "advertising-router": "111.87.5.253",
+                    "age": "2415",
+                    "checksum": "0x13d7",
+                    "lsa-id": "0.0.0.2",
+                    "lsa-length": "56",
+                    "lsa-type": "Link",
+                    "sequence-number": "0x80001787"
+                },
+                {
+                    "advertising-router": "106.187.14.240",
+                    "age": "1047",
+                    "checksum": "0xbe92",
+                    "lsa-id": "0.0.0.3",
+                    "lsa-length": "56",
+                    "lsa-type": "Link",
+                    "sequence-number": "0x8000179e"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "2913",
+                    "checksum": "0x607c",
+                    "lsa-id": "0.0.0.3",
+                    "lsa-length": "56",
+                    "lsa-type": "Link",
+                    "sequence-number": "0x80001789"
+                },
+                {
+                    "advertising-router": "111.87.5.252",
+                    "age": "2413",
+                    "checksum": "0xa440",
+                    "lsa-id": "0.0.0.1",
+                    "lsa-length": "44",
+                    "lsa-type": "Link",
+                    "sequence-number": "0x8000178b"
+                }
+            ],
+            "ospf3-intf-header": [
+                {
+                    "ospf-area": "0.0.0.8",
+                    "ospf-intf": "ge-0/0/0.0"
+                },
+                {
+                    "ospf-area": "0.0.0.8",
+                    "ospf-intf": "ge-0/0/1.0"
+                },
+                {
+                    "ospf-area": "0.0.0.8",
+                    "ospf-intf": "lo0.0"
+                }
+            ]
+        }
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowOspf3Database(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = ShowOspf3Database(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
 
 class TestShowOspf3Neighbor(unittest.TestCase):
     """ Unit tests for:
