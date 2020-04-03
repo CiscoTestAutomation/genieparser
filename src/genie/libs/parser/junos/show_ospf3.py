3734cfe2b644a371fdd039f04b4f68e6933d03a7
Andrew Delph <adelph@cisco.com>
2020-04-03 16:08:00 -0400

    change ip addresses

--- src/genie/libs/parser/junos/show_ospf3.py
+++ src/genie/libs/parser/junos/show_ospf3.py
@@ -170,7 +170,7 @@ class ShowOspf3Database(ShowOspf3DatabaseSchema):
         p1 = re.compile(r'^( *)OSPF3( +)database,( +)Area( +)(?P<ospf_area>(\*{0,1})[0-9]{1,3}(\.[0-9]{1,3}){3})$')
 
         # Type       ID               Adv Rtr           Seq         Age  Cksum  Len
-        # Router      0.0.0.0          59.128.2.250     0x800018ed  2407  0xaf2d  56
+        # Router      0.0.0.0          10.34.2.250     0x800018ed  2407  0xaf2d  56
         p2 = re.compile(r'^( *)(?P<lsa_type>\S+) +(\*{0,1})(?P<lsa_id>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<advertising_router>[0-9]{1,3}(\.[0-9]{1,3}){3}) +(?P<sequence_number>\S+) +(?P<age>\d+) +(?P<checksum>\S+) +(?P<lsa_length>\d+)$')
 
         # OSPF3 Link-Local database, interface ge-0/0/0.0 Area 0.0.0.8
@@ -193,7 +193,7 @@ class ShowOspf3Database(ShowOspf3DatabaseSchema):
                 ret_dict["ospf3-database-information"]["ospf3-area-header"]["ospf-area"] = group["ospf_area"]
                 continue
 
-            # Router      0.0.0.0          59.128.2.250     0x800018ed  2407  0xaf2d  56
+            # Router      0.0.0.0          10.34.2.250     0x800018ed  2407  0xaf2d  56
             m = p2.match(line)
             if m:
                 entry_list = ret_dict.setdefault("ospf3-database-information", {}).setdefault("ospf3-database", [])
