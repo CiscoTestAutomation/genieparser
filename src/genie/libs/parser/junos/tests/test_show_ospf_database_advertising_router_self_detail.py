e7614f6f0bf826ed5c6aa6c697710f2cb8c59e7c
Andrew Delph <adelph@cisco.com>
2020-04-16 18:33:10 -0400

    update test output

--- src/genie/libs/parser/junos/tests/test_show_ospf_database_advertising_router_self_detail.py
+++ src/genie/libs/parser/junos/tests/test_show_ospf_database_advertising_router_self_detail.py
@@ -14,7 +14,250 @@ class TestShowOspfDatabaseAdvertisingRouterSelfDetail(unittest.TestCase):
     empty_output = {'execute.return_value': ''}
 
     golden_output = {'execute.return_value':
-    ''' d
+    '''
+            show ospf database advertising-router self detail | no-more
+
+            OSPF database, Area 0.0.0.8
+        Type       ID               Adv Rtr           Seq      Age  Opt  Cksum  Len
+        Router  *111.87.5.252     111.87.5.252     0x80001b9e  1801  0x22 0x1e2  120
+        bits 0x2, link count 8
+        id 111.87.5.253, data 111.87.5.93, Type PointToPoint (1)
+            Topology count: 0, Default metric: 5
+        id 111.87.5.92, data 255.255.255.252, Type Stub (3)
+            Topology count: 0, Default metric: 5
+        id 106.187.14.240, data 106.187.14.122, Type PointToPoint (1)
+            Topology count: 0, Default metric: 100
+        id 106.187.14.120, data 255.255.255.252, Type Stub (3)
+            Topology count: 0, Default metric: 100
+        id 27.86.198.239, data 27.86.198.25, Type PointToPoint (1)
+            Topology count: 0, Default metric: 1000
+        id 27.86.198.24, data 255.255.255.252, Type Stub (3)
+            Topology count: 0, Default metric: 1000
+        id 100.0.0.0, data 255.255.255.0, Type Stub (3)
+            Topology count: 0, Default metric: 100
+        id 111.87.5.252, data 255.255.255.255, Type Stub (3)
+            Topology count: 0, Default metric: 0
+        Topology default (ID 0)
+            Type: PointToPoint, Node ID: 27.86.198.239
+            Metric: 1000, Bidirectional
+            Type: PointToPoint, Node ID: 106.187.14.240
+            Metric: 100, Bidirectional
+            Type: PointToPoint, Node ID: 111.87.5.253
+            Metric: 5, Bidirectional
+        OpaqArea*1.0.0.1          111.87.5.252     0x80001a15   424  0x22 0xd49a  28
+        Opaque LSA
+        RtrAddr (1), length 4:
+            111.87.5.252
+        OpaqArea*1.0.0.3          111.87.5.252     0x80000322   153  0x22 0x95cd 136
+        Opaque LSA
+        Link (2), length 112:
+            Linktype (1), length 1:
+            1
+            LinkID (2), length 4:
+            111.87.5.253
+            LocIfAdr (3), length 4:
+            111.87.5.93
+            RemIfAdr (4), length 4:
+            111.87.5.94
+            TEMetric (5), length 4:
+            5
+            MaxBW (6), length 4:
+            1000Mbps
+            MaxRsvBW (7), length 4:
+            1000Mbps
+            UnRsvBW (8), length 32:
+                Priority 0, 1000Mbps
+                Priority 1, 1000Mbps
+                Priority 2, 1000Mbps
+                Priority 3, 1000Mbps
+                Priority 4, 1000Mbps
+                Priority 5, 1000Mbps
+                Priority 6, 1000Mbps
+                Priority 7, 1000Mbps
+            LinkLocalRemoteIdentifier (11), length 8:
+            Local 333, Remote 0
+            Color (9), length 4:
+            0
+        OpaqArea*1.0.0.4          111.87.5.252     0x800013e8  2604  0x22 0xb804 136
+        Opaque LSA
+        Link (2), length 112:
+            Linktype (1), length 1:
+            1
+            LinkID (2), length 4:
+            106.187.14.240
+            LocIfAdr (3), length 4:
+            106.187.14.122
+            RemIfAdr (4), length 4:
+            106.187.14.121
+            TEMetric (5), length 4:
+            100
+            MaxBW (6), length 4:
+            1000Mbps
+            MaxRsvBW (7), length 4:
+            1000Mbps
+            UnRsvBW (8), length 32:
+                Priority 0, 1000Mbps
+                Priority 1, 1000Mbps
+                Priority 2, 1000Mbps
+                Priority 3, 1000Mbps
+                Priority 4, 1000Mbps
+                Priority 5, 1000Mbps
+                Priority 6, 1000Mbps
+                Priority 7, 1000Mbps
+            LinkLocalRemoteIdentifier (11), length 8:
+            Local 334, Remote 0
+            Color (9), length 4:
+            10
+        OpaqArea*1.0.0.5          111.87.5.252     0x800001bb  1505  0x22 0x79b5 136
+        Opaque LSA
+        Link (2), length 112:
+            Linktype (1), length 1:
+            1
+            LinkID (2), length 4:
+            27.86.198.239
+            LocIfAdr (3), length 4:
+            27.86.198.25
+            RemIfAdr (4), length 4:
+            27.86.198.26
+            TEMetric (5), length 4:
+            1000
+            MaxBW (6), length 4:
+            1000Mbps
+            MaxRsvBW (7), length 4:
+            1000Mbps
+            UnRsvBW (8), length 32:
+                Priority 0, 1000Mbps
+                Priority 1, 1000Mbps
+                Priority 2, 1000Mbps
+                Priority 3, 1000Mbps
+                Priority 4, 1000Mbps
+                Priority 5, 1000Mbps
+                Priority 6, 1000Mbps
+                Priority 7, 1000Mbps
+            LinkLocalRemoteIdentifier (11), length 8:
+            Local 336, Remote 0
+            Color (9), length 4:
+            2
+        OpaqArea*4.0.0.0          111.87.5.252     0x80001a2a   964  0x22 0xe5ef  44
+        Opaque LSA
+        SR-Algorithm (8), length 1:
+            Algo (1), length 1:
+                0
+        SID/Label Range (9), length 12:
+            Range Size (1), length 3:
+                8000
+            SID/Label (1), length 3:
+            Label (1), length 3:
+                16000
+        OpaqArea*7.0.0.1          111.87.5.252     0x80001b9e  1801  0x22 0x8c7f  44
+        Opaque LSA
+        Extended Prefix (1), length 20:
+            Route Type (1), length 1:
+                1
+            Prefix Length (2), length 1:
+                32
+            AF (3), length 1:
+                0
+            Flags (4),  length 1:
+                0x40
+            Prefix (5), length 32:
+                111.87.5.252
+            Prefix Sid (2), length 8:
+            Flags (1), length 1:
+                0x00
+            MT ID (2), length 1:
+                0
+            Algorithm (3), length 1:
+                0
+            SID (4), length 4:
+                71
+        OpaqArea*8.0.0.52         111.87.5.252     0x80000308   694  0x22 0x7efa  60
+        Opaque LSA
+        Extended Link (1), length 36:
+            Link Type (1), length 1:
+            1
+            Link Id (2), length 4:
+            106.187.14.240
+            Link Data (3), length 4:
+            106.187.14.122
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0xe0
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                2567
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0x60
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                2568
+        OpaqArea*8.0.0.54         111.87.5.252     0x800002dc  1235  0x22 0x1839  60
+        Opaque LSA
+        Extended Link (1), length 36:
+            Link Type (1), length 1:
+            1
+            Link Id (2), length 4:
+            111.87.5.253
+            Link Data (3), length 4:
+            111.87.5.93
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0xe0
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                28985
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0x60
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                28986
+        OpaqArea*8.0.0.55         111.87.5.252     0x800001bb  2069  0x22 0x92eb  60
+        Opaque LSA
+        Extended Link (1), length 36:
+            Link Type (1), length 1:
+            1
+            Link Id (2), length 4:
+            27.86.198.239
+            Link Data (3), length 4:
+            27.86.198.25
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0xe0
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                167966
+            Adjacency Sid (2), length 7:
+                Flags (1), length 1:
+                0x60
+                MT ID (2), length 1:
+                0
+                Weight (3), length 1:
+                0
+                Label (4), length 3:
+                167967
+            OSPF AS SCOPE link state database
+        Type       ID               Adv Rtr           Seq      Age  Opt  Cksum  Len
+        Extern  *106.187.14.240   111.87.5.252     0x80001a3a  2336  0x22 0xc3fb  36
+        mask 255.255.255.255
+        Topology default (ID 0)
+            Type: 1, Metric: 50, Fwd addr: 0.0.0.0, Tag: 0.0.0.0
 
     '''}
 
