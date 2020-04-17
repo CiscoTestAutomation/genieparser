86cbd79897f78dd8270048e74f7e801327a7fd5c
Abdurahman Hersi -X (abhersi - ALBANY SERVICES  INC at Cisco) <abhersi@cisco.com>
2020-04-17 10:26:39 -0400

    show commands, chassis firmware & no-forwarding

--- src/genie/libs/parser/junos/tests/test_show_chassis.py
+++ src/genie/libs/parser/junos/tests/test_show_chassis.py
@@ -3,7 +3,10 @@ from unittest.mock import Mock
 
 from pyats.topology import loader, Device
 from genie.metaparser.util.exceptions import SchemaEmptyParserError
-from genie.libs.parser.junos.show_chassis import ShowChassisFpcDetail
+from genie.libs.parser.junos.show_chassis import ShowChassisFpcDetail,\
+                                                 ShowChassisEnvironmentRoutingEngine,\
+                                                 ShowChassisFirmware,\
+                                                 ShowChassisFirmwareNoForwarding
 
 class TestShowChassisFpcDetail(unittest.TestCase):
 
@@ -59,6 +62,138 @@ class TestShowChassisFpcDetail(unittest.TestCase):
     def test_golden(self):
         self.device = Mock(**self.golden_output)
         obj = ShowChassisFpcDetail(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+
+class TestShowChassisEnvironmentRoutingEngine(unittest.TestCase):
+
+    maxDiff = None
+
+    device = Device(name='test-device')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': '''
+        show chassis environment routing-engine
+        Routing Engine 0 status:
+        State                      Online Master
+    '''}
+
+    golden_parsed_output = {
+        "environment-component-information": {
+        "environment-component-item": {
+            "name": "Routing Engine 0",
+            "state": "Online Master"
+        }
+    }
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowChassisEnvironmentRoutingEngine(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = ShowChassisEnvironmentRoutingEngine(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+class TestShowChassisFirmware(unittest.TestCase):
+
+    maxDiff = None
+
+    device = Device(name='test-device')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': 
+    ''' show chassis firmware
+        Part                     Type       Version
+        FPC 0                    ROM        PC Bios                                    
+                                O/S        Version 19.2R1.8 by builder on 2019-06-21 17:52:23 UTC
+    '''}
+
+    golden_parsed_output = {
+        "firmware-information": {
+        "chassis": {
+            "chassis-module": {
+                "firmware": [
+                    {
+                        "firmware-version": "PC Bios",
+                        "type": "ROM"
+                    },
+                    {
+                        "firmware-version": "Version 19.2R1.8 by builder on 2019-06-21 17:52:23 UTC",
+                        "type": "O/S"
+                    }
+                ],
+                "name": "FPC 0"
+            }
+        }
+    }
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowChassisFirmware(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = ShowChassisFirmware(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+
+class TestShowChassisFirmwareNoForwarding(unittest.TestCase):
+
+    maxDiff = None
+
+    device = Device(name='test-device')
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_output = {'execute.return_value': 
+    ''' show chassis firmware no-forwarding
+        Part                     Type       Version
+        FPC 0                    ROM        PC Bios                                    
+                                O/S        Version 19.2R1.8 by builder on 2019-06-21 17:52:23 UTC
+    '''}
+
+    golden_parsed_output = {
+        "firmware-information": {
+        "chassis": {
+            "chassis-module": {
+                "firmware": [
+                    {
+                        "firmware-version": "PC Bios",
+                        "type": "ROM"
+                    },
+                    {
+                        "firmware-version": "Version 19.2R1.8 by builder on 2019-06-21 17:52:23 UTC",
+                        "type": "O/S"
+                    }
+                ],
+                "name": "FPC 0"
+            }
+        }
+    }
+        
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowChassisFirmwareNoForwarding(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = ShowChassisFirmwareNoForwarding(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
 
