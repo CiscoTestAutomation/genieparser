35324d30816c79dbfd268efc689951c2aaaa3878
Andrew Delph <adelph@cisco.com>
2020-04-20 13:56:30 -0400

    move backt o base folder

--- src/genie/libs/parser/junos/show_system.py
+++ src/genie/libs/parser/junos/show_system.py
@@ -256,6 +256,22 @@ class ShowSystemBuffers(ShowSystemBuffersSchema):
 
         return ret_dict
 
+
+class ShowSystemBufferNoForwarding(ShowSystemBuffer):
+    """ Parser for:
+            - 'show system buffer no-forwarding'
+    """
+
+    cli_command = "show system buffers no-forwarding"
+
+     def cli(self, output=None):
+        if not output:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        return super().cli(output=out)
+
 class ShowSystemUsersSchema(MetaParser):
     """ Schema for:
             * show system users
