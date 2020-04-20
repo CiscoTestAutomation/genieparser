ecebb6b54820dfa34eb1f6b9b90c6a85f3bfc5be
Andrew Delph <adelph@cisco.com>
2020-04-20 13:46:27 -0400

    move to serperate file for conflicts

--- src/genie/libs/parser/junos/show_system.py
+++ src/genie/libs/parser/junos/show_system.py
@@ -252,18 +252,3 @@ class ShowSystemBuffer(ShowSystemBufferSchema):
                 continue
 
         return ret_dict
-
-class ShowSystemBufferNoForwarding(ShowSystemBuffer):
-    """ Parser for:
-            - 'show system buffer no-forwarding'
-    """
-
-    cli_command = "show system buffers no-forwarding"
-
-     def cli(self, output=None):
-        if not output:
-            out = self.device.execute(self.cli_command)
-        else:
-            out = output
-
-        return super().cli(output=out)
