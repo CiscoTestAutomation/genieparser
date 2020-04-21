d348fd7475a3a2e3796cb1add5767b7623fddf32
Terry Zheng <tezheng@cisco.com>
2020-04-21 11:37:06 -0700

    Remove old searches, add new algorithm work in progress

---
+++ tests/utils/test_fuzzy_regex_search_command.py
@@ -0,0 +1,138 @@
+
+import unittest
+import re
+
+from genie.libs.parser.utils.common import (
+    _matches_fuzzy_regex, 
+    _fuzzy_regex_search_command,
+    parser_data
+)
+
+class TestFuzzyRegexSearchCommand(unittest.TestCase):
+    def test_search_normal_arguments(self):
+        return #TODO remove
+        for command, expected_source in parser_data.items(): 
+            arguments = re.findall('{(.*?)}', command)
+            search = re.sub('{.*?}', 'argument', command)
+            expected_kwargs = {argument:'argument' for argument in arguments}
+            results = _fuzzy_regex_search_command(search, False)
+            is_found = False
+            for result in results:
+                found_command, source, kwargs = result
+                if source == expected_source and command == found_command:
+                    is_found = True
+                    self.assertDictEqual(kwargs, expected_kwargs, search)
+            self.assertTrue(is_found)
+
+    def test_search_normal_arguments_with_regex(self):
+        return #TODO remove 
+        for command, expected_source in parser_data.items(): 
+            arguments = re.findall('{(.*?)}', command)
+            search = re.sub('{.*?}', 'argument', command)
+            expected_kwargs = {argument:'argument' for argument in arguments}
+            results = _fuzzy_regex_search_command(search, False)
+            is_found = False
+            for result in results:
+                found_command, source, kwargs = result
+                if source == expected_source and command == found_command:
+                    is_found = True
+                    self.assertDictEqual(kwargs, expected_kwargs, search)
+            self.assertTrue(is_found)
+
+    def test_special_command(self):
+        results = _fuzzy_regex_search_command('/dna/intent/api/v1/interface', False)
+        self.assertTrue(len(results), 1)
+        self.assertEqual(results[0][0], '/dna/intent/api/v1/interface')
+
+        results = _fuzzy_regex_search_command('/dna/intent/api/v1/interface/argument', False)
+        self.assertTrue(len(results), 1)
+        self.assertEqual(results[0][0], '/dna/intent/api/v1/interface/{interface}')
+        self.assertEqual(results[0][2], {'interface': 'argument'})
+
+        results = _fuzzy_regex_search_command(r'\/dna\/intent\/api\/v1\/interface\/argument', True)
+        self.assertTrue(len(results), 1)
+        self.assertEqual(results[0][0], '/dna/intent/api/v1/interface/{interface}')
+        self.assertEqual(results[0][2], {'interface': 'argument'})
+
+        results = _fuzzy_regex_search_command(r'\/dna\/intent\/api\/v1\/interface', True)
+        self.assertTrue(len(results), 1)
+        self.assertEqual(results[0][0], '/dna/intent/api/v1/interface')
+
+
+if __name__ == '__main__':
+    #print(_fuzzy_regex_search_command('/dna/intent/api/v1/interface', False))
+
+    #results = _fuzzy_regex_search_command('\/dna\/intent\/api\/v1\/interface\/argument', True)
+    #exit()
+    # unittest.main() 
+    # exit()
+    # print(_fuzzy_regex_search_command('\/dna\/intent\/api\/v1\/interface\/apcd', True)[0][0])
+    # exit()
+
+    for command, expected_source in parser_data.items(): 
+        arguments = re.findall('{(.*?)}', command)
+        search = re.sub('{.*?}', 'machester', command)
+        expected_kwargs = {argument:'argument' for argument in arguments}
+        #results = _fuzzy_search_command(search)
+        results = _fuzzy_regex_search_command(search, False)
+        # cmd, _, _ = results
+        # if cmd != command:
+        #     raise Exception(cmd + '|' + command)
+        if len(results) > 1:
+            print()
+            print()
+            print("Searching For: " + command)
+            print("====================")
+            print("Found:")
+            for s, _, _ in results:
+                print(s)
+            print('====================')
+        
+        if not command in set(result for result, _, _ in results):
+            print('---------------')
+            for s, _, _ in results:
+                print(s)
+            raise Exception(command)
+    #_fuzzy_regex_search_command('show access-lists wjosf', False)
+    
+    # print("started")
+    # count = 0
+
+    # for command in parser_data.keys(): 
+    #     arguments = re.findall('{(.*?)}', command)
+    #     command = re.sub('{.*?}', 'argument', command)
+    #     expected_kwargs = {argument:'argument' for argument in arguments}
+    #     results = _fuzzy_regex_search_command(command, False)
+    #     if len(results) != 1:
+    #         print("ERROR ", command)
+    #         count += 1
+    #         break
+
+    # print(count)
+    # print(len(parser_data))
+
+
+    #print(_fuzzy_regex_search_command('/mgmt/tm/auth/radius', False))
+
+    # self.assertTrue(result)
+
+    # command, source, kwargs = result
+
+    # self.assertDictEqual(kwargs, expected_kwargs)
+    #print(_matches_fuzzy_regex(0,0, 's v'.split(), 'show version'.split(), {} ))
+    #print(_matches_fuzzy_regex(0,0, 'sh ver blue'.split(), 'show version {arg}'.split(), {} ))
+    #print(_matches_fuzzy_regex(0,0, 'sh ver blue blue green'.split(), 'show version {arg} {ww}'.split(), {} ))
+    #print(_matches_fuzzy_regex(0,0, 'sh ver (vs|ww) ww'.split(), 'show version vs ww'.split(), {} ))
+    #print(_matches_fuzzy_regex(0,0, 'sh ver .* az blue .* .* ajs .* x x'.split(), 'show version a az {int} abc d ajs bjos {x} x'.split(), {} ))
+    #sh ver .* az {int}
+    # print(_matches_fuzzy_regex(0,0, 'sh ver .* blue'.split(), 'show version wx abc {arg}'.split(), {} ))
+
+    #argument as 1\.3\.4\.5 etc
+    #print(_matches_fuzzy_regex(0,0, 'sh v.* .*'.split(), 'ps -ef'.split(), {} ))
+    # buggg 'sh b .* blue .* n|p d'
+    # # OR operator
+    # for w in _fuzzy_regex_search_command('sh b .* blue .* [n|p] d'):
+    #     print(w[0])
+    #     print(w[2])
+    #     print('========')
+
