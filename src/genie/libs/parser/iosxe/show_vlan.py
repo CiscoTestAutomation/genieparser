be7a460e6a991d0bc8b4d3eadfcec0c2cd4eabbc
Xiaoxin Zhou -X (xiaoxinz - ALBANY SERVICES  INC at Cisco) <xiaoxinz@cisco.com>
2020-05-13 14:32:33 -0400

    test the github action

--- src/genie/libs/parser/iosxe/show_vlan.py
+++ src/genie/libs/parser/iosxe/show_vlan.py
@@ -86,13 +86,10 @@ class ShowVlan(ShowVlanSchema):
         # 2    VLAN_0002                        active
         # 20   VLAN-0020                        active
         # 100  V100                             suspended
-        # 101  VLAN-0101                        active
-        # 102  VLAN_0102                        active
-        # 103  VLAN-0103                        act/unsup
-        # 104  VLAN_0104                        act/lshut
         # 105  Misc. Name                       active    Gi1/0/13, Gi1/0/14, Gi1/0/15, Gi1/0/16, Gi1/0/19
         p1 = re.compile(r'^(?P<vlan_id>[0-9]+)\s+(?P<name>(?=\S).*(?<=\S))'
-                         '\s+(?P<status>(active|suspended|(.*)lshut|(.*)unsup)+)(?P<interfaces>[\w\d\/\d, ]+)?$')
+                         '\s+(?P<status>(active|suspended|(.*)lshut|(.*)unsup)+)'
+                         '(?P<interfaces>[\w\d\/\d, ]+)?$')
 
         #                                                Gi1/0/19, Gi1/0/20, Gi1/0/21, Gi1/0/22
         p2 = re.compile(r'^\s*(?P<space>\s{48})(?P<interfaces>[\w\s\/\,]+)?$')
