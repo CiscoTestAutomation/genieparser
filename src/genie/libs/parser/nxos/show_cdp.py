b10bf28d968a56624ce062dd31c4301f23873cc7
Xiaoxin Zhou -X (xiaoxinz - ALBANY SERVICES  INC at Cisco) <xiaoxinz@cisco.com>
2020-05-13 14:47:26 -0400

    create an error

--- src/genie/libs/parser/nxos/show_cdp.py
+++ src/genie/libs/parser/nxos/show_cdp.py
@@ -27,9 +27,6 @@ class ShowCdpNeighborsSchema(MetaParser):
             {Optional('index'):
                 {Any():
                     {'device_id': str,
-                     'local_interface': str,
-                     'hold_time': int,
-                     'capability': str,
                      'platform': str,
                      'port_id': str, }, }, },
     }
