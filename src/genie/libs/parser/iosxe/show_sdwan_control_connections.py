eb2375dc98cd78591e26f68aaac85666f7152068
Xiaoxin Zhou -X (xiaoxinz - ALBANY SERVICES  INC at Cisco) <xiaoxinz@cisco.com>
2020-05-15 10:46:30 -0400

    address comments

--- src/genie/libs/parser/iosxe/show_sdwan_control_connections.py
+++ src/genie/libs/parser/iosxe/show_sdwan_control_connections.py
@@ -29,6 +29,7 @@ class ShowSdwanControlConnectionsSchema(MetaParser):
                         "peer_type": str,
                         "proxy_state": str,
                         "site_id": str,
+                        "state": str,
                         "uptime": str,
                     },
                 },
@@ -56,12 +57,14 @@ class ShowSdwanControlConnections(ShowSdwanControlConnectionsSchema):
 
         parsed_dict = {}
 
+        # peer_type     peer_protocol   peer_system_ip  site_id   domain_id peer_private_ip     peer_private_port   peer_public_ip  peer_public_port    local_color     proxy_state    state         uptime      controller_group_id
+        # vsmart        dtls            4.4.4.70        100        1         10.10.20.70        12446               10.10.20.70     12446               default         No              up      2:13:54:01  0
         p1 = re.compile(
             r"(?P<peer_type>\w+)\s+(?P<peer_protocol>\w+)\s+(?P<peer_system_ip>\S+)"
-            "\s+(?P<site_id>\d+)\s+(?P<domain_id>\d+)\s+(?P<peer_private_ip>\S+)"
-            "\s+(?P<peer_private_port>\d+)\s+(?P<peer_public_ip>\S+)\s+"
-            "(?P<peer_public_port>\d+)\s+(?P<local_color>\S+)\s+(?P<proxy_state>\w+)"
-            "\s+(?P<uptime>\S+)\s+(?P<controller_group_id>\S+)"
+            r"\s+(?P<site_id>\d+)\s+(?P<domain_id>\d+)\s+(?P<peer_private_ip>\S+)"
+            r"\s+(?P<peer_private_port>\d+)\s+(?P<peer_public_ip>\S+)\s+"
+            r"(?P<peer_public_port>\d+)\s+(?P<local_color>\S+)\s+(?P<proxy_state>\w+)"
+            r"\s+(?P<state>\S+)\s+(?P<uptime>\S+)\s+(?P<controller_group_id>\S+)"
         )
 
         for line in out.splitlines():
@@ -85,6 +88,7 @@ class ShowSdwanControlConnections(ShowSdwanControlConnectionsSchema):
                 color_dict["peer_public_port"] = group["peer_public_port"]
                 color_dict["proxy_state"] = group["proxy_state"]
                 color_dict["uptime"] = group["uptime"]
+                color_dict["state"] = group["state"]
                 color_dict["controller_group_id"] = group["controller_group_id"]
 
                 continue
