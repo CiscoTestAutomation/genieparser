4a20df32b8c7afe83ab58b26b225235d454a506d
Badal Soyantar <bsoyanta@cisco.com>
2020-05-20 09:23:14 -0700

    Schema change, removed \r

--- src/genie/libs/parser/junos/monitor.py
+++ src/genie/libs/parser/junos/monitor.py
@@ -13,13 +13,19 @@ from genie.metaparser.util.schemaengine import (Any,
 
 class MonitorInterfaceTrafficSchema(MetaParser):
     schema = {
-        "interface": {
+        "monitor-time": {
             Any(): {
-                "link": str,
-                "input-packets": int,
-                Optional("input-pps"): int,
-                "output-packets": int,
-                Optional("output-pps"): int,
+                "hostname": str,
+                "seconds": str,
+                Optional("interface"): {
+                    Any(): {
+                        "link": str,
+                        "input-packets": int,
+                        Optional("input-pps"): int,
+                        "output-packets": int,
+                        Optional("output-pps"): int,
+                    }
+                }
             }
         } 
     }
@@ -31,7 +37,7 @@ class MonitorInterfaceTraffic(MonitorInterfaceTrafficSchema):
     def cli(self, output=None, timeout=3):
         if not output:
             self.device.sendline(self.cli_command[0])
-            out = self.device.expect([r'Interface[\S\s]+Down=\^D'], 
+            out = self.device.expect([r'(?P<hostname>\S+) +Seconds: +(?P<seconds>\d+) +Time:[\S\s]+Down=\^D'], 
                 timeout=timeout).match_output
             ansi_escape = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]')
             out = ansi_escape.sub('\t', out)
@@ -41,7 +47,10 @@ class MonitorInterfaceTraffic(MonitorInterfaceTrafficSchema):
 
         ret_dict = {}
         
-        p1 = re.compile(r'^(?P<interface>\S+)\s+(?P<link>Up|Down)\s+'
+        p1 = re.compile(r'^(?P<hostname>\S+) +Seconds: +(?P<seconds>\d+) +'
+            r'Time: +(?P<monitor_time>\S+)$')
+
+        p2 = re.compile(r'^(?P<interface>\S+)\s+(?P<link>Up|Down)\s+'
             r'(?P<input_packets>\d+)(\s+\((?P<input_pps>\d+)\))?\s+'
             r'(?P<output_packets>\d+)(\s+\((?P<output_pps>\d+)\))?$')
 
@@ -51,7 +60,19 @@ class MonitorInterfaceTraffic(MonitorInterfaceTrafficSchema):
             m = p1.match(line)
             if m:
                 group = m.groupdict()
-                interface_dict = ret_dict.setdefault('interface', {}). \
+                monitor_time = group['monitor_time']
+                seconds = group['seconds']
+                hostname = group['hostname']
+                monitor_time_dict = ret_dict.setdefault('monitor-time', {}). \
+                    setdefault(monitor_time, {})
+                monitor_time_dict.update({'hostname': hostname})
+                monitor_time_dict.update({'seconds': seconds})
+                continue
+
+            m = p2.match(line)
+            if m:
+                group = m.groupdict()
+                interface_dict = monitor_time_dict.setdefault('interface', {}). \
                     setdefault(group['interface'], {})
                 interface_dict.update({'link': group['link']})
                 interface_dict.update({'input-packets': int(group['input_packets'])})
