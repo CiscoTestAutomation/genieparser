e21be3439529964e1f0adbb33e1b2c272013e732
Thomas Ryan <thomryan@cisco.com>
2020-05-28 13:08:21 -0400

    Updated sameple output in ShowRouteTable

--- src/genie/libs/parser/junos/show_route.py
+++ src/genie/libs/parser/junos/show_route.py
@@ -105,6 +105,7 @@ class ShowRouteTable(ShowRouteTableSchema):
 
         # 10.64.4.4/32         *[LDP/9] 03:40:50, metric 110
         # 10.64.4.4/32   *[L-OSPF/9/5] 1d 02:16:51, metric 110
+        # 118420             *[VPN/170] 31w3d 20:13:54
         r2 = re.compile(r'^ *(?P<rt_destination>\S+) +(?P<active_tag>\*)?'
                         r'\[(?P<protocol_name>\w+)/(?P<preference>\d+)/?(?P<preference2>\d+)?\]'
                         r' +(?P<age>[^,]+)(, +metric +(?P<metric>\d+))?$')
