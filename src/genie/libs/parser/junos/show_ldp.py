44dbef789da369778adea0bd85b1e93fe5308297
Abdurahman Hersi -X (abhersi - ALBANY SERVICES  INC at Cisco) <abhersi@cisco.com>
2020-07-13 19:12:55 -0400

    completed show ldp neighbor parser

--- src/genie/libs/parser/junos/show_ldp.py
+++ src/genie/libs/parser/junos/show_ldp.py
@@ -2,6 +2,7 @@
 
 JUNOS parsers for the following commands:
     * show ldp session
+    * show ldp neighbor
 """
 
 import re
@@ -74,5 +75,90 @@ class ShowLDPSession(ShowLDPSessionSchema):
                     k.replace('_', '-'): v
                     for k, v in group.items() if v is not None
                 })
+
+        return ret_dict
+
+
+class ShowLdpNeighborSchema(MetaParser):
+    """ Schema for:
+            * show ldp neighbor
+    """
+
+    schema = {
+    Optional("@xmlns:junos"): str,
+    "ldp-neighbor-information": {
+        Optional("@xmlns"): str,
+        "ldp-neighbor": [
+                {
+            "interface-name": str,
+            "ldp-label-space-id": str,
+            "ldp-neighbor-address": str,
+            "ldp-remaining-time": str
+                }
+            ]
+        }
+    }
+
+    def validate_ldp_neighbor(value):
+        if not isinstance(value, list):
+            raise SchemaTypeError('LDP neighbor is not a list')
+
+        ldp_neighbor = Schema({
+            "interface-name": str,
+            "ldp-label-space-id": str,
+            "ldp-neighbor-address": str,
+            "ldp-remaining-time": str
+        })
+
+        for item in value:
+            ldp_neighbor.validate(item)
+        return value
+
+    schema = {
+    Optional("@xmlns:junos"): str,
+    "ldp-neighbor-information": {
+        Optional("@xmlns"): str,
+        "ldp-neighbor": Use(validate_ldp_neighbor)
+        }
+    }
+
+class ShowLdpNeighbor(ShowLdpNeighborSchema):
+    """ Parser for:
+            * show ldp neighbor
+    """
+
+    cli_command = 'show ldp neighbor'
+
+    def cli(self, output=None):
+        if not output:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        ret_dict = {}
+
+        # 106.187.14.158                      ge-0/0/0.0      59.128.2.250:0       14
+        p1 = re.compile(
+            r'^(?P<ldp_neighbor_address>\S+) '
+            r'+(?P<interface_name>\S+) +(?P<ldp_label_space_id>\S+) '
+            r'+(?P<ldp_remaining_time>\S+)$'
+        )
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            # 106.187.14.158                      ge-0/0/0.0      59.128.2.250:0       14
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                ldp_neighbor_list = ret_dict.setdefault('ldp-neighbor-information', {}).\
+                    setdefault('ldp-neighbor', [])
+                ldp_dict = {}
+                for group_key, group_value in group.items():
+                    entry_key = group_key.replace('_', '-')
+                    ldp_dict[entry_key] = group_value
+
+                ldp_neighbor_list.append(ldp_dict)
+                continue
 
         return ret_dict
--- src/genie/libs/parser/junos/tests/test_show_ldp.py
+++ src/genie/libs/parser/junos/tests/test_show_ldp.py
@@ -8,7 +8,7 @@ from pyats.topology import Device
 # Metaparser
 from genie.metaparser.util.exceptions import SchemaEmptyParserError
 from genie.libs.parser.junos.show_ldp import (
-    ShowLDPSession, )
+    ShowLDPSession, ShowLdpNeighbor)
 
 
 # =================================
@@ -50,6 +50,75 @@ class TestShowLDPSession(unittest.TestCase):
     def test_golden(self):
         self.device = Mock(**self.golden_output)
         obj = ShowLDPSession(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# =================================
+# Unit test for 'show ldp neighbor'
+# =================================
+class TestShowLDPSession(unittest.TestCase):
+    '''unit test for "show ldp session'''
+    device = Device(name='aDevice')
+    maxDiff = None
+
+    empty_output = {'execute.return_value': ''}
+
+    golden_parsed_output = {
+            'ldp-neighbor-information': 
+                {'ldp-neighbor': [
+                    {'interface-name': 'ge-0/0/0.0',
+                     'ldp-label-space-id': '59.128.2.250:0',
+                     'ldp-neighbor-address': '106.187.14.158',
+                     'ldp-remaining-time': '14'
+                     }
+                ]
+            }
+    }
+
+    golden_output = {
+        'execute.return_value':
+        '''
+          show ldp neighbor
+        Address                             Interface       Label space ID     Hold time
+        106.187.14.158                      ge-0/0/0.0      59.128.2.250:0       14
+        '''
+    }
+
+    def test_empty(self):
+        self.device = Mock(**self.empty_output)
+        obj = ShowLdpNeighbor(device=self.device)
+        with self.assertRaises(SchemaEmptyParserError):
+            parsed_output = obj.parse()
+
+    def test_golden(self):
+        self.device = Mock(**self.golden_output)
+        obj = ShowLdpNeighbor(device=self.device)
         parsed_output = obj.parse()
         self.assertEqual(parsed_output, self.golden_parsed_output)
 
