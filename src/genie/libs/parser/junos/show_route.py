209d77b6a3e88a2c8b223d9ee10aab31e1c27981
Xiaoxin Zhou -X (xiaoxinz - ALBANY SERVICES  INC at Cisco) <xiaoxinz@cisco.com>
2020-07-31 13:32:22 -0400

    add sample output

--- src/genie/libs/parser/junos/show_route.py
+++ src/genie/libs/parser/junos/show_route.py
@@ -1647,6 +1647,8 @@ class ShowRouteReceiveProtocol(ShowRouteReceiveProtocolSchema):
 
         # * 10.220.0.0/16           Self                 12003   120        (65151 65000) I
         # 10.220.0.0/16           10.189.5.253         12003   120        (65151 65000) I
+        # * 10.4.1.1/32              Self                                    I
+        # * 10.36.3.3/32              Self                                    2 I        
         p2 = re.compile(r'(?P<active_tag>\*) +(?P<rt_destination>\S+)'
                         r' +(?P<to>\S+)( +(?P<med>\d+) +(?P<local_preference>\d+))? '
                         r'+(?P<as_path>(\(([\S\s]+\)) +\w+)|((\d\s)?\w))')
