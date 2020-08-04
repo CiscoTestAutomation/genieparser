29eac45d6be76355791eb72910c086a83fc136ad
VIVEKNELSON FRANCIS <vivfranc@cisco.com>
2020-08-04 22:52:52 +0530

    Fixed review comments.

--- src/genie/libs/parser/iosxe/tests/test_show_sslproxy.py
+++ src/genie/libs/parser/iosxe/tests/test_show_sslproxy.py
@@ -4,9 +4,8 @@ from unittest.mock import Mock
 # ATS
 from pyats.topology import Device
 
-# Metaparset
-from genie.metaparser.util.exceptions import SchemaEmptyParserError,\
-                                             SchemaMissingKeyError
+# Metaparser
+from genie.metaparser.util.exceptions import SchemaEmptyParserError
 
 # Parser
 from genie.libs.parser.iosxe.show_sslproxy import ShowSslproxyStatus
@@ -20,51 +19,52 @@ class TestShowSslproxyStatus(unittest.TestCase):
     maxDiff = None
     empty_output = {'execute.return_value' : ''}
     golden_output = {'execute.return_value': '''
-        ==========================================================                                                          
-                                                                                                                            
-                    SSL Proxy Status                                                                                     
-        ==========================================================                                                          
-                                                                                                                            
-        -------------                                                                                                       
-                                                                                                                            
-        CA Cert Bundle                 : /bootflash/vmanage-admin/apache.pem                                                
-        CA TP Label                    : PROXY-SIGNING-CA                                                                   
-        Cert Lifetime                  : 730                                                                                
-        RSA Key Modulus                : 2048                                                                               
-                                                                                                                            
-        Cert Revocation                : NONE                                                                               
-        Expired Cert                   : drop                                                                               
-        Untrusted Cert                 : drop                                                                               
-        Unsupported Protocol Ver       : drop                                                                               
-                                                                                                                            
-        Unsupported Cipher Suites      : drop                                                                               
-        Failure Mode Action            : close                                                                              
-        Min TLS Ver                    : TLS Version 1                                                                      
-        Status                                                                                                              
-                                                                                                                            
-        ------                                                                                                              
-        SSL Proxy Operational State    : RUNNING                                                                            
-        TCP Proxy Operational State    : RUNNING                                                                            
+        Configuration
+        -------------
+        CA Cert Bundle                 : /bootflash/vmanage-admin/sslProxyDefaultCAbundle.pem
+        CA TP Label                    : PROXY-SIGNING-CA
+        Cert Lifetime                  : 730
+        EC Key type                    : P256
+        RSA Key Modulus                : 2048
+        Cert Revocation                : NONE
+        Expired Cert                   : drop
+        Untrusted Cert                 : drop
+        Unknown Status                 : drop
+        Unsupported Protocol Ver       : drop
+        Unsupported Cipher Suites      : drop
+        Failure Mode Action            : close
+        Min TLS Ver                    : TLS Version 1.1
+
+        Status
+        ------
+        SSL Proxy Operational State    : RUNNING
+        TCP Proxy Operational State    : RUNNING
         Clear Mode                     : FALSE
         '''
         }
 
     golden_parsed_output = {
-        'ca_cert_bundle': '/bootflash/vmanage-admin/apache.pem',
-        'ca_tp_label': 'PROXY-SIGNING-CA',
-        'cert_lifetime': 730,
-        'rsa_key_modulus': 2048,
-        'cert_revocation': 'NONE',
-        'expired_cert': 'drop',
-        'untrusted_cert': 'drop',
-        'unsupported_protocol_ver': 'drop',
-        'unsupported_cipher_suites': 'drop',
-        'failure_mode_action': 'close',
-        'min_tls_ver': 'TLS Version 1',
-        'ssl_proxy_operational_state': 'RUNNING',
-        'tcp_proxy_operational_state': 'RUNNING',
-        'clear_mode': 'FALSE'
-    }
+        'configuration': {
+            'ca_cert_bundle': '/bootflash/vmanage-admin/sslProxyDefaultCAbundle.pem',
+            'ca_tp_label': 'PROXY-SIGNING-CA',
+            'cert_lifetime': 730,
+            'ec_key_type': 'P256',
+            'rsa_key_modulus': 2048,
+            'cert_revocation': 'NONE',
+            'expired_cert': 'drop',
+            'untrusted_cert': 'drop',
+            'unknown_status': 'drop',
+            'unsupported_protocol_ver': 'drop',
+            'unsupported_cipher_suites': 'drop',
+            'failure_mode_action': 'close',
+            'min_tls_ver': 'TLS Version 1.1'
+            },
+        'status': {
+            'ssl_proxy_operational_state': 'RUNNING',
+            'tcp_proxy_operational_state': 'RUNNING',
+            'clear_mode': 'FALSE'
+            }
+        }
 
 
     def test_empty(self):
