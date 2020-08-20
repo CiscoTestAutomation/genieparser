0dfe94e8aef4b5313949e51440bd501e6107aae7
itdependsnetworks <ken@celenza.org>
2020-08-19 22:18:49 -0400

    convert IOS to folder structure

--- src/genie/libs/parser/ios/tests/ShowConfigurationLock/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/ios/tests/ShowConfigurationLock/cli/equal/golden_output_expected.py
@@ -1,17 +1,41 @@
 expected_output = {
-    "config_session_lock": {
+    "parser_configure_lock": {
         "owner_pid": {
-            578: {
-                "tty_number": 2,
-                "tty_username": "testuser",
-                "user_debug_info": "CLI Session Lock",
-                "lock_active_time_in_sec": 17,
+            3: {
+                "user": "unknown",
+                "tty": 0,
+                "type": "EXCLUSIVE",
+                "state": "LOCKED",
+                "class": "EXPOSED",
+                "count": 1,
+                "pending_requests": 0,
+                "user_debug_info": "configure terminal",
+                "session_idle_state": "TRUE",
+                "num_of_exec_cmds_executed": 0,
+                "num_of_exec_cmds_blocked": 0,
+                "config_wait_for_show_completion": "FALSE",
+                "remote_ip_address": "Unknown",
+                "lock_active_time_in_sec": 6,
+                "lock_expiration_timer_in_sec": 593,
             },
-            5781: {
-                "tty_number": 21,
-                "tty_username": "testuser1",
-                "user_debug_info": "CLI Session Lock",
-                "lock_active_time_in_sec": 171,
+            -1: {
+                "user": "unknown",
+                "tty": -1,
+                "type": "NO LOCK",
+                "state": "FREE",
+                "class": "unknown",
+                "count": 0,
+                "pending_requests": 0,
+            },
+            10: {
+                "user": "User1",
+                "tty": 3,
+                "type": "EXCLUSIVE",
+                "state": "LOCKED",
+                "class": "Exposed",
+                "count": 0,
+                "pending_requests": 0,
+                "user_debug_info": "0",
             },
         }
     }
---
+++ src/genie/libs/parser/ios/tests/ShowNtpStatus/cli/equal/golden_output_2_expected.py
@@ -0,0 +1,20 @@
+expected_output = {
+    "clock_state": {
+        "system_status": {
+            "act_freq": 1000.4923,
+            "last_update": "1301 sec ago",
+            "nom_freq": 1000.0003,
+            "offset": 0.0,
+            "peerdispersion": 0.0,
+            "poll": 64,
+            "precision": "2**13",
+            "reftime": "DFA98D6B.F2F229A7 (16:55:55.949 EST Wed Nov 28 2018)",
+            "resolution": 1000,
+            "rootdelay": 0.0,
+            "rootdispersion": 18.84,
+            "status": "unsynchronized",
+            "stratum": 16,
+            "uptime": "1938800 (1/100 of seconds)",
+        }
+    }
+}
