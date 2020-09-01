9f0a7e1ee50e009afe58c20a0b619a1b02c74ebc
reustaquio22 <roy.eustaquio@networktocode.com>
2020-09-01 17:46:03 -0400

    Added parser for iosxe/show_wlan_summary

---
+++ src/genie/libs/parser/iosxe/tests/ShowWlanSummary/cli/equal/golden_output_expected.py
@@ -0,0 +1,31 @@
+expected_output = {
+    "wlan_summary": {
+        "wlan_count": 4,
+        "wlan_id": {
+            17: {
+                "profile_name": "lizzard_Global",
+                "ssid": "lizzard",
+                "wlan_status": "UP",
+                "status_security": "[WPA2][802.1x][FT + 802.1x][AES],[FT Enabled]"
+            },
+            18: {
+                "profile_name": "wip_Global",
+                "ssid": "wip",
+                "wlan_status": "UP",
+                "status_security": "[WPA2][802.1x + CCKM][AES]"
+            },
+            19: {
+                "profile_name": "internet_Global",
+                "ssid": "internet",
+                "wlan_status": "UP",
+                "status_security": "[open],MAC Filtering"
+            },
+            20: {
+                "profile_name": "lizzard-l_Global",
+                "ssid": "lizzard-legacy",
+                "wlan_status": "UP",
+                "status_security": "[WPA2][802.1x][AES]"
+            }
+        }
+    }
+}
