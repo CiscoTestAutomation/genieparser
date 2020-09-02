e67eff55ab88bca800430ce3049473d2eb505999
reustaquio22 <roy.eustaquio@networktocode.com>
2020-09-02 09:44:44 -0400

    Added parser for iosxe/show_wireless_client_summary

---
+++ src/genie/libs/parser/iosxe/tests/ShowWirelessClientSummary/cli/equal/golden_output_expected.py
@@ -0,0 +1,160 @@
+expected_output = {
+    "wireless_client_summary": {
+        "wireless_client_count": 13,
+        "included_clients": {
+            1: {
+                "id": 17,
+                "mac_address": "58bf.ea58.b399",
+                "ap_name": "b80-72-cap16",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            2: {
+                "id": 17,
+                "mac_address": "58bf.ea58.b463",
+                "ap_name": "b80-82-cap16",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            3: {
+                "id": 17,
+                "mac_address": "58bf.ea58.b4ae",
+                "ap_name": "b80-12-cap17",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            4: {
+                "id": 17,
+                "mac_address": "58bf.ea58.b4b1",
+                "ap_name": "b80-32-cap13",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            5: {
+                "id": 17,
+                "mac_address": "58bf.ea58.b4b6",
+                "ap_name": "b80-42-cap13",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            6: {
+                "id": 17,
+                "mac_address": "58bf.ea30.90cc",
+                "ap_name": "b80-22-cap14",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            7: {
+                "id": 19,
+                "mac_address": "58bf.eaa4.c601",
+                "ap_name": "b80-32-cap4",
+                "type": "WLAN",
+                "state": "IP Learn",
+                "protocol": "11n(2.4)",
+                "method": "MAB",
+                "role": "Local"
+            },
+            8: {
+                "id": 17,
+                "mac_address": "58bf.eae8.9e46",
+                "ap_name": "b80-51-cap7",
+                "type": "WLAN",
+                "state": "Authenticating",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Unknown"
+            },
+            9: {
+                "id": 17,
+                "mac_address": "58bf.ea28.dbba",
+                "ap_name": "b80-61-cap4",
+                "type": "WLAN",
+                "state": "Authenticating",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Unknown"
+            },
+            10: {
+                "id": 19,
+                "mac_address": "58bf.ea32.c516",
+                "ap_name": "b80-62-cap1",
+                "type": "WLAN",
+                "state": "IP Learn",
+                "protocol": "11n(5)",
+                "method": "MAB",
+                "role": "Local"
+            },
+            11: {
+                "id": 17,
+                "mac_address": "58bf.eaf0.3746",
+                "ap_name": "b80-12-cap14",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            12: {
+                "id": 17,
+                "mac_address": "58bf.eab2.9354",
+                "ap_name": "b80-51-cap6",
+                "type": "WLAN",
+                "state": "Run",
+                "protocol": "11n(5)",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            13: {
+                "id": 19,
+                "mac_address": "58bf.ea15.f311",
+                "ap_name": "b80-61-cap4",
+                "type": "WLAN",
+                "state": "Webauth Pending",
+                "protocol": "11n(2.4)",
+                "method": "MAB",
+                "role": "Local"
+            }
+        },
+        "wireless_excluded_client_count": 2,
+        "excluded_clients": {
+            1: {
+                "id": 17,
+                "mac_address": "58bf.ea8a.20e6",
+                "ap_name": "b80-61-cap20",
+                "type": "WLAN",
+                "state": "Excluded",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            },
+            2: {
+                "id": 17,
+                "mac_address": "58bf.ea37.4442",
+                "ap_name": "b80-62-cap14",
+                "type": "WLAN",
+                "state": "Excluded",
+                "protocol": "11ac",
+                "method": "Dot1x",
+                "role": "Local"
+            }
+        }
+    }
+}
