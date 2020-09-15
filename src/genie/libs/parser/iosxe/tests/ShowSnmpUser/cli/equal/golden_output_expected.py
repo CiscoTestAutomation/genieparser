b6228b958712d32942c10a6116b6a3f6b9779d06
Adam Byczkowski <adam.byczkowski@networktocode.com>
2020-09-15 17:18:52 -0500

    Resolved changes requested by badalsoyantar and tahigash

--- src/genie/libs/parser/iosxe/tests/ShowSnmpUser/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowSnmpUser/cli/equal/golden_output_expected.py
@@ -1,52 +1,54 @@
 expected_output = {
-    "SNMPv3-alfa": {
-        "engine-id": "800000090300002790FBCF00",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "SHA",
-        "priv-protocol": "AES128",
-        "group-name": "ALFA"
-    },
-    "bob": {
-        "engine-id": "800000090300001B53CEDC01",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "SHA",
-        "priv-protocol": "AES256",
-        "group-name": "group1"
-    },
-    "bad2": {
-        "engine-id": "800000090300001B53CEDC01",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "MD5",
-        "priv-protocol": "AES128",
-        "group-name": "group1"
-    },
-    "bad3": {
-        "engine-id": "800000090300001B53CEDC01",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "MD5",
-        "priv-protocol": "3DES",
-        "group-name": "group1"
-    },
-    "bad4": {
-        "engine-id": "800000090300001B53CEDC01",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "SHA",
-        "priv-protocol": "DES",
-        "group-name": "group1"
-    },
-    "user1": {
-        "engine-id": "800000090300001B53CEDC01",
-        "storage-type": "nonvolatile",
-        "auth-protocol": "SHA",
-        "priv-protocol": "AES128",
-        "group-name": "group1"
-    },
-    "nmsops": {
-        "engine-id": "00000063000100A20A101B3E",
-        "storage-type": "nonvolatile",
-        "access-list": "69",
-        "auth-protocol": "MD5",
-        "priv-protocol": "DES",
-        "group-name": "nmcigroup"
+    "user_name": {
+        "SNMPv3-alfa": {
+            "engine_id": "800000090300002790FBCF00",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "SHA",
+            "priv_protocol": "AES128",
+            "group_name": "ALFA"
+        },
+        "bob": {
+            "engine_id": "800000090300001B53CEDC01",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "SHA",
+            "priv_protocol": "AES256",
+            "group_name": "group1"
+        },
+        "bad2": {
+            "engine_id": "800000090300001B53CEDC01",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "MD5",
+            "priv_protocol": "AES128",
+            "group_name": "group1"
+        },
+        "bad3": {
+            "engine_id": "800000090300001B53CEDC01",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "MD5",
+            "priv_protocol": "3DES",
+            "group_name": "group1"
+        },
+        "bad4": {
+            "engine_id": "800000090300001B53CEDC01",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "SHA",
+            "priv_protocol": "DES",
+            "group_name": "group1"
+        },
+        "user1": {
+            "engine_id": "800000090300001B53CEDC01",
+            "storage_type": "nonvolatile",
+            "auth_protocol": "SHA",
+            "priv_protocol": "AES128",
+            "group_name": "group1"
+        },
+        "nmsops": {
+            "engine_id": "00000063000100A20A101B3E",
+            "storage_type": "nonvolatile",
+            "access_list": "69",
+            "auth_protocol": "MD5",
+            "priv_protocol": "DES",
+            "group_name": "nmcigroup"
+        }
     }
 }
