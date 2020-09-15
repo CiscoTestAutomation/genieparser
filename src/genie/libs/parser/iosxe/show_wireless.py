4180499db5b79d4f90d4f1391c1a491a63f06d1e
Robert Schneider <robert.schneider@networktocode.com>
2020-09-15 11:12:50 -0400

    added elif statements and output example

--- src/genie/libs/parser/iosxe/show_wireless.py
+++ src/genie/libs/parser/iosxe/show_wireless.py
@@ -11,13 +11,7 @@ from genie.metaparser.util.schemaengine import Any, Optional
 class ShowWirelessMobilityApListSchema(MetaParser):
     """Schema for show wireless mobility ap-list."""
 
-    schema = {
-         Any(): {
-             "controller_ip": str,
-             "learnt_from": str,
-             "radio_mac": str
-         }
-     }
+    schema = {Any(): {"controller_ip": str, "learnt_from": str, "radio_mac": str}}
 
 
 # ===================================
@@ -27,7 +21,7 @@ class ShowWirelessMobilityApListSchema(MetaParser):
 class ShowWirelessMobilityApList(ShowWirelessMobilityApListSchema):
     """Parser for show wireless mobility ap-list"""
 
-    cli_command = ['show wireless mobility ap-list']
+    cli_command = ["show wireless mobility ap-list"]
 
     def cli(self, output=None):
         if output is None:
@@ -36,16 +30,25 @@ class ShowWirelessMobilityApList(ShowWirelessMobilityApListSchema):
             output = output
 
         # AP name                           AP radio MAC      Controller IP     Learnt from
+        # --------------------------------------------------------------------------------------
+        # b80-72-cap30                    58bf.eab3.1420    10.10.7.177      Self
+        # b80-81-cap4                     58bf.ea13.62a0    10.10.7.177      Self
+        # b80-52-cap6                     58bf.ea13.75e0    10.10.7.177      Self
+
+        # AP name                           AP radio MAC      Controller IP     Learnt from
         ap_header_capture = re.compile(
-            r"^AP\s+name\s+AP\s+radio\s+MAC\s+Controller\s+IP\s+Learnt\s+from$")
+            r"^AP\s+name\s+AP\s+radio\s+MAC\s+Controller\s+IP\s+Learnt\s+from$"
+        )
 
         # --------------------------------------------------------------------------------------
         delimiter_capture = re.compile(
-            r"^--------------------------------------------------------------------------------------$")
+            r"^--------------------------------------------------------------------------------------$"
+        )
 
         # b80-72-cap30                    58bf.eab3.1420    10.10.7.177      Self
         ap_info_capture = re.compile(
-                    r"^(?P<ap_name>\S+)\s+(?P<radio_mac>\S+)\s+(?P<controller_ip>\d+\.\d+\.\d+\.\d+)\s+(?P<learnt_from>\S+)") # could also do (?P<learnt_from>(Self))
+            r"^(?P<ap_name>\S+)\s+(?P<radio_mac>\S+)\s+(?P<controller_ip>\d+\.\d+\.\d+\.\d+)\s+(?P<learnt_from>\S+)"
+        )  # could also do (?P<learnt_from>(Self))
 
         ap_info_obj = {}
 
@@ -53,21 +56,21 @@ class ShowWirelessMobilityApList(ShowWirelessMobilityApListSchema):
 
             if ap_header_capture.match(line):
                 continue
-            if delimiter_capture.match(line):
+            elif delimiter_capture.match(line):
                 continue
-            
-            if ap_info_capture.match(line):
+
+            elif ap_info_capture.match(line):
                 ap_info_capture_match = ap_info_capture.match(line)
                 groups = ap_info_capture_match.groupdict()
 
                 # ap_name: b80-72-cap30
-                ap_info_obj[groups['ap_name']] = {
+                ap_info_obj[groups["ap_name"]] = {
                     # radio_mac: 58bf.eab3.1420
-                    "radio_mac" : groups['radio_mac'],
+                    "radio_mac": groups["radio_mac"],
                     # controller_ip: 10.10.7.177
-                    "controller_ip": groups['controller_ip'],
+                    "controller_ip": groups["controller_ip"],
                     # learnt_from: Self
-                    "learnt_from": groups['learnt_from']
+                    "learnt_from": groups["learnt_from"],
                 }
 
-        return ap_info_obj    
+        return ap_info_obj
