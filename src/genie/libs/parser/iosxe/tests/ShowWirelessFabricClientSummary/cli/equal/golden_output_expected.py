b6661ff268ea0f03fa4d3a5dbe35de5e479740b8
Adam Byczkowski <adam.byczkowski@networktocode.com>
2020-09-18 17:18:57 -0500

    Changes performed per tahigash requests. Also refactored regex statements

--- src/genie/libs/parser/iosxe/tests/ShowWirelessFabricClientSummary/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowWirelessFabricClientSummary/cli/equal/golden_output_expected.py
@@ -1,266 +1,233 @@
 expected_output = {
     "number_of_fabric_clients": 33,
-    "clients": {
-        1: {
-            "mac_address": "58bf.ea71.80f0",
+    "mac_address": {
+        "58bf.ea71.80f0": {
             "ap_name": "a4-21-cap28",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        2: {
-            "mac_address": "58bf.ea71.8b10",
+        "58bf.ea71.8b10": {
             "ap_name": "a4-21-cap34",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        3: {
-            "mac_address": "58bf.ea71.9230",
+        "58bf.ea71.9230": {
             "ap_name": "a4-22-cap9",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        4: {
-            "mac_address": "58bf.ea71.9810",
+        "58bf.ea71.9810": {
             "ap_name": "a4-21-cap45",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        5: {
-            "mac_address": "58bf.ea71.9af0",
+        "58bf.ea71.9af0": {
             "ap_name": "a4-31-cap22",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        6: {
-            "mac_address": "58bf.ea71.a930",
+        "58bf.ea71.a930": {
             "ap_name": "a4-21-cap37",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        7: {
-            "mac_address": "58bf.ea71.baf0",
+        "58bf.ea71.baf0": {
             "ap_name": "a4-32-cap2",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        8: {
-            "mac_address": "58bf.ea71.bd30",
+        "58bf.ea71.bd30": {
             "ap_name": "a4-11-cap26",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        9: {
-            "mac_address": "58bf.ea71.bf70",
+        "58bf.ea71.bf70": {
             "ap_name": "a4-11-cap31",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        10: {
-            "mac_address": "58bf.ea71.c070",
+        "58bf.ea71.c070": {
             "ap_name": "a4-22-cap20",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        11: {
-            "mac_address": "58bf.ea71.c090",
+        "58bf.ea71.c090": {
             "ap_name": "a4-32-cap8",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        12: {
-            "mac_address": "58bf.ea71.d490",
+        "58bf.ea71.d490": {
             "ap_name": "a4-32-cap10",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        13: {
-            "mac_address": "58bf.ea71.d810",
+        "58bf.ea71.d810": {
             "ap_name": "a4-12-cap8",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        14: {
-            "mac_address": "58bf.ea71.e150",
+        "58bf.ea71.e150": {
             "ap_name": "a4-32-cap7",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        15: {
-            "mac_address": "58bf.ea71.e170",
+        "58bf.ea71.e170": {
             "ap_name": "a4-11-cap27",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        16: {
-            "mac_address": "58bf.ea71.e190",
+        "58bf.ea71.e190": {
             "ap_name": "a4-31-cap33",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        17: {
-            "mac_address": "58bf.ea71.e1d0",
+        "58bf.ea71.e1d0": {
             "ap_name": "a4-31-cap31",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        18: {
-            "mac_address": "58bf.ea71.e1f0",
+        "58bf.ea71.e1f0": {
             "ap_name": "a4-31-cap33",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        19: {
-            "mac_address": "58bf.ea71.e290",
+        "58bf.ea71.e290": {
             "ap_name": "a4-21-cap31",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        20: {
-            "mac_address": "58bf.ea71.e370",
+        "58bf.ea71.e370": {
             "ap_name": "a4-22-cap13",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        21: {
-            "mac_address": "58bf.ea72.8fd0",
+        "58bf.ea72.8fd0": {
             "ap_name": "a4-32-cap17",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        22: {
-            "mac_address": "58bf.ea72.a950",
+        "58bf.ea72.a950": {
             "ap_name": "a4-22-cap20",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        23: {
-            "mac_address": "58bf.ea72.ab50",
+        "58bf.ea72.ab50": {
             "ap_name": "a4-31-cap31",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        24: {
-            "mac_address": "58bf.ea72.abd0",
+        "58bf.ea72.abd0": {
             "ap_name": "a4-21-cap35",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        25: {
-            "mac_address": "58bf.ea72.acb0",
+        "58bf.ea72.acb0": {
             "ap_name": "a4-12-cap5",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        26: {
-            "mac_address": "58bf.ea72.b0b0",
+        "58bf.ea72.b0b0": {
             "ap_name": "a4-32-cap15",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        27: {
-            "mac_address": "58bf.ea72.b4d0",
+        "58bf.ea72.b4d0": {
             "ap_name": "a4-11-cap26",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        28: {
-            "mac_address": "58bf.ea72.c170",
+        "58bf.ea72.c170": {
             "ap_name": "a4-21-cap28",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        29: {
-            "mac_address": "58bf.ea72.c250",
+        "58bf.ea72.c250": {
             "ap_name": "a4-22-cap22",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        30: {
-            "mac_address": "58bf.ea72.c2b0",
+        "58bf.ea72.c2b0": {
             "ap_name": "a4-32-cap13",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        31: {
-            "mac_address": "58bf.ea72.c350",
+        "58bf.ea72.c350": {
             "ap_name": "a4-12-cap15",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        32: {
-            "mac_address": "58bf.ea72.c450",
+        "58bf.ea72.c450": {
             "ap_name": "a4-22-cap16",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Run",
             "protocol": "11ac",
             "method": "MAB"
         },
-        33: {
-            "mac_address": "58bf.eae6.b4de",
+        "58bf.eae6.b4de": {
             "ap_name": "a4-11-cap29",
-            "wlan": "19",
+            "wlan": 19,
             "state": "Webauth Pending",
             "protocol": "11ac",
             "method": "MAB"
