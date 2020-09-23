ab79d931e54b4b9e0768a8cf1614905ce206c5b5
William Morris <wmorris@securebearing.com>
2020-09-23 07:16:05 -0700

    removed debug statements and added examples

--- src/genie/libs/parser/asa/show_traffic.py
+++ src/genie/libs/parser/asa/show_traffic.py
@@ -66,51 +66,55 @@ class ShowTraffic(ShowTrafficSchema):
 
         ret_dict = {}
 
-
-        #Single output from 'show traffic' command
         # GigabitEthernet0/0:
-        # received (in 1618693.940 secs):
-        # 415466249 packets       29074806307 bytes
-        # 1 pkts/sec      17001 bytes/sec
-        # transmitted (in 1618693.940 secs):
-        # 316636 packets  64502596 bytes
-        # 0 pkts/sec      0 bytes/sec
-        # 1 minute input rate 9452 pkts/sec,  661142 bytes/sec
-        # 1 minute output rate 206 pkts/sec,  41887 bytes/sec
-        # 1 minute drop rate, 0 pkts/sec
-        # 5 minute input rate 11309 pkts/sec,  790978 bytes/sec
-        # 5 minute output rate 0 pkts/sec,  0 bytes/sec
-        # 5 minute drop rate, 0 pkts/sec
-        
-    
-
-        # RegEx named capture group name assigned to key, matched text assigned to values
-        # {'capture-group-name': 'capture group value'}
         p1 = re.compile(r'^(?P<name>\S+):$')
+
+        # received (in 1618693.940 secs):
+        # transmitted (in 1618693.940 secs):
         p2 = re.compile(r'^(?P<queue>\S+).+?(?P<duration>\d+\.\d+)\s+\S+')
+
+        # 415466249 packets       29074806307 bytes
         p3 = re.compile(r'(?P<packets>\d+)\s+packets\s+(?P<bytes>\d+)\s+\S*')
+
+        # 1 pkts/sec      17001 bytes/sec
         p4 = re.compile(r'(?P<packets_sec>\d+)\s+pkts\/sec\s+(?P<bytes_sec>\d+)\s+bytes\/sec$')
+
+        # 1 minute input rate 9452 pkts/sec,  661142 bytes/sec
         p5 = re.compile(r'^1 minute input rate\s+(?P<packets_input_1_minute>\d+)\s+pkts\/sec,\s+'
             '(?P<bytes_input_1_minute>\d+)\s+bytes\/sec$')
+
+        # 1 minute output rate 206 pkts/sec,  41887 bytes/sec    
         p6 = re.compile(r'^1 minute output rate\s+(?P<packets_output_1_minute>\d+)\s+pkts\/sec,\s+'
             '(?P<bytes_output_1_minute>\d+)\s+bytes\/sec$')
+
+        # 1 minute drop rate, 0 pkts/sec    
         p7 = re.compile(r'^1 minute drop rate,\s+(?P<packets_drop_rate_1_minute>\d+)\s+pkts\/sec$')
+        
+        # 5 minute input rate 11309 pkts/sec,  790978 bytes/sec
         p8 = re.compile(r'^5 minute input rate\s+(?P<packets_input_5_minute>\d+)\s+pkts\/sec,\s+'
             '(?P<bytes_input_5_minute>\d+)\s+bytes\/sec$')
+        
+        # 5 minute output rate 0 pkts/sec,  0 bytes/sec
         p9 = re.compile(r'^5 minute output rate\s+(?P<packets_output_5_minute>\d+)\s+pkts\/sec,\s+'
             '(?P<bytes_output_5_minute>\d+)\s+bytes\/sec$')
+        
+        # 5 minute drop rate, 0 pkts/sec
         p10 = re.compile(r'^5 minute drop rate,\s+(?P<packets_drop_rate_5_minute>\d+)\s+pkts\/sec$')
 
 
 
         for line in out.splitlines():
             line=line.strip()
+            
+            # GigabitEthernet0/0:
             m = p1.match(line)
-            #print(line)
             if m:
                 groups = m.groupdict()
                 instance_dict = ret_dict.setdefault(groups['name'], {})
-                continue
+                continue           
+            
+            # received (in 1618693.940 secs):
+            # transmitted (in 1618693.940 secs):
             m = p2.match(line)
             if m:
                 groups = m.groupdict()
@@ -121,14 +125,15 @@ class ShowTraffic(ShowTrafficSchema):
                     #Each iteration that will change to be the value that lives in queue. Reusable variable
                     queue = groups['queue']            
                     continue
-            #print(json.dumps(ret_dict, indent=2))
+            
+            # 415466249 packets       29074806307 bytes
             m = p3.match(line)
             if m:
                 groups = m.groupdict()
                 instance_dict[queue].update({'packets' : int(groups['packets']), 'bytes' : int(groups['bytes'])})
-                #print({groups['queue'] : { 'packets' : groups['packets']}})
-                #instance_dict['queue'].update({ 'packets' : groups['packets']})      
                 continue
+            
+            # 1 pkts/sec      17001 bytes/sec
             m = p4.match(line)
             if m:
                 groups = m.groupdict()
@@ -136,6 +141,8 @@ class ShowTraffic(ShowTrafficSchema):
                     {'packets_sec' : int(groups['packets_sec']), 'bytes_sec' : int(groups['bytes_sec'])}
                 )
                 continue
+            
+            # 1 minute input rate 9452 pkts/sec,  661142 bytes/sec
             m = p5.match(line)
             if m:
                 groups = m.groupdict()
@@ -143,6 +150,8 @@ class ShowTraffic(ShowTrafficSchema):
                     {'packets_input_1_minute' : int(groups['packets_input_1_minute']), 'bytes_input_1_minute' : int(groups['bytes_input_1_minute'])}
                 )
                 continue
+            
+            # 1 minute output rate 206 pkts/sec,  41887 bytes/sec 
             m = p6.match(line)
             if m:
                 groups = m.groupdict()
@@ -150,11 +159,15 @@ class ShowTraffic(ShowTrafficSchema):
                     {'packets_output_1_minute' : int(groups['packets_output_1_minute']), 'bytes_output_1_minute' : int(groups['bytes_output_1_minute'])}
                 )
                 continue
+            
+            # 1 minute drop rate, 0 pkts/sec 
             m = p7.match(line)
             if m:
                 groups = m.groupdict()
                 instance_dict.update({'packets_drop_rate_1_minute' : int(groups['packets_drop_rate_1_minute'])})
                 continue
+            
+            # 5 minute input rate 11309 pkts/sec,  790978 bytes/sec
             m = p8.match(line)
             if m:
                 groups = m.groupdict()
@@ -162,6 +175,8 @@ class ShowTraffic(ShowTrafficSchema):
                     {'packets_input_5_minute' : int(groups['packets_input_5_minute']), 'bytes_input_5_minute' : int(groups['bytes_input_5_minute'])}
                 )
                 continue
+            
+            # 5 minute output rate 0 pkts/sec,  0 bytes/sec
             m = p9.match(line)
             if m:
                 groups = m.groupdict()
@@ -169,9 +184,12 @@ class ShowTraffic(ShowTrafficSchema):
                     {'packets_output_5_minute' : int(groups['packets_output_5_minute']), 'bytes_output_5_minute' : int(groups['bytes_output_5_minute'])}
                 )
                 continue
+            
+            # 5 minute drop rate, 0 pkts/sec
             m = p10.match(line)
             if m:
                 groups = m.groupdict()
                 instance_dict.update({'packets_drop_rate_5_minute' : int(groups['packets_drop_rate_5_minute'])})
                 continue 
+            
         return ret_dict
