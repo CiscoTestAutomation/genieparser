8588243cca42047c8d48222c24f95bba4e28e731
Robert Schneider <robert.schneider@networktocode.com>
2020-09-29 10:56:44 -0400

    initial commit

---
+++ src/genie/libs/parser/iosxe/show_wireless.py
@@ -0,0 +1,30 @@
+import re
+
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Any, Optional
+
+
+# ========================================
+# Schema for:
+#  * 'show wireless stats ap join summary'
+# ========================================
+class ShowWirelessStatsApJoinSummarySchema(MetaParser):
+    """Schema for show wireless stats ap join summary."""
+
+    schema = {
+        
+    }
+
+
+# ========================================
+# Parser for:
+#  * 'show wireless stats ap join summary'
+# ========================================
+class ShowWirelessStatsApJoinSummary(ShowWirelessStatsApJoinSummarySchema):
+    """Parser for show wireless stats ap join summary"""
+
+    cli_command = ['show wireless stats ap join summary']
+
+    def cli(self, output=None):
+        if output is None:
+            output = self.device.execute(self.cli_command[0])
