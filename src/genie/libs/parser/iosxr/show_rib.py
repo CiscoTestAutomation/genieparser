4658801640b1725edc38d2d8f7b302e5954e8ac9
Gary Woodward <63735312+gwoodwa1@users.noreply.github.com>
2020-10-06 06:25:02 +0100

    Update show_rib.py
    Corrections made after review

--- src/genie/libs/parser/iosxr/show_rib.py
+++ src/genie/libs/parser/iosxr/show_rib.py
@@ -1,7 +1,7 @@
 """ show_rib.py
     supports commands:
-        * show rib table
-        * show rib summary 
+        * show rib tables
+        * show rib tables summary
 """
 
 # Python
@@ -39,11 +39,10 @@ class ShowRibTablesSchema(MetaParser):
 
 class ShowRibTables(ShowRibTablesSchema):
     """ Parser for show rib tables"""
-
     
-    cli_command = ['show rib tables']
+    cli_command = 'show rib tables'
 
-    def cli(self,port="",output=None):
+    def cli(self,output=None):
         """parsing mechanism: cli
         Function cli() defines the cli type output parsing mechanism which
         typically contains 3 steps: exe
@@ -55,7 +54,12 @@ class ShowRibTables(ShowRibTablesSchema):
             out = output
             
         result_dict = {}
+        result_dict.setdefault('rib_tables', {}).setdefault('table_id', {})
         
+        # <vrf>   / <table>    <safi>  <table_id>  <prefix_limit>  <prefix_count> <table_ver>
+        # default/default        uni   0xe0000000  10000000            12              13  
+        # Prefix Limit Notified Forward Referenced Table Deleted   Table Reached Convergence
+        #         N                      N                N                 Y
         p1 = re.compile(r'^(?P<vrf>\S+)\/(?P<table>\w+) +(?P<safi>\w+)'
                         ' +(?P<table_id>\S+) +(?P<prfx_lmt>\d+) +(?P<prfx_cnt>\d+)'
                         ' +(?P<tbl_ver>\d+) +(?P<pfx_notif>\w) +(?P<forw_refe>\w)'
@@ -74,37 +78,35 @@ class ShowRibTables(ShowRibTablesSchema):
             if m:
                 group = m.groupdict()
                 table_id=group['table_id']
-                result_dict[table_id]={}
-                result_dict[table_id].update({'vrf_name': group['vrf']})
-                result_dict[table_id].update({'table_name': group['table']})
-                result_dict[table_id].update({'safi': group['safi']})
-                result_dict[table_id].update({'table_id': group['table_id']})
-                result_dict[table_id].update({'prefix_limit': int(group['prfx_lmt'])})
-                result_dict[table_id].update({'prefix_count': int(group['prfx_cnt'])})
-                result_dict[table_id].update({'table_version': int(group['tbl_ver'])})
+                result_dict['rib_tables']['table_id'][table_id]={}
+                result_dict['rib_tables']['table_id'][table_id].update({'vrf_name': group['vrf']})
+                result_dict['rib_tables']['table_id'][table_id].update({'table_name': group['table']})
+                result_dict['rib_tables']['table_id'][table_id].update({'safi': group['safi']})
+                result_dict['rib_tables']['table_id'][table_id].update({'table_id': group['table_id']})
+                result_dict['rib_tables']['table_id'][table_id].update({'prefix_limit': int(group['prfx_lmt'])})
+                result_dict['rib_tables']['table_id'][table_id].update({'prefix_count': int(group['prfx_cnt'])})
+                result_dict['rib_tables']['table_id'][table_id].update({'table_version': int(group['tbl_ver'])})
                 
                 if group['pfx_notif']=='N':
-                    result_dict[table_id].update({'prefix_limit_notified': 'No'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'prefix_limit_notified': 'No'})
                 elif group['pfx_notif']=='Y':
-                    result_dict[table_id].update({'prefix_limit_notified': 'Yes'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'prefix_limit_notified': 'Yes'})
                     
                 if group['forw_refe']=='N':
-                    result_dict[table_id].update({'forward_referenced': 'No'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'forward_referenced': 'No'})
                 elif group['forw_refe']=='Y':
-                    result_dict[table_id].update({'forward_referenced': 'Yes'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'forward_referenced': 'Yes'})
                     
                 if group['tbl_del']=='N':
-                    result_dict[table_id].update({'table_deleted': 'No'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'table_deleted': 'No'})
                 elif group['tbl_del']=='Y':
-                    result_dict[table_id].update({'table_deleted': 'Yes'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'table_deleted': 'Yes'})
 
                 if group['tbl_conv']=='N':
-                    result_dict[table_id].update({'table_reached_convergence': 'No'})
+                    result_dict['rib_tables']['table_id'][table_id].update({'table_reached_convergence': 'No'})
                 elif group['tbl_conv']=='Y':
-                    result_dict[table_id].update({'table_reached_convergence': 'Yes'})                                
+                    result_dict['rib_tables']['table_id'][table_id].update({'table_reached_convergence': 'Yes'})                                
         
-        result_dict = {'table_id': result_dict}
-        result_dict = {'rib_tables': result_dict}
         return result_dict
 
 
@@ -132,13 +134,12 @@ class ShowRibTablesSummarySchema(MetaParser):
 class ShowRibTablesSummary(ShowRibTablesSummarySchema):
     """ Parser for show rib tables summary"""
 
-    cli_command = ['show rib tables summary']
+    cli_command = 'show rib tables summary'
 
-    def cli(self,port="",output=None):
+    def cli(self,output=None):
         """parsing mechanism: cli
         Function cli() defines the cli type output parsing mechanism which
-        typically contains 3 steps: exe
-        cuting, transforming, returning
+        typically contains 3 steps: executing, transforming, returning
         """
         if output is None:
             out = self.device.execute(self.cli_command)
@@ -147,6 +148,8 @@ class ShowRibTablesSummary(ShowRibTablesSummarySchema):
             
         result_dict = {}
         
+        result_dict.setdefault('rib_summary', {}).setdefault('table_id', {})
+
         p1 = re.compile(r'^Summary\sof.+in +(?P<rib>\S+)')
         p2 = re.compile(r'^Number of unicast tables:\s+(?P<uni_tbl>\d+)')
         p3 = re.compile(r'^Total number of unicast prefixes:\s+(?P<uni_pfx>\d+)')
@@ -166,35 +169,31 @@ class ShowRibTablesSummary(ShowRibTablesSummarySchema):
             if m:
                 group = m.groupdict()
                 table_id=group['rib'].lower()
-                result_dict[table_id]={}
-            
+                result_dict['rib_summary']['table_id'][table_id]={}
         
             m = p2.match(line)
             
             if m:
                 group = m.groupdict()
-                result_dict[table_id].update({'num_unicast_tables': int(group['uni_tbl'])})
+                result_dict['rib_summary']['table_id'][table_id].update({'num_unicast_tables': int(group['uni_tbl'])})
 
             m = p3.match(line)
             
             if m:
                 group = m.groupdict()
-                result_dict[table_id].update({'total_unicast_prefixes': int(group['uni_pfx'])})
+                result_dict['rib_summary']['table_id'][table_id].update({'total_unicast_prefixes': int(group['uni_pfx'])})
 
             m = p4.match(line)
             
             if m:
                 group = m.groupdict()
-                result_dict[table_id].update({'num_multicast_tables': int(group['multi_tbl'])})
-        
-        
-        
+                result_dict['rib_summary']['table_id'][table_id].update({'num_multicast_tables': int(group['multi_tbl'])})
+               
             m = p5.match(line)
             
             if m:
                 group = m.groupdict()
-                result_dict[table_id].update({'total_multicast_prefixes': int(group['multi_pfx'])})
+                result_dict['rib_summary']['table_id'][table_id].update({'total_multicast_prefixes': int(group['multi_pfx'])})
         
-        result_dict = {'table_id': result_dict}
-        result_dict = {'rib_summary': result_dict}
-        return result_dict           
+        return result_dict
+    
