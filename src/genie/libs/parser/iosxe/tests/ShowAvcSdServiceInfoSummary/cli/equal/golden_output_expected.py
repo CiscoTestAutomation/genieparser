62798b8296ce7faedd3f751a6a4687c3100e5f77
Robert Schneider <robert.schneider@networktocode.com>
2020-10-07 14:58:08 -0400

    uploading outputs

--- src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_expected.py
@@ -1,3 +1,13 @@
 expected_output = {
-
+    "active_controller": {
+        "ipv4_address": "10.11.236.21",
+        "status": "Connected",
+        "type": "Primary",
+    },
+    "device": {
+        "id": "sd-sw2.lab.com",
+        "ipv4_address": "10.18.29.33",
+        "segment_name": "core-pop",
+    },
+    "sdavc": {"status": "CONNECTED"},
 }
