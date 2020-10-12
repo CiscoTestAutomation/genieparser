81ab3432098d1dcc105cc8b36f6a20eaeadb4382
Gary Woodward <63735312+gwoodwa1@users.noreply.github.com>
2020-10-12 07:14:43 +0100

    Create golden_output1_expected.py

---
+++ src/genie/libs/parser/iosxr/tests/ShowImDampeningIntf/cli/equal/golden_output1_expected.py
@@ -0,0 +1,14 @@
+expected_output = {
+            'interface': {
+                'GigabitEthernet0/2/0/0': {
+                    'currently_suppressed': 'no',
+                    'dampening_status': 'enabled',
+                    'half_life': 1,
+                    'max_supress_time': 4,
+                    'penalty': 0,
+                    'reuse': 750,
+                    'suppress': 2000,
+                    'underlying_state': 'up'
+                }
+            }
+        }
