4e937a91e219bb552d9e6d0329c03f8cfb3c7945
dangrazi <dangrazi@cisco.com>
2020-10-14 12:01:53 -0400

    fixed IOSXE ShowPlatformIntegrity with nonce cli

--- src/genie/libs/parser/iosxe/show_platform.py
+++ src/genie/libs/parser/iosxe/show_platform.py
@@ -6013,7 +6013,7 @@ class ShowPlatformIntegritySchema(MetaParser):
         'os_hashes': {
             Any(): str,
         },
-        Optional('signature_version'): str,
+        Optional('signature_version'): int,
         Optional('signature'): str,
     }
 
@@ -6021,14 +6021,12 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
     # cli_command = 'show platform integrity'
     cli_command = ['show platform integrity', 'show platform integrity sign nonce {nonce}']
 
-    def cli(self, output=None, nonce=None):
+    def cli(self, nonce=None, output=None):
         if not output:
             if nonce:
-                out = self.device.execute(self.cli_command[1].format(nonce=nonce))
+                output = self.device.execute(self.cli_command[1].format(nonce=nonce))
             else:
-                out = self.device.execute(self.cli_command[0])
-        else:
-            out = output
+                output = self.device.execute(self.cli_command[0])
         
         ret_dict = {}
 
@@ -6040,7 +6038,7 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
         p3 = re.compile(r'^Boot +(?P<boot>\d+) +Hash: +(?P<hash>\S+)$')
         # Boot Loader Version: System Bootstrap, Version 16.10.1r[FC2], DEVELOPMENT SOFTWARE
         p4 = re.compile(r'^Boot +Loader +Version: +(?P<boot_loader_version>[\S ]+)$')
-        # Boot Loader Hash: 
+        # Boot Loader Hash: 523DD459C650AF0F5AB5396060605E412C1BE99AF51F4FA88AD26049612921FF
         p5 = re.compile(r'^Boot +Loader +Hash: *(?P<hash>\S+)$')
         # 51CE6FB9AE606330810EBFFE99D71D56640FD48F780EDE0C19FB5A75E31EF2192A58A196D18B244ADF67D18BF6B3AA6A16229C66DCC03D8A900753760B252C57
         p6 = re.compile(r'^(?P<hash>\S+)$')
@@ -6055,10 +6053,9 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
         # Signature version: 1
         p11 = re.compile(r'^Signature version: +(?P<signature_version>\S+)$')
         # Signature:
-        # AD7B89E69E9F09C1F476007760D14EED79B7E2FB0A9E2872AF39E051D4A0751EA8D0EED7366E9E6313B53D327ED13A13C9EFE27169FDB7D6C201549AE2F32E7CFDE6B1EF588113BAFB1AC834B7A48FDB93102AC68E6AA19B4A8D2BAEDD08A89F720D67B96274D9D3BF41236A250BAF517A31DBC22D0D455926E9CED4604DEC6132B47B5F45D9F0D266DD7FCD824236F3BCA5C357AFB880A7738FA6384B3D88B62654E76E2723039DAF63443CA70FE18E9A7523D1B4EAE51363094C85468EB264104251099A75EAF53F0A03C5491B604CF5489769F666EA254115984EDE19F3B8536391D1C89791501B0FE7E80942F0C4984E072D5E5F89B1D50AF445AA5DE270
-        p12 = re.compile(r'^Signature:*(?P<hash>\S+)$')
+        p12 = re.compile(r'^Signature:$')
 
-        for line in out.splitlines():
+        for line in output.splitlines():
             line = line.strip()
 
             # Platform: C9300-24U
@@ -6100,7 +6097,7 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
                 boot_loader_dict.update({'version': boot_loader_version})
                 continue
             
-            # Boot Loader Hash: 
+            # Boot Loader Hash: 523DD459C650AF0F5AB5396060605E412C1BE99AF51F4FA88AD26049612921FF
             m = p5.match(line)
             if m:
                 group = m.groupdict()
@@ -6126,6 +6123,12 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
             if m:
                 hash_type = 'os_hashes'
                 continue
+
+            # Signature:
+            m = p12.match(line)
+            if m:
+                hash_type = 'signature'
+                continue
             
             # PCR0: BB33E3FE338B82635B1BD3F1401CF442ACC9BB12A405A424FBE0A5776569884E
             m = p9.match(line)
@@ -6148,21 +6151,12 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
                 os_hash_dict = ret_dict.setdefault('os_hashes', {})
                 os_hash_dict.update({os_hash: ''})
                 continue
+
             # Signature version: 1
             m = p11.match(line)
             if m:
                 group = m.groupdict()
-                ret_dict.update({'signature_version': group['signature_version']})
-                continue
-            m = p12.match(line)
-            if m:
-                group = m.groupdict()
-                hash_val = group['hash']
-                hash_type = 'signature'
-                signature_dict = ret_dict.setdefault('signature', {}).setdefault('signature', {})
-                signature_hash = ret_dict.get('signature', '')
-                signature_hash = '{}{}'.format(signature_hash, hash_val)
-                signature_dict.update({'hash': signature_hash})
+                ret_dict.update({'signature_version': int(group['signature_version'])})
                 continue
 
             # 51CE6FB9AE606330810EBFFE99D71D56640FD48F780EDE0C19FB5A75E31EF2192A58A196D18B244ADF67D18BF6B3AA6A16229C66DCC03D8A900753760B252C57
@@ -6179,23 +6173,19 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
                     os_hash_val = '{}{}'.format(os_hash_val, hash_val)
                     os_hash_dict.update({'os_hash': os_hash_val})
                 elif hash_type == 'signature':
-                    signature_hash = ret_dict.get('signature_hash', '')
-                    signature_hash = '{}{}'.format(signature_hash, hash_val)
-                    signature_dict.update({'hash': signature_hash})
+                    ret_dict.update({'signature': hash_val})
                 continue
 
         return ret_dict
     
-    def yang(self, output=None, nonce=None):
+    def yang(self, nonce=None, output=None):
         if not output:
             # xpath is the same regardless of if nonce is passed or not
-            out = self.device.get(filter=('xpath', '/boot-integrity-oper-data')).data_xml
-        else:
-            out = output
+            output = self.device.get(filter=('xpath', '/boot-integrity-oper-data')).data_xml
 
-        log.info(minidom.parseString(out).toprettyxml())
+        log.info(minidom.parseString(output).toprettyxml())
 
-        root = ET.fromstring(out)
+        root = ET.fromstring(output)
         boot_integrity_oper_data = Common.retrieve_xml_child(root=root, key='boot-integrity-oper-data')
         boot_integrity = Common.retrieve_xml_child(root=boot_integrity_oper_data, key='boot-integrity')
         ret_dict = {}
@@ -6238,7 +6228,7 @@ class ShowPlatformIntegrity(ShowPlatformIntegritySchema):
                         os_hashes.update({name: sub_child.text})
                         name = None
             elif child.tag.endswith('sig-version'):
-                ret_dict.update({'signature_version': child.text})
+                ret_dict.update({'signature_version': int(child.text)})
             elif child.tag.endswith('signature'):
                 ret_dict.update({'signature': child.text})
         return ret_dict
