b261d07c8e663711a40c9fc4a783ce6ac3ffd7da
Adam Byczkowski <adam.byczkowski@networktocode.com>
2020-10-14 02:06:49 -0500

    Updated per tahigash's request

--- src/genie/libs/parser/iosxe/show_class.py
+++ src/genie/libs/parser/iosxe/show_class.py
@@ -16,12 +16,14 @@ class ShowClassMapSchema(MetaParser):
             str: {
               "match_criteria": str,
               "cm_id": int,
-              Optional("match_policy"): str,
               Optional("description"): str,
-              Optional("match_access_group"): list,
-              Optional("match_dscp"): list,
-              Optional("match_cos"): list,
-              Optional("match_protocol_attribute"): list
+              "index": {
+                  int: {
+                      Optional("match"): {
+                          Optional(str): list
+                      }
+                  }
+              }
             }
         }
     }
@@ -144,10 +146,10 @@ class ShowClassMap(ShowClassMapSchema):
         # Class Map match-any system-cpp-police-ios-feature (id 22)
         cm_header_capture = re.compile(
             r"^(\s+|)Class\sMap\s(?P<match_criteria>\S+)\s+(?P<cm_name>\S+)\s+\(id\s+(?P<cm_id>\d+)\)")
+
         #  Description: ICMPGEN,BROADCAST,ICMP,L2LVXCntrl,ProtoSnoop,PuntWebauth,MCASTData,Transit,DOT1XAuth,Swfwd,LOGGING,L2LVXData,ForusTraffic,ForusARP,McastEndStn,Openflow,Exception,EGRExcption,NflSampled,RpfFailed
         description_capture = re.compile(r"^Description:\s+(?P<description>.*$)")
-        #   Match none
-        # match_policy_capture = re.compile(r"^Match\s+(?P<match_policy>\S+)")
+
         # Match access-group name AutoConf-4.0-Acl-Transactional-Data
         p_match_access = re.compile(r"^Match\s+access-group\s+name\s+(?P<value>.*)$")
 
@@ -169,16 +171,15 @@ class ShowClassMap(ShowClassMapSchema):
         # Match cos  2
         p_match_cos = re.compile(r"^Match\s+cos\s+(?P<values>.*)$")
 
+        match_index = 1
 
         # Remove unwanted lines from raw text
         for line in out.splitlines():
             line = line.strip()
-
             # Class Map match-any system-cpp-police-ios-feature (id 22)
             cm_match = cm_header_capture.match(line)
             if not class_map_dict.get('class_maps'):
                 class_map_dict['class_maps'] = {}
-
             if cm_match:
                 groups = cm_match.groupdict()
                 cm_name = groups['cm_name']
@@ -187,8 +188,8 @@ class ShowClassMap(ShowClassMapSchema):
                 if not class_map_dict['class_maps'].get(cm_name, {}):
                     class_map_dict['class_maps'].update({cm_name: {}})
                 class_map_dict['class_maps'][cm_name].update({'match_criteria': match_criteria, 'cm_id': cm_id})
+                match_index = 1
                 continue
-            
             #  Description: ICMPGEN,BROADCAST,ICMP,L2LVXCntrl,ProtoSnoop,PuntWebauth,MCASTData,Transit,DOT1XAuth,Swfwd,LOGGING,L2LVXData,ForusTraffic,ForusARP,McastEndStn,Openflow,Exception,EGRExcption,NflSampled,RpfFailed
             desc_match = description_capture.match(line)
             if desc_match:
@@ -200,45 +201,71 @@ class ShowClassMap(ShowClassMapSchema):
             elif p_match_access.match(line):
                 # Match access-group name AutoConf-4.0-Acl-Transactional-Data
                 match = p_match_access.match(line)
-                if not class_map_dict["class_maps"][cm_name].get("match_access_group"):
-                  class_map_dict["class_maps"][cm_name].update({ "match_access_group": [] })
-                class_map_dict["class_maps"][cm_name]["match_access_group"].append(match.group("value"))
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                  class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "access_group": [] }})
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["access_group"].append(match.group("value"))
+                match_index += 1
                 continue
             elif p_match_protocol.match(line):
                 # Match protocol attribute traffic-class multimedia-conferencing
                 match = p_match_protocol.match(line)
-                if not class_map_dict['class_maps'][cm_name].get("match_protocol_attribute"):
-                  class_map_dict['class_maps'][cm_name].update({ "match_protocol_attribute": [] })
-                class_map_dict['class_maps'][cm_name]["match_protocol_attribute"].append(match.group("value"))
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                  class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "protocol_attribute": [] }})
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["protocol_attribute"].append(match.group("value"))
+                match_index += 1
                 continue
             elif p_match_none.match(line):
                 # Match none
-                class_map_dict['class_maps'][cm_name].update({ "match_policy": "none"})
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                  class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "policy": [] }})
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["policy"].append("none")
+                match_index += 1
                 continue
             elif p_match_any.match(line):
-                 # Match any
-                 class_map_dict['class_maps'][cm_name].update({ "match_policy": "any"})
-                 continue
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                   class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "policy": [] }})
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["policy"].append("any")
+                match_index += 1
+                continue
             elif p_match_non_client.match(line):
                 # Match non-client-nrt
-                class_map_dict['class_maps'][cm_name].update({ "match_policy": "non_client_nrt"})
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                   class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "policy": [] }})
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["policy"].append("non_client_nrt")
+                match_index += 1
                 continue
             elif p_match_dscp.match(line):
                 # Match   dscp af21 (18) af22 (20) af23 (22)
                 match = p_match_dscp.match(line)
+                if not class_map_dict['class_maps'][cm_name].get("index"):
+                  class_map_dict['class_maps'][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict['class_maps'][cm_name]["index"][match_index].update({ "match": { "dscp": [] }})
                 dscp_values = match.group("values").split(" ")
-                if not class_map_dict['class_maps'][cm_name].get("match_dscp"):
-                  class_map_dict['class_maps'][cm_name].update({ "match_dscp": [] })
                 dscp_list = [dscp_values[index] for index in range(0, len(dscp_values), 2)]
-                class_map_dict['class_maps'][cm_name]["match_dscp"] = dscp_list
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["dscp"] = dscp_list
+                match_index += 1
                 continue
             elif p_match_cos.match(line):
                 # Match cos  2
                 match = p_match_cos.match(line)
+                if not class_map_dict["class_maps"][cm_name].get("index"):
+                  class_map_dict["class_maps"][cm_name].update({ "index": {} })
+                class_map_dict["class_maps"][cm_name]["index"].update({ match_index: {} })
+                class_map_dict["class_maps"][cm_name]["index"][match_index].update({ "match": { "cos": [] }})
                 cos_values = match.group("values").split(" ")
-                if not class_map_dict['class_maps'][cm_name].get("match_cos"):
-                  class_map_dict['class_maps'][cm_name].update({ "match_cos": [] })
-                class_map_dict['class_maps'][cm_name]["match_cos"] = cos_values
+                class_map_dict["class_maps"][cm_name]["index"][match_index]["match"]["cos"] = cos_values
+                match_index += 1
                 continue
-  
+
         return class_map_dict
--- src/genie/libs/parser/iosxe/tests/ShowClassMap/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowClassMap/cli/equal/golden_output_expected.py
@@ -3,14 +3,24 @@ expected_output = {
         "AutoConf-4.0-Output-Trans-Data-Queue": {
             "match_criteria": "match-any",
             "cm_id": 6,
-            "match_dscp": [
-                "af21",
-                "af22",
-                "af23"
-            ],
-            "match_cos": [
-                "2"
-            ]
+            "index": {
+                1: {
+                    "match": {
+                        "dscp": [
+                            "af21",
+                            "af22",
+                            "af23"
+                        ]
+                    }
+                },
+                2: {
+                    "match": {
+                        "cos": [
+                            "2"
+                        ]
+                    }
+                }
+            }
         }
     }
 }
