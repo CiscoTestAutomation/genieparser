1ec7ef0770e2a458800d9af7cdadddc8d9c22871
Robert Schneider <robert.schneider@networktocode.com>
2020-10-15 13:53:52 -0400

    updating to include last_connection

--- src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_expected.py
@@ -1,12 +1,13 @@
 expected_output = {
     "active_controller": {
         "ip": "10.11.236.21",
+        "last_connection": "18:42:02.000 UTC Fri Oct 2 2020",
         "status": "Connected",
         "type": "Primary",
     },
     "device": {
-        "id": "sd-sw2.lab.com",
         "address": "10.18.29.33",
+        "id": "sd-sw2.lab.com",
         "segment_name": "core-pop",
     },
     "sdavc": {"status": "CONNECTED"},
