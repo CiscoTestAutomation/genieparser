9f72d0f1e5522b118af91b9be5ef05507767cc39
Robert Schneider <robert.schneider@networktocode.com>
2020-10-15 13:26:42 -0400

    updating for ip keys

--- src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_2_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_2_expected.py
@@ -1,5 +1,5 @@
 expected_output = {
     "active_controller": {"status": "not configured"},
-    "device": {"id": "", "ipv4_address": "0.0.0.0", "segment_name": "global"},
+    "device": {"id": "", "address": "0.0.0.0", "segment_name": "global"},
     "sdavc": {"status": "DISCONNECTED"},
 }
