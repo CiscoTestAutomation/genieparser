433c48d9db1e872ade50baa61561b4862374b6c6
Robert Schneider <robert.schneider@networktocode.com>
2020-10-15 14:21:23 -0400

    updated output to exclude active_controller

--- src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_2_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowAvcSdServiceInfoSummary/cli/equal/golden_output_2_expected.py
@@ -1,5 +1,4 @@
 expected_output = {
-    "active_controller": {"status": "not configured"},
-    "device": {"id": "", "address": "0.0.0.0", "segment_name": "global"},
+    "device": {"address": "0.0.0.0", "id": "", "segment_name": "global"},
     "sdavc": {"status": "DISCONNECTED"},
 }
