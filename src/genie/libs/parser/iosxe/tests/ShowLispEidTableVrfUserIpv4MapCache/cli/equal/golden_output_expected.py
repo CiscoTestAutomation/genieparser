013e44b5e8ac4723251b2371cd14405539b99249
Adam Byczkowski <adam.byczkowski@networktocode.com>
2020-10-22 15:22:28 -0500

    IOSXE/show lisp eid-table vrf User ipv4 map-cache

---
+++ src/genie/libs/parser/iosxe/tests/ShowLispEidTableVrfUserIpv4MapCache/cli/equal/golden_output_expected.py
@@ -0,0 +1,570 @@
+expected_output = {
+    "iid": 4100,
+    "number_of_entries": 2186,
+    "eid": {
+        "0.0.0.0/0": {
+            "subnet": "/0",
+            "uptime": "1w6d",
+            "expire": "never",
+            "source": [
+                "static-send-map-request"
+            ],
+            "rloc": {
+                "status": "negative_cache_entry",
+                "action": "send-map-request"
+            }
+        },
+        "0.0.0.0": {
+            "subnet": "/5",
+            "uptime": "4w6d",
+            "expire": "00:14:38",
+            "source": [
+                "map-reply",
+                "forward-native"
+            ],
+            "rloc": {
+                "status": "encapsulating_to_proxy_etr"
+            }
+        },
+        "8.0.0.0": {
+            "subnet": "/7",
+            "uptime": "07:18:55",
+            "expire": "00:07:19",
+            "source": [
+                "map-reply",
+                "forward-native"
+            ],
+            "rloc": {
+                "status": "encapsulating_to_proxy_etr"
+            }
+        },
+        "10.0.0.0": {
+            "subnet": "/9",
+            "uptime": "1w4d",
+            "expire": "00:00:13",
+            "source": [
+                "map-reply",
+                "forward-native"
+            ],
+            "rloc": {
+                "status": "encapsulating_to_proxy_etr"
+            }
+        },
+        "10.128.0.0": {
+            "subnet": "/11",
+            "uptime": "4w6d",
+            "expire": "00:04:45",
+            "source": [
+                "map-reply",
+                "forward-native"
+            ],
+            "rloc": {
+                "status": "encapsulating_to_proxy_etr"
+            }
+        },
+        "10.16.0.17": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "13:36:24",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.94",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.18": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "13:40:30",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.65",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.21": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "12:55:06",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.124",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.22": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "20:44:39",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.94",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.24": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "20:44:39",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.94",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.25": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "12:45:50",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.94",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.26": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "12:52:40",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.124",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.27": {
+            "subnet": "/32",
+            "uptime": "00:54:02",
+            "expire": "23:06:55",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.65",
+                "uptime": "00:54:02",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.28": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "20:44:39",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.112",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.29": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "20:44:39",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.138",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.30": {
+            "subnet": "/32",
+            "uptime": "01:56:35",
+            "expire": "22:44:59",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.16",
+                "uptime": "01:56:35",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.31": {
+            "subnet": "/32",
+            "uptime": "03:12:53",
+            "expire": "20:47:07",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.129.17",
+                "uptime": "03:12:53",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.130": {
+            "subnet": "/32",
+            "uptime": "1w1d",
+            "expire": "20:26:54",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.173",
+                "uptime": "1w1d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.131": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "20:44:39",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.135",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.132": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "22:07:00",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.147",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.133": {
+            "subnet": "/32",
+            "uptime": "5d21h",
+            "expire": "02:29:10",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.152",
+                "uptime": "5d21h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.134": {
+            "subnet": "/32",
+            "uptime": "1w6d",
+            "expire": "22:04:32",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.141",
+                "uptime": "1w6d",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.135": {
+            "subnet": "/32",
+            "uptime": "08:39:11",
+            "expire": "15:20:49",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.153",
+                "uptime": "08:39:11",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.136": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:20:44",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.141",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.137": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:20:40",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.178",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.138": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:20:46",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.152",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.139": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:21:32",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.140",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.140": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:20:46",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.140",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.141": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:21:41",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.140",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.142": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:21:29",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.147",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.143": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:20:37",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.152",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.144": {
+            "subnet": "/32",
+            "uptime": "1d08h",
+            "expire": "15:21:28",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.178",
+                "uptime": "1d08h",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        },
+        "10.16.0.145": {
+            "subnet": "/32",
+            "uptime": "08:39:13",
+            "expire": "15:20:46",
+            "source": [
+                "map-reply",
+                "self",
+                "complete"
+            ],
+            "rloc": {
+                "ip": "10.8.128.178",
+                "uptime": "08:39:13",
+                "state": "up",
+                "priority": 10,
+                "weight": 10,
+                "encap_iid": ""
+            }
+        }
+    }
+}
