5df7e932eacfd724a362457a83dd4b86309ef042
Robert Schneider <robert.schneider@networktocode.com>
2020-10-26 10:45:53 -0400

    uploading regex and schema

--- src/genie/libs/parser/iosxe/show_telemetry.py
+++ src/genie/libs/parser/iosxe/show_telemetry.py
@@ -12,7 +12,17 @@ class ShowTelemetryInternalConnectionSchema(MetaParser):
     """Schema for show telemetry internal connection."""
 
     schema = {
-        
+        Optional("index"): {
+            Optional(int): {
+                "peer_address": str,
+                "port": int,
+                Optional("profile"): str,
+                "source_address": str,
+                "state": str,
+                Optional("transport"): str,
+                "vrf": int,
+            },
+        }
     }
 
 
@@ -23,8 +33,71 @@ class ShowTelemetryInternalConnectionSchema(MetaParser):
 class ShowTelemetryInternalConnection(ShowTelemetryInternalConnectionSchema):
     """Parser for show telemetry internal connection"""
 
-    cli_command = ['show telemetry internal connection']
+    cli_command = "show telemetry internal connection"
 
     def cli(self, output=None):
         if output is None:
-            output = self.device.execute(self.cli_command[0])
+            out = self.device.execute(self.cli_command)
+
+        else:
+            out = output
+
+        # Telemetry connection
+
+        # Peer Address    Port  VRF Source Address  Transport  State         Profile
+        # --------------- ----- --- --------------- ---------- ------------- -------------
+        # 10.37.174.7    25103   0 10.8.138.4      tls-native Active        sdn-network-101-wan
+
+        # 10.37.174.7    25103   0 10.8.138.4      tls-native Active        sdn-network-101-wan
+        no_index_capture = re.compile(
+            r"^(?P<peer_address>\d+\.\d+\.\d+\.\d+)\s+(?P<port>\d+)\s+(?P<vrf>\d+)\s+(?P<source_address>\d+\.\d+\.\d+\.\d+)\s+(?P<transport>\S+)\s+(?P<state>\S+)\s+(?P<profile>\S+)$"
+        )
+
+        # 6 10.10.76.186              20830   0 10.64.47.177               Active
+        index_capture = re.compile(
+            r"(?P<index>\d+)\s+(?P<peer_address>\d+\.\d+\.\d+\.\d+)\s+(?P<port>\d+)\s+(?P<vrf>\d+)\s+(?P<source_address>\d+\.\d+\.\d+\.\d+)\s+(?P<state>\S+)"
+        )
+
+        tele_info_obj = {}
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            match = no_index_capture.match(line)
+            if match:
+                group = match.groupdict()
+
+                # convert str to int
+                int_list = ["port", "vrf"]
+                for item in int_list:
+                    group[item] = int(group[item])
+
+                tele_info_obj.update(group)
+
+                continue
+
+            match = index_capture.match(line)
+            if match:
+                group = match.groupdict()
+
+                # convert str to int
+                int_list = ["port", "vrf", "index"]
+                for item in int_list:
+                    group[item] = int(group[item])
+
+                # pull a key from group to use as new_key
+                new_key = "index"
+                new_group = {group[new_key]: {}}
+
+                # update and pop new_key
+                new_group[group[new_key]].update(group)
+                new_group[group[new_key]].pop(new_key)
+
+                if not tele_info_obj.get(new_key):
+                    tele_info_obj = {new_key: {}}
+
+                tele_info_obj[new_key].update(new_group)
+
+                continue
+
+        return tele_info_obj
