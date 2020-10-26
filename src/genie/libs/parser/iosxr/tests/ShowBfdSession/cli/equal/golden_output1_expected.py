dbfc3e5e17deb14bae6d964664fd75220f02dcfe
medward2 <62306152+medward2@users.noreply.github.com>
2020-10-26 15:16:29 +0000

    Create golden_output1_expected.py

---
+++ src/genie/libs/parser/iosxr/tests/ShowBfdSession/cli/equal/golden_output1_expected.py
@@ -0,0 +1,153 @@
+expected_output= {
+    "interface": {
+        "Gi0/0/0/15.103": {
+            "dest_ip_address": {
+                "172.16.100.130": {
+                    "async": "6s",
+                    "async_multiplier": "(2s*3)",
+                    "echo": "450ms",
+                    "echo_multiplier": "(150ms*3)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/15.50": {
+            "dest_ip_address": {
+                "10.100.100.10": {
+                    "async": "6s",
+                    "async_multiplier": "(2s*3)",
+                    "echo": "450ms",
+                    "echo_multiplier": "(150ms*3)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/16.104": {
+            "dest_ip_address": {
+                "172.16.100.66": {
+                    "async": "6s",
+                    "async_multiplier": "(2s*3)",
+                    "echo": "450ms",
+                    "echo_multiplier": "(150ms*3)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/16.200": {
+            "dest_ip_address": {
+                "10.100.100.3": {
+                    "async": "6s",
+                    "async_multiplier": "(2s*3)",
+                    "echo": "45ms",
+                    "echo_multiplier": "(15ms*3)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/26.100": {
+            "dest_ip_address": {
+                "10.0.100.2": {
+                    "async": "900ms",
+                    "async_multiplier": "(300ms*3)",
+                    "echo": "0s",
+                    "echo_multiplier": "(0s*0)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                },
+                "10.0.100.3": {
+                    "async": "600ms",
+                    "async_multiplier": "(200ms*3)",
+                    "echo": "0s",
+                    "echo_multiplier": "(0s*0)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                },
+            }
+        },
+        "Gi0/0/0/26.102": {
+            "dest_ip_address": {
+                "10.150.23.98": {
+                    "async": "600ms",
+                    "async_multiplier": "(200ms*3)",
+                    "echo": "0s",
+                    "echo_multiplier": "(0s*0)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/26.120": {
+            "dest_ip_address": {
+                "10.0.221.98": {
+                    "async": "0s",
+                    "dampening": "DAMP",
+                    "echo": "0s",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "DOWN",
+                }
+            }
+        },
+        "Gi0/0/0/26.900": {
+            "dest_ip_address": {
+                "10.150.180.91": {
+                    "async": "900ms",
+                    "async_multiplier": "(300ms*3)",
+                    "echo": "0s",
+                    "echo_multiplier": "(0s*0)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/26.901": {
+            "dest_ip_address": {
+                "10.150.180.98": {
+                    "async": "900ms",
+                    "async_multiplier": "(300ms*3)",
+                    "echo": "0s",
+                    "echo_multiplier": "(0s*0)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Gi0/0/0/36.902": {
+            "dest_ip_address": {
+                "172.16.100.38": {
+                    "async": "6s",
+                    "async_multiplier": "(2s*3)",
+                    "echo": "450ms",
+                    "echo_multiplier": "(150ms*3)",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "UP",
+                }
+            }
+        },
+        "Te0/5/0/1.400": {
+            "dest_ip_address": {
+                "10.150.100.1": {
+                    "async": "0s",
+                    "echo": "0s",
+                    "hardware": "No",
+                    "npu": "n/a",
+                    "state": "DOWN",
+                }
+            }
+        },
+    }
+}
