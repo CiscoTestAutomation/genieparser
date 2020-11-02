b0d0bcf8062891be6cb0c42b3bbdbc554abd9db6
medward2 <62306152+medward2@users.noreply.github.com>
2020-11-02 09:21:27 +0000

    updated golden output1 file

--- src/genie/libs/parser/iosxr/tests/ShowBfdSession/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxr/tests/ShowBfdSession/cli/equal/golden_output1_expected.py
@@ -1,153 +1,173 @@
-expected_output= {
-    "interface": {
-        "Gi0/0/0/15.103": {
-            "dest_ip_address": {
-                "172.16.100.130": {
-                    "async": "6s",
-                    "async_multiplier": "(2s*3)",
-                    "echo": "450ms",
-                    "echo_multiplier": "(150ms*3)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+expected_output = {
+        "interface": {
+            "GigabitEthernet0/0/0/15.103": {
+                "dest_ip_address": {
+                    "172.16.100.130": {
+                        "async_msec": 2000,
+                        "async_multiplier": 3,
+                        "async_total_msec": 6000,
+                        "echo_msec": 150,
+                        "echo_multiplier": 3,
+                        "echo_total_msec": 450,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/15.50": {
-            "dest_ip_address": {
-                "10.100.100.10": {
-                    "async": "6s",
-                    "async_multiplier": "(2s*3)",
-                    "echo": "450ms",
-                    "echo_multiplier": "(150ms*3)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/15.50": {
+                "dest_ip_address": {
+                    "10.100.100.10": {
+                        "async_msec": 2000,
+                        "async_multiplier": 3,
+                        "async_total_msec": 6000,
+                        "echo_msec": 150,
+                        "echo_multiplier": 3,
+                        "echo_total_msec": 450,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/16.104": {
-            "dest_ip_address": {
-                "172.16.100.66": {
-                    "async": "6s",
-                    "async_multiplier": "(2s*3)",
-                    "echo": "450ms",
-                    "echo_multiplier": "(150ms*3)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/16.104": {
+                "dest_ip_address": {
+                    "172.16.100.66": {
+                        "async_msec": 2000,
+                        "async_multiplier": 3,
+                        "async_total_msec": 6000,
+                        "echo_msec": 150,
+                        "echo_multiplier": 3,
+                        "echo_total_msec": 450,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/16.200": {
-            "dest_ip_address": {
-                "10.100.100.3": {
-                    "async": "6s",
-                    "async_multiplier": "(2s*3)",
-                    "echo": "45ms",
-                    "echo_multiplier": "(15ms*3)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/16.200": {
+                "dest_ip_address": {
+                    "10.100.100.3": {
+                        "async_msec": 2000,
+                        "async_multiplier": 3,
+                        "async_total_msec": 6000,
+                        "echo_msec": 15,
+                        "echo_multiplier": 3,
+                        "echo_total_msec": 45,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/26.100": {
-            "dest_ip_address": {
-                "10.0.100.2": {
-                    "async": "900ms",
-                    "async_multiplier": "(300ms*3)",
-                    "echo": "0s",
-                    "echo_multiplier": "(0s*0)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
-                },
-                "10.0.100.3": {
-                    "async": "600ms",
-                    "async_multiplier": "(200ms*3)",
-                    "echo": "0s",
-                    "echo_multiplier": "(0s*0)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
-                },
-            }
-        },
-        "Gi0/0/0/26.102": {
-            "dest_ip_address": {
-                "10.150.23.98": {
-                    "async": "600ms",
-                    "async_multiplier": "(200ms*3)",
-                    "echo": "0s",
-                    "echo_multiplier": "(0s*0)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/26.100": {
+                "dest_ip_address": {
+                    "10.0.100.2": {
+                        "async_msec": 300,
+                        "async_multiplier": 3,
+                        "async_total_msec": 900,
+                        "echo_msec": 0,
+                        "echo_multiplier": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    },
+                    "10.0.100.3": {
+                        "async_msec": 200,
+                        "async_multiplier": 3,
+                        "async_total_msec": 600,
+                        "echo_msec": 0,
+                        "echo_multiplier": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    },
                 }
-            }
-        },
-        "Gi0/0/0/26.120": {
-            "dest_ip_address": {
-                "10.0.221.98": {
-                    "async": "0s",
-                    "dampening": "DAMP",
-                    "echo": "0s",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "DOWN",
+            },
+            "GigabitEthernet0/0/0/26.102": {
+                "dest_ip_address": {
+                    "10.150.23.98": {
+                        "async_msec": 200,
+                        "async_multiplier": 3,
+                        "async_total_msec": 600,
+                        "echo_msec": 0,
+                        "echo_multiplier": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/26.900": {
-            "dest_ip_address": {
-                "10.150.180.91": {
-                    "async": "900ms",
-                    "async_multiplier": "(300ms*3)",
-                    "echo": "0s",
-                    "echo_multiplier": "(0s*0)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/26.120": {
+                "dest_ip_address": {
+                    "10.0.221.98": {
+                        "async_total_msec": 0,
+                        "dampening": "DAMP",
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "DOWN",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/26.901": {
-            "dest_ip_address": {
-                "10.150.180.98": {
-                    "async": "900ms",
-                    "async_multiplier": "(300ms*3)",
-                    "echo": "0s",
-                    "echo_multiplier": "(0s*0)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/26.900": {
+                "dest_ip_address": {
+                    "10.150.180.91": {
+                        "async_msec": 300,
+                        "async_multiplier": 3,
+                        "async_total_msec": 900,
+                        "echo_msec": 0,
+                        "echo_multiplier": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Gi0/0/0/36.902": {
-            "dest_ip_address": {
-                "172.16.100.38": {
-                    "async": "6s",
-                    "async_multiplier": "(2s*3)",
-                    "echo": "450ms",
-                    "echo_multiplier": "(150ms*3)",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "UP",
+            },
+            "GigabitEthernet0/0/0/26.901": {
+                "dest_ip_address": {
+                    "10.150.180.98": {
+                        "async_msec": 300,
+                        "async_multiplier": 3,
+                        "async_total_msec": 900,
+                        "echo_msec": 0,
+                        "echo_multiplier": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
-        "Te0/5/0/1.400": {
-            "dest_ip_address": {
-                "10.150.100.1": {
-                    "async": "0s",
-                    "echo": "0s",
-                    "hardware": "No",
-                    "npu": "n/a",
-                    "state": "DOWN",
+            },
+            "GigabitEthernet0/0/0/36.902": {
+                "dest_ip_address": {
+                    "172.16.100.38": {
+                        "async_msec": 2000,
+                        "async_multiplier": 3,
+                        "async_total_msec": 6000,
+                        "echo_msec": 150,
+                        "echo_multiplier": 3,
+                        "echo_total_msec": 450,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "UP",
+                    }
                 }
-            }
-        },
+            },
+            "TenGigabitEthernet0/5/0/1.400": {
+                "dest_ip_address": {
+                    "10.150.100.1": {
+                        "async_total_msec": 0,
+                        "echo_total_msec": 0,
+                        "hardware": "No",
+                        "npu": "n/a",
+                        "state": "DOWN",
+                    }
+                }
+            },
+        }
     }
-}
