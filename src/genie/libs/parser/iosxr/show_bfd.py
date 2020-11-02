e26ccd268cf03c5b01e86237b81c37c2832b8474
medward2 <62306152+medward2@users.noreply.github.com>
2020-11-02 09:25:52 +0000

    updated BFD parser

--- src/genie/libs/parser/iosxr/show_bfd.py
+++ src/genie/libs/parser/iosxr/show_bfd.py
@@ -23,10 +23,12 @@ class ShowBfdSessionSchema(MetaParser):
             Any(): {
                 'dest_ip_address': {
                     Any(): {
-                        'echo': str,
-                        Optional('echo_multiplier'): str,
-                        'async': str,
-                        Optional('async_multiplier'): str,
+                        'echo_total_msec': int,
+                        Optional('echo_multiplier'): int,
+                        Optional('echo_msec'): int,
+                        'async_total_msec': int,
+                        Optional('async_multiplier'): int,
+                        Optional('async_msec'): int,
                         'state': str,
                         'hardware': str,
                         'npu': str,
@@ -36,6 +38,9 @@ class ShowBfdSessionSchema(MetaParser):
             }
         }
     }
+
+
+
 
 
 class ShowBfdSession(ShowBfdSessionSchema):
@@ -57,13 +62,13 @@ class ShowBfdSession(ShowBfdSessionSchema):
         result_dict = {}
         #Gi0/0/0/36.52       10.129.196.34   450ms(150ms*3)   6s(2s*3)         UP
         p1 = re.compile(
-            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo>\d+\w+)(?P<echo_multiplier>\(\d+\w+\*\d\)) +(?P<async>\d+\w+)(?P<async_multiplier>\(\d+\w+\*\d\)) +(?P<state>\S+)')
+            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo_total_msec>\d+\w+)\((?P<echo_msec>\d+\w+)\*(?P<echo_multiplier>\d+)\) +(?P<async_total_msec>\d+\w+)\((?P<async_msec>\d+\w+)\*(?P<async_multiplier>\d+)\) +(?P<state>\S+)')
         #Gi0/0/0/26.110      10.0.221.98     0s               0s               DOWN DAMP
         p2 = re.compile(
-            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo>\d+\w+) +(?P<async>\d+\w+) +(?P<state>\S+) ?(?P<damp>\S+)?')
+            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo_total_msec>\d+\w+) +(?P<async_total_msec>\d+\w+) +(?P<state>\S+) ?(?P<damp>\S+)?')
         #BE300               172.16.253.53   n/a              n/a              UP
         p3 = re.compile(
-            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo>\w+\/\w+) +(?P<async>\w+\/\w+) +(?P<state>\S+) ?(?P<damp>\S+)?')
+            r'^(?P<intf>\S+) +(?P<dest>\d+\.\d+\.\d+\.\d+) +(?P<echo_total_msec>\w+\/\w+) +(?P<async_total_msec>\w+\/\w+) +(?P<state>\S+) ?(?P<damp>\S+)?')
         #                                                             No    n/a
         p4 = re.compile(
             r'(?P<hw>[No|Yes]+) +(?P<npu>\S+)')
@@ -79,13 +84,35 @@ class ShowBfdSession(ShowBfdSessionSchema):
 
             if m:
                 group = m.groupdict()
-                interface = group['intf']
+                interface = Common.convert_intf_name(group['intf'])
                 destaddress = group['dest']
                 bfd_dict = result_dict.setdefault('interface',{}).setdefault(interface,{}).setdefault('dest_ip_address',{}).setdefault(destaddress,{})
-                bfd_dict.update({'echo': group['echo']})
-                bfd_dict.update({'echo_multiplier': group['echo_multiplier']})
-                bfd_dict.update({'async': group['async']})
-                bfd_dict.update({'async_multiplier': group['async_multiplier']})
+                if group['echo_total_msec'] and re.findall(r'\d+ms',group['echo_total_msec']) != []:
+                    group['echo_total_msec'] = group['echo_total_msec'].rstrip('ms')
+                    bfd_dict.update({'echo_total_msec': int(group['echo_total_msec'])})
+                else:
+                    group['echo_total_msec'] = int(group['echo_total_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'echo_total_msec': int(group['echo_total_msec'])})
+                if group['echo_msec'] and re.findall(r'\d+ms',group['echo_msec']) != []:
+                    group['echo_msec'] = group['echo_msec'].rstrip('ms')
+                    bfd_dict.update({'echo_msec': int(group['echo_msec'])})
+                else:
+                    group['echo_msec'] = int(group['echo_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'echo_msec': int(group['echo_msec'])})
+                bfd_dict.update({'echo_multiplier': int(group['echo_multiplier'])})
+                if group['async_total_msec'] and re.findall(r'\d+ms',group['async_total_msec']) != []:
+                    group['async_total_msec'] = group['async_total_msec'].rstrip('ms')
+                    bfd_dict.update({'async_total_msec': int(group['async_total_msec'])})
+                else:
+                    group['async_total_msec'] = int(group['async_total_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'async_total_msec': int(group['async_total_msec'])})
+                if group['async_msec'] and re.findall(r'\d+ms',group['async_msec']) != []:
+                    group['async_msec'] = group['async_msec'].rstrip('ms')
+                    bfd_dict.update({'async_msec': int(group['async_msec'])})
+                else:
+                    group['async_msec'] = int(group['async_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'async_msec': int(group['async_msec'])})
+                bfd_dict.update({'async_multiplier': int(group['async_multiplier'])})
                 bfd_dict.update({'state': group['state']})
 
             #Gi0/0/0/26.110      10.0.221.98     0s               0s               DOWN DAMP
@@ -93,11 +120,21 @@ class ShowBfdSession(ShowBfdSessionSchema):
 
             if m:
                 group = m.groupdict()
-                interface = group['intf']
+                interface = Common.convert_intf_name(group['intf'])
                 destaddress = group['dest']
                 bfd_dict = result_dict.setdefault('interface',{}).setdefault(interface,{}).setdefault('dest_ip_address',{}).setdefault(destaddress,{})
-                bfd_dict.update({'echo': group['echo']})
-                bfd_dict.update({'async': group['async']})
+                if group['echo_total_msec'] and re.findall(r'\d+ms',group['echo_total_msec']) != []:
+                    group['echo_total_msec'] = group['echo_total_msec'].rstrip('ms')
+                    bfd_dict.update({'echo_total_msec': int(group['echo_total_msec'])})
+                else:
+                    group['echo_total_msec'] = int(group['echo_total_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'echo_total_msec': int(group['echo_total_msec'])})
+                if group['async_total_msec'] and re.findall(r'\d+ms',group['async_total_msec']) != []:
+                    group['async_total_msec'] = group['async_total_msec'].rstrip('ms')
+                    bfd_dict.update({'async_total_msec': int(group['async_total_msec'])})
+                else:
+                    group['async_total_msec'] = int(group['async_total_msec'].rstrip('s'))*1000
+                    bfd_dict.update({'async_total_msec': int(group['async_total_msec'])})
                 bfd_dict.update({'state': group['state']})
                 if group['damp']:
                     bfd_dict.update({'dampening': group['damp']})
@@ -107,11 +144,11 @@ class ShowBfdSession(ShowBfdSessionSchema):
 
             if m:
                 group = m.groupdict()
-                interface = group['intf']
+                interface = Common.convert_intf_name(group['intf'])
                 destaddress = group['dest']
                 bfd_dict = result_dict.setdefault('interface',{}).setdefault(interface,{}).setdefault('dest_ip_address',{}).setdefault(destaddress,{})
-                bfd_dict.update({'echo': group['echo']})
-                bfd_dict.update({'async': group['async']})
+                bfd_dict.update({'echo_total_msec': group['echo_total_msec']})
+                bfd_dict.update({'async_total_msec': group['async_total_msec']})
                 bfd_dict.update({'state': group['state']})
                 if group['damp']:
                     bfd_dict.update({'dampening': group['damp']})
@@ -126,3 +163,4 @@ class ShowBfdSession(ShowBfdSessionSchema):
                 continue
 
         return result_dict
+
