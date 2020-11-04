c8e115148e6266001d4f08bc758812132fcec007
Robert Schneider <robert.schneider@networktocode.com>
2020-11-04 17:28:37 -0500

    updated schema

--- src/genie/libs/parser/iosxe/tests/ShowLispInstanceIdEthernetServer/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowLispInstanceIdEthernetServer/cli/equal/golden_output_expected.py
@@ -2,94 +2,101 @@ expected_output = {
     "instance_id": {
         4100: {"lisp": 0},
         8188: {
-            "eid_prefix": {
-                "1416.9d28.c100/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.c2c0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.c300/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.c3a0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.ea00/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.eac0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.eda0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d28.fb00/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d29.e680/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d2b.d5c0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d2b.efa0/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d2b.f860/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d2b.f920/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-                "1416.9d2b.fa20/48": {
-                    "inst_id": 8188,
-                    "last_register": "2w1d",
-                    "up": "yes#",
-                    "who_last_registered": "10.8.130.4:61275",
-                },
-            },
             "lisp": 0,
-            "site_name": "site_uci",
+            "site_name": {
+                "site_uci": {
+                    "any-mac": {
+                        "last_register": "never",
+                        "up": "no",
+                        "who_last_registered": "--",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.c100/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.c2c0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.c300/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.c3a0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.ea00/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.eac0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.eda0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d28.fb00/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d29.e680/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d2b.d5c0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d2b.efa0/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d2b.f860/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d2b.f920/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                    "1416.9d2b.fa20/48": {
+                        "last_register": "2w1d",
+                        "up": "yes#",
+                        "who_last_registered": "10.8.130.4:61275",
+                        "inst_id": 8188,
+                    },
+                }
+            },
         },
     }
 }
