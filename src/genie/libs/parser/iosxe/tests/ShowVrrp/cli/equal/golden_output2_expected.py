9a75bcc86af01ead73b0c2315bc7bd41695973f4
Jmahaja1 <61688610+Jmahaja1@users.noreply.github.com>
2020-11-12 11:38:39 +0000

    Add files via upload

---
+++ src/genie/libs/parser/iosxe/tests/ShowVrrp/cli/equal/golden_output2_expected.py
@@ -0,0 +1,21 @@
+expected_output = {
+    "interface": {
+        "GigabitEthernet1": {
+            "group_number": {
+                "Group 100": {
+                    "auth_text": '"hash"',
+                    "device_advertise_interval": 1.0,
+                    "master_adv_int": 1.0,
+                    "master_down_int": 3.55,
+                    "master_ip_address": "192.168.1.233",
+                    "master_router_priority": 120,
+                    "preemption_state": "enabled",
+                    "priority": 115,
+                    "virtual_ip_address": "192.168.10.1",
+                    "virtual_mac_address": "0000.5e00.0164",
+                    "vrrp_state": "Init",
+                }
+            }
+        }
+    }
+}
