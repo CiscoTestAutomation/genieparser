66b9ae1902e0372522a402083e50ce50d67c78aa
Moataz Hammouda <moatazhammouda4@gmail.com>
2020-11-18 11:46:18 +0000

    Modified the show_ip_sla.py file according to reviews suggestions, added regex

--- src/genie/libs/parser/iosxe/show_ip_sla.py
+++ src/genie/libs/parser/iosxe/show_ip_sla.py
@@ -11,6 +11,7 @@ from genie import parsergen
 from genie.metaparser import MetaParser
 from genie.metaparser.util.schemaengine import Any, Or, Optional
 from genie.metaparser.util.schemaengine import Schema
+import re
 
 # ==============================
 # Schema for 'show ip sla summary'
@@ -77,60 +78,53 @@ class ShowIpSlaSummary(ShowIpSlaSummarySchema):
                 for id, id_dict in struct_output.items():
                     if id:
                         # Defining patterns to be matched for different columns
-                # The patterns for the state of the ip sla probe defined by the Codes: * active, ^ inactive, ~ pending
-                active_pattern = re.compile(r'\*(\S+)')
-                inactive_pattern = re.compile(r'\^(\S+)')
-                pending_pattern = re.compile(r'\~(\S+)')
-                
-                # The pattern for the last_run_seconds_ago collumn
-                last_run_pattern = re.compile(r'(\S+) sec\S+')
+                        # The patterns for the state of the ip sla probe defined by the Codes: * active, ^ inactive, ~ pending
+                        active_pattern = re.compile(r'\*(\S+)')
+                        inactive_pattern = re.compile(r'\^(\S+)')
+                        pending_pattern = re.compile(r'\~(\S+)')
+                        
+                        # The pattern for the last_run_seconds_ago collumn
+                        last_run_pattern = re.compile(r'(\S+) sec\S+')
 
-                # The pattern for the rtt_stats_mseconds (for milliseconds) 
-                # The first pattern is for normal RTT denoted in milliseconds and therefore would not require any further processing
-                # The second pattern is for RTT denoted in microseconds and for standardisation purposes will be converted to milliseconds 
-                rtt_pattern_milliseconds = re.compile(r'RTT=(\d{1,4})')
-                rtt_pattern_microseconds = re.compile(r'RTT=(\d{1,4})u')
+                        # The pattern for the rtt_stats_mseconds (for milliseconds) 
+                        # The first pattern is for normal RTT denoted in milliseconds and therefore would not require any further processing
+                        # The second pattern is for RTT denoted in microseconds and for standardisation purposes will be converted to milliseconds 
+                        rtt_pattern_milliseconds = re.compile(r'RTT=(\d{1,4})')
+                        rtt_pattern_microseconds = re.compile(r'RTT=(\d{1,4})u')
 
-                # Setting the probe_id and the codes values
-                if active_pattern.match(id):
-                    probe_id = active_pattern.match(id).group(1)
-                    id_dict['probe_status'] = 'active'
-                elif inactive_pattern.match(id):
-                    probe_id = inactive_pattern.match(id).group(1)
-                    id_dict['probe_status'] = 'inactive'
-                elif pending_pattern.match(id):
-                    probe_id = pending_pattern.match(id).group(1)
-                    id_dict['probe_status'] = 'pending'
-                del id_dict['id']
+                        # Setting the probe_id and the codes values
+                        if active_pattern.match(id):
+                            probe_id = active_pattern.match(id).group(1)
+                            id_dict['probe_status'] = 'active'
+                        elif inactive_pattern.match(id):
+                            probe_id = inactive_pattern.match(id).group(1)
+                            id_dict['probe_status'] = 'inactive'
+                        elif pending_pattern.match(id):
+                            probe_id = pending_pattern.match(id).group(1)
+                            id_dict['probe_status'] = 'pending'
+                        del id_dict['id']
 
-                # Setting the rtt_stats_mseconds column value
-                # If the value is in milliseconds
-                if rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']):
-                    id_dict['rtt_stats_mseconds'] = rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']).group(1)
-                # If the value is in microseconds
-                elif rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']):
-                    id_dict['rtt_stats_mseconds'] = int(rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']).group(1))/1000
-                # If the value is a dash, no further processing is needed
-                else:
-                    pass
+                        # Setting the rtt_stats_mseconds column value
+                        # If the value is in milliseconds
+                        if rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']):
+                            id_dict['rtt_stats_mseconds'] = rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']).group(1)
+                        # If the value is in microseconds
+                        elif rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']):
+                            id_dict['rtt_stats_mseconds'] = int(rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']).group(1))/1000
+                        # If the value is a dash, no further processing is needed
+                        else:
+                            pass
 
-                # Setting the value for the last_run_seconds_ago collumn
-                if last_run_pattern.match(id_dict['last_run_seconds_ago']):
-                    id_dict['last_run_seconds_ago'] = last_run_pattern.match(id_dict['last_run_seconds_ago']).group(1)
-                
-                #if 'ago' not in id_dict['last_run']:
-                    #id_dict['last_run'] = id_dict['last_run'] + 'o'
-                """ if 'RTT=' in rtt:
-                    rtt_updated = rtt.split('RTT=')[1]
-                    id_dict['rtt_stats'] = rtt_updated
-                else:
-                    pass """
-                
-                parsed_dict.setdefault('id',{}).update({probe_id: id_dict})
-            else:
-                # This else clause is added to mitigate the limitation that sometimes the cli output line for this command
-                # may include just the letter 'o' as it would be cut from the Last Run column due to width limitation.
-                pass
+                        # Setting the value for the last_run_seconds_ago collumn
+                        if last_run_pattern.match(id_dict['last_run_seconds_ago']):
+                            id_dict['last_run_seconds_ago'] = last_run_pattern.match(id_dict['last_run_seconds_ago']).group(1)
+                        
+                        
+                        parsed_dict.setdefault('id',{}).update({probe_id: id_dict})
+                    else:
+                        # This else clause is added to mitigate the limitation that sometimes the cli output line for this command
+                        # may include just the letter 'o' as it would be cut from the Last Run column due to width limitation.
+                        pass
 
         return parsed_dict
 
