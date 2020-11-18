b646c640af8542ebcd2fe0d724796fe492ee0e11
Moataz Hammouda <moatazhammouda4@gmail.com>
2020-11-18 11:38:36 +0000

    Modified the schema and parser classes according to reviews

--- src/genie/libs/parser/iosxe/show_ip_sla.py
+++ src/genie/libs/parser/iosxe/show_ip_sla.py
@@ -18,15 +18,16 @@ from genie.metaparser.util.schemaengine import Schema
 
 class ShowIpSlaSummarySchema(MetaParser):
     ''' Schema for "show ip sla summary" '''
-    schema ={'probes':
+    schema ={'id':
     {Any(): 
     {
         Optional('id'): str,
+        Optional('probe_status'): str,
         Optional('type'): str,
         Optional('destination'): str,
-        Optional('rtt_stats'): str,
+        Optional('rtt_stats_mseconds'): str,
         Optional('return_code'): str,
-        Optional('last_run'): str
+        Optional('last_run_seconds_ago'): str
     },
     },
     }
@@ -70,26 +71,66 @@ class ShowIpSlaSummary(ShowIpSlaSummarySchema):
 
         if out:
             headers = [['ID','Type','Destination','Stats','Return','Last'],['','','','','Code','Run']]
-            result = parsergen.oper_fill_tabular(device_output = out, header_fields=headers, label_fields = ['id','type','destination','rtt_stats','return_code','last_run'])
+            result = parsergen.oper_fill_tabular(device_output = out, header_fields=headers, label_fields = ['id','type','destination','rtt_stats_mseconds','return_code','last_run_seconds_ago'])
             struct_output = result.entries
             if struct_output:
                 for id, id_dict in struct_output.items():
                     if id:
-                        probe_id = id.split('*')[1]
-                        del id_dict['id']
-                        rtt = id_dict['rtt_stats']
-                        if 'ago' not in id_dict['last_run']:
-                            id_dict['last_run'] = id_dict['last_run'] + 'o'
-                        if 'RTT=' in rtt:
-                            rtt_updated = rtt.split('RTT=')[1]
-                            id_dict['rtt_stats'] = rtt_updated
-                        else:
-                            pass
-                        parsed_dict.setdefault('probes',{}).update({probe_id: id_dict})
-                    else:
-                        # This else clause is added to mitigate the limitation that sometimes the cli output line for this command
-                        # may include just the letter 'o' as it would be cut from the Last Run column due to width limitation.
-                        pass
+                        # Defining patterns to be matched for different columns
+                # The patterns for the state of the ip sla probe defined by the Codes: * active, ^ inactive, ~ pending
+                active_pattern = re.compile(r'\*(\S+)')
+                inactive_pattern = re.compile(r'\^(\S+)')
+                pending_pattern = re.compile(r'\~(\S+)')
+                
+                # The pattern for the last_run_seconds_ago collumn
+                last_run_pattern = re.compile(r'(\S+) sec\S+')
+
+                # The pattern for the rtt_stats_mseconds (for milliseconds) 
+                # The first pattern is for normal RTT denoted in milliseconds and therefore would not require any further processing
+                # The second pattern is for RTT denoted in microseconds and for standardisation purposes will be converted to milliseconds 
+                rtt_pattern_milliseconds = re.compile(r'RTT=(\d{1,4})')
+                rtt_pattern_microseconds = re.compile(r'RTT=(\d{1,4})u')
+
+                # Setting the probe_id and the codes values
+                if active_pattern.match(id):
+                    probe_id = active_pattern.match(id).group(1)
+                    id_dict['probe_status'] = 'active'
+                elif inactive_pattern.match(id):
+                    probe_id = inactive_pattern.match(id).group(1)
+                    id_dict['probe_status'] = 'inactive'
+                elif pending_pattern.match(id):
+                    probe_id = pending_pattern.match(id).group(1)
+                    id_dict['probe_status'] = 'pending'
+                del id_dict['id']
+
+                # Setting the rtt_stats_mseconds column value
+                # If the value is in milliseconds
+                if rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']):
+                    id_dict['rtt_stats_mseconds'] = rtt_pattern_milliseconds.match(id_dict['rtt_stats_mseconds']).group(1)
+                # If the value is in microseconds
+                elif rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']):
+                    id_dict['rtt_stats_mseconds'] = int(rtt_pattern_microseconds.match(id_dict['rtt_stats_mseconds']).group(1))/1000
+                # If the value is a dash, no further processing is needed
+                else:
+                    pass
+
+                # Setting the value for the last_run_seconds_ago collumn
+                if last_run_pattern.match(id_dict['last_run_seconds_ago']):
+                    id_dict['last_run_seconds_ago'] = last_run_pattern.match(id_dict['last_run_seconds_ago']).group(1)
+                
+                #if 'ago' not in id_dict['last_run']:
+                    #id_dict['last_run'] = id_dict['last_run'] + 'o'
+                """ if 'RTT=' in rtt:
+                    rtt_updated = rtt.split('RTT=')[1]
+                    id_dict['rtt_stats'] = rtt_updated
+                else:
+                    pass """
+                
+                parsed_dict.setdefault('id',{}).update({probe_id: id_dict})
+            else:
+                # This else clause is added to mitigate the limitation that sometimes the cli output line for this command
+                # may include just the letter 'o' as it would be cut from the Last Run column due to width limitation.
+                pass
 
         return parsed_dict
 
