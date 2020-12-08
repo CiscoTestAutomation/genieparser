d4e7f7495a5b309684426252609e76ef242a6625
“Ernesto <bleuzkernel@gmail.com>
2020-12-08 12:20:49 -0600

    2020-12-08T12:20:49-06:00_ADD_IOSXR_Genie_Parser_ShowRouteAllSummary

---
+++ src/genie/libs/parser/iosxr/tests/ShowRouteAllSummary/cli/equal/golden_output5_expected.py
@@ -0,0 +1,192 @@
+expected_output = {
+   "vrf": {
+      "default": {
+         "address_family": {
+            "ipv4_unicast": {
+               "route_source": {
+                  "connected": {
+                     "routes": 45,
+                     "backup": 8,
+                     "deleted": 0,
+                     "memory_bytes": 8480
+                  },
+                  "local": {
+                     "routes": 53,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 8480
+                  },
+                  "static": {
+                     "routes": 125,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 20072
+                  },
+                  "application": {
+                     "Service-layer": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     }
+                  },
+                  "ospf": {
+                     "10": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     },
+                     "ID_001": {
+                        "routes": 1218,
+                        "backup": 18,
+                        "deleted": 0,
+                        "memory_bytes": 204168
+                     },
+                     "ID_Zero": {
+                        "routes": 13,
+                        "backup": 2,
+                        "deleted": 0,
+                        "memory_bytes": 2400
+                     },
+                     "101": {
+                        "routes": 47,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 7520
+                     },
+                     "1": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     }
+                  },
+                  "dagr": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  },
+                  "bgp": {
+                     "1": {
+                        "routes": 16,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 2560
+                     }
+                  },
+                  "isis": {
+                     "IGP": {
+                        "routes": 199,
+                        "backup": 1,
+                        "deleted": 0,
+                        "memory_bytes": 32000
+                     }
+                  }
+               },
+               "total_route_source": {
+                  "routes": 1716,
+                  "backup": 29,
+                  "deleted": 0,
+                  "memory_bytes": 285680
+               }
+            },
+            "ipv4_multicast": {
+               "route_source": {
+                  "connected": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  },
+                  "local": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  },
+                  "isis": {
+                     "IGP": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     }
+                  }
+               },
+               "total_route_source": {
+                  "routes": 0,
+                  "backup": 0,
+                  "deleted": 0,
+                  "memory_bytes": 0
+               }
+            },
+            "ipv6_unicast": {
+               "route_source": {
+                  "local": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  },
+                  "connected": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0,
+                     "l2tpv3_xconnect": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     }
+                  },
+                  "local-srv6": {
+                     "isis-IGP": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     },
+                     "bgp-177": {
+                        "routes": 0,
+                        "backup": 0,
+                        "deleted": 0,
+                        "memory_bytes": 0
+                     }
+                  }
+               },
+               "total_route_source": {
+                  "routes": 0,
+                  "backup": 0,
+                  "deleted": 0,
+                  "memory_bytes": 0
+               }
+            },
+            "ipv6_multicast": {
+               "route_source": {
+                  "local": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  },
+                  "connected": {
+                     "routes": 0,
+                     "backup": 0,
+                     "deleted": 0,
+                     "memory_bytes": 0
+                  }
+               },
+               "total_route_source": {
+                  "routes": 0,
+                  "backup": 0,
+                  "deleted": 0,
+                  "memory_bytes": 0
+               }
+            }
+         }
+      }
+   }
+}
