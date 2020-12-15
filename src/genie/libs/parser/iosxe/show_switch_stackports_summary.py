ec2856145daf5babc1c03a44b0fbb7c068eef7df
Spiro Mitsialis <spiro.mitsialis@mcgill.ca>
2020-12-15 16:23:12 -0500

    Fixed IOSXE 'show switch stack-ports summary'

--- src/genie/libs/parser/iosxe/show_switch_stackports_summary.py
+++ src/genie/libs/parser/iosxe/show_switch_stackports_summary.py
@@ -1,4 +1,4 @@
-''' show_switch_stackports_sum.py
+''' show_switch_stackports_summary.py
 
 
 '''
@@ -12,21 +12,6 @@ from genie.libs.parser.utils.common import Common
 
 class ShowSwitchStackPortsSummarySchema(MetaParser):
     """Schema for ShowSwitchStackPortsSummary"""
-    # schema = {
-    #     'index': {
-    #         Any(): {
-    #             'switch_port': str,
-    #             'port_status': str,
-    #             'neighbor': str,
-    #             'cable_length': str,
-    #             'link_ok': str,
-    #             'link_active': str,
-    #             'sync_ok': str,
-    #             'link_changes_count': str,
-    #             'in_loopback': str,
-    #         }
-    #     }
-    # } ]
 
     schema = {
         'stackports': {
@@ -88,23 +73,6 @@ class ShowSwitchStackPortsSummary(ShowSwitchStackPortsSummarySchema):
       
             m = p1.match(line)
             if m:
-            # Works with index
-                # group = m.groupdict()
-                # index += 1
-
-                # index_dict = ret_dict.setdefault('index', {}).setdefault(index, {})
-
-                # index_dict.update({'stackport_id': group['stackport_id']})
-                # index_dict.update({'port_status': group['port_status']})
-                # index_dict.update({'neighbor': group['neighbor']})
-                # index_dict.update({'cable_length': group['cable_length']})
-                # index_dict.update({'link_ok': group['link_ok']})
-                # index_dict.update({'link_active': group['link_active']})
-                # index_dict.update({'sync_ok': group['sync_ok']})
-                # index_dict.update({'link_changes_count': group['link_changes_count']})
-                # index_dict.update({'in_loopback': group['in_loopback']})
-
-            # try without index
                 stackport_id = m.groupdict()['stackport_id']
                 if 'stackports' not in ret_dict:
                     ret_dict['stackports'] = {}
