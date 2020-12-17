b98d7ec929e9c51c3dbdc759eac0e01e4cdf0a2b
Xiaoxin Zhou -X (xiaoxinz - ALBANY SERVICES  INC at Cisco) <xiaoxinz@cisco.com>
2020-12-17 00:48:12 -0500

    update a few keys into Optional in ShowDdosProtectionStatistics(junos)

--- src/genie/libs/parser/junos/tests/test_show_ddos.py
+++ src/genie/libs/parser/junos/tests/test_show_ddos.py
@@ -59,6 +59,37 @@ class TestShowDdosProtectionStatistics(unittest.TestCase):
             }
         }
 
+    golden_output_2 = {'execute.return_value':'''
+     DDOS protection global statistics:
+        Policing on routing engine:         Yes
+        Policing on FPC:                    Yes
+        Flow detection:                     No
+        Logging:                            Yes
+        Policer violation report rate:      100
+        Flow report rate:                   100
+        Currently violated packet types:    0
+        Packet types have seen violations:  6
+        Total violation counts:             19
+        Currently tracked flows:            0
+        Total detected flows:               0    
+    '''}
+
+    golden_parsed_output_2 = {
+        "ddos-statistics-information": {
+            "ddos-flow-detection-enabled": "No",
+            "ddos-logging-enabled": "Yes",
+            "ddos-policing-fpc-enabled": "Yes",
+            "ddos-policing-re-enabled": "Yes",
+            "flow-report-rate": "100",
+            "flows-cumulative": "0",
+            "flows-current": "0",
+            "packet-types-in-violation": "0",
+            "packet-types-seen-violation": "6",
+            "total-violations": "19",
+            "violation-report-rate": "100"
+        }
+    }
+
 
     def test_empty(self):
         self.device = Mock(**self.empty_output)
@@ -70,7 +101,13 @@ class TestShowDdosProtectionStatistics(unittest.TestCase):
         self.device = Mock(**self.golden_output)
         obj = ShowDdosProtectionStatistics(device=self.device)
         parsed_output = obj.parse()
-        self.assertEqual(parsed_output, self.golden_parsed_output)    
+        self.assertEqual(parsed_output, self.golden_parsed_output) 
+
+    def test_golden_2(self):
+        self.device = Mock(**self.golden_output_2)
+        obj = ShowDdosProtectionStatistics(device=self.device)
+        parsed_output = obj.parse()
+        self.assertEqual(parsed_output, self.golden_parsed_output_2)            
 
 if __name__ == '__main__':
     unittest.main()        
