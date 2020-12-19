1598d3813c7436630cf0ab41a6db7f8eeae14060
KnoxHutchinson <knox@knoxsdata.com>
2020-12-19 18:33:54 +0000

    added unit tests and changelog

---
+++ src/genie/libs/parser/dellos6/tests/ShowIPInterface/cli/equal/golden_output1_expected.py
@@ -0,0 +1,18 @@
+expected_output = {
+    'ints': {
+        'gateway': '0.0.0.0',
+        'L3_MAC': 'F8B1.5683.8734',
+        'Vl1': {
+            'state': 'Down',
+            'ip_address': '0.0.0.0',
+            'mask': '0.0.0.0',
+            'method': 'DHCP'
+        },
+        'Vl20': {
+            'state': 'Up',
+            'ip_address': '10.10.21.70',
+            'mask': '255.255.255.0',
+            'method': 'DHCP'
+        }
+    }
+}
