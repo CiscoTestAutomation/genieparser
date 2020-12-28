c1d64407f5bdb27e6e592a8fc4a5fa9692cf509a
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2020-12-28 12:53:23 +1000

    [ironware] ShowIPRoute Parser Updates & Unittest - Added unittesting for IronWare ShowIPRoute - Further error handling in ShowIPRoute (e.g. handling equal cost paths & the lack of terminal width in Brocade) - Restructured Schema

--- src/genie/libs/parser/ironware/show_routing.py
+++ src/genie/libs/parser/ironware/show_routing.py
@@ -23,13 +23,18 @@ class ShowIPRouteSchema(MetaParser):
             Any(): {
                 'network': str,
                 'cidr': int,
-                'gateway': str,
-                'cost': str,
-                'type': str,
-                'uptime': str,
-                'vrf': str
+                'via': {
+                    Any(): {
+                    'cost': str,
+                    'interface': str,
+                    'type': str,
+                    'uptime': str,
+                    'src-vrf': str
+                    }
+                }
             }
-        }
+        },
+        'total_routes': int
     }
 
 # ====================================================
@@ -42,7 +47,7 @@ class ShowIPRoute(ShowIPRouteSchema):
     cli_command = 'show ip route'
 
     """
-    Total number of IP routes: 449
+    Total number of IP routes: 13
     Type Codes - B:BGP D:Connected I:ISIS O:OSPF R:RIP S:Static; Cost - Dist/Metric
     BGP  Codes - i:iBGP e:eBGP
     ISIS Codes - L1:Level-1 L2:Level-2
@@ -59,6 +64,12 @@ class ShowIPRoute(ShowIPRouteSchema):
     8       10.200.0.73/32     10.254.248.10   eth 2/2        110/73        O    40d0h  -
     9       10.200.0.80/30     10.254.248.10   eth 2/2        110/76        O    1d1h   -
     10      10.200.0.85/32     10.254.248.10   eth 2/2        110/27        O    40d0h  -
+    11     200.200.200.200/32   DIRECT          loopback 1     0/0           D    248d   
+    -
+    12      1.1.1.1/32         10.254.251.2    eth 5/1       110/52        O    15h47m -
+            1.1.1.1/32         10.254.251.108  eth 7/1       110/52        O    15h47m -
+    13      2.2.2.2/32         10.254.251.2    eth 5/1       110/42        O    15h47m -
+            2.2.2.2/32         10.254.251.108  eth 7/1       110/42        O    15h47m -
     """
 
     def cli(self, output=None):
@@ -71,35 +82,86 @@ class ShowIPRoute(ShowIPRouteSchema):
         result_dict = {}
 
         p0 = re.compile(
-            r'((?P<number>^\d+)\s+(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
-            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(?P<port>eth\s\d{1,2}\/\d{1,2})\s+ '
-            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w)\s+(?P<uptime>\w+)\s+(?P<vrf>[\w+|-]))')
+            r'(^Total number of IP routes:\s+(?P<total>\d+))'
+        )
+
+        p1 = re.compile(
+            r'((?P<number>^\d+)\s+ '
+            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
+            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+(?P<vrf>[\w+|-]))'
+        )
+
+        # Used when src-vrf output wraps to the next line, Brocade has no terminal-width
+        p2 = re.compile(
+            r'((?P<number>^\d+)\s+ '
+            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
+            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)$)'
+        )
+
+        # Used when equal cost paths exist
+        p3 = re.compile(
+            r'(^(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
+            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+(?P<vrf>[\w+|-]))'
+        )
 
         for line in out.splitlines():
             line = line.strip()
 
             m = p0.match(line)
             if m:
-                number = m.groupdict()['number']
+                ip_dict['total_routes'] = int(m.groupdict()['total'])
+
+            m = p1.match(line)
+            n = p2.match(line)
+
+            # Will only even match on one as EOL provided in P2
+            if m or n:
+                if m:
+                    m = m
+                if n:
+                    m = n
+
                 network = m.groupdict()['network']
-                cidr = m.groupdict()['cidr']
-                gateway = m.groupdict()['gateway']
-                cost = m.groupdict()['cost']
-                route_type = m.groupdict()['type']
-                uptime = m.groupdict()['uptime']
-                vrf = m.groupdict()['vrf']
+                cidr = int(m.groupdict()['cidr'])
+                srcvrf = m.groupdict().get('vrf')
 
                 if 'routes' not in ip_dict:
                     result_dict = ip_dict.setdefault('routes', {})
 
-                result_dict[network + '/' + cidr] = {
+                result_dict['{0}/{1}'.format(network, cidr)] = {
                   'network': network,
-                  'cidr': cidr, 
-                  'gateway': gateway,
-                  'cost': cost,
-                  'type': route_type,
-                  'uptime': uptime,
-                  'vrf': vrf if vrf != '-' else None
+                  'cidr': cidr,
+                  'via': {
+                      m.groupdict()['gateway']: {
+                          'interface': m.groupdict()['port'],
+                          'cost': m.groupdict()['cost'],
+                          'type': m.groupdict()['type'],
+                          'uptime': m.groupdict()['uptime'],
+                          'src-vrf': srcvrf if srcvrf != None else 'Unknown'
+                      }
+                  }
                 }
                 continue
+
+            m = p3.match(line)
+            if m:
+                network = m.groupdict()['network']
+                cidr = int(m.groupdict()['cidr'])
+                route = '{0}/{1}'.format(network, cidr)
+
+                result_dict[route]['via'][m.groupdict()['gateway']] = {
+                    'interface': m.groupdict()['port'],
+                    'cost': m.groupdict()['cost'],
+                    'type': m.groupdict()['type'],
+                    'uptime': m.groupdict()['uptime'],
+                    'src-vrf': srcvrf if srcvrf != None else 'Unknown'
+                }
+                continue
+                
         return ip_dict
