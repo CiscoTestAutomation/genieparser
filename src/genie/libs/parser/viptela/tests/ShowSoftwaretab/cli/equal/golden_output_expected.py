2fcff2a9a55a2a019330f8d40725021f9111a74e
itdependsnetworks <ken@celenza.org>
2020-12-28 13:38:17 -0500

    Add viptela to folder based tests

---
+++ src/genie/libs/parser/viptela/tests/ShowSoftwaretab/cli/equal/golden_output_expected.py
@@ -0,0 +1,25 @@
+expected_output = {
+    "version": {
+        "10.106.1.0.12": {
+            "active": "false",
+            "confirmed": "user",
+            "default": "false",
+            "previous": "true",
+            "timestamp": "2020-03-30T02:15:00-00:00",
+        },
+        "10.106.1.0.32": {
+            "active": "true",
+            "confirmed": "user",
+            "default": "false",
+            "previous": "false",
+            "timestamp": "2020-04-11T09:43:37-00:00",
+        },
+        "10.106.2.0.1857": {
+            "active": "false",
+            "confirmed": "auto",
+            "default": "true",
+            "previous": "false",
+            "timestamp": "2020-03-30T02:13:24-00:00",
+        },
+    }
+}
