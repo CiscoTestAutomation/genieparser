5b9b44a4746abed8ca719747b987a1a86f506a97
James Di Trapani <51264648+jamesditrapani@users.noreply.github.com>
2020-12-29 16:17:14 +1000

    [ironware] Parser PEP8 Cleanup - Bring files into PEP8 compliance - Shorted line length - Cleaned up whitespace - Fixed indentation - etc.

--- src/genie/libs/parser/ironware/show_routing.py
+++ src/genie/libs/parser/ironware/show_routing.py
@@ -18,6 +18,7 @@ import re
 
 __author__ = 'James Di Trapani <james@ditrapani.com.au>'
 
+
 # ======================================================
 # Schema for 'show ip route'
 # ======================================================
@@ -30,17 +31,18 @@ class ShowIPRouteSchema(MetaParser):
                 'cidr': int,
                 'via': {
                     Any(): {
-                    'cost': str,
-                    'interface': str,
-                    'type': str,
-                    'uptime': str,
-                    'src-vrf': str
+                        'cost': str,
+                        'interface': str,
+                        'type': str,
+                        'uptime': str,
+                        'src-vrf': str
                     }
                 }
             }
         },
         'total_routes': int
     }
+
 
 # ====================================================
 #  parser for 'show ip route'
@@ -52,29 +54,29 @@ class ShowIPRoute(ShowIPRouteSchema):
     cli_command = 'show ip route'
 
     """
-    Total number of IP routes: 13
-    Type Codes - B:BGP D:Connected I:ISIS O:OSPF R:RIP S:Static; Cost - Dist/Metric
-    BGP  Codes - i:iBGP e:eBGP
-    ISIS Codes - L1:Level-1 L2:Level-2
-    OSPF Codes - i:Inter Area 1:External Type 1 2:External Type 2 s:Sham Link
-    STATIC Codes - d:DHCPv6
-            Destination        Gateway         Port           Cost          Type Uptime src-vrf
-    1       10.200.0.12/30     10.254.248.10   eth 2/2        110/28        O    40d0h  -
-    2       10.200.0.16/30     10.254.248.10   eth 2/2        110/194       O    6d12h  -
-    3       10.200.0.28/30     10.254.248.10   eth 2/2        110/138       O    40d0h  -
-    4       10.200.0.32/30     10.254.248.10   eth 2/2        110/36        O    40d0h  -
-    5       10.200.0.56/30     10.254.248.10   eth 2/2        110/36        O    12d4h  -
-    6       10.200.0.60/30     10.254.248.10   eth 2/2        110/38        O    40d0h  -
-    7       10.200.0.65/32     10.254.248.10   eth 2/2        110/63        O    40d0h  -
-    8       10.200.0.73/32     10.254.248.10   eth 2/2        110/73        O    40d0h  -
-    9       10.200.0.80/30     10.254.248.10   eth 2/2        110/76        O    1d1h   -
-    10      10.200.0.85/32     10.254.248.10   eth 2/2        110/27        O    40d0h  -
-    11     200.200.200.200/32   DIRECT          loopback 1     0/0           D    248d   
-    -
-    12      1.1.1.1/32         10.254.251.2    eth 5/1       110/52        O    15h47m -
-            1.1.1.1/32         10.254.251.108  eth 7/1       110/52        O    15h47m -
-    13      2.2.2.2/32         10.254.251.2    eth 5/1       110/42        O    15h47m -
-            2.2.2.2/32         10.254.251.108  eth 7/1       110/42        O    15h47m -
+Total number of IP routes: 13
+Type Codes - B:BGP D:Connected I:ISIS O:OSPF R:RIP S:Static; Cost -Dist/Metric
+BGP  Codes - i:iBGP e:eBGP
+ISIS Codes - L1:Level-1 L2:Level-2
+OSPF Codes - i:Inter Area 1:External Type 1 2:External Type 2 s:Sham Link
+STATIC Codes - d:DHCPv6
+    Destination        Gateway         Port      Cost      Type Uptime src-vrf
+1   10.200.0.12/30     10.254.248.10   eth 2/2   110/28     O   40d0h  -
+2   10.200.0.16/30     10.254.248.10   eth 2/2   110/194    O   6d12h  -
+3   10.200.0.28/30     10.254.248.10   eth 2/2   110/138    O   40d0h  -
+4   10.200.0.32/30     10.254.248.10   eth 2/2   110/36     O   40d0h  -
+5   10.200.0.56/30     10.254.248.10   eth 2/2   110/36     O   12d4h  -
+6   10.200.0.60/30     10.254.248.10   eth 2/2   110/38     O   40d0h  -
+7   10.200.0.65/32     10.254.248.10   eth 2/2   110/63     O   40d0h  -
+8   10.200.0.73/32     10.254.248.10   eth 2/2   110/73     O   40d0h  -
+9   10.200.0.80/30     10.254.248.10   eth 2/2   110/76     O   1d1h   -
+10  10.200.0.85/32     10.254.248.10   eth 2/2   110/27     O   40d0h  -
+11  200.200.200.200/32   DIRECT        loopback 1 0/0       D   248d
+-
+12  1.1.1.1/32         10.254.251.2    eth 5/1    110/52     O   15h47m -
+    1.1.1.1/32         10.254.251.108  eth 7/1    110/52     O   15h47m -
+13  2.2.2.2/32         10.254.251.2    eth 5/1    110/42     O   15h47m -
+    2.2.2.2/32         10.254.251.108  eth 7/1    110/42     O   15h47m -
     """
 
     def cli(self, output=None):
@@ -91,28 +93,34 @@ class ShowIPRoute(ShowIPRouteSchema):
         )
 
         p1 = re.compile(
-            r'((?P<number>^\d+)\s+ '
-            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
-            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
-            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
-            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+(?P<vrf>[\w+|-]))'
+            r'((?P<number>^\d+)\s+'
+            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/'
+            r'(?P<cidr>\d{1,2})\s+'
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+'
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+'
+            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+'
+            r'(?P<vrf>[\w+|-]))'
         )
 
-        # Used when src-vrf output wraps to the next line, Brocade has no terminal-width
+        # Used when src-vrf output wraps to the next line,
+        # Brocade has no terminal-width
         p2 = re.compile(
-            r'((?P<number>^\d+)\s+ '
-            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
-            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
-            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
+            r'((?P<number>^\d+)\s+'
+            r'(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/'
+            r'(?P<cidr>\d{1,2})\s+'
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+'
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+'
             r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)$)'
         )
 
         # Used when equal cost paths exist
         p3 = re.compile(
-            r'(^(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/(?P<cidr>\d{1,2})\s+ '
-            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+ '
-            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+ '
-            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+(?P<vrf>[\w+|-]))'
+            r'(^(?P<network>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\/'
+            r'(?P<cidr>\d{1,2})\s+'
+            r'(?P<gateway>\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}|DIRECT)\s+'
+            r'(?P<port>eth\s\d{1,2}\/\d{1,2}|loopback\s\d{1,3})\s+'
+            r'(?P<cost>\w+\/\w+)\s+(?P<type>\w+)\s+(?P<uptime>\w+)\s+'
+            r'(?P<vrf>[\w+|-]))'
         )
 
         for line in out.splitlines():
@@ -144,7 +152,7 @@ class ShowIPRoute(ShowIPRouteSchema):
                           'cost': m.groupdict()['cost'],
                           'type': m.groupdict()['type'],
                           'uptime': m.groupdict()['uptime'],
-                          'src-vrf': srcvrf if srcvrf != None else 'Unknown'
+                          'src-vrf': srcvrf if srcvrf is not None else 'Unknown'
                       }
                   }
                 }
@@ -161,8 +169,8 @@ class ShowIPRoute(ShowIPRouteSchema):
                     'cost': m.groupdict()['cost'],
                     'type': m.groupdict()['type'],
                     'uptime': m.groupdict()['uptime'],
-                    'src-vrf': srcvrf if srcvrf != None else 'Unknown'
+                    'src-vrf': srcvrf if srcvrf is not None else 'Unknown'
                 }
                 continue
-                
+
         return ip_dict
--- src/genie/libs/parser/ironware/tests/test_show_mpls_lsp.py
+++ src/genie/libs/parser/ironware/tests/test_show_mpls_lsp.py
@@ -31,52 +31,52 @@ class test_show_mpls_lsp(unittest.TestCase):
 
     empty_output = {'execute.return_value': ''}
     golden_parsed_output_brief = expected_output = {
-    'lsps': {
-        'mlx8.1_to_ces.2': {
-        'destination': '1.1.1.1',
-        'admin': 'UP',
-        'operational': 'UP',
-        'flap_count': 1,
-        'retry_count': 0,
-        'tunnel_interface': 'tnl0'
-        },
-        'mlx8.1_to_ces.1': {
-        'destination': '2.2.2.2',
-        'admin': 'UP',
-        'operational': 'UP',
-        'flap_count': 1,
-        'retry_count': 0,
-        'tunnel_interface': 'tnl56'
-        },
-        'mlx8.1_to_mlx8.2': {
-        'destination': '3.3.3.3',
-        'admin': 'UP',
-        'operational': 'UP',
-        'flap_count': 1,
-        'retry_count': 0,
-        'tunnel_interface': 'tnl63'
-        },
-        'mlx8.1_to_mlx8.3': {
-        'destination': '4.4.4.4',
-        'admin': 'DOWN',
-        'operational': 'DOWN',
-        'flap_count': 0,
-        'retry_count': 0
+        'lsps': {
+            'mlx8.1_to_ces.2': {
+                'destination': '1.1.1.1',
+                'admin': 'UP',
+                'operational': 'UP',
+                'flap_count': 1,
+                'retry_count': 0,
+                'tunnel_interface': 'tnl0'
+            },
+            'mlx8.1_to_ces.1': {
+                'destination': '2.2.2.2',
+                'admin': 'UP',
+                'operational': 'UP',
+                'flap_count': 1,
+                'retry_count': 0,
+                'tunnel_interface': 'tnl56'
+            },
+            'mlx8.1_to_mlx8.2': {
+                'destination': '3.3.3.3',
+                'admin': 'UP',
+                'operational': 'UP',
+                'flap_count': 1,
+                'retry_count': 0,
+                'tunnel_interface': 'tnl63'
+            },
+            'mlx8.1_to_mlx8.3': {
+                'destination': '4.4.4.4',
+                'admin': 'DOWN',
+                'operational': 'DOWN',
+                'flap_count': 0,
+                'retry_count': 0
+            }
         }
     }
-}
 
     golden_output_brief = {
-    'execute.return_value': '''
-      Note: LSPs marked with * are taking a Secondary Path
-                                                                        Admin Oper  Tunnel   Up/Dn Retry Active
-      Name                                              To              State State Intf     Times No.   Path
-      mlx8.1_to_ces.2                                   1.1.1.1  UP    UP    tnl0     1     0     --   
-      mlx8.1_to_ces.1                                   2.2.2.2   UP    UP    tnl56    1     0     --   
-      mlx8.1_to_mlx8.2                                  3.3.3.3   UP    UP    tnl63    1     0     --   
-      mlx8.1_to_mlx8.3                                  4.4.4.4   DOWN  DOWN  --       0     0     --
-    '''
-  }
+        'execute.return_value': '''
+        Note: LSPs marked with * are taking a Secondary Path
+                                    Admin Oper  Tunnel   Up/Dn Retry Active
+        Name                To      State State Intf     Times No.   Path
+        mlx8.1_to_ces.2     1.1.1.1  UP    UP    tnl0     1     0     --
+        mlx8.1_to_ces.1     2.2.2.2   UP    UP    tnl56    1     0     --
+        mlx8.1_to_mlx8.2    3.3.3.3   UP    UP    tnl63    1     0     --
+        mlx8.1_to_mlx8.3    4.4.4.4   DOWN  DOWN  --       0     0     --
+        '''
+    }
 
     def test_show_mpls_lsp(self):
         self.device = Mock(**self.golden_output_brief)
@@ -85,4 +85,4 @@ class test_show_mpls_lsp(unittest.TestCase):
         self.assertEqual(parsed_output, self.golden_parsed_output_brief)
 
 if __name__ == '__main__':
-    unittest.main() 
+    unittest.main()
