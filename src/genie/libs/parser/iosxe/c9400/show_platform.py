c7c79867b8fcb9b41a6d07126f565b10f10e7b9d
Takashi Higashimura <tahigash@cisco.com>
2021-01-06 17:23:01 -0500

    formatted and fixed UT

--- src/genie/libs/parser/iosxe/c9400/show_platform.py
+++ src/genie/libs/parser/iosxe/c9400/show_platform.py
@@ -51,8 +51,7 @@ class ShowEnvironment(ShowEnvironmentSchema):
     """Parser for show environment
                   show environment | include {include}"""
 
-    cli_command = ['show environment', 
-                   'show environment | include {include}']
+    cli_command = ['show environment', 'show environment | include {include}']
 
     def cli(self, include='', output=None):
         if not output:
@@ -67,7 +66,8 @@ class ShowEnvironment(ShowEnvironmentSchema):
         ret_dict = {}
 
         # Number of Critical alarms:  0
-        p1 = re.compile(r'^Number +of +Critical +alarms: +(?P<critic_alarms>\d+)$')
+        p1 = re.compile(
+            r'^Number +of +Critical +alarms: +(?P<critic_alarms>\d+)$')
 
         # Number of Major alarms:     0
         p2 = re.compile(r'^Number +of +Major +alarms: +(?P<maj_alarms>\d+)$')
@@ -82,7 +82,9 @@ class ShowEnvironment(ShowEnvironmentSchema):
         # R0          HotSwap: Volts  Normal          53   V DC   	na
         # R0          Temp:   outlet  Normal          39   Celsius	(63 ,73 ,103,105)(Celsius)
         # R0          Temp:    inlet  Normal          32   Celsius	(56 ,66 ,96 ,98 )(Celsius)
-        p4 = re.compile(r'(?P<slot>\S+)\s+(?P<sensor_name>\S+(:\s+\S+)?)\s+(?P<state>\S+)\s+(?P<reading>\d+\s+\S+(\s+(AC|DC))?)\s+(\((?P<minor>\d+\s*),(?P<major>\d+\s*),(?P<critical>\d+\s*),(?P<shutdown>\d+\s*)\)\((?P<unit>\S+)\))?')
+        p4 = re.compile(
+            r'(?P<slot>\S+)\s+(?P<sensor_name>\S+(:\s+\S+)?)\s+(?P<state>\S+)\s+(?P<reading>\d+\s+\S+(\s+(AC|DC))?)\s+(\((?P<minor>\d+\s*),(?P<major>\d+\s*),(?P<critical>\d+\s*),(?P<shutdown>\d+\s*)\)\((?P<unit>\S+)\))?'
+        )
 
         for line in output.splitlines():
             line = line.strip()
@@ -120,15 +122,20 @@ class ShowEnvironment(ShowEnvironmentSchema):
                 group = m.groupdict()
                 sensor_name = group.pop('sensor_name')
                 slot = group.pop('slot')
-                fin_dict = ret_dict.setdefault('slot', {}).setdefault(slot, {}).\
-                    setdefault('sensor', {}).setdefault(sensor_name, {})
+                fin_dict = ret_dict.setdefault('slot', {}).\
+                                    setdefault(slot, {}).\
+                                    setdefault('sensor',{}).\
+                                    setdefault(sensor_name, {})
 
                 fin_dict['state'] = group['state']
                 fin_dict['reading'] = group['reading']
                 if group['minor']:
                     fin_dict.setdefault('threshold', {})
-                    for key in ['minor', 'major', 'critical', 'shutdown', 'unit']:
-                        fin_dict['threshold'][key] = int(group[key]) if key != 'unit' else group[key]
+                    for key in [
+                            'minor', 'major', 'critical', 'shutdown', 'unit'
+                    ]:
+                        fin_dict['threshold'][key] = int(
+                            group[key]) if key != 'unit' else group[key]
                 continue
 
         return ret_dict
@@ -194,8 +201,9 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
     """Parser for show environment all
                   show environment all | include {include}"""
 
-    cli_command = ['show environment all', 
-                   'show environment all | include {include}']
+    cli_command = [
+        'show environment all', 'show environment all | include {include}'
+    ]
 
     def cli(self, include='', output=None):
         if not output:
@@ -209,7 +217,8 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
         ret_dict = {}
 
         # Number of Critical alarms:  0
-        p1 = re.compile(r'^Number +of +Critical +alarms: +(?P<critic_alarms>\d+)$')
+        p1 = re.compile(
+            r'^Number +of +Critical +alarms: +(?P<critic_alarms>\d+)$')
 
         # Number of Major alarms:     0
         p2 = re.compile(r'^Number +of +Major +alarms: +(?P<maj_alarms>\d+)$')
@@ -225,20 +234,25 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
         #  Temp: UADP       R0                Normal            56 Celsius          	(107,117,123,125)(Celsius)
         #  V1: VX1          R0                Normal            869 mV               	na
         #  Temp:    inlet   R0                Normal            32 Celsius          	(56 ,66 ,96 ,98 )(Celsius)
-        p5 = re.compile(r'(?P<sensor_name>\S+(:\s+\S+)?)\s+(?P<slot>([A-Z][0-9]|\d/\d))\s+(?P<state>\S+)\s+(?P<reading>\d+\s+\S+(\s+(AC|DC))?)\s+(\((?P<minor>\d+\s*),(?P<major>\d+\s*),(?P<critical>\d+\s*),(?P<shutdown>\d+\s*)\)\((?P<unit>\S+)\))?')
+        p5 = re.compile(
+            r'(?P<sensor_name>\S+(:\s+\S+)?)\s+(?P<slot>([A-Z][0-9]|\d/\d))\s+(?P<state>\S+)\s+(?P<reading>\d+\s+\S+(\s+(AC|DC))?)\s+(\((?P<minor>\d+\s*),(?P<major>\d+\s*),(?P<critical>\d+\s*),(?P<shutdown>\d+\s*)\)\((?P<unit>\S+)\))?'
+        )
 
         # Power                                                       Fan States
         # Supply  Model No              Type  Capacity  Status        1     2
         # ------  --------------------  ----  --------  ------------  -----------
         # PS1     C9400-PWR-3200AC      ac    3200 W    active        good  good
         # PS2     C9400-PWR-3200AC      ac    n.a.      faulty        good  good
-        p6 = re.compile(r'(?P<ps_slot>PS\S+)\s+(?P<model_no>\S+)\s+(?P<type>\S+)\s+(?P<capacity>\S+(\s+\S+)?)\s+(?P<status>\S+)\s+(?P<fan_1_state>\S+)\s+(?P<fan_2_state>\S+)$')
+        p6 = re.compile(
+            r'(?P<ps_slot>PS\S+)\s+(?P<model_no>\S+)\s+(?P<type>\S+)\s+(?P<capacity>\S+(\s+\S+)?)\s+(?P<status>\S+)\s+(?P<fan_1_state>\S+)\s+(?P<fan_2_state>\S+)$'
+        )
 
         # PS Current Configuration Mode : Combined
         # PS Current Operating State    : Combined
         # Power supplies currently active    : 1
         # Power supplies currently available : 1
-        p7 = re.compile(r'(PS|Power supplies)\s+(?P<ps_key>.+)\s+:\s+(?P<ps_value>\S+)')
+        p7 = re.compile(
+            r'(PS|Power supplies)\s+(?P<ps_key>.+)\s+:\s+(?P<ps_value>\S+)')
 
         # Fantray : good
         # Power consumed by Fantray : 540 Watts
@@ -246,7 +260,9 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
         # Fantray beacon LED: off
         # Fantray status LED: green
         # SYSTEM : GREEN
-        p8 = re.compile(r'(?P<fantray_key>((.+)?Fantray(.+)?)|SYSTEM)(\s+)?:\s+(?P<fantray_value>(\S+)|(\d+\s+Watts))')
+        p8 = re.compile(
+            r'(?P<fantray_key>((.+)?Fantray(.+)?)|SYSTEM)(\s+)?:\s+(?P<fantray_value>(\S+)|(\d+\s+Watts))'
+        )
 
         for line in output.splitlines():
             line = line.strip()
@@ -276,9 +292,11 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
             m = p4.match(line)
             if m:
                 group = m.groupdict()
-                sensor_dict = ret_dict.setdefault('sensor_list', {}).setdefault(group['sensor_list'], {})
+                sensor_dict = ret_dict.setdefault('sensor_list',
+                                                  {}).setdefault(
+                                                      group['sensor_list'], {})
                 continue
-                
+
             #  Sensor           Location          State          Reading           Threshold(Minor,Major,Critical,Shutdown)
             #  Temp: Coretemp   R0                Normal            48 Celsius          	(107,117,123,125)(Celsius)
             #  Temp: UADP       R0                Normal            56 Celsius          	(107,117,123,125)(Celsius)
@@ -296,8 +314,11 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
                 fin_dict['reading'] = group['reading']
                 if group['minor']:
                     fin_dict.setdefault('threshold', {})
-                    for key in ['minor', 'major', 'critical', 'shutdown', 'unit']:
-                        fin_dict['threshold'][key] = int(group[key]) if key != 'unit' else group[key]
+                    for key in [
+                            'minor', 'major', 'critical', 'shutdown', 'unit'
+                    ]:
+                        fin_dict['threshold'][key] = int(
+                            group[key]) if key != 'unit' else group[key]
                 continue
 
             # Power                                                       Fan States
@@ -310,7 +331,8 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
                 group = m.groupdict()
                 ps_slot = group.pop('ps_slot')
                 ps_dict = ret_dict.setdefault('power_supply', {})
-                ps_slot_dict = ps_dict.setdefault('slot', {}).setdefault(ps_slot, {})
+                ps_slot_dict = ps_dict.setdefault('slot',
+                                                  {}).setdefault(ps_slot, {})
                 ps_slot_dict.update({k: v for k, v in group.items()})
 
             # PS Current Configuration Mode : Combined
@@ -325,7 +347,7 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
                     ps_value = int(group['ps_value'])
                 else:
                     ps_value = group['ps_value']
-                ps_dict.setdefault(ps_key, ps_value) 
+                ps_dict.setdefault(ps_key, ps_value)
 
             # Fantray : good
             # Power consumed by Fantray : 540 Watts
@@ -336,15 +358,19 @@ class ShowEnvironmentAll(ShowEnvironmentAllSchema):
             m = p8.match(line)
             if m:
                 group = m.groupdict()
-                fantray_key = group['fantray_key'].strip().lower().replace(' ', '_')
+                fantray_key = group['fantray_key'].strip().lower().replace(
+                    ' ', '_')
                 if 'power_consumed' in fantray_key:
                     fantray_value = int(group['fantray_value'])
                     fantray_key += '_watts'
                 else:
                     fantray_value = group['fantray_value']
                 if 'fantray' == fantray_key:
-                    ret_dict.setdefault('fantray', {}).setdefault('status', fantray_value)
+                    ret_dict.setdefault('fantray',
+                                        {}).setdefault('status', fantray_value)
                 else:
-                    ret_dict.setdefault('fantray', {}).setdefault(fantray_key, fantray_value)
+                    ret_dict.setdefault('fantray',
+                                        {}).setdefault(fantray_key,
+                                                       fantray_value)
 
         return ret_dict
