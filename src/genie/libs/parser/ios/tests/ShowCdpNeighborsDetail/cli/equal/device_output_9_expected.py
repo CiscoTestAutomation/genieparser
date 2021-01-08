9723f0e4138cc980b773ba4d2f4cc8aa908085b3
Jamie Caesar <jamie@thecaesars.us>
2021-01-07 19:21:55 -0500

    Fix for Issue374 - Parsing error if double quote in platform name

---
+++ src/genie/libs/parser/ios/tests/ShowCdpNeighborsDetail/cli/equal/device_output_9_expected.py
@@ -0,0 +1,20 @@
+expected_output = {
+    "total_entries_displayed": 1,
+    "index": {
+        1: {
+            "device_id": "SEP009E2E69AC48",
+            "duplex_mode": "",
+            "vtp_management_domain": "",
+            "native_vlan": "",
+            "management_addresses": {},
+            "entry_addresses": {"10.0.0.8": {}},
+            "capabilities": "Host Phone",
+            "platform": '"CTS-CODEC-SX80"',
+            "port_id": "gbe0",
+            "local_interface": "GigabitEthernet3/0/38",
+            "hold_time": 166,
+            "software_version": '"ce9.1.3 75ff735 2017-07-10"',
+            "advertisement_ver": 2,
+        }
+    },
+}
