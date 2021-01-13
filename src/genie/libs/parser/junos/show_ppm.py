a447fc61de974bf6a76eae02105a78eb329e65ef
Abdurahman Hersi -X (abhersi - ALBANY SERVICES  INC at Cisco) <abhersi@cisco.com>
2021-01-13 12:54:32 -0500

    update for show ppm parser

--- src/genie/libs/parser/junos/show_ppm.py
+++ src/genie/libs/parser/junos/show_ppm.py
@@ -9,6 +9,7 @@ import re
 
 # Genie
 from genie.metaparser import MetaParser
+from pyats.utils.exceptions import SchemaError
 from genie.metaparser.util.schemaengine import Schema, Any, \
                     Optional, Use
 
@@ -44,13 +45,21 @@ class ShowPPMTransmissionsProtocolBfdDetailSchema(MetaParser):
 
         transmission_data = Schema({
             "protocol": str,
+            Optional("transmission-count"): str,
+            Optional("transmission-delay-difference"): str,
+            Optional("transmission-delayed"): str,
+            Optional("transmission-delayed-count"): str,
             "transmission-destination": str,
             "transmission-distributed": str,
             Optional("transmission-host"): str,
             "transmission-interface-index": str,
             "transmission-interval": str,
-            "transmission-pfe-addr": str,
-            "transmission-pfe-handle": str
+            Optional("transmission-interval-threshold"): str,
+            Optional("transmission-jitter"): str,
+            Optional("transmission-largest-difference"): str,
+            Optional("transmission-last-interval"): str,
+            Optional("transmission-pfe-addr"): str,
+            Optional("transmission-pfe-handle"): str
         })
 
         for item in value:
@@ -59,6 +68,8 @@ class ShowPPMTransmissionsProtocolBfdDetailSchema(MetaParser):
 
     schema = {
         "ppm-transmissions": {
+            Optional("remote-transmissions"): str,
+            Optional("total-transmissions"): str,
             "transmission-data": Use(validate_transmission_data)
         }
     }
@@ -88,17 +99,27 @@ class ShowPPMTransmissionsProtocolBfdDetail(ShowPPMTransmissionsProtocolBfdDetai
         ret_dict = {}
 
         # Destination: 27.85.194.102, Protocol: BFD, Transmission interval: 300
-        p1 = re.compile(r'^Destination: +(?P<transmission_destination>[\d\.]+), '
+        p1 = re.compile(r'^Destination: +(?P<transmission_destination>\S+), '
                         r'+Protocol: +(?P<protocol>\S+), Transmission +interval: '
                         r'+(?P<transmission_interval>\d+)$')
 
         # Distributed: TRUE, Distribution handle: 6918, Distribution address: fpc9
-        p2 = re.compile(r'^Distributed: +(?P<transmission_distributed>\S+), '
-                        r'+Distribution +handle: +(?P<transmission_pfe_handle>\d+), '
-                        r'Distribution +address: +(?P<transmission_pfe_addr>\S+)$')
+        p2 = re.compile(r'^Distributed: +(?P<transmission_distributed>\S+)'
+                        r'(, +Distribution +handle: +(?P<transmission_pfe_handle>\d+), '
+                        r'Distribution +address: +(?P<transmission_pfe_addr>\S+))?$')
 
         # IFL-index: 783
         p3 = re.compile(r'^IFL-index: +(?P<transmission_interface_index>\d+)$')
+
+        # Tx Jitter: 25, Tx Delayed: FALSE, Delay Difference: 0, Last Tx Interval: 821, Tx Delayed count: 0, Tx Interval Threshold: 1250, Tx Largest Interval: 0, Tx Counter: 15891
+        p4 = re.compile(r'^Tx +Jitter: +(?P<transmission_jitter>[\d\.]+), Tx +Delayed: +'
+                        r'(?P<transmission_delayed>\S+), +Delay +Difference: +(?P<transmission_delay_difference>\d+), '
+                        r'Last+ Tx +Interval: +(?P<transmission_last_interval>\d+), +Tx +Delayed +count: +'
+                        r'(?P<transmission_delayed_count>\d+), +Tx +Interval +Threshold: +(?P<transmission_interval_threshold>\d+), '
+                        r'+Tx +Largest +Interval: +(?P<transmission_largest_difference>\d+), +Tx +Counter: (?P<transmission_count>\d+)$')
+
+        # Transmission entries: 6, Remote transmission entries: 3
+        p5 = re.compile(r'^Transmission +entries: +(?P<total_transmissions>\d+), +Remote +transmission +entries: +(?P<remote_transmissions>\d+)$')
 
         for line in out.splitlines():
             line = line.strip()
@@ -107,8 +128,9 @@ class ShowPPMTransmissionsProtocolBfdDetail(ShowPPMTransmissionsProtocolBfdDetai
             m = p1.match(line)
             if m:
                 group = m.groupdict()
-                ppm_transmissions = ret_dict.setdefault('ppm-transmissions', {}). \
-                    setdefault('transmission-data', [])
+                ppm_transmissions = ret_dict.setdefault('ppm-transmissions', {})
+
+                transmission_data_list = ppm_transmissions.setdefault('transmission-data', [])
                 
                 transmission_data_dict = {}
 
@@ -116,7 +138,7 @@ class ShowPPMTransmissionsProtocolBfdDetail(ShowPPMTransmissionsProtocolBfdDetai
                     key = key.replace('_', '-')
                     transmission_data_dict[key] = value
 
-                ppm_transmissions.append(transmission_data_dict)
+                transmission_data_list.append(transmission_data_dict)
                 continue
 
             # Distributed: TRUE, Distribution handle: 6918, Distribution address: fpc9
@@ -124,8 +146,9 @@ class ShowPPMTransmissionsProtocolBfdDetail(ShowPPMTransmissionsProtocolBfdDetai
             if m:
                 group = m.groupdict()
                 for key, value in group.items():
-                    key = key.replace('_', '-')
-                    transmission_data_dict.update({key:value})
+                    if value != None:
+                        key = key.replace('_', '-')
+                        transmission_data_dict.update({key:value})
                 continue
 
             # IFL-index: 783
@@ -136,5 +159,23 @@ class ShowPPMTransmissionsProtocolBfdDetail(ShowPPMTransmissionsProtocolBfdDetai
                     key = key.replace('_', '-')
                     transmission_data_dict.update({key:value})
                 continue
-            
+
+            # Tx Jitter: 25, Tx Delayed: FALSE, Delay Difference: 0, Last Tx Interval: 821, Tx Delayed count: 0, Tx Interval Threshold: 1250, Tx Largest Interval: 0, Tx Counter: 15891
+            m = p4.match(line)
+            if m:
+                group = m.groupdict()
+                for key, value in group.items():
+                    key = key.replace('_', '-')
+                    transmission_data_dict.update({key:value})
+                continue
+
+            # Transmission entries: 6, Remote transmission entries: 3
+            m = p5.match(line)
+            if m:
+                group = m.groupdict()
+                for key, value in group.items():
+                    key = key.replace('_', '-')
+                    ppm_transmissions.update({key:value})
+                continue
+
         return ret_dict
