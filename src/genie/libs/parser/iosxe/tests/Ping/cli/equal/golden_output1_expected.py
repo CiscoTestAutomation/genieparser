3a5c04d34289ac5d63f300847ec7ee78f5d1328e
Kamyar Ziabari <mziabari@cisco.com>
2021-01-20 16:41:53 -0500

    ping parser all files for xe xr

---
+++ src/genie/libs/parser/iosxe/tests/Ping/cli/equal/golden_output1_expected.py
@@ -0,0 +1,20 @@
+expected_output = {
+   'ping': {
+      'repeat': 10,
+      'data_bytes': 100,
+      'address': '21.1.1.1',
+      'timeout_secs': 2,
+      'source':
+      '21.1.1.2',
+      'statistics': {
+         'success_percent': 100,
+         'received': 10,
+         'send': 10,
+         'round-trip': {
+            'min_ms': 1,
+            'max_ms': 1,
+            'avg_ms': 1
+            }
+         }
+      }
+   }
