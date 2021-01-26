ac08f67d2ad1fbb43bab58f28d82327148ca79b1
Takashi Higashimura <tahigash@cisco.com>
2021-01-26 07:59:40 -0500

    updated ShowServicesAccountingAggregationTemplate

---
+++ src/genie/libs/parser/junos/tests/ShowServicesAccountingAggregationTemplate/cli/equal/golden_output_4_expected.py
@@ -0,0 +1,17 @@
+expected_output = {
+    "services-accounting-information": {
+        "flow-aggregate-template-detail": {
+            "flow-aggregate-template-detail-ipv4": {
+                "detail-entry": {
+                    "byte-count": "184",
+                    "input-snmp-interface-index": "1014",
+                    "mpls-label-1": "299792",
+                    "mpls-label-2": "16",
+                    "mpls-label-3": "0",
+                    "output-snmp-interface-index": "624",
+                    "packet-count": "2"
+                }
+            }
+        }
+    }
+}
