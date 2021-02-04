4001949d348748fe05b1fc543fe7ffc72205b140
rampselv <rampselv@cisco.com>
2021-02-04 16:25:53 +0530

    Added parser for show flow monitor sdwan_flow_monitor statistics command

---
+++ src/genie/libs/parser/iosxe/tests/ShowFlowMonitorSdwanFlowMonitorStatistics/cli/equal/golden_output_expected.py
@@ -0,0 +1,12 @@
+expected_output={
+    "cache_type": "Normal (Platform cache)", 
+    "high_watermark": 16030, 
+    "flows_added": 197808242, 
+    "current_entries": 12534, 
+    "cache_size": 200000, 
+    "flows_aged": {
+        "total_flows_aged": 197795708, 
+        "active_timeout_60secs": 11897289, 
+        "inactive_timeout_10secs": 185898419
+    }
+}
