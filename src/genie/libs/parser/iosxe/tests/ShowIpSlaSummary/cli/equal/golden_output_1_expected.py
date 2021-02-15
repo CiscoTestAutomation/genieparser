d65bdde0ddea009415d8a7dbd51d3ac18ba6232a
lumcclel <lumcclel@cisco.com>
2021-02-15 12:57:28 -0500

    Updated expected output test files

--- src/genie/libs/parser/iosxe/tests/ShowIpSlaSummary/cli/equal/golden_output_1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowIpSlaSummary/cli/equal/golden_output_1_expected.py
@@ -1,46 +1,60 @@
 expected_output = {
       'id': {
-            '1': {'probe_status': 'active',
+            '1': {
+                  'state': 'active',
                   'destination': '123.23.213.32',
-                  'last_run_seconds_ago': '21',
+                  'last_run': '21 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '44',
-                  'type': 'tcp-connect'},
-            '2': {'probe_status': 'active',
+                  'rtt_stats_in_ms_or_us': '44',
+                  'type': 'tcp-connect'
+            },
+            '2': {
+                  'state': 'active',
                   'destination': '11.121.2.123',
-                  'last_run_seconds_ago': '7',
-                  'return_code': 'OK',
-                  'rtt_stats_mseconds': '79',
-                  'type': 'dns'},
-            '3': {'probe_status': 'active',
+                  'last_run': '7 seconds ago',
+                  'return_code': 'Timeout',
+                  'rtt_stats_in_ms_or_us': '-',
+                  'type': 'dns'
+            },
+            '3': {
+                  'state': 'active',
                   'destination': '121.32.11.1',
-                  'last_run_seconds_ago': '54',
+                  'last_run': '54 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '1',
-                  'type': 'udp-jitter'},
-            '4': {'probe_status': 'active',
+                  'rtt_stats_in_ms_or_us': '1',
+                  'type': 'udp-jitter'
+            },
+            '4': {
+                  'state': 'active',
                   'destination': '12.223.33.3',
-                  'last_run_seconds_ago': '15',
+                  'last_run': '15 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '1',
-                  'type': 'udp-jitter'},
-            '5': {'probe_status': 'active',
+                  'rtt_stats_in_ms_or_us': '1',
+                  'type': 'udp-jitter'
+            },
+            '5': {
+                  'state': 'active',
                   'destination': '13.132.32.2',
-                  'last_run_seconds_ago': '8',
+                  'last_run': '8 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '1',
-                  'type': 'udp-jitter'},
-            '6': {'probe_status': 'active',
+                  'rtt_stats_in_ms_or_us': '1',
+                  'type': 'udp-jitter'
+            },
+            '6': {
+                  'state': 'active',
                   'destination': '11.311.31.2',
-                  'last_run_seconds_ago': '40',
+                  'last_run': '40 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '1',
-                  'type': 'udp-jitter'},
-            '7': {'probe_status': 'active',
+                  'rtt_stats_in_ms_or_us': '1',
+                  'type': 'udp-jitter'
+            },
+            '7': {
+                  'state': 'active',
                   'destination': '131.31.11.1',
-                  'last_run_seconds_ago': '2',
+                  'last_run': '2 seconds ago',
                   'return_code': 'OK',
-                  'rtt_stats_mseconds': '1',
-                  'type': 'icmp-echo'}
-              }
-              }
+                  'rtt_stats_in_ms_or_us': '1',
+                  'type': 'icmp-echo'
+            }
+      }
+}
