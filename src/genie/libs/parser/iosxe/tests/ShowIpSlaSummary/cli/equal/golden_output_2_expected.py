d1b8d12fa0425571a1fc962113f0fd7bfe497f8a
lumcclel <lumcclel@cisco.com>
2021-02-16 16:19:11 -0500

    Improved test formatting

--- src/genie/libs/parser/iosxe/tests/ShowIpSlaSummary/cli/equal/golden_output_2_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowIpSlaSummary/cli/equal/golden_output_2_expected.py
@@ -1,39 +1,44 @@
 expected_output = {
     'ids': {
-        '100': {'state': 'active',
-                'type': 'icmp-jitter',
-                'destination': '192.0.2.2',
-                'rtt_stats': '100 millisecond(s)',
-                'return_code': 'OK',
-                'last_run': '22:49:53 PST Tue May 3 2011'
+        '100': {
+            'state': 'active',
+            'type': 'icmp-jitter',
+            'destination': '192.0.2.2',
+            'rtt_stats': '100 millisecond(s)',
+            'return_code': 'OK',
+            'last_run': '22:49:53 PST Tue May 3 2011'
         },
-        '101': {'state': 'active',
-                'type': 'udp-jitter',
-                'destination': '192.0.2.2',
-                'rtt_stats': '100 millisecond(s)',
-                'return_code': 'OK',
-                'last_run': '22:49:53 PST Tue May 3 2011'
+        '101': {
+            'state': 'active',
+            'type': 'udp-jitter',
+            'destination': '192.0.2.2',
+            'rtt_stats': '100 millisecond(s)',
+            'return_code': 'OK',
+            'last_run': '22:49:53 PST Tue May 3 2011'
         },
-        '102': {'state': 'active',
-                'type': 'tcp-connect',
-                'destination': '192.0.2.2',
-                'rtt_stats': '-',
-                'return_code': 'NoConnection',
-                'last_run': '22:49:53 PST Tue May 3 2011'
+        '102': {
+            'state': 'active',
+            'type': 'tcp-connect',
+            'destination': '192.0.2.2',
+            'rtt_stats': '-',
+            'return_code': 'NoConnection',
+            'last_run': '22:49:53 PST Tue May 3 2011'
         },
-        '103': {'state': 'active',
-                'type': 'video',
-                'destination': '1232:232::222',
-                'rtt_stats': '100 millisecond(s)',
-                'return_code': 'OK',
-                'last_run': '22:49:53 PST Tue May 3 2011'
+        '103': {
+            'state': 'active',
+            'type': 'video',
+            'destination': '1232:232::222',
+            'rtt_stats': '100 millisecond(s)',
+            'return_code': 'OK',
+            'last_run': '22:49:53 PST Tue May 3 2011'
         },
-        '104': {'state': 'active',
-                'type': 'video',
-                'destination': '1232:232::222',
-                'rtt_stats': '100 millisecond(s)',
-                'return_code': 'OK',
-                'last_run': '22:49:53 PST Tue May 3 2011'
+        '104': {
+            'state': 'active',
+            'type': 'video',
+            'destination': '1232:232::222',
+            'rtt_stats': '100 millisecond(s)',
+            'return_code': 'OK',
+            'last_run': '22:49:53 PST Tue May 3 2011'
         }
     }
 }
