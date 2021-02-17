f401b7b32dad0c169f3e7df65468bdec62e34aad
lumcclel <lumcclel@cisco.com>
2021-02-17 18:29:59 -0500

    Going back to original golden_output 1 and 2 tests

--- src/genie/libs/parser/iosxe/tests/ShowVrrp/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowVrrp/cli/equal/golden_output1_expected.py
@@ -3,26 +3,27 @@ expected_output = {
         "GigabitEthernet1": {
             "group": {
                 100: {
+                    "advertise_interval_secs": 1.0,
+                    "auth_text": "hash",
                     "description": "DC-LAN Subnet",
+                    "master_advertisement_interval_secs": 1.0,
+                    "master_down_interval_secs": 3.531,
+                    "master_router": "local",
+                    "master_router_ip": "192.168.1.233",
+                    "master_router_priority": 120,
+                    "preemption": "enabled",
+                    "priority": 120,
                     "state": "Master",
                     "virtual_ip_address": "192.168.10.1",
                     "virtual_mac_address": "0000.5e00.0164",
-                    "advertise_interval_secs": 1.0,
-                    "preemption": "enabled",
-                    "priority": 120,
-                    'vrrs_group_name': 'DC_LAN',
-                    "track_object": {
-                       1: {
-                            "state": "Up",
-                            "decrement": 30,
-                        },
+                    "vrrs_name": {
+                        "DC_LAN": {
+                            "track_object":
+                               {1:
+                                   {"decrement": 30,
+                                    "state": "Up"}}
+                        }
                     },
-                    "auth_text": "hash",
-                    "master_router_ip": "192.168.1.233",
-                    "master_router": "local",
-                    "master_router_priority": 120,
-                    "master_advertisement_interval_secs": 1.0,
-                    "master_down_interval_secs": 3.531,
                 }
             }
         }
