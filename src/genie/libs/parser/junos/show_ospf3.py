7cb45b9e5866865bcb6cb171066cb2c045c0d903
Abdurahman Hersi -X (abhersi - ALBANY SERVICES  INC at Cisco) <abhersi@cisco.com>
2021-02-22 18:41:13 -0500

    fix for show ospf3

--- src/genie/libs/parser/junos/show_ospf3.py
+++ src/genie/libs/parser/junos/show_ospf3.py
@@ -596,7 +596,7 @@ class ShowOspf3InterfaceExtensiveSchema(MetaParser):
         if not isinstance(value, list):
             raise SchemaError('ospf3-interface is not a list')
         ospf3_interface_schema = Schema({
-            "adj-count": str,
+            Optional("adj-count"): str,
             "bdr-id": str,
             "dead-interval": str,
             "dr-id": str,
