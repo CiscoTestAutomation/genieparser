47b51aa3adc3f950f1f64e08938d6e44338bdb51
Thomas Ryan <thomryan@cisco.com>
2021-03-08 14:58:46 -0500

    Cleaned up job file

--- tests/folder_parsing_job.py
+++ tests/folder_parsing_job.py
@@ -1,5 +1,5 @@
-from genie.libs.parser.utils import unittests
 from genie.libs.parser.utils.unittests import main
 
 if __name__ == '__main__':
-    unittests.run_tests()
+    from genie.libs.parser.utils.unittests import run_tests
+    run_tests()
