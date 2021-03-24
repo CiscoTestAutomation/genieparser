62b905ac36d4bfe1d30514a4d93c83677fd71903
rampselv <rampselv@cisco.com>
2021-03-24 12:51:49 +0530

    Modified the parser to adapt show redundancy application group all

--- src/genie/libs/parser/iosxe/tests/ShowRedundancyApplicationGroup/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowRedundancyApplicationGroup/cli/equal/golden_output_expected.py
@@ -1,17 +1,20 @@
 expected_output={
-    "administrative_state": "No Shutdown", 
-    "peer_comm": "Yes", 
-    "peer_role": "STANDBY", 
-    "peer_presence": "Yes", 
-    "my_role": "ACTIVE", 
-    "group_name": "group1", 
-    "aggregate_oper_state": "Up", 
-    "rf_domain": {
-        "btob-one": {
-            "rf_state": "ACTIVE", 
-            "peer_rf_state": "STANDBY HOT"
+    "group_id": {
+        "1": {
+            "administrative_state": "No Shutdown", 
+            "peer_comm": "Yes", 
+            "peer_role": "ACTIVE", 
+            "peer_presence": "Yes", 
+            "my_role": "STANDBY", 
+            "group_name": "group1", 
+            "aggregate_oper_state": "Up", 
+            "rf_domain": {
+                "btob-one": {
+                    "rf_state": "STANDBY HOT", 
+                    "peer_rf_state": "ACTIVE"
+                }
+            }, 
+            "peer_progression_started": "Yes"
         }
-    }, 
-    "group_id": "1", 
-    "peer_progression_started": "Yes"
+    }
 }
