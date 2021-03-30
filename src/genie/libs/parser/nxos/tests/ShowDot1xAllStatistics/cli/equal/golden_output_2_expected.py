27f496ef70b5a003747fb8bdac6e7356ce508423
itdependsnetworks <ken@celenza.org>
2021-03-30 00:15:32 -0400

    First batch of nxos tests

---
+++ src/genie/libs/parser/nxos/tests/ShowDot1xAllStatistics/cli/equal/golden_output_2_expected.py
@@ -0,0 +1,21 @@
+expected_output = {
+    "interfaces": {
+        "Ethernet1/1": {
+            "interface": "Ethernet1/1",
+            "statistics": {
+                "txreq": 5,
+                "rxlogoff": 25,
+                "txtotal": 6,
+                "txreqid": 89,
+                "lastrxsrcmac": "02:45:44:ff:bb:cd",
+                "rxinvalid": 5,
+                "rxrespid": 34,
+                "rxlenerr": 6,
+                "rxversion": 78,
+                "rxstart": 111,
+                "rxresp": 224,
+                "rxtotal": 543,
+            },
+        }
+    }
+}
