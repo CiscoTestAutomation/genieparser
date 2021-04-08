aa547dbc3850625cf691e2979d985c658f48f291
Eric Leung (ericleu) <ericleu@cisco.com>
2021-04-08 16:13:20 -0400

    Fix for devAT external parsers/apis issue: updated common.py and entry_points.py

--- src/genie/libs/parser/utils/tests/test_add_parser.py
+++ src/genie/libs/parser/utils/tests/test_add_parser.py
@@ -1,13 +1,19 @@
+import logging
 import unittest
 from unittest.mock import Mock
 
-from genie.libs.parser.utils.common import (
-    parser_data
-)
+from genie.libs.parser.utils import common 
 from genie.libs.parser.utils.entry_points import add_parser
 
 
 class TestAddParser(unittest.TestCase):
+    
+    def setUp(self):
+        try:
+            del common.parser_data
+        except AttributeError as err:
+            logging.warning(err)
+            pass
 
     def test_add_parser_command_string(self):
         cli_command = 'show test_add_parser_single_command_string'
@@ -16,11 +22,12 @@ class TestAddParser(unittest.TestCase):
         mock_parser.MockParser.__name__ = 'asa.MockParser'
         mock_parser.MockParser.__package__ = 'asa'
 
-        self.assertNotIn(cli_command, parser_data)
+        with self.assertRaises(AttributeError):
+            getattr(common.parser_data)
 
         add_parser(parser=mock_parser.MockParser, os_name='asa')
 
-        self.assertIn(cli_command, parser_data)
+        self.assertIn(cli_command, common.parser_data)
 
     def test_add_parser_command_iterable(self):
         cli_command = ['show test_add_parser_single_command_iterable1',
@@ -31,14 +38,13 @@ class TestAddParser(unittest.TestCase):
         mock_parser.MockParser.__name__ = 'asa.MockParser'
         mock_parser.MockParser.__package__ = 'asa'
 
-        for cmd in cli_command:
-            self.assertNotIn(cmd, parser_data)
+        with self.assertRaises(AttributeError):
+            getattr(common.parser_data)
 
         add_parser(parser=mock_parser.MockParser, os_name='asa')
 
         for cmd in cli_command:
-            self.assertIn(cmd, parser_data)
-
+            self.assertIn(cmd, common.parser_data)
 
 if __name__ == '__main__':
     unittest.main()
