4e7a7c0d4e0d0a106736d1e8c57b3141b4011400
Amir Boresh Kazemy <aboreshk@cisco.com>
2021-04-21 09:32:11 -0400

    update parser

--- src/genie/libs/parser/iosxe/show_ip_nbar_version.py
+++ src/genie/libs/parser/iosxe/show_ip_nbar_version.py
@@ -1,7 +1,7 @@
 """show_ip_nbar_version.py
-    supported commands:
-        * show ip nbar version
 
+    IOSXE parsers for the following show commands:
+        * 'show ip nbar version'
 """
 
 # Python
@@ -9,10 +9,7 @@ import re
 
 # Metaparser
 from genie.metaparser import MetaParser
-from genie.metaparser.util.schemaengine import (Schema,
-                                                Any,
-                                                Or,
-                                                Optional)
+from genie.metaparser.util.schemaengine import Schema, Any, Or, Optional
 
 # parser utils
 from genie.libs.parser.utils.common import Common
@@ -21,11 +18,10 @@ from genie.libs.parser.utils.common import Common
 # Schema for:
 #    * 'show ip nbar version'
 # ==============================
-class ShowIpNbarVersionSchema(MetaParser):
-    """ Schema for the commands:
-            *  show ip nbar version
-    """
 
+class ShowIpNbarVersionSchema(MetaParser):
+    """Schema for show ip nbar version"""
+    
     schema = {
         'file': str,
         'name': str,
@@ -38,20 +34,13 @@ class ShowIpNbarVersionSchema(MetaParser):
         'version': str
     }
 
-# ==============================
-# Parser for:
-#    * 'show ip nbar version'
-# ==============================
 class ShowIpNbarVersion(ShowIpNbarVersionSchema):
-    '''
-    Parser for:
-    show ip nbar version
-    '''
+    """Parser for show ip nbar version"""
 
     cli_command = ['show ip nbar version']
 
     #* could add extra parameters if needed
-    def cli(self, output = None):
+    def cli(self, output=None):
         if output is None:
             out = self.device.execute(self.cli_command)
 
@@ -60,15 +49,32 @@ class ShowIpNbarVersion(ShowIpNbarVersionSchema):
 
         # initial variables
         parsed_dict = {}
-
+        
+        # NBAR software version:    34
         p1 = re.compile('^NBAR software version:\s+(?P<nbar_software_version>.+)')
+        
+        # NBAR minimum backward compatible version: 41
         p2 = re.compile('^NBAR minimum backward compatible version:\s+(?P<nbar_minimum_backward_compatible_version>.+)')
+        
+        # Name: Advanced Protocol Pack
         p3 = re.compile('^Name:\s+(?P<name>.+)')
+        
+        # Version:  41.0
         p4 = re.compile('^Version:\s+(?P<version>.+)')
+
+        # Publisher:    Cisco Systems Inc.
         p5 = re.compile('^Publisher:\s+(?P<publisher>.+)')
+
+        # NBAR Engine Version:  31
         p6 = re.compile('^NBAR Engine Version:\s+(?P<nbar_engine_version>.+)')
+        
+        # Creation time:    Mon Feb 11 09:42:11 UTC 2019
         p7 = re.compile('^Creation time:\s+(?P<creation_time>.+)')
+        
+        # File: bootflash:sdavc/pp-adv-all-166.2-31-41.0.0.pack
         p8 = re.compile('^File:\s+(?P<file>.+)')
+        
+        # State:    Active
         p9 = re.compile('^State:\s+(?P<state>.+)')
 
 
