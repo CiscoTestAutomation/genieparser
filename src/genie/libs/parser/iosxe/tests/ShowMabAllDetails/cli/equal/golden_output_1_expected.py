d969a44921cc5ad0ed3ba264b130677b985d1dc3
rreddyc <rreddyc@cisco.com>
2021-05-31 15:40:01 +0530

    Parser for show mab all details

---
+++ src/genie/libs/parser/iosxe/tests/ShowMabAllDetails/cli/equal/golden_output_1_expected.py
@@ -0,0 +1,23 @@
+expected_output = {
+    "GigabitEthernet2/0/1": {
+        "mab_client_ist": {
+            "authen_status": "SUCCESS",
+            "client_mac": "0000.0001.0003",
+            "mab_sm_state": "TERMINATE",
+            "session_id": "000000000000000C82FA130E",
+        },
+        "mac_auth_bypass": "Enabled",
+    },
+    "GigabitEthernet2/0/2": {
+        "mab_client_ist": {
+            "authen_status": "SUCCESS",
+            "client_mac": "0000.0001.0003",
+            "mab_sm_state": "TERMINATE",
+            "session_id": "000000000000000C82FA130E",
+        },
+        "mac_auth_bypass": "Enabled",
+    },
+    "GigabitEthernet2/0/3": {
+        "mab_client_ist": {}, 
+        "mac_auth_bypass": "Enabled"},
+}
