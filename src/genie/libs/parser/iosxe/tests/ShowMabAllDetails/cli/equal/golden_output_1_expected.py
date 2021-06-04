247b4189fdb6007d1f2fca30e65512ee3b7f465b
Rajesh Reddy C -X (rreddyc - TATA CONSULTANCY SERVICES LIMITED at Cisco) <rreddyc@cisco.com>
2021-06-04 12:27:51 +0530

    Update golden_output_1_expected.py
    Fixed comments

--- src/genie/libs/parser/iosxe/tests/ShowMabAllDetails/cli/equal/golden_output_1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowMabAllDetails/cli/equal/golden_output_1_expected.py
@@ -1,23 +1,33 @@
 expected_output = {
-    "GigabitEthernet2/0/1": {
-        "mab_client_ist": {
-            "authen_status": "SUCCESS",
-            "client_mac": "0000.0001.0003",
-            "mab_sm_state": "TERMINATE",
-            "session_id": "000000000000000C82FA130E",
+    "interfaces": {
+        "GigabitEthernet2/0/1": {
+            "client_mac": {
+                "0000.0001.0003": {
+                    "authen_status": "SUCCESS",
+                    "mab_sm_state": "TERMINATE",
+                    "session_id": "000000000000000C82FA130E",
+                },
+                "0000.0001.0004": {
+                    "authen_status": "SUCCESS",
+                    "mab_sm_state": "TERMINATE",
+                    "session_id": "000000000000000C82FA130F",
+                },
+            },
+            "mac_auth_bypass": "Enabled",
         },
-        "mac_auth_bypass": "Enabled",
-    },
-    "GigabitEthernet2/0/2": {
-        "mab_client_ist": {
-            "authen_status": "SUCCESS",
-            "client_mac": "0000.0001.0003",
-            "mab_sm_state": "TERMINATE",
-            "session_id": "000000000000000C82FA130E",
+        "GigabitEthernet2/0/2": {
+            "client_mac": {
+                "0000.0001.0003": {
+                    "authen_status": "SUCCESS",
+                    "mab_sm_state": "TERMINATE",
+                    "session_id": "000000000000000C82FA130E",
+                }
+            },
+            "mac_auth_bypass": "Enabled",
         },
-        "mac_auth_bypass": "Enabled",
-    },
-    "GigabitEthernet2/0/3": {
-        "mab_client_ist": {}, 
-        "mac_auth_bypass": "Enabled"},
+        "GigabitEthernet2/0/3": {
+            "client_mac": {}, 
+            "mac_auth_bypass": "Enabled"
+            },
+    }
 }
