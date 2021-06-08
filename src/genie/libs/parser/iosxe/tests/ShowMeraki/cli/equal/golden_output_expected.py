c1aae1e76bdcf81d23e6347a097c7e94d4b15cf3
Suneetha Maddineni <sunmaddi@cisco.com>
2021-06-08 15:30:04 -0700

    added the golden_output_output and golden_expected_output.py

---
+++ src/genie/libs/parser/iosxe/tests/ShowMeraki/cli/equal/golden_output_expected.py
@@ -0,0 +1,42 @@
+expected_output = {
+    'meraki': {
+        'switch': {
+            1: {
+                'switch_no': '1',
+                'PID': 'C9300-24T',
+                'Serial_Number': 'FJC2311T0DA',
+                'Meraki_SN': 'Q5EE-DJYN-CRGR',
+                'Mac_Addr': '4cbc.4812.3550',
+                'Conversion_Status': 'Registered',
+                'Current_Mode': 'C9K-C'
+            },
+            2: {
+                'switch_no': '2',
+                'PID': 'C9300-24U',
+                'Serial_Number': 'FJC1527A0BC',
+                'Meraki_SN': 'N/A',
+                'Mac_Addr': '4cbc.4812.2881',
+                'Conversion_Status': 'ACT2 write failed',
+                'Current_Mode': 'C9K-C'
+            },
+            3: {
+                'switch_no': '3',
+                'PID': 'C9300-48UX',
+                'Serial_Number': 'FJC2317T0DT',
+                'Meraki_SN': 'Q3EA-AZYP-WDFH',
+                'Mac_Addr': '4cbc.4812.2501',
+                'Conversion_Status': 'Registered',
+                'Current_Mode': 'C9K-C'
+            },
+            4: {
+                'switch_no': '4',
+                'PID': 'C9300-48TX',
+                'Serial_Number': 'FJC2311T0AJ',
+                'Meraki_SN': 'N/A',
+                'Mac_Addr': '5cbc.4812.3479',
+                'Conversion_Status': 'Timeout',
+                'Current_Mode': 'C9K-C'
+            }
+         }
+     }
+}
