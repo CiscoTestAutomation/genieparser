125d5728219dd020682f7c715151953da7808f7c
Suneetha Maddineni <sunmaddi@cisco.com>
2021-06-12 10:11:42 -0700

    added unit tests to check on show stack power budgeting

---
+++ src/genie/libs/parser/iosxe/tests/ShowStackPowerBudgeting/cli/equal/golden_output_expected.py
@@ -0,0 +1,30 @@
+expected_output = { 
+    "power_stack": {
+        "Powerstack-1": {
+            "switch_num": '1',
+            "power_supply_a": '1100',
+            "power_supply_b": '0',
+            "power_budget": '1100',
+            "allocated_power": '575',
+            "poe_avail_pwr": '525',
+            "consumed_pwr_sys_poe": '155/0'
+        },
+        "Powerstack-2": {
+            "switch_num": '2',
+            "power_supply_a": '1100',
+            "power_supply_b": '0',
+            "power_budget": '1100',
+            "allocated_power": '575',
+            "poe_avail_pwr": '525',
+            "consumed_pwr_sys_poe": '155/0'
+        },
+     },
+    "total_power": {
+        "stackpower": {
+            "total_allocated_pwr_sys_poe": '1150',
+            "total_poe_avail_pwr_sys_poe": '1050',
+            "total_consumed_pwr_sys_poe": '310/0'
+
+        }
+    }
+}
---
+++ src/genie/libs/parser/iosxe/tests/ShowStackPowerBudgeting/cli/equal/golden_output_expected_oriig.py
@@ -0,0 +1,16 @@
+expected_output = { 
+    "power_stack": {
+        "Powerstack-1": {
+            "switch_num": 1,
+            "power_supply_a": 1100,
+            "power_supply_b": 0,
+            "power_budget": 1100,
+            "allocated_power": 575,
+            "poe_avail_pwr": 525,
+            "consumed_pwr_sys_poe": '155/0',
+            "total_allocated_pwr_sys_poe": 575,
+            "total_available_pwr_sys_poe": 525,
+            "total_consumed_pwr_sys_poe" : '155/0'
+        }
+     }
+}
