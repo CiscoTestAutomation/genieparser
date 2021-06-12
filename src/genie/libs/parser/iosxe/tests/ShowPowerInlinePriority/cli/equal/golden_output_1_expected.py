7d6ff3c46f4c05b2a63ae92fa1d7092c44c0e1ee
Suneetha Maddineni <sunmaddi@cisco.com>
2021-06-12 10:10:10 -0700

    added unit tests to check on show power inline priority

---
+++ src/genie/libs/parser/iosxe/tests/ShowPowerInlinePriority/cli/equal/golden_output_1_expected.py
@@ -0,0 +1,14 @@
+expected_output = {
+    "interface": {
+        "Gi1/0/1": {
+            "admin_state": "auto",
+            "oper_state": "off",
+            "priority": "low"
+        },
+        "Gi1/0/11": {
+            "admin_state": "auto",
+            "oper_state": "off",
+            "priority": "low"
+        }
+    }
+}
