f9f5d31350003d8a89a7a604212580302c05c32a
Suneetha Maddineni <sunmaddi@cisco.com>
2021-06-12 10:07:58 -0700

    Updated conversion status to N/A to check another possibility

--- src/genie/libs/parser/iosxe/tests/ShowMeraki/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowMeraki/cli/equal/golden_output_expected.py
@@ -2,7 +2,7 @@ expected_output = {
     'meraki': {
         'switch': {
             1: {
-                'switch_num': '1',
+                'switch_num': 1,
                 'pid': 'C9300-24T',
                 'serial_number': 'FJC2311T0DA',
                 'meraki_sn': 'Q5EE-DJYN-CRGR',
@@ -11,7 +11,7 @@ expected_output = {
                 'current_mode': 'C9K-C'
             },
             2: {
-                'switch_no': '2',
+                'switch_num': 2,
                 'pid': 'C9300-24U',
                 'serial_number': 'FJC1527A0BC',
                 'meraki_sn': 'N/A',
@@ -20,7 +20,7 @@ expected_output = {
                 'current_mode': 'C9K-C'
             },
             3: {
-                'switch_no': '3',
+                'switch_num': 3,
                 'pid': 'C9300-48UX',
                 'serial_number': 'FJC2317T0DT',
                 'meraki_sn': 'Q3EA-AZYP-WDFH',
@@ -29,12 +29,12 @@ expected_output = {
                 'current_mode': 'C9K-C'
             },
             4: {
-                'switch_no': '4',
+                'switch_num': 4,
                 'pid': 'C9300-48TX',
                 'serial_number': 'FJC2311T0AJ',
                 'meraki_sn': 'N/A',
                 'mac_addr': '5cbc.4812.3479',
-                'conversion_status': 'Timeout',
+                'conversion_status': 'N/A',
                 'current_mode': 'C9K-C'
             }
          }
