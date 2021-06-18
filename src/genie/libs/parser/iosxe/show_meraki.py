d75afa5a8ac4b9ae489120543f99c5dc35c4bee3
Lukas McClelland <lumcclel@cisco.com>
2021-06-17 21:12:45 -0400

    Update with files from other branch, adjust format

--- src/genie/libs/parser/iosxe/show_meraki.py
+++ src/genie/libs/parser/iosxe/show_meraki.py
@@ -3,6 +3,7 @@
 IOSXE parsers for the following show command:
 
     * 'show meraki'
+    * 'show meraki switch {switch}'
 
 '''
 
@@ -10,96 +11,77 @@ IOSXE parsers for the following show command:
 import re
 
 # Metaparser
-from genie.libs.parser.utils.common import Common
 from genie.metaparser import MetaParser
 from genie.metaparser.util.schemaengine import Any, Optional
 
 
 class ShowMerakiSchema(MetaParser):
-
     """ Schema for:
         * 'show meraki'
-        CLI:
-
-    	                   Serial                      MAC             Conversion         Current    
-
-	Switch#.  PID      Number      Meraki SN       Addr             Status             Mode           
- 	---------------------------------------------------------------------------------------
-
-	1       C9300-24T  FJC2311T0DA Q5EE-DJYN-CRGR  4cbc.4812.3550   Registered         C9K-C
-
-	2       C9300-24U  FJC1527A0BC N/A             4cbc.4812.2881   ACT2 write failed  C9K-C
-
-	3       C9300-48UX FJC2317T0DT Q3EA-AZYP-WDFH  4cbc.4812.2501   Registered         C9K-C         
-
-	4       C9300-48TX FJC2311T0AJ N/A             5cbc.4812.3479   Timeout            C9K-C
-
+        * 'show meraki switch {switch}'
     """ 
 
     schema = {
-        'meraki':
-            {Optional('switch'):
-                {Any():
-                     {
-                     Optional('switch_no'): str,
-                     Optional('PID'): str,
-                     Optional('Serial_Number'): str,
-                     Optional('Meraki_SN'): str,
-                     Optional('Mac_Addr'): str,
-                     Optional('Conversion_Status'): str, 
-                     Optional('Current_Mode'): str   }, }, },
+        'meraki': {
+            'switch': {
+                Any(): {
+                    'switch_num': int,
+                    'pid': str,
+                    'serial_number': str,
+                    'meraki_sn': str,
+                    'mac_addr': str,
+                    'conversion_status': str, 
+                    'current_mode': str   
+                }, 
+            }, 
+        },
     }
 
 
-# ================================
-# Parser for 'show meraki'
-# ================================
 class ShowMeraki(ShowMerakiSchema):
+    '''Parser for:
+        * 'show meraki'
+        * 'show meraki switch {switch}'
+    '''
 
-    cli_command = 'show meraki'
+    cli_command = ['show meraki', 'show meraki switch {switch}']
 
-    def cli(self, output=None):
-
+    def cli(self, switch = '',output=None):
         if output is None:
-            out = self.device.execute(self.cli_command)
+            if switch:
+                cmd = self.cli_command[1].format(switch=switch)
+            else:
+                cmd = self.cli_command[0]
+            out = self.device.execute(cmd)
         else:
             out = output
 
-
-#        1       C9300-24T  FJC2311T0DA Q5EE-DJYN-CRGR  4cbc.4812.3550   Registered         C9K-C
-
-#        2       C9300-24U  FJC1527A0BC N/A             4cbc.4812.2881   ACT2 write failed  C9K-C
-
-#        3       C9300-48UX FJC2317T0DT Q3EA-AZYP-WDFH  4cbc.4812.2501   Registered         C9K-C
-
-#        4       C9300-48TX FJC2311T0AJ N/A             5cbc.4812.3479   Timeout            C9K-C
-
-
-
-        p1 = re.compile ( r'^(?P<switch_no>\d+)(?P<PID>\s+\w+\d+\-\d+\w+)(?P<Serial_Number>\s+[a-zA-Z0-9]+)(?P<Meraki_SN>\s+[a-zA-Z0-9\/\-]+)(?P<Mac_Addr>\s+[a-zA-Z0-9\.]+)(?P<Conversion_Status>\s+[a-zA-Z0-9\s]+)(?P<Current_Mode>\s+[a-zA-Z0-9\-]+)')
+        # 1       C9300-24T  FJC2311T0DA Q5EE-DJYN-CRGR  4cbc.4812.3550   Registered         C9K-C
+        # 2       C9300-24U  FJC1527A0BC N/A             4cbc.4812.2881   ACT2 write failed  C9K-C
+        # 3       C9300-48UX FJC2317T0DT Q3EA-AZYP-WDFH  4cbc.4812.2501   Registered         C9K-C
+        # 4       C9300-48TX FJC2311T0AJ N/A             5cbc.4812.3479   N/A                C9K-C
+        p1 = re.compile(r'^(?P<switch_num>\d+)\s+(?P<pid>[\w-]+)\s+(?P<serial_number>[\w]+)\s+(?P<meraki_sn>[\w\/-]+)\s+(?P<mac_addr>[\w\.]+)\s+(?P<conversion_status>[\w\s\/-]+)\s+(?P<current_mode>[\w\-]+)$')
 
         parsed_dict = {}
-        switch_id_index = 1
+
         for line in out.splitlines():
             line = line.strip()
-            print("result after splitting the line :",line)
-            result = p1.match(line)
-            device_dict = parsed_dict.setdefault('meraki', {}) \
-                        .setdefault('switch', {}).setdefault(switch_id_index, {})
 
-            if result:
-
-                switch_id_index += 1
-
-                group = result.groupdict()
-                device_dict['switch_no'] = group['switch_no'].strip()
-                device_dict['PID'] = group['PID'].strip()
-                device_dict['Serial_Number'] = group['Serial_Number'].strip()
-                device_dict['Meraki_SN'] = group['Meraki_SN'].strip()
-                device_dict['Mac_Addr'] = group['Mac_Addr'].strip()
-                device_dict['Conversion_Status'] = group['Conversion_Status'].strip()
-                device_dict['Current_Mode'] = group['Current_Mode'].strip()
-                continue
+            # 1       C9300-24T  FJC2311T0DA Q5EE-DJYN-CRGR  4cbc.4812.3550   Registered         C9K-C
+            # 2       C9300-24U  FJC1527A0BC N/A             4cbc.4812.2881   ACT2 write failed  C9K-C
+            # 3       C9300-48UX FJC2317T0DT Q3EA-AZYP-WDFH  4cbc.4812.2501   Registered         C9K-C
+            # 4       C9300-48TX FJC2311T0AJ N/A             5cbc.4812.3479   N/A                C9K-C
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                device_dict = parsed_dict.setdefault('meraki', {}) \
+                    .setdefault('switch', {}).setdefault(group['switch_num'], {})
+                device_dict['switch_num'] = int(group['switch_num'])
+                device_dict['pid'] = group['pid']
+                device_dict['serial_number'] = group['serial_number']
+                device_dict['meraki_sn'] = group['meraki_sn']
+                device_dict['mac_addr'] = group['mac_addr']
+                device_dict['conversion_status'] = group['conversion_status'].strip()
+                device_dict['current_mode'] = group['current_mode']
 
         return parsed_dict
-
--- src/genie/libs/parser/iosxe/tests/ShowMeraki/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowMeraki/cli/equal/golden_output_expected.py
@@ -1,42 +1,42 @@
 expected_output = {
-    'meraki': {
-        'switch': {
-            1: {
-                'switch_no': '1',
-                'PID': 'C9300-24T',
-                'Serial_Number': 'FJC2311T0DA',
-                'Meraki_SN': 'Q5EE-DJYN-CRGR',
-                'Mac_Addr': '4cbc.4812.3550',
-                'Conversion_Status': 'Registered',
-                'Current_Mode': 'C9K-C'
+    "meraki": {
+        "switch": {
+            "1": {
+                "conversion_status": "Registered",
+                "current_mode": "C9K-C",
+                "mac_addr": "4cbc.4812.3550",
+                "meraki_sn": "Q5EE-DJYN-CRGR",
+                "pid": "C9300-24T",
+                "serial_number": "FJC2311T0DA",
+                "switch_num": 1
             },
-            2: {
-                'switch_no': '2',
-                'PID': 'C9300-24U',
-                'Serial_Number': 'FJC1527A0BC',
-                'Meraki_SN': 'N/A',
-                'Mac_Addr': '4cbc.4812.2881',
-                'Conversion_Status': 'ACT2 write failed',
-                'Current_Mode': 'C9K-C'
+            "2": {
+                "conversion_status": "ACT2 write failed",
+                "current_mode": "C9K-C",
+                "mac_addr": "4cbc.4812.2881",
+                "meraki_sn": "N/A",
+                "pid": "C9300-24U",
+                "serial_number": "FJC1527A0BC",
+                "switch_num": 2
             },
-            3: {
-                'switch_no': '3',
-                'PID': 'C9300-48UX',
-                'Serial_Number': 'FJC2317T0DT',
-                'Meraki_SN': 'Q3EA-AZYP-WDFH',
-                'Mac_Addr': '4cbc.4812.2501',
-                'Conversion_Status': 'Registered',
-                'Current_Mode': 'C9K-C'
+            "3": {
+                "conversion_status": "Registered",
+                "current_mode": "C9K-C",
+                "mac_addr": "4cbc.4812.2501",
+                "meraki_sn": "Q3EA-AZYP-WDFH",
+                "pid": "C9300-48UX",
+                "serial_number": "FJC2317T0DT",
+                "switch_num": 3
             },
-            4: {
-                'switch_no': '4',
-                'PID': 'C9300-48TX',
-                'Serial_Number': 'FJC2311T0AJ',
-                'Meraki_SN': 'N/A',
-                'Mac_Addr': '5cbc.4812.3479',
-                'Conversion_Status': 'Timeout',
-                'Current_Mode': 'C9K-C'
+            "4": {
+                "conversion_status": "Timeout",
+                "current_mode": "C9K-C",
+                "mac_addr": "5cbc.4812.3479",
+                "meraki_sn": "N/A",
+                "pid": "C9300-48TX",
+                "serial_number": "FJC2311T0AJ",
+                "switch_num": 4
             }
-         }
-     }
+        }
+    }
 }
