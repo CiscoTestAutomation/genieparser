790d0da56fa78f2109c76898fab9b5ff6c5a0ff2
Lukas McClelland <lumcclel@cisco.com>
2021-06-17 22:57:57 -0400

    Removed last meraki diff, adjusted formatting

--- src/genie/libs/parser/iosxe/show_power.py
+++ src/genie/libs/parser/iosxe/show_power.py
@@ -1,16 +1,21 @@
-"""show_platform.py
+"""  show_platform.py
 
+IOSXE parsers for the following show command:
+
+    * 'show stack-power'
+    * 'show stack-power budgeting'
+    * 'show power inline'
+    * 'show power inline {interface}'
+    * 'show power inline priority'
+    * 'show power inline priority {interface}'
+    * 'show power inline upoe-plus'
+    * 'show power inline upoe-plus {interface}'
 """
+
 import re
 
 from genie.metaparser import MetaParser
-from genie.metaparser.util.schemaengine import Schema, \
-                                         Any, \
-                                         Optional, \
-                                         Or, \
-                                         And, \
-                                         Default, \
-                                         Use
+from genie.metaparser.util.schemaengine import Any, Optional
 
 # import parser utils
 from genie.libs.parser.utils.common import Common
@@ -31,34 +36,6 @@ class ShowStackPowerSchema(MetaParser):
                 'power_supply_num': int
             },
         }
-    }
-class ShowStackPowerBudgetingSchema(MetaParser):
-    """Schema for show stack-power budgeting """
-    schema = {
-        'power_stack': {
-            Any(): {
-                'switch_num': str,
-                'power_supply_a': str,
-                'power_supply_b' : str,
-                'power_budget': str,
-                'allocated_power': str,
-                'poe_avail_pwr': str,
-                'consumed_pwr_sys_poe': str,
-                Optional('total_allocated'): int,
-                Optional('total_available'): int,
-                Optional('total_consumed'): str
-                 
-             },
-         },
-
-      'total_power': {
-          Any(): {
-            'total_allocated_pwr_sys_poe': str,
-            'total_poe_avail_pwr_sys_poe': str,
-            'total_consumed_pwr_sys_poe': str
-          }
-
-       }
     }
 
 
@@ -108,6 +85,33 @@ class ShowStackPower(ShowStackPowerSchema):
         return ret_dict
 
 
+class ShowStackPowerBudgetingSchema(MetaParser):
+    """Schema for show stack-power budgeting """
+    schema = {
+        'power_stack': {
+            Any(): {
+                'switch_num': str,
+                'power_supply_a': str,
+                'power_supply_b' : str,
+                'power_budget': str,
+                'allocated_power': str,
+                'poe_avail_pwr': str,
+                'consumed_pwr_sys_poe': str,
+                Optional('total_allocated'): int,
+                Optional('total_available'): int,
+                Optional('total_consumed'): str     
+            },
+        },
+        'total_power': {
+            Any(): {
+                'total_allocated_pwr_sys_poe': str,
+                'total_poe_avail_pwr_sys_poe': str,
+                'total_consumed_pwr_sys_poe': str
+            }
+        }
+    }
+
+
 class ShowStackPowerBudgeting(ShowStackPowerBudgetingSchema):
     """Parser for show stack-power Budgeting"""
 
@@ -115,7 +119,6 @@ class ShowStackPowerBudgeting(ShowStackPowerBudgetingSchema):
 
     def cli(self,output=None):
         if output is None:
-            # get output from device
             out = self.device.execute(self.cli_command)
         else:
             out = output
@@ -142,38 +145,38 @@ class ShowStackPowerBudgeting(ShowStackPowerBudgetingSchema):
         for line in out.splitlines():
             line = line.strip()
 
-             #  Power Stack         PS-A PS-B Power    Alloc    Poe_Avail Consumd Pwr
-             #     SW Name          (W) (W)   Budgt(W) Power(W) Pwr(W)    Sys/PoE(W)
-             # -- -------------------- ----- ----- -------- -------- -------- ------------
-             #      1 Powerstack-1   1100 0    1100       575     525      155/0
-             # -- -------------------- ----- ----- -------- -------- -------- ------------
 
+             # 1 Powerstack-1   1100 0    1100       575     525      155/0
             m = p1.match(line)
             if m:
                 group = m.groupdict()
                 name = group.pop('name')
-                stack_dict = ret_dict.setdefault('power_stack', {}).setdefault(name, {})
+                stack_dict = ret_dict.setdefault('power_stack', {})\
+                    .setdefault(name, {})
                 stack_dict['switch_num'] = group.pop('switch_num')
                 stack_dict['power_supply_a'] = group.pop('power_supply_a')
                 stack_dict['power_supply_b'] = group.pop('power_supply_b')
                 stack_dict['power_budget'] = group.pop('power_budget')
                 stack_dict['allocated_power'] = group.pop('allocated_power')
                 stack_dict['poe_avail_pwr'] = group.pop('poe_avail_pwr')
-                stack_dict['consumed_pwr_sys_poe'] = group.pop('consumed_pwr_sys_poe')
-                stack_dict.update(
-                       {k:int(v) for k, v in group.items()})
+                stack_dict['consumed_pwr_sys_poe'] = \
+                    group.pop('consumed_pwr_sys_poe')
+                stack_dict.update({k:int(v) for k, v in group.items()})
                 continue
 
-            #    Totals:                                  575    525      155/0
+            # Totals:                                  575    525      155/0
             m = p2.match(line)
             if m :
                 group = m.groupdict()
-                power_dict = ret_dict.setdefault('total_power', {}).setdefault('stackpower', {})
-                power_dict['total_allocated_pwr_sys_poe'] = group.pop('total_allocated_pwr_sys_poe')
-                power_dict['total_poe_avail_pwr_sys_poe'] = group.pop('total_poe_avail_pwr_sys_poe')
-                power_dict['total_consumed_pwr_sys_poe'] = group.pop('total_consumed_pwr_sys_poe')
-                power_dict.update(
-                       {k:int(v) for k, v in group.items()})
+                power_dict = ret_dict.setdefault('total_power', {})\
+                    .setdefault('stackpower', {})
+                power_dict['total_allocated_pwr_sys_poe'] = \
+                    group.pop('total_allocated_pwr_sys_poe')
+                power_dict['total_poe_avail_pwr_sys_poe'] = \
+                    group.pop('total_poe_avail_pwr_sys_poe')
+                power_dict['total_consumed_pwr_sys_poe'] = \
+                    group.pop('total_consumed_pwr_sys_poe')
+                power_dict.update({k:int(v) for k, v in group.items()})
                 continue
 
         return ret_dict
@@ -203,6 +206,94 @@ class ShowPowerInlineSchema(MetaParser):
         }
     }
 
+
+class ShowPowerInline(ShowPowerInlineSchema):
+    """Parser for show power inline
+                  show power inline <interface>"""
+
+    cli_command = ['show power inline', 'show power inline {interface}']
+
+    def cli(self, interface='', output=None):
+        if output is None:
+            if interface:
+                cmd = self.cli_command[1].format(interface=interface)
+            else:
+                cmd = self.cli_command[0]
+            out = self.device.execute(cmd)
+        else:
+            out = output
+
+        # initial return dictionary
+        ret_dict = {}
+
+        # initial regexp pattern
+        p1 = re.compile(r'^(?P<intf>[\w\-\/\.]+)\s*'
+                        r'(?P<admin_state>\w+)\s+'
+                        r'(?P<oper_state>\w+)\s+'
+                        r'(?P<power>[\d\.]+)\s+'
+                        r'(?P<device>(?=\S).*(?<=\S))\s+'
+                        r'(?P<class>[\w\/]+)\s+'
+                        r'(?P<max>[\d\.]+)$')
+
+
+        # 1          1550.0      147.0      1403.0
+        p2 = re.compile(r'^(?P<module>\d+)\s+'
+                         '(?P<available>[\d\.]+)\s+'
+                         '(?P<used>[\d\.]+)\s+'
+                         '(?P<remaining>[\d\.]+)\s*$')
+
+        # Available:1170.0(w)  Used:212.2(w)  Remaining:957.8(w)
+        p3 = re.compile(r'^\s*[Aa]vailable\:(?P<available>[\d\.]+)\(\w+\)\s+'
+                         '[Uu]sed\:(?P<used>[\d\.]+)\(\w+\)\s+'
+                         '[Rr]emaining\:(?P<remaining>[\d\.]+)\(\w+\)\s*$')
+
+        for line in out.splitlines():
+            line = line.strip()
+            
+            # Gi1/0/13  auto   on         15.4    AIR-CAP2602I-A-K9   3     30.0
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                intf = Common.convert_intf_name(group.pop('intf'))
+                intf_dict = ret_dict.setdefault('interface', {}).setdefault(intf, {})
+                intf_dict['power'] = float(group.pop('power'))
+                intf_dict['max'] = float(group.pop('max'))
+                intf_dict.update({k: v for k, v in group.items() if 'n/a' not in v})
+                continue
+
+            # 1          1550.0      147.0      1403.0
+            m = p2.match(line)
+            if m:
+                group = m.groupdict()
+
+                module = group.pop('module')
+                stat_dict = ret_dict.setdefault('watts', {}).setdefault(module, {})
+
+                stat_dict['module'] = module
+                stat_dict['available'] = float(group.pop('available'))
+                stat_dict['used'] = float(group.pop('used'))
+                stat_dict['remaining'] = float(group.pop('remaining'))
+                continue
+
+            # Available:1170.0(w)  Used:212.2(w)  Remaining:957.8(w)
+            m = p3.match(line)
+            if m:
+                group = m.groupdict()
+
+                stat_dict = ret_dict.setdefault('watts', {}).setdefault('0', {})
+
+                stat_dict['module'] = '0'
+                stat_dict['available'] = float(group.pop('available'))
+                stat_dict['used'] = float(group.pop('used'))
+                stat_dict['remaining'] = float(group.pop('remaining'))
+            
+        # Remove statistics if we don't have any interfaces
+        if 'interface' not in ret_dict and 'watts' in ret_dict:
+            ret_dict.pop('watts', None)
+
+        return ret_dict
+
+
 class ShowPowerInlinePrioritySchema(MetaParser):
     """Schema for show power inline priority """
     schema = {
@@ -228,18 +319,18 @@ class ShowPowerInlinePriority(ShowPowerInlineSchema):
                 cmd = self.cli_command[1].format(interface=interface)
             else:
                 cmd = self.cli_command[0]
-
-            # get output from device
             out = self.device.execute(cmd)
         else:
             out = output
 
         # initial return dictionary
         ret_dict = {}
+
+        #Gi1/0/1    auto   off        low
         p1 = re.compile(r'^(?P<intf>\w+\d+\/\d+\/\d+)\s*'
-                         '(?P<admin_state>\w+)\s+'
-                         '(?P<oper_state>\w+)\s+'
-                         '(?P<priority>\w+)$')
+                        r'(?P<admin_state>\w+)\s+'
+                        r'(?P<oper_state>\w+)\s+'
+                        r'(?P<priority>\w+)$')
 
         for line in out.splitlines():
             line = line.strip()
@@ -259,104 +350,6 @@ class ShowPowerInlinePriority(ShowPowerInlineSchema):
         return ret_dict
 
 
-class ShowPowerInline(ShowPowerInlineSchema):
-    """Parser for show power inline
-                  show power inline <interface>"""
-
-    cli_command = ['show power inline', 'show power inline {interface}']
-
-    def cli(self, interface='', output=None):
-        if output is None:
-            if interface:
-                cmd = self.cli_command[1].format(interface=interface)
-            else:
-                cmd = self.cli_command[0]
-
-            # get output from device
-            out = self.device.execute(cmd)
-        else:
-            out = output
-
-        # initial return dictionary
-        ret_dict = {}
-
-        # initial regexp pattern
-        p1 = re.compile(r'^(?P<intf>[\w\-\/\.]+)\s*'
-                         '(?P<admin_state>\w+)\s+'
-                         '(?P<oper_state>\w+)\s+'
-                         '(?P<power>[\d\.]+)\s+'
-                         '(?P<device>(?=\S).*(?<=\S))\s+'
-                         '(?P<class>[\w\/]+)\s+'
-                         '(?P<max>[\d\.]+)$')
-
-        # Module   Available     Used     Remaining
-        #          (Watts)     (Watts)    (Watts)
-        # ------   ---------   --------   ---------
-        # 1          1550.0      147.0      1403.0
-        p2 = re.compile(r'^(?P<module>\d+)\s+'
-                         '(?P<available>[\d\.]+)\s+'
-                         '(?P<used>[\d\.]+)\s+'
-                         '(?P<remaining>[\d\.]+)\s*$')
-
-        # Available:1170.0(w)  Used:212.2(w)  Remaining:957.8(w)
-        p3 = re.compile(r'^\s*[Aa]vailable\:(?P<available>[\d\.]+)\(\w+\)\s+'
-                         '[Uu]sed\:(?P<used>[\d\.]+)\(\w+\)\s+'
-                         '[Rr]emaining\:(?P<remaining>[\d\.]+)\(\w+\)\s*$')
-
-        for line in out.splitlines():
-            line = line.strip()
-            
-            # Interface Admin  Oper       Power   Device              Class Max
-            #                             (Watts)                            
-            # --------- ------ ---------- ------- ------------------- ----- ----
-            # Gi1/0/13  auto   on         15.4    AIR-CAP2602I-A-K9   3     30.0
-            m = p1.match(line)
-            if m:
-                group = m.groupdict()
-                intf = Common.convert_intf_name(group.pop('intf'))
-                intf_dict = ret_dict.setdefault('interface', {}).setdefault(intf, {})
-                intf_dict['power'] = float(group.pop('power'))
-                intf_dict['max'] = float(group.pop('max'))
-                intf_dict.update({k: v for k, v in group.items() if 'n/a' not in v})
-
-                continue
-
-            # Module   Available     Used     Remaining
-            #          (Watts)     (Watts)    (Watts)
-            # ------   ---------   --------   ---------
-            # 1          1550.0      147.0      1403.0
-            m = p2.match(line)
-            if m:
-                group = m.groupdict()
-
-                module = group.pop('module')
-                stat_dict = ret_dict.setdefault('watts', {}).setdefault(module, {})
-
-                stat_dict['module'] = module
-                stat_dict['available'] = float(group.pop('available'))
-                stat_dict['used'] = float(group.pop('used'))
-                stat_dict['remaining'] = float(group.pop('remaining'))
-
-                continue
-
-            # Available:1170.0(w)  Used:212.2(w)  Remaining:957.8(w)
-            m = p3.match(line)
-            if m:
-                group = m.groupdict()
-
-                stat_dict = ret_dict.setdefault('watts', {}).setdefault('0', {})
-
-                stat_dict['module'] = '0'
-                stat_dict['available'] = float(group.pop('available'))
-                stat_dict['used'] = float(group.pop('used'))
-                stat_dict['remaining'] = float(group.pop('remaining'))
-            
-        # Remove statistics if we don't have any interfaces
-        if 'interface' not in ret_dict and 'watts' in ret_dict:
-            ret_dict.pop('watts', None)
-
-        return ret_dict
-
 class ShowPowerInlineUpoePlusSchema(MetaParser):
     """Schema for show power inline upoe-plus """
     schema = {
@@ -372,6 +365,8 @@ class ShowPowerInlineUpoePlusSchema(MetaParser):
             }
         }
     }
+
+
 class ShowPowerInlineUpoePlus(ShowPowerInlineUpoePlusSchema):
     """Parser for show power inline upoe-plus
                   show power inline upoe-plus <interface>"""
@@ -393,20 +388,17 @@ class ShowPowerInlineUpoePlus(ShowPowerInlineUpoePlusSchema):
         # initial return dictionary
         ret_dict = {}
 
-        # initial regexp pattern
-
+        # Gi1/0/4     auto   SP   on            4.0       3.8       1       Ieee PD
+        # Gi1/0/15    auto   SS   on,on         60.0      10.5      6       Ieee PD
+        # Gi1/0/23    auto   DS   on,on         45.4      26.9      3,4     Ieee PD
         p1 = re.compile(r'^(?P<intf>\w+\d+\/\d+\/\d+)\s+(?P<admin_state>[a-zA-Z]+)\s+(?P<type>\w+)\s+(?P<oper_state>[\,\w+]+)\s+(?P<allocated_power>[\d\.]+)\s+(?P<utilized_power>[\d\.]+)\s+(?P<class>[\w\,\/]+)\s+(?P<device>(?=\S).*(?<=\S))$')
-
 
         for line in out.splitlines():
             line = line.strip()
   
-#           ----------- ------ ---- ----------  --------- --------- ------- -----------
-#		Gi1/0/4     auto   SP   on            4.0       3.8       1       Ieee PD
-#		Gi1/0/15    auto   SS   on,on         60.0      10.5      6       Ieee PD
-#		Gi1/0/23    auto   DS   on,on         45.4      26.9      3,4     Ieee PD
-
-
+            # Gi1/0/4     auto   SP   on            4.0       3.8       1       Ieee PD
+            # Gi1/0/15    auto   SS   on,on         60.0      10.5      6       Ieee PD
+            # Gi1/0/23    auto   DS   on,on         45.4      26.9      3,4     Ieee PD
             m = p1.match(line)
             if m:
                 group = m.groupdict()
@@ -420,7 +412,6 @@ class ShowPowerInlineUpoePlus(ShowPowerInlineUpoePlusSchema):
                 intf_dict['class'] = group.pop('class')
                 intf_dict['device'] = group.pop('device')
                 intf_dict.update({k: v for k, v in group.items() if 'n/a' not in v})
-
                 continue
-        return ret_dict
 
+        return ret_dict
