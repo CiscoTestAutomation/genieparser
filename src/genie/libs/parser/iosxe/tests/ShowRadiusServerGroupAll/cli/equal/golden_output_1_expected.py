25af83a832a0fd0eedc79812cdca4feb5662633f
Rajesh Reddy C -X (rreddyc - TATA CONSULTANCY SERVICES LIMITED at Cisco) <rreddyc@cisco.com>
2021-07-05 16:20:39 +0530

    Update golden_output_1_expected.py

--- src/genie/libs/parser/iosxe/tests/ShowRadiusServerGroupAll/cli/equal/golden_output_1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowRadiusServerGroupAll/cli/equal/golden_output_1_expected.py
@@ -1,55 +1,66 @@
 expected_output = {
     "radius": {
-        "memlocks": "1",
-        "server(10.106.26.213:1812,1813,mgmt-rad)_transactions": {
-            "acct": "0",
-            "authen": "0",
-            "author": "0",
-            "keywrap_enabled": "FALSE",
-            "server_auto_test_enabled": "FALSE",
+        "memlocks": 1,
+        "server": {
+            "10.106.26.213": {
+                "acct_port": 1813,
+                "auth_port": 1812,
+                "auto_test_enabled": False,
+                "keywrap_enabled": False,
+                "server_name": "mgmt-rad",
+                "transactions": {"acct": 0, "authen": 0, "author": 0},
+            },
+            "121.0.0.1": {
+                "acct_port": 1813,
+                "auth_port": 1812,
+                "auto_test_enabled": False,
+                "keywrap_enabled": False,
+                "server_name": "data-rad",
+                "transactions": {"acct": 0, "authen": 0, "author": 0},
+            },
+            "44AA::1": {
+                "acct_port": 1813,
+                "auth_port": 1812,
+                "auto_test_enabled": False,
+                "keywrap_enabled": False,
+                "server_name": "ipv6-rad",
+                "transactions": {"acct": 0, "authen": 0, "author": 0},
+            },
         },
-        "server(121.0.0.1:1812,1813,data-rad)_transactions": {
-            "acct": "0",
-            "authen": "0",
-            "author": "0",
-            "keywrap_enabled": "FALSE",
-            "server_auto_test_enabled": "FALSE",
-        },
-        "server(44aa::1:1812,1813,ipv6-rad)_transactions": {
-            "acct": "0",
-            "authen": "0",
-            "author": "0",
-            "keywrap_enabled": "FALSE",
-            "server_auto_test_enabled": "FALSE",
-        },
-        "sg_unconfigured": "FALSE",
-        "sharecount": "1",
+        "sg_unconfigured": False,
+        "sharecount": 1,
         "type": "standard",
     },
     "radius-1": {
-        "memlocks": "1",
-        "server(10.106.26.213:1812,1813,mgmt-rad)_transactions": {
-            "acct": "4",
-            "authen": "0",
-            "author": "0",
-            "keywrap_enabled": "FALSE",
-            "server_auto_test_enabled": "FALSE",
+        "memlocks": 1,
+        "server": {
+            "10.106.26.213": {
+                "acct_port": 1813,
+                "auth_port": 1812,
+                "auto_test_enabled": False,
+                "keywrap_enabled": False,
+                "server_name": "mgmt-rad",
+                "transactions": {"acct": 4, "authen": 0, "author": 0},
+            }
         },
-        "sg_unconfigured": "FALSE",
-        "sharecount": "1",
+        "sg_unconfigured": False,
+        "sharecount": 1,
         "type": "standard",
     },
     "radius-2": {
-        "memlocks": "1",
-        "server(121.0.0.1:1812,1813,data-rad)_transactions": {
-            "acct": "0",
-            "authen": "0",
-            "author": "0",
-            "keywrap_enabled": "FALSE",
-            "server_auto_test_enabled": "FALSE",
+        "memlocks": 1,
+        "server": {
+            "121.0.0.1": {
+                "acct_port": 1813,
+                "auth_port": 1812,
+                "auto_test_enabled": False,
+                "keywrap_enabled": False,
+                "server_name": "data-rad",
+                "transactions": {"acct": 0, "authen": 0, "author": 0},
+            }
         },
-        "sg_unconfigured": "FALSE",
-        "sharecount": "1",
+        "sg_unconfigured": False,
+        "sharecount": 1,
         "type": "standard",
     },
 }
