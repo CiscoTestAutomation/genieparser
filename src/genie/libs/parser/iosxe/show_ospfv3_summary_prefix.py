6a8804c411bb4f8fe4273b4191e0cb7f45bd7017
Patrick A Soltani <pasoltan@cisco.com>
2021-07-06 09:35:11 -0700

    updated the schema per review and implemented the changes

--- src/genie/libs/parser/iosxe/show_ospfv3_summary_prefix.py
+++ src/genie/libs/parser/iosxe/show_ospfv3_summary_prefix.py
@@ -1,7 +1,7 @@
 """ ShowOspfv3SummaryPrefix.py
 
 IOSXE parser for the following show command:
-    *show ospfv3 summary-prefix
+    * show ospfv3 summary-prefix
 """
 
 # python
@@ -10,7 +10,6 @@ import re
 # metaparser
 from genie.metaparser import MetaParser
 from genie.metaparser.util.schemaengine import Schema, Any, Or, Optional, Use, Default
-from genie.libs.parser.utils.common import Common
 
 # ===============================================
 # Schema for 'show ospfv3 summary-prefix'
@@ -18,20 +17,26 @@ from genie.libs.parser.utils.common import Common
 # ===============================================
 
 class ShowOspfv3SummaryPrefixSchema(MetaParser):
-
     schema = {
-            Optional('ospf_id'): int,
-            Optional('add_family'): str,
-            Optional('rtr_id'): str,
-            Optional('null_metric'): str,
-            Optional('sum_prefix'): str,
-            Optional('sum_type'): str,
-            Optional('sum_tag'): int
+        'process_id': {
+            Any(): {
+                'add_family': str,
+                'rtr_id': str,
+                'null_metric': str,
+                'summary': {
+                    Any(): {
+                        'sum_type': str,
+                        'sum_tag': int
+                    },
+                },
+            },
+        },
     }
 
-# ================================
+
+# ====================================
 # Parser for 'ShowOspfv3SummaryPrefix'
-# ================================
+# ====================================
 
 class ShowOspfv3SummaryPrefix(ShowOspfv3SummaryPrefixSchema):
     """
@@ -57,7 +62,8 @@ class ShowOspfv3SummaryPrefix(ShowOspfv3SummaryPrefixSchema):
         ret_dict = {}
 
         # OSPFv3 10000 address-family ipv6 (router-id 10.2.2.21)
-        p1 = re.compile(r'^OSPFv3 +(?P<ospf_id>(\d+)) +address-family +(?P<add_family>(\S+)) +\(router-id +(?P<rtr_id>(\S+))\)')
+        p1 = re.compile(
+            r'^OSPFv3 +(?P<ospf_id>(\d+)) +address-family +(?P<add_family>(\S+)) +\(router-id +(?P<rtr_id>(\S+))\)')
 
         # 10:2::/96           Metric <unreachable>
         p2 = re.compile(r'^[\d:/]+\s+Metric\s+(?P<null_metric>(\S+$))')
@@ -65,29 +71,38 @@ class ShowOspfv3SummaryPrefix(ShowOspfv3SummaryPrefixSchema):
         # 10:2:2::/96         Metric 111, External metric type 2, Tag 111
         p3 = re.compile(r'^(?P<sum_prefix>(\S+)) +.* type +(?P<sum_type>(\d)),\s+Tag +(?P<sum_tag>(\S+))')
 
+        #ret_dict = ret_dict.setdefault('process_id')
+
         for line in out.splitlines():
             line = line.strip()
             m = p1.search(line)
             if m:
                 group = m.groupdict()
-                ret_dict ['ospf_id'] = int(group ['ospf_id'].strip())
-                ret_dict ['add_family'] = group ['add_family'].strip()
-                ret_dict ['rtr_id'] = group ['rtr_id'].strip()
+                ret_dict['process_id'] = {}
+                ospf_id = group['ospf_id'].strip()
+                ret_dict['process_id'][ospf_id] = {}
+                ret_dict['process_id'][ospf_id]['summary'] = {}
+                ret_dict['process_id'][ospf_id]['add_family'] = group['add_family'].strip()
+                ret_dict['process_id'][ospf_id]['rtr_id'] = group['rtr_id'].strip()
                 continue
 
             m = p2.search(line)
             if m:
                 group = m.groupdict()
-                ret_dict ['null_metric'] = group ['null_metric'].strip()
+                ret_dict['process_id'][ospf_id]['null_metric'] = group['null_metric'].strip()
                 continue
 
             m = p3.search(line)
             if m:
                 group = m.groupdict()
-                ret_dict ['sum_prefix'] = group ['sum_prefix'].strip()
-                ret_dict ['sum_type'] = group ['sum_type'].strip()
-                ret_dict ['sum_tag'] = int(group ['sum_tag'].strip())
-                continue
+                if group['sum_prefix'].strip():
+                    prefix = group['sum_prefix'].strip()
+                    ret_dict['process_id'][ospf_id]['summary'][prefix] = {}
+                    ret_dict['process_id'][ospf_id]['summary'][prefix]['sum_type'] = group['sum_type'].strip()
+                    ret_dict['process_id'][ospf_id]['summary'][prefix]['sum_tag'] = int(group['sum_tag'].strip())
+                    continue
+
+        import pdb
+        #pdb.set_trace()
 
         return ret_dict
-
--- src/genie/libs/parser/iosxe/tests/ShowOspfv3SummaryPrefix/cli/equal/golden_output_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowOspfv3SummaryPrefix/cli/equal/golden_output_expected.py
@@ -1,9 +1,15 @@
 expected_output = {
-   'ospf_id': 10000,
-   'add_family': 'ipv6',
-   'rtr_id': '10.2.2.21',
-   'null_metric': '<unreachable>',
-   'sum_prefix': '10:2:2::/96',
-   'sum_type': '2',
-   'sum_tag': 111
+    'process_id': {
+        '10000': {
+            'add_family': 'ipv6',
+            'rtr_id': '10.2.2.21',
+            'null_metric': '<unreachable>',
+            'summary': {
+                '10:2:2::/96': {
+                    'sum_type': '2',
+                    'sum_tag': 111,
+                },
+            },
+        }
+    },
 }
