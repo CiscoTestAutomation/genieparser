4174777b0740d1dd90ac88a862899c01b448e0a5
Rajkumar Subramanian <rajksubr@cisco.com>
2021-07-13 05:05:41 -0700

    added test report

---
+++ src/genie/libs/parser/nxos/tests/ShowIpMrouteSummaryVrfAll/cli/equal/golden_output_expected.py
@@ -0,0 +1,20 @@
+expected_output = {
+        'vrf': {
+            'default': {
+                'address_family': {
+                    'ipv4': {
+                        'count_multicast_starg': '41',
+                        'count_multicast_sg': '123'
+                            }
+                        }
+                       },        
+            'vxlan-1009': {
+                'address_family': {
+                    'ipv4': {
+                        'count_multicast_starg': '11',
+                        'count_multicast_sg': '18'
+                            }
+                        }
+                       }        
+                     }
+                                    }
