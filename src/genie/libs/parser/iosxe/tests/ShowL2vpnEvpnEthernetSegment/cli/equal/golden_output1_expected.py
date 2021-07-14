d49b3b73dda45d163c31132948927ee85f97d516
Dianbin Jin <jojin@cisco.com>
2021-07-14 23:40:56 +0000

    address comment

--- src/genie/libs/parser/iosxe/tests/ShowL2vpnEvpnEthernetSegment/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowL2vpnEvpnEthernetSegment/cli/equal/golden_output1_expected.py
@@ -4,13 +4,13 @@
 expected_output = {
     'esi': {
         "03AA.AABB.BBCC.CC00.0001": {
-            "port": "Et0/2",
+            "port": "Ethernet0/2",
             "redundancy_mode": "single-active",
             "df_wait_time": 3,
             "split_horizon_label": 16,
         },
         "03AA.BBCC.DDEE.FF00.0002": {
-            "port": "Et0/3",
+            "port": "Ethernet0/3",
             "redundancy_mode": "all-active",
             "df_wait_time": 3,
             "split_horizon_label": 17,
--- src/genie/libs/parser/iosxe/tests/ShowL2vpnEvpnEthernetSegmentDetail/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowL2vpnEvpnEthernetSegmentDetail/cli/equal/golden_output1_expected.py
@@ -3,32 +3,32 @@
 
 expected_output = {
     "03AA.BB00.0000.0200.0001": {
-        "interface": "Po1",
+        "interface": "Port-channel1",
         "redundancy_mode": "all-active",
         "df_wait_time": 3,
         "split_horizon_label": 16,
         "state": "Ready",
         "encap_type": "mpls",
-        "ordinal": "1",
+        "ordinal": 1,
         "core_isolation": "No",
         "rd": "4.4.4.3:1",
-        "export_rt": "100:2",
+        "export_rt": ["100:2"],
         "forwarder_list": [
             "3.3.3.3",
             "4.4.4.3"
         ],
     },
     "03AA.AABB.BBCC.CC00.0001": {
-        "interface": "Et0/2",
+        "interface": "Ethernet0/2",
         "redundancy_mode": "single-active",
         "df_wait_time": 3,
         "split_horizon_label": 16,
         "state": "Ready",
         "encap_type": "mpls",
-        "ordinal": "0",
+        "ordinal": 0,
         "core_isolation": "No",
         "rd": "10.10.10.10:1",
-        "export_rt": "1:1 1:2 1:3 1:65533 1:65534",
+        "export_rt": ["1:1", "1:2", "1:3", "1:65533", "1:65534"],
         "forwarder_list": [
             "10.10.10.10",
             "26.26.26.26"
