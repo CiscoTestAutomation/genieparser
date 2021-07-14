057525e1ad773b6dd9fe2210299c457991e60d97
Adam Payzant <apayzant@cisco.com>
2021-07-14 11:26:38 -0400

    Added parser for show device-tracking features and show ipv6 destination-guard

---
+++ changelog/undistributed/changelog_show_device_tracking_mac_and_ipv6_20210714.rst
@@ -0,0 +1,11 @@
+--------------------------------------------------------------------------------
+                            New
+--------------------------------------------------------------------------------
+* IOSXE
+    * Added ShowDeviceTrackingMac
+        * show device-tracking mac
+        * show device-tracking mac details
+        * show device-tracking mac {mac}
+        * show device-tracking mac {mac} details
+    * Added ShowIpv6SourceGuard
+        * show ipv6 destination-guard policy {policy_name}
---
+++ src/genie/libs/parser/iosxe/show_ipv6_destination-guard_policy.py
@@ -0,0 +1,103 @@
+import re
+import pprint
+
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Any, Optional
+
+# ==================================
+# Schema for:
+#  * 'show ipv6 destination-guard policy <policy name>'
+# ==================================
+class ShowIpv6DestinationGuardPolicySchema(MetaParser):
+    """Schema for show ipv6 destination-guard policy <policy>"""
+    schema = {
+        'enforcement': str,
+        'entries': {
+            int: {
+                'target': str,
+                'type': str,
+                'policy': str,
+                'feature': str,
+                'target_type': str,
+                'range': str,
+            }
+        }
+    }
+
+# ==================================
+# Parser for:
+#  * 'show ipv6 destination-guard policy <policy name>'
+# ==================================
+class ShowIpv6DestinationGuardPolicy(ShowIpv6DestinationGuardPolicySchema):
+    """Parser for show ipv6 destination-guard policy <policy>"""
+    cli_command = 'show ipv6 destination-guard policy {policy}'
+
+    def cli(self, policy, output=None):
+        if output is None:
+            cmd = self.cli_command.format(policy=policy)
+            out = self.device.execute(cmd)
+        else:
+            out = output
+
+        destination_guard_policy_dict = {}
+        # Destination guard policy poll configuration: 
+        #   enforcement stressed
+        # Policy poll is applied on the following targets: 
+        # Target               Type  Policy               Feature        Target range
+        # Twe1/0/1             PORT  poll                 Destination Guard vlan all
+        # vlan 5               VLAN  poll                 Destination Guard vlan all
+        # vlan 38              VLAN  poll                 Destination Guard vlan all
+        # vlan 39              VLAN  poll                 Destination Guard vlan all
+
+
+        #   enforcement always 
+        enforcement_capture = re.compile(
+            r"enforcement\s+(?P<enforcement>\S+)$"
+        )
+        # vlan 5               VLAN  poll                 Destination Guard vlan all
+        entry_capture = re.compile(
+            r"^(?P<target>((\S+\s\d+)|(\S+/\d+(/\d+)?)))\s+"
+            r"(?P<type>\S+)\s+"
+            r"(?P<policy>\S+)\s+"
+            r"(?P<feature>\S+\s\S+)\s+"
+            r"(?P<target_type>\S+)\s+"
+            r"(?P<range>\S+)$"
+        )
+
+        entry_counter = 0
+        for line in out.splitlines():
+            line = line.strip()
+
+            if enforcement_capture.match(line):
+                match = enforcement_capture.match(line)
+                groups = match.groupdict()
+
+                enforcement = groups['enforcement']
+
+                destination_guard_policy_dict['enforcement'] = enforcement
+                continue
+            elif entry_capture.match(line):
+                entry_counter += 1
+                match = entry_capture.match(line)
+                groups = match.groupdict()
+
+                target = groups['target']
+                type = groups['type']
+                policy = groups['policy']
+                feature = groups['feature']
+                target_type = groups['target_type']
+                rang = groups['range']
+
+                if not destination_guard_policy_dict.get('entries', {}):
+                    destination_guard_policy_dict['entries'] = {}
+                destination_guard_policy_dict['entries'][entry_counter] = {}
+                destination_guard_policy_dict['entries'][entry_counter]['target'] = target
+                destination_guard_policy_dict['entries'][entry_counter]['type'] = type
+                destination_guard_policy_dict['entries'][entry_counter]['policy'] = policy
+                destination_guard_policy_dict['entries'][entry_counter]['feature'] = feature
+                destination_guard_policy_dict['entries'][entry_counter]['target_type'] = target_type
+                destination_guard_policy_dict['entries'][entry_counter]['range'] = rang
+
+                continue
+        pprint.pprint(destination_guard_policy_dict)
+        return destination_guard_policy_dict
