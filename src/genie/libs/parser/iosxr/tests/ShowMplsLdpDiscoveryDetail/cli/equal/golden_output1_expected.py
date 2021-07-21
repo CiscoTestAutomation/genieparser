aca782ca2c7798d02dd9f5d62b1ac86d579d8d27
Arash Khosravi <akhosrav@cisco.com>
2021-07-21 15:06:55 -0700

    Modify golden output

--- src/genie/libs/parser/iosxr/tests/ShowMplsLdpDiscoveryDetail/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxr/tests/ShowMplsLdpDiscoveryDetail/cli/equal/golden_output1_expected.py
@@ -7,13 +7,13 @@ expected_output = {
                 "interfaces": [
                     {
                         "interface": "HundredGigE0/0/0/0",
-                        "xmit": True,
-                        "recv": True,
+                        "xmit": 'True',
+                        "recv": 'True',
                         "vrf_hex": "0x60000000",
                         "source_ip_addr": "10.10.10.1",
                         "transport_ip_addr": "17.17.17.17",
-                        "hello_interval_ms": 5000,
-                        "hello_due_time_ms": 563,
+                        "hello_interval_ms": '5000',
+                        "hello_due_time_ms": '563',
                         "quick_start": "enabled",
                         "ldp_id": {
                             "network_addr": "141.141.141.141:0",
@@ -21,10 +21,10 @@ expected_output = {
                                 {
                                     "source_ip_addr": "10.10.10.2",
                                     "transport_ip_addr": "141.141.141.141",
-                                    "holdtime_sec": 15,
-                                    "proposed_local": 15,
-                                    "proposed_peer": 15,
-                                    "expiring_in": 10.4,
+                                    "holdtime_sec": '15',
+                                    "proposed_local": '15',
+                                    "proposed_peer": '15',
+                                    "expiring_in": '10.4',
                                     "established_date": "Mar 11 09:41:07.477",
                                     "established_elapsed": "00:33:46",
                                     "last_session_connection_failures": [
@@ -45,24 +45,24 @@ expected_output = {
                     },
                     {
                         "interface": "TenGigE0/2/0/0",
-                        "xmit": True,
-                        "recv": False,
+                        "xmit": 'True',
+                        "recv": 'False',
                         "vrf_hex": "0x60000000",
                         "source_ip_addr": "100.20.0.1",
                         "transport_ip_addr": "17.17.17.17",
-                        "hello_interval_ms": 5000,
-                        "hello_due_time_ms": 3400,
+                        "hello_interval_ms": '5000',
+                        "hello_due_time_ms": '3400',
                         "quick_start": "enabled"
                     },
                     {
                         "interface": "TenGigE0/2/0/3",
-                        "xmit": False,
-                        "recv": True,
+                        "xmit": 'False',
+                        "recv": 'True',
                         "vrf_hex": "0x60000000",
                         "source_ip_addr": "100.10.0.1",
                         "transport_ip_addr": "17.17.17.17",
-                        "hello_interval_ms": 5000,
-                        "hello_due_time_ms": 3600,
+                        "hello_interval_ms": '5000',
+                        "hello_due_time_ms": '3600',
                         "quick_start": "enabled",
                         "ldp_id": {
                             "network_addr": "107.107.107.107:0",
@@ -70,10 +70,10 @@ expected_output = {
                                 {
                                     "source_ip_addr": "100.10.0.2",
                                     "transport_ip_addr": "107.107.107.107",
-                                    "holdtime_sec": 15,
-                                    "proposed_local": 15,
-                                    "proposed_peer": 15,
-                                    "expiring_in": 10.3,
+                                    "holdtime_sec": '15',
+                                    "proposed_local": '15',
+                                    "proposed_peer": '15',
+                                    "expiring_in": '10.3',
                                     "established_date": "Mar 11 09:31:57.896",
                                     "established_elapsed": "00:42:55"
                                 }
