884bbb12bd9f77afda4833803742739517a255ab
domachad <domachad@cisco.com>
2021-07-23 15:28:12 -0400

    Fixed IOSXE ShowPlatformSoftwareYangManagementProcessState

---
+++ src/genie/libs/parser/iosxe/tests/ShowPlatformSoftwareYangManagementProcessState/cli/equal/golden_output2_expected.py
@@ -0,0 +1,33 @@
+expected_output = {
+    "confd-status": "Not Running",
+    "processes": {
+        "nesd": {
+            "status": "Not Running",
+            "state": "Active"
+        },
+        "syncfd": {
+            "status": "Not Running",
+            "state": "Active"
+        },
+        "ncsshd": {
+            "status": "Not Running",
+            "state": "Not Applicable"
+        },
+        "dmiauthd": {
+            "status": "Not Running",
+            "state": "Active"
+        },
+        "nginx": {
+            "status": "Running",
+            "state": "Not Applicable"
+        },
+        "ndbmand": {
+            "status": "Not Running",
+            "state": "Down"
+        },
+        "pubd": {
+            "status": "Running",
+            "state": "Reset"
+        }
+    }
+}
