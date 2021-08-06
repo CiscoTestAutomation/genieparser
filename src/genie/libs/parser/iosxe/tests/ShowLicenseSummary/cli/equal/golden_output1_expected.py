c59c4c2d4f090adead462f70d5b342de61d7d192
dannywade <danny.wade35@gmail.com>
2021-08-06 11:01:02 -0400

    updated expected output

--- src/genie/libs/parser/iosxe/tests/ShowLicenseSummary/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowLicenseSummary/cli/equal/golden_output1_expected.py
@@ -1,12 +1,12 @@
 expected_output = {
    'license_usage':{
       'network-advantage':{
-         'entitlement':'(C9300-48 Network Advan...)',
+         'entitlement':'C9300-48 Network Advan...',
          'count':'1',
          'status':'IN USE'
       },
       'dna-advantage':{
-         'entitlement':'(C9300-48 DNA Advantage)',
+         'entitlement':'C9300-48 DNA Advantage',
          'count':'1',
          'status':'IN USE'
       }
