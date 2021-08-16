144af2fadd7b4dcef7773a8373fa1c7e7ea287cf
DIVYA PURANAM <dipurana@cisco.com>
2021-08-16 18:19:30 +0530

    added changes from review

--- src/genie/libs/parser/nxos/tests/ShowHsrpEventHistoryDebugs/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/nxos/tests/ShowHsrpEventHistoryDebugs/cli/equal/golden_output1_expected.py
@@ -1,144 +1,146 @@
 expected_output = {
-    "debug_logs": {
-        "1": {
-            "date": "2021 May 18",
-            "time": "20:19:01.952265",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000556"
-        },
-        "2": {
-            "date": "2021 May 18",
-            "time": "20:19:01.952260",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "3": {
-            "date": "2021 May 18",
-            "time": "20:19:01.951701",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "4": {
-            "date": "2021 May 18",
-            "time": "20:19:01.921566",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000575"
-        },
-        "5": {
-            "date": "2021 May 18",
-            "time": "20:19:01.921561",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "6": {
-            "date": "2021 May 18",
-            "time": "20:19:01.920983",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "7": {
-            "date": "2021 May 18",
-            "time": "20:17:47.755495",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000772"
-        },
-        "8": {
-            "date": "2021 May 18",
-            "time": "20:17:47.755489",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "9": {
-            "date": "2021 May 18",
-            "time": "20:17:47.754709",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "10": {
-            "date": "2021 May 18",
-            "time": "20:17:47.738852",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000523"
-        },
-        "11": {
-            "date": "2021 May 18",
-            "time": "20:17:47.738847",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "12": {
-            "date": "2021 May 18",
-            "time": "20:17:47.738321",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "13": {
-            "date": "2021 May 18",
-            "time": "20:16:35.011020",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000566"
-        },
-        "14": {
-            "date": "2021 May 18",
-            "time": "20:16:35.011013",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "15": {
-            "date": "2021 May 18",
-            "time": "20:16:35.010446",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "16": {
-            "date": "2021 May 18",
-            "time": "20:16:34.979256",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.001010"
-        },
-        "17": {
-            "date": "2021 May 18",
-            "time": "20:16:34.979244",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
-        },
-        "18": {
-            "date": "2021 May 18",
-            "time": "20:16:34.978236",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen called"
-        },
-        "19": {
-            "date": "2021 May 18",
-            "time": "20:15:21.097689",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "Time Taken For Show Run: Time=0.000600"
-        },
-        "20": {
-            "date": "2021 May 18",
-            "time": "20:15:21.097683",
-            "proc_name": "hsrp_engine",
-            "pid": "24530",
-            "debug_msg": "fu_acfg_gen done"
+    "event_type": {
+        "debug": {
+            1: {
+                "date": "2021 Aug 13",
+                "time": "14:13:49.073749",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000328"
+            },
+            2: {
+                "date": "2021 Aug 13",
+                "time": "14:13:49.073730",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            3: {
+                "date": "2021 Aug 13",
+                "time": "14:13:49.073401",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            4: {
+                "date": "2021 Aug 13",
+                "time": "14:13:48.941352",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000321"
+            },
+            5: {
+                "date": "2021 Aug 13",
+                "time": "14:13:48.941350",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            6: {
+                "date": "2021 Aug 13",
+                "time": "14:13:48.941027",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            7: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.557675",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000335"
+            },
+            8: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.557672",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            9: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.557334",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            10: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.540824",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000352"
+            },
+            11: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.540820",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            12: {
+                "date": "2021 Aug 13",
+                "time": "14:12:38.540466",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            13: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.214180",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000350"
+            },
+            14: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.214176",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            15: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.213824",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            16: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.202190",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.001072"
+            },
+            17: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.202165",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            },
+            18: {
+                "date": "2021 Aug 13",
+                "time": "14:11:28.201104",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen called"
+            },
+            19: {
+                "date": "2021 Aug 13",
+                "time": "14:10:18.407993",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "Time Taken For Show Run: Time=0.000365"
+            },
+            20: {
+                "date": "2021 Aug 13",
+                "time": "14:10:18.407989",
+                "proc_name": "hsrp_engine",
+                "pid": 10587,
+                "msg": "fu_acfg_gen done"
+            }
         }
     }
 }
