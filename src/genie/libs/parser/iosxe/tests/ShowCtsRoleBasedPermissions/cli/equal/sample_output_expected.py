c2c1c1a6c993d2ea4fe28cb621e3296e5863df1a
komurzak-cisco <89828894+komurzak-cisco@users.noreply.github.com>
2021-09-02 21:51:58 -0700

    Create sample_output_expected.py

---
+++ src/genie/libs/parser/iosxe/tests/ShowCtsRoleBasedPermissions/cli/equal/sample_output_expected.py
@@ -0,0 +1,10 @@
+expected_output = {
+    "indexes": {
+        1: {
+            "action_policy": "Permit",
+            "action_policy_group": "IP-00"
+        },
+        "monitor_dynamic": False,
+        "monitor_configured": False
+    }
+}
