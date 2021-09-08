d8fc354f982a59f53992351c12dd7e6feb037ebf
dannywade <danny.wade35@gmail.com>
2021-09-08 13:50:17 -0400

    added 2nd example and updated unit test

---
+++ src/genie/libs/parser/iosxe/tests/ShowLicenseSummary/cli/equal/golden_output2_expected.py
@@ -0,0 +1,24 @@
+expected_output = {
+   'license_usage':{
+      'C9300 48P DNA Advantage':{
+         'entitlement':'C9300-48 DNA Advantage',
+         'count':'2',
+         'status':'AUTHORIZED'
+      },
+      'C9300 48P Network Adv...':{
+         'entitlement':'C9300-48 Network Advan...',
+         'count':'2',
+         'status':'AUTHORIZED'
+      },
+      'C9300 24P Network Adv...':{
+         'entitlement':'C9300-24 Network Advan...',
+         'count':'1',
+         'status':'AUTHORIZED'
+      },
+      'C9300 24P DNA Advantage':{
+         'entitlement':'C9300-24 DNA Advantage',
+         'count':'1',
+         'status':'AUTHORIZED'
+      }
+   }
+}
