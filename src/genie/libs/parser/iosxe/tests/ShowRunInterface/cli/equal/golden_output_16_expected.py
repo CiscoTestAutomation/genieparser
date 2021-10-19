7a5ac185dfc04fc667932ed3e96c9ef03aaf426f
Jean-Michel DILLY <jmdilly@adista.fr>
2021-10-19 10:49:09 +0200

    Fix indents (comment in #556)

--- src/genie/libs/parser/iosxe/tests/ShowRunInterface/cli/equal/golden_output_16_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowRunInterface/cli/equal/golden_output_16_expected.py
@@ -1,20 +1,20 @@
 
 expected_output = {
-  'interfaces':{
-    'Port-channel1.1': {
-      'encapsulation': {
-        'first_dot1q': 1,
-        'dot1q_native': True,
-        'type': 'dot1Q'
-        },
-        'ipv4': { 'ip': '1.1.1.1',
-                  'netmask': '255.255.255.252'},
-        'ipv4_secondary': { '2.2.2.2': { 'ip': '2.2.2.2',
-                                        'netmask': '255.255.255.252'},
-                            '3.3.3.2': { 'ip': '3.3.3.2',
-                                        'netmask': '255.255.255.252'},
-                          },
-        'vrf': 'TEST'
-      }
+    'interfaces':{
+        'Port-channel1.1': {
+                'encapsulation': {
+                'first_dot1q': 1,
+                'dot1q_native': True,
+                'type': 'dot1Q'
+            },
+            'ipv4': { 'ip': '1.1.1.1',
+                        'netmask': '255.255.255.252'},
+            'ipv4_secondary': { '2.2.2.2': { 'ip': '2.2.2.2',
+                                            'netmask': '255.255.255.252'},
+                                '3.3.3.2': { 'ip': '3.3.3.2',
+                                            'netmask': '255.255.255.252'},
+                                },
+            'vrf': 'TEST'
+        }
     }
 }
--- src/genie/libs/parser/iosxe/tests/ShowRunInterface/cli/equal/golden_output_17_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowRunInterface/cli/equal/golden_output_17_expected.py
@@ -1,18 +1,18 @@
 
 expected_output = {
-  'interfaces':{
-    'Port-channel1.3': {
-      'encapsulation': {
-        'first_dot1q': 10,
-        'dot1q_native': False,
-        'type': 'dot1Q',
-        'second_dot1q': '20',
-        },
-        'ipv4': { 'ip': '4.4.4.4',
-                  'netmask': '255.255.255.248'},
-        'mtu': 9000,
-        'mpls_mtu': 9000,
-        'clns_mtu': 1497
-      }
+    'interfaces':{
+        'Port-channel1.3': {
+            'encapsulation': {
+            'first_dot1q': 10,
+            'dot1q_native': False,
+            'type': 'dot1Q',
+            'second_dot1q': '20',
+            },
+            'ipv4': { 'ip': '4.4.4.4',
+                        'netmask': '255.255.255.248'},
+            'mtu': 9000,
+            'mpls_mtu': 9000,
+            'clns_mtu': 1497
+        }
     }
 }
