905cfa69dcb4884e627790de52db62040fab9113
Daniel Graziano <dangrazi@cisco.com>
2021-11-02 18:10:05 -0400

    sync internal to external

---
+++ src/genie/libs/parser/iosxe/tests/ShowLispIpv4RouteImportMapCache/cli/equal/golden_output13_expected.py
@@ -0,0 +1,21 @@
+expected_output = {
+  "lisp_id": {
+    0: {
+      "instance_id": {
+        102: {
+          "eid_table": "blue",
+          "config": 2,
+          "entries": 1,
+          "limit": 5000,
+          "eids": {
+            "50.1.1.0/24": {
+              "source": "static",
+              "cache_db": "installed",
+              "uptime": "04:39:33"
+            }
+          }
+        }
+      }
+    }
+  }
+}
