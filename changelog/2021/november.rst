870c355ce1607b69ca8fdc867a209740d6604b65
thismatters <paul@thismatters.net>
2021-11-03 12:37:47 -0500

    updating tests, changelog

---
+++ changelog/2021/november.rst
@@ -0,0 +1,9 @@
+--------------------------------------------------------------------------------
+                                      Fix
+--------------------------------------------------------------------------------
+
+* asa
+    * Modified ShowRouteSchema
+        * Added index to routes and supporting multiple routes to same subnet
+    * Modified ShowRoute
+        * Added index to routes and supporting multiple routes to same subnet
--- src/genie/libs/parser/asa/tests/ShowRoute/cli/equal/golden_output_2_expected.py
+++ src/genie/libs/parser/asa/tests/ShowRoute/cli/equal/golden_output_2_expected.py
@@ -5,149 +5,167 @@ expected_output = {
                 "ipv4": {
                     "routes": {
                         "10.121.65.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "7w0d",
-                            "metric": 20,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.121.64.35",
-                                        "outgoing_interface_name": "inside",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.121.64.34",
-                                        "outgoing_interface_name": "inside",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "7w0d",
+                                "route": "10.121.65.0/24",
+                                "source_protocol_codes": "O",
+                                "source_protocol": "ospf",
+                                "metric": 20,
+                                "route_preference": 110,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.121.64.35",
+                                            "outgoing_interface_name": "inside"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.121.64.34",
+                                            "outgoing_interface_name": "inside"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.65.0/24",
-                            "route_preference": 110,
-                            "source_protocol": "ospf",
-                            "source_protocol_codes": "O",
+                            }
                         },
                         "10.121.67.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "2w1d",
-                            "metric": 345856,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.9.193.99",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.9.193.98",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "2w1d",
+                                "route": "10.121.67.0/24",
+                                "source_protocol_codes": "EX",
+                                "source_protocol": "eigrp",
+                                "metric": 345856,
+                                "route_preference": 170,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.9.193.99",
+                                            "outgoing_interface_name": "esavpn"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.9.193.98",
+                                            "outgoing_interface_name": "esavpn"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.67.0/24",
-                            "route_preference": 170,
-                            "source_protocol": "eigrp",
-                            "source_protocol_codes": "EX",
+                            }
                         },
                         "10.121.68.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "2w1d",
-                            "metric": 345856,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.9.193.99",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.9.193.98",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "2w1d",
+                                "route": "10.121.68.0/24",
+                                "source_protocol_codes": "EX",
+                                "source_protocol": "eigrp",
+                                "metric": 345856,
+                                "route_preference": 170,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.9.193.99",
+                                            "outgoing_interface_name": "esavpn"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.9.193.98",
+                                            "outgoing_interface_name": "esavpn"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.68.0/24",
-                            "route_preference": 170,
-                            "source_protocol": "eigrp",
-                            "source_protocol_codes": "EX",
+                            }
                         },
                         "10.121.69.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "7w0d",
-                            "metric": 20,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.121.64.35",
-                                        "outgoing_interface_name": "inside",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.121.64.34",
-                                        "outgoing_interface_name": "inside",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "7w0d",
+                                "route": "10.121.69.0/24",
+                                "source_protocol_codes": "O",
+                                "source_protocol": "ospf",
+                                "metric": 20,
+                                "route_preference": 110,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.121.64.35",
+                                            "outgoing_interface_name": "inside"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.121.64.34",
+                                            "outgoing_interface_name": "inside"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.69.0/24",
-                            "route_preference": 110,
-                            "source_protocol": "ospf",
-                            "source_protocol_codes": "O",
+                            }
                         },
                         "10.121.70.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "2w1d",
-                            "metric": 345856,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.9.193.99",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.9.193.98",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "2w1d",
+                                "route": "10.121.70.0/24",
+                                "source_protocol_codes": "EX",
+                                "source_protocol": "eigrp",
+                                "metric": 345856,
+                                "route_preference": 170,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.9.193.99",
+                                            "outgoing_interface_name": "esavpn"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.9.193.98",
+                                            "outgoing_interface_name": "esavpn"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.70.0/24",
-                            "route_preference": 170,
-                            "source_protocol": "eigrp",
-                            "source_protocol_codes": "EX",
+                            }
                         },
                         "10.121.71.0/24": {
-                            "active": True,
-                            "candidate_default": False,
-                            "date": "2w1d",
-                            "metric": 345856,
-                            "next_hop": {
-                                "next_hop_list": {
-                                    1: {
-                                        "index": 1,
-                                        "next_hop": "10.9.193.99",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
-                                    2: {
-                                        "index": 2,
-                                        "next_hop": "10.9.193.98",
-                                        "outgoing_interface_name": "esavpn",
-                                    },
+                            1: {
+                                "index": 1,
+                                "candidate_default": False,
+                                "active": True,
+                                "date": "2w1d",
+                                "route": "10.121.71.0/24",
+                                "source_protocol_codes": "EX",
+                                "source_protocol": "eigrp",
+                                "metric": 345856,
+                                "route_preference": 170,
+                                "next_hop": {
+                                    "next_hop_list": {
+                                        1: {
+                                            "index": 1,
+                                            "next_hop": "10.9.193.99",
+                                            "outgoing_interface_name": "esavpn"
+                                        },
+                                        2: {
+                                            "index": 2,
+                                            "next_hop": "10.9.193.98",
+                                            "outgoing_interface_name": "esavpn"
+                                        }
+                                    }
                                 }
-                            },
-                            "route": "10.121.71.0/24",
-                            "route_preference": 170,
-                            "source_protocol": "eigrp",
-                            "source_protocol_codes": "EX",
-                        },
+                            }
+                        }
                     }
                 }
             }
