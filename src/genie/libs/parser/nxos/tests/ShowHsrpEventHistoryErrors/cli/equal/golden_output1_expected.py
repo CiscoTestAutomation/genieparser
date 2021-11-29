129bd440254259c3ea465482e579d2435faabd21
Divya Puranam <dipurana@cisco.com>
2021-11-29 18:52:47 +0530

    new

---
+++ src/genie/libs/parser/nxos/tests/ShowHsrpEventHistoryErrors/cli/equal/golden_output1_expected.py
@@ -0,0 +1,146 @@
+expected_output = {
+    "event_type": {
+        "error": {
+            1: {
+                "date": "2021 May 18",
+                "time": "03:14:54.489303",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            2: {
+                "date": "2021 May 18",
+                "time": "03:14:54.217444",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv6, VLAN=3"
+            },
+            3: {
+                "date": "2021 May 18",
+                "time": "03:14:54.217428",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv4, VLAN=3"
+            },
+            4: {
+                "date": "2021 May 18",
+                "time": "03:14:54.217021",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            5: {
+                "date": "2021 May 18",
+                "time": "03:14:46.160122",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            6: {
+                "date": "2021 May 18",
+                "time": "03:14:13.887116",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=7972"
+            },
+            7: {
+                "date": "2021 May 18",
+                "time": "03:14:13.886187",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            8: {
+                "date": "2021 May 18",
+                "time": "03:14:13.870140",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv6, VLAN=2"
+            },
+            9: {
+                "date": "2021 May 18",
+                "time": "03:14:13.870123",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv4, VLAN=2"
+            },
+            10: {
+                "date": "2021 May 18",
+                "time": "03:14:13.869766",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            11: {
+                "date": "2021 May 18",
+                "time": "03:14:06.694680",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            12: {
+                "date": "2021 May 18",
+                "time": "03:13:41.464033",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=7972"
+            },
+            13: {
+                "date": "2021 May 18",
+                "time": "03:13:41.462313",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            14: {
+                "date": "2021 May 18",
+                "time": "03:13:41.444570",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv6, VLAN=3"
+            },
+            15: {
+                "date": "2021 May 18",
+                "time": "03:13:41.444549",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv4, VLAN=3"
+            },
+            16: {
+                "date": "2021 May 18",
+                "time": "03:13:41.444133",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            17: {
+                "date": "2021 May 18",
+                "time": "03:13:28.308280",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            18: {
+                "date": "2021 May 18",
+                "time": "03:13:17.423021",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=7972"
+            },
+            19: {
+                "date": "2021 May 18",
+                "time": "03:13:17.422138",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "Engine Demux: Info='Unexpected MTS message', Opcode=409604"
+            },
+            20: {
+                "date": "2021 May 18",
+                "time": "03:13:17.389821",
+                "proc_name": "hsrp_engine",
+                "pid": 24530,
+                "msg": "(Acast) Get Bundle For IOD Failed: Reason='No bundle found for VLAN', AddrType=IPv6, VLAN=3"
+            }
+        }
+    }
+}
