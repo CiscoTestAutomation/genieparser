20f9f0675ab5155699649c7b653b4740c5853514
Spiro Mitsialis <spiro.mitsialis@mcgill.ca>
2021-12-10 16:23:40 -0500

    fixed filenames

--- src/genie/libs/parser/iosxe/tests/ShowCableTdrInterface/cli/equal/golden_output1_expected.py
+++ src/genie/libs/parser/iosxe/tests/ShowCableTdrInterface/cli/equal/golden_output1_expected.py
@@ -1,20 +1,20 @@
 expected_output = {
-    'Gi1/0/1':{
+    'Gi1/0/1': {
         'interface': 'Gi1/0/1',
         'speed': 'auto',
         'date': 'January 09',
         'time':'16:19:29',
         'pair_a': {
-            'length' :21,
-            'tolerance' :1,
-            'remote_pair' :'N/A',
-            'status' :'Open'
+            'length': 21,
+            'tolerance': 1,
+            'remote_pair': 'N/A',
+            'status': 'Open'
         },
         'pair_b': {
-            'length' :20,
-            'tolerance' :3,
-            'remote_pair' :'N/A',
-            'status' :'Open'
+            'length': 20,
+            'tolerance': 3,
+            'remote_pair': 'N/A',
+            'status': 'Open'
         },
         'pair_c': {
             'length': 2,
