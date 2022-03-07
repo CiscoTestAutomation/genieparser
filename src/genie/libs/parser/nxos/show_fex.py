60a37877fbd1b2c58cc52554e302875847e13b1b
Sarah Virr <virrsa@gmail.com>
2022-03-07 15:06:01 -0500

    Added initial show_fex parser

---
+++ src/genie/libs/parser/nxos/show_fex.py
@@ -0,0 +1,61 @@
+# Metaparser
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Any
+
+# Python
+import re
+
+# ==============================
+# Schema for 'show fex'
+# ==============================
+
+class ShowFexSchema(MetaParser):
+
+    ''' Schema for "show fex" '''
+
+    schema = {
+        'fexes': {
+            Any(): {
+                'description': str,
+                'state': str,
+                'model': str,
+                'serial_number': str
+            }
+        }
+    }
+
+# ==============================
+# Parser for 'show fex'
+# ==============================
+
+class ShowFex(ShowFexSchema):
+
+    ''' Parser for "show fex"'''
+
+    cli_command = 'show fex'
+
+    def cli(self, output=None):
+        if output is None:
+            # Get output from device
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output 
+        # Initial return dictionary
+        parsed_dict = {}
+
+        p1 = re.compile(r'(?P<number>(\d+)) +(?P<description>(\S+)) +(?P<state>(Online|Offline)) +(?P<model>(\S+)) +(?P<serial>(\S+))$')
+
+        for line in out.splitlines():
+            line = line.strip()
+            m = p1.match(line)
+            if m:
+                group = m.groupdict()
+                fex = group['number']
+                fex_dict = parsed_dict.setdefault('fexes', {}).setdefault(fex, {})
+                fex_dict['description'] = group['description']
+                fex_dict['state'] = group['state']
+                fex_dict['model'] = group['model']
+                fex_dict['serial'] = group['serial']
+
+        return parsed_dict
+
