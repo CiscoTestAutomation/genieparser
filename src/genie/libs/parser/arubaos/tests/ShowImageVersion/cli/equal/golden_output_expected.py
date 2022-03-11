b62cf622c3d8eab77c7b69e932c214ed04a2565a
Yannick Koehler <yannick.koehler@hpe.com>
2022-03-11 16:55:19 +0000

    Added support for 'show image version' in ArubaOS

---
+++ src/genie/libs/parser/arubaos/tests/ShowImageVersion/cli/equal/golden_output_expected.py
@@ -0,0 +1,23 @@
+expected_output = {
+    "partition": {
+        0: {
+            "disk": "/mnt/disk1",
+            "signature_type": "SHA1/SHA256",
+            "build_type": "Production",
+            "firmware_version": "ArubaOS 8.6.0.16",
+            "build_number": 0,
+            "label": "82590",
+            "built_on": "Thu Dec 16 13:28:34 UTC 2021",
+        },
+        1: {
+            "disk": "/mnt/disk2",
+            "signature_type": "SHA1/SHA256",
+            "build_type": "Developer/Internal",
+            "firmware_version": "ArubaOS 8.10.0.0 LSR",
+            "build_number": 0,
+            "label": "jenkins@yk-aos-preflight-ENG.0000",
+            "built_on": "Wed Mar 9 09:41:5",
+        },
+        "default_partition": 1,
+    },
+}
