05595f3c9e5c5bd9a951a53cce9973e0b01d4159
Dave Wapstra <dwapstra@cisco.com>
2022-04-06 21:42:30 +0200

    Fix xr policy map parser

---
+++ src/genie/libs/parser/iosxr/tests/ShowPolicyMapInterface/cli/equal/golden_output_5_expected.py
@@ -0,0 +1,60 @@
+expected_output = {
+    'interface': {
+        'GigabitEthernet 0/0/0/2': {
+            'service_policy': {
+                'input': {
+                    'policy_status': 'Service Policy not installed',
+                },
+                'output': {
+                    'policy_name': {
+                        'fb-be1011': {
+                            'class': {
+                                'class-default': {
+                                    'classification_statistics': {
+                                        'matched': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'total_dropped': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'transmitted': 'N/A',
+                                    },
+                                },
+                                'fb': {
+                                    'classification_statistics': {
+                                        'matched': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'total_dropped': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'transmitted': 'N/A',
+                                    },
+                                },
+                                'ping': {
+                                    'classification_statistics': {
+                                        'matched': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'total_dropped': {
+                                            'packets/bytes': '0/0',
+                                            'rate/kbps': 0,
+                                        },
+                                        'transmitted': 'N/A',
+                                    },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    },
+}
+
+
