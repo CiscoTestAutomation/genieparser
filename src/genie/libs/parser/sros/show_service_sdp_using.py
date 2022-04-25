358437e7b61aaf6ade05df281ff15213d2161096
Michael Kopp <michael.kopp@isarnet.de>
2022-04-25 19:18:54 +0200

    modifications from review and fixed cornercase with "None" label

--- src/genie/libs/parser/sros/show_service_sdp_using.py
+++ src/genie/libs/parser/sros/show_service_sdp_using.py
@@ -17,8 +17,8 @@ class ShowServiceSdpUsingSchema(MetaParser):
                 'type': str,
                 'far_end': str,
                 'oper_state': str,
-                'ingress_label': int,
-                'egress_label': int,
+                'ingress_label': str,
+                'egress_label': str,
             }
         }
     }
@@ -31,7 +31,7 @@ class ShowServiceSdpUsing(ShowServiceSdpUsingSchema):
     def cli(self, output=None):
 
         if output is None:
-            out = self.device.execute(self.cli_command)
+            output = self.device.execute(self.cli_command)
 
         result_dict = {}
         #60001      1000:60001         Spok   10.0.1.5             Up    131064  131066
@@ -40,13 +40,13 @@ class ShowServiceSdpUsing(ShowServiceSdpUsingSchema):
         #300101     1000:20002         Spok   10.0.1.3             Up    131066  131068
         p1 = re.compile(r'(?P<service>\d+)'
                         r' +(?P<sdp>\S+) +(?P<type>\S+) +(?P<far_end>\S+)'
-                        r' +(?P<oper>Up|Down) +(?P<ingress_label>\d+)' 
-                        r' +(?P<egress_label>\d+)')
+                        r' +(?P<oper>Up|Down) +(?P<ingress_label>\d+|None)' 
+                        r' +(?P<egress_label>\d+|None)')
 
         # Number of SDPs : 4
         p2 = re.compile(r'^Number of SDPs : (?P<total>\d+)')
 
-        for line in out.splitlines():
+        for line in output.splitlines():
             if line:
                 line = line.strip()
             else:
@@ -63,8 +63,8 @@ class ShowServiceSdpUsing(ShowServiceSdpUsingSchema):
                 sdp=group['sdp']
                 result_dict.setdefault('sdp', {}).setdefault(sdp,{})
                 result_dict['sdp'][sdp].update({'service_id': int(group['service'])})
-                result_dict['sdp'][sdp].update({'ingress_label': int(group['ingress_label'])})
-                result_dict['sdp'][sdp].update({'egress_label': int(group['egress_label'])})
+                result_dict['sdp'][sdp].update({'ingress_label': group['ingress_label']})
+                result_dict['sdp'][sdp].update({'egress_label': group['egress_label']})
                 result_dict['sdp'][sdp].update({'type': (group['type'])})
                 result_dict['sdp'][sdp].update({'far_end': (group['far_end'])})
                 result_dict['sdp'][sdp].update({'oper_state': (group['oper'])})
