cd05ea52b0d7869c1447a82b798134630419f786
Mike James (MikeJames15) <miguel.andres.ricardo.james@intl.att.com>
2022-06-16 11:31:00 -0300

    Updated test expected output to match parser output

--- src/genie/libs/parser/asa/tests/ShowNat/cli/equal/golden_expected.py
+++ src/genie/libs/parser/asa/tests/ShowNat/cli/equal/golden_expected.py
@@ -1,41 +1,49 @@
-expected_output = {'nat': { 1: {'from-interface': 'if1',
-                                'to-interface:': 'if2',
-                                'type': 'static',
+expected_output = {'nat': { 1: {'internal-interface': 'if1',
+                                'external-interface:': 'if2',
+                                'source': {
+                                    'type': 'static',
+                                    'object-name': 'obj_a',
+                                    'natted-address': '1.1.1.1',
+                                    'real-address-and-mask': '1.1.1.1/32',
+                                    'natted-address-and-mask': '2.2.2.2/32'
+                                },
                                 'dns': True,
                                 'proxy-arp': True,
-                                'object-name': 'obj_a',
                                 'hits': {
                                     'translate': 5,
                                     'untranslate': 18
+                                }
+                            }},
+                          { 2: {'internal-interface': 'if3',
+                                'external-interface:': 'if4',
+                                'source': {
+                                    'type': 'static',
+                                    'object-name': 'obj_b',
+                                    'natted-address': '3.3.3.3'
+                                    'real-address-and-mask': '3.3.3.3/32',
+                                    'natted-address-and-mask': '4.4.4.4/32'
                                 },
-                                'source': '1.1.1.1/32',
-                                'translated': '2.2.2.2/32'
-                                }},
-                          { 2: {'from-interface': 'if3',
-                                'to-interface:': 'if4',
-                                'type': 'static',
                                 'dns': False,
                                 'proxy-arp': True,
-                                'object-name': 'obj_b',
                                 'hits': {
                                     'translate': 104,
                                     'untranslate': 203
-                                },
-                                'source': '3.3.3.3/32',
-                                'translated': '4.4.4.4/32'
-                                }}, 
-                          { 3: {'from-interface': 'if5',
-                                'to-interface:': 'if6',
-                                'type': 'static',
+                                }
+                            }}, 
+                          { 3: {'internal-interface': 'if5',
+                                'external-interface:': 'if6',
+                                'source': {
+                                    'type': 'static',
+                                    'object-name': 'obj_c',
+                                    'natted-address': '6.6.6.6/32'
+                                    'real-address-and-mask': '5.5.5.5/32',
+                                    'natted-address-and-mask': '6.6.6.6/32'
+                                }
                                 'dns': True,
                                 'proxy-arp': False,
-                                'object-name': 'obj_c',
                                 'hits': {
                                     'translate': 1000,
                                     'untranslate': 506
-                                },
-                                'source': '5.5.5.5/32',
-                                'translated': '6.6.6.6/32'
-                                }}
+                                }
+                            }}
                   } 
-                   
