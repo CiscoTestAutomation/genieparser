e9f149df82c2f74d2488a8770bba5f68d0178592
Rita Yu <ritayu@cisco.com>
2022-09-28 08:58:24 -0700

    vsmart and vmanage wan-interface table output expected parsing result# Please enter the commit message for your changes. Lines starting

---
+++ src/genie/libs/parser/viptela/tests/ShowControlLocalProperties/cli/equal/golden_output1_expected.py
@@ -0,0 +1,56 @@
+expected_output = {
+    'certificate_not_valid_after': 'Sep 23 04:02:04 2023 GMT',
+    'certificate_not_valid_before': 'Sep 23 04:02:04 2022 GMT',
+    'certificate_status': 'Installed',
+    'certificate_validity': 'Valid',
+    'chassis_num_unique_id': 'aecb941b-00cb-4885-b540-4b91877f0060',
+    'dns_cache_ttl': '0:00:00:00',
+    'dns_name': '10.0.12.26',
+    'domain_id': '1',
+    'no_activity_exp_interval': '0:00:00:20',
+    'number_active_wan_interfaces': '2',
+    'number_vbond_peers': '1',
+    'organization_name': 'vIPtela Inc Regression',
+    'personality': 'vsmart',
+    'port_hopped': 'FALSE',
+    'protocol': 'dtls',
+    'retry_interval': '0:00:00:15',
+    'root_ca_chain_status': 'Installed',
+    'serial_num': '12345701',
+    'site_id': '100',
+    'sp_organization_name': 'vIPtela Inc Regression',
+    'system_ip': '172.16.255.19',
+    'time_since_last_port_hop': '0:00:00:00',
+    'tls_port': '23456',
+    'token': '-NA-',
+    'wan_interfaces': {
+        '0': {
+            'eth1': {
+                'color': 'default',
+                'last_connection': '0:00:00:13',
+                'private_ipv4': '10.0.5.19',
+                'private_ipv6': '::',
+                'private_port': '12355',
+                'public_ipv4': '10.0.5.19',
+                'public_port': '12355',
+                'state': 'up',
+                'vmanage': '1',
+                'vsmart': '1'
+            }
+        },
+        '1': {
+            'eth1': {
+                'color': 'default',
+                'last_connection': '0:00:00:11',
+                'private_ipv4': '10.0.5.19',
+                'private_ipv6': '::',
+                'private_port': '12455',
+                'public_ipv4': '10.0.5.19',
+                'public_port': '12455',
+                'state': 'up',
+                'vmanage': '0',
+                'vsmart': '0'
+            }
+        },
+    },
+}
