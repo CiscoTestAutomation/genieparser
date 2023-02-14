318ec383771518a22cfe9f7c62fd25cb5dbcab0e
Rohit Saluja <rohsaluj@cisco.com>
2023-02-14 19:55:33 +0530

    Adding TCs

---
+++ src/genie/libs/parser/nxos/tests/ShowLoggingLevel/cli/equal/golden_output_expected.py
@@ -0,0 +1,38 @@
+
+
+expected_output = {
+    'facility': {
+        "aaa": {
+            'def_severity': 3
+            'curr_severity' 3
+        },
+        "acllog": {
+            'def_severity': 2
+            'curr_severity' 1
+        },
+        "aclmgr": {
+            'def_severity': 3
+            'curr_severity' 3
+        },
+        "aclqos": {
+            'def_severity': 5
+            'curr_severity' 4
+        },
+        "vlan_mgr": {
+            'def_severity': 2
+            'curr_severity' 5
+        },
+        "vshd": {
+            'def_severity': 5
+            'curr_severity' 5
+        },
+        "xbar": {
+            'def_severity': 5
+            'curr_severity' 0
+        },
+        "xmlma": {
+            'def_severity': 3
+            'curr_severity' 7
+        },
+    },
+}
