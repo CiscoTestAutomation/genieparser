455f8fea88b168d916c8ffb7f15a9dcf36d0c03e
Rohit Saluja <rohsaluj@cisco.com>
2023-02-14 19:55:53 +0530

    Adding schema and regex

--- src/genie/libs/parser/nxos/show_logging.py
+++ src/genie/libs/parser/nxos/show_logging.py
@@ -74,3 +74,57 @@ class ShowLoggingLogfile(ShowLoggingLogfileSchema):
                 continue
 
         return parsed_dict
+
+class ShowLoggingLevelSchema(MetaParser):
+    '''Schema for:
+        * 'show logging level'
+        * 'show logging level {name}'
+    '''
+
+    schema = { 'facility':
+                { Any():
+                    { def_severity: int,
+                      curr_severity: int }
+                }
+            }
+
+class ShowLoggingLevel(ShowLoggingLevelSchema):
+    '''Schema for:
+        * 'show logging level'
+    '''
+
+    cli_command = 'show logging level'
+
+    def cli(self, output=None):
+
+        if (output is None):
+            # Build the command
+            cmd = self.cli_command
+
+            # Execute the command
+            out = self.device.execute(cmd)
+
+        else:
+            out = output
+
+        p = re.compile(r'(?P<facility>\S+)\s+'
+                        '(?P<def_severity>\d)\s+'
+                        '(?P<curr_severity>\d)')
+        ret_dict = {
+            'facility': {}
+        }
+
+        for line in out.splitlines():
+            line = line.strip()
+
+            m = p.match(line)
+            if m:
+                facility = m.groupdict()['facility']
+                if facility not in ret_dict['facility']:
+                    ret_dict['facility'][facility] = {}
+
+                for x in ['def_severity', 'curr_severity']:
+                    x_val = m.groupdict()[x]
+                    ret_dict['facility'][facility][x] = x_val
+
+        return ret_dict
