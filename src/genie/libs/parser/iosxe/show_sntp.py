07e055d10592198e09b32ffaf59342a1810ea0c3
Eric Fetty <eric@kingfetty.com>
2023-06-27 14:10:58 -0500

    Added IOSXE show sntp parser

---
+++ src/genie/libs/parser/iosxe/show_sntp.py
@@ -0,0 +1,87 @@
+"""show_ntp.py
+
+IOSXE parsers for the following show commands:
+
+    * show sntp
+
+"""
+
+# Python
+import re
+
+# Metaparser
+from genie.metaparser import MetaParser
+from genie.metaparser.util.schemaengine import Schema, Any, Optional, Or
+
+
+# ==============================================
+#  Schema for show sntp
+# ==============================================
+class ShowSntpSchema(MetaParser):
+    """Schema for show sntp"""
+
+    schema = {
+        'servers': {
+                Any(): {
+                    'stratum': int,
+                    'version': int,
+                    'updated': str,
+                    'synced': bool
+                    },
+                },
+            }
+
+
+# ==============================================
+#  Parser for show sntp
+# ==============================================
+class ShowSntp(ShowSntpSchema):
+    """Parser for show sntp"""
+
+
+    cli_command = 'show sntp'
+
+    def cli(self,output=None):
+        if output is None:
+            out = self.device.execute(self.cli_command)
+        else:
+            out = output
+
+        # initial variables
+        ret_dict = {}
+        # SNTP server                Stratum   Version    Last Received
+        # 192.168.131.58              3           4        15w5d     
+        # 192.168.131.59              3           4        15w5d     
+        # 172.18.1.10                 16          4        never     
+        # 192.168.131.59              3           4        00:01:00      Synced
+
+        p1 = re.compile( r'^(?P<server>[\w\.\:]+) +(?P<stratum>\d+) +(?P<version>\d+) +(?P<updated>[\w\:\.]+) *(?P<status>\w*)$' )
+        p2 = re.compile( r'^(?P<server>[\w\.\:]+) +(?P<stratum>\d+) +(?P<version>\d+) +(?P<updated>[\w\:\.]+) +$' )
+
+        for line in out.splitlines():
+            line = line.strip()
+            
+            if not line:
+                continue
+
+            # 192.168.131.58              3           4        15w5d     
+            m = p1.match(line)
+            if m:
+                groups = m.groupdict()
+                server = groups['server']
+                if groups['status']:
+                    synced = True
+                else:
+                    synced = False
+                peer_dict = ret_dict.setdefault('servers', {}).setdefault(server, {})
+                   
+                peer_dict.update({
+                                  'stratum': int(groups['stratum']),
+                                  'version': int(groups['version']),
+                                  'updated': groups['updated'],
+                                  'synced': synced
+                                })
+
+        return ret_dict
+
+
---
+++ src/genie/libs/parser/iosxe/tests/ShowSntp/cli/equal/golden_output_sntp_expected.py
@@ -0,0 +1,22 @@
+expected_output = {
+    "servers": {
+        "192.168.131.58": {
+            "stratum": 3,
+            "version": 4,
+            "updated": "15w5d",
+            "synced": False,
+        },
+        "192.168.131.59": {
+            "stratum": 3,
+            "version": 4,
+            "updated": "00:00:35",
+            "synced": True,
+        },
+        "172.18.1.10": {
+            "stratum": 16,
+            "version": 4,
+            "updated": "never",
+            "synced": False,
+        },
+    }
+}
