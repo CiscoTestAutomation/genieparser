a8c07b3bd22b7450f38d080b0674b7a189fe310e
Mehdi Cherifi <73710391+cherifimehdi@users.noreply.github.com>
2023-11-01 18:57:56 +0100

    Add files via upload

---
+++ src/genie/libs/parser/ios/tests/ShowVlanInternalUsage/cli/equal/golden_output1_expected.py
@@ -0,0 +1,13 @@
+expected_output = {
+   "Internal Vlan":{
+      "1006":{
+         "Usage":"GigabitEthernet0/0"
+      },
+      "1007":{
+         "Usage":"GigabitEthernet0/1"
+      },
+      "1008":{
+         "Usage":"GigabitEthernet0/2"
+      }
+   }
+}
