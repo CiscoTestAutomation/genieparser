58aa49b13a99aa866723161b7642aa46d0a7dc81
Michael Clover <michael.clover@optus.com.au>
2023-12-04 06:49:47 +1000

    added test  files

---
+++ src/genie/libs/parser/iosxe/tests/ShowPolicyMap/cli/equal/golden_output16_expected.py
@@ -0,0 +1,19 @@
+expected_output = {
+    "policy_map": {
+        "name": "map1",
+        "classes": [
+            {
+                "name": "cs1",
+                "priority_percent": 9,
+                },
+            {
+                "name": "cs2",
+                "bandwidth_percent": 9,
+                },
+            {
+                "name": "cs2",
+                "bandwidth_percent": 31,
+                }
+            ]
+        }
+    }
