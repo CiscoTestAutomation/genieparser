f027cc5c93724f96bdb3bd7619a6fb59c92edfa9
AJ Cruz <15045766-a-cruz@users.noreply.gitlab.com>
2024-01-26 07:33:51 -0600

    Port-channel show_cts iosxe fix

---
+++ src/genie/libs/parser/iosxe/tests/ShowCtsInterface/cli/equal/golden_output4_expected.py
@@ -0,0 +1,10 @@
+expected_output={
+    'interfaces': {
+        'Port-channel2': {
+            'cts': {
+                'cts_status': 'disabled'
+            },
+            'l3_ipm': 'disabled'
+        }
+    }
+}
