a9c0504c042aefa9fe33df71821336cd4874c353
AJ Cruz <15045766-a-cruz@users.noreply.gitlab.com>
2024-01-29 18:37:42 -0600

    New golden_output

---
+++ src/genie/libs/parser/iosxe/tests/ShowCtsInterface/cli/equal/golden_output4_expected.py
@@ -0,0 +1,10 @@
+expected_output={
+    'interfaces': {
+        'port-channel2': {
+            'cts': {
+                'cts_status': 'disabled'
+            },
+            'l3_ipm': 'disabled'
+        }
+    }
+}
