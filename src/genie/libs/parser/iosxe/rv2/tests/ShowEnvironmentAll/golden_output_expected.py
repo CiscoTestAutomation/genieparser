249269892613fa2a852f4178f7859ea9773a5a67
Alex Knop <aknop@blueally.com>
2025-10-20 15:20:12 -0400

    moved tests

---
+++ src/genie/libs/parser/iosxe/rv2/tests/ShowEnvironmentAll/golden_output_expected.py
@@ -0,0 +1,633 @@
+expected_output = {
+  "critical_alarms": 0,
+  "major_alarms": 0,
+  "minor_alarms": 0,
+  "sensor_list": {
+    "Environmental Monitoring": {
+      "slot": {
+        "Chassis1-2/0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "352 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "54 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "20 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 65,
+                "major": 55,
+                "minor": 45,
+                "shutdown": 72,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "27 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 75,
+                "major": 65,
+                "minor": 55,
+                "shutdown": 100,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "1001 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "1003 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1034 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1501 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1800 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "3295 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "3313 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1499 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1029 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "3295 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "12035 mV",
+              "state": "Normal"
+            }
+          }
+        },
+        "Chassis1-7/0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "441 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "53 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "24 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 105,
+                "major": 58,
+                "minor": 48,
+                "shutdown": 107,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "30 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 110,
+                "major": 63,
+                "minor": 53,
+                "shutdown": 112,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "1001 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "1000 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1033 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1504 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1800 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "3296 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "3323 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1497 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1031 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "3299 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "12075 mV",
+              "state": "Normal"
+            }
+          }
+        },
+        "Chassis1-R0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "273 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "53 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "25 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 96,
+                "major": 66,
+                "minor": 56,
+                "shutdown": 98,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "37 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 103,
+                "major": 73,
+                "minor": 63,
+                "shutdown": 105,
+                "unit": "Celsius"
+              }
+            },
+            "Temp: Coretemp": {
+              "reading": "44 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 123,
+                "major": 117,
+                "minor": 107,
+                "shutdown": 125,
+                "unit": "Celsius"
+              }
+            },
+            "Temp: UADP": {
+              "reading": "53 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 123,
+                "major": 117,
+                "minor": 107,
+                "shutdown": 125,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "871 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "1700 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1223 mV",
+              "state": "Normal"
+            },
+            "V1: VX12": {
+              "reading": "1804 mV",
+              "state": "Normal"
+            },
+            "V1: VX13": {
+              "reading": "2503 mV",
+              "state": "Normal"
+            },
+            "V1: VX14": {
+              "reading": "3290 mV",
+              "state": "Normal"
+            },
+            "V1: VX15": {
+              "reading": "5035 mV",
+              "state": "Normal"
+            },
+            "V1: VX16": {
+              "reading": "899 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1497 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1055 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "851 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "1509 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1301 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1005 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "1100 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "1204 mV",
+              "state": "Normal"
+            }
+          }
+        },
+        "Chassis2-2/0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "345 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "53 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "20 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 65,
+                "major": 55,
+                "minor": 45,
+                "shutdown": 72,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "26 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 75,
+                "major": 65,
+                "minor": 55,
+                "shutdown": 100,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "1001 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "1000 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1031 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1498 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1802 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "3286 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "3323 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1498 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1030 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "3305 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "12015 mV",
+              "state": "Normal"
+            }
+          }
+        },
+        "Chassis2-7/0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "437 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "53 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "23 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 105,
+                "major": 58,
+                "minor": 48,
+                "shutdown": 107,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "29 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 110,
+                "major": 63,
+                "minor": 53,
+                "shutdown": 112,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "1002 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "999 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1031 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1498 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1801 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "3301 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "3343 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1501 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1031 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "3312 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "12043 mV",
+              "state": "Normal"
+            }
+          }
+        },
+        "Chassis2-R0": {
+          "sensor": {
+            "HotSwap: Power": {
+              "reading": "276 Watts",
+              "state": "Normal"
+            },
+            "HotSwap: Volts": {
+              "reading": "53 V DC",
+              "state": "Normal"
+            },
+            "Temp:    Inlet": {
+              "reading": "25 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 96,
+                "major": 66,
+                "minor": 56,
+                "shutdown": 98,
+                "unit": "Celsius"
+              }
+            },
+            "Temp:   Outlet": {
+              "reading": "36 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 103,
+                "major": 73,
+                "minor": 63,
+                "shutdown": 105,
+                "unit": "Celsius"
+              }
+            },
+            "Temp: Coretemp": {
+              "reading": "44 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 123,
+                "major": 117,
+                "minor": 107,
+                "shutdown": 125,
+                "unit": "Celsius"
+              }
+            },
+            "Temp: UADP": {
+              "reading": "52 Celsius",
+              "state": "Normal",
+              "threshold": {
+                "critical": 123,
+                "major": 117,
+                "minor": 107,
+                "shutdown": 125,
+                "unit": "Celsius"
+              }
+            },
+            "V1: VX1": {
+              "reading": "871 mV",
+              "state": "Normal"
+            },
+            "V1: VX10": {
+              "reading": "1706 mV",
+              "state": "Normal"
+            },
+            "V1: VX11": {
+              "reading": "1227 mV",
+              "state": "Normal"
+            },
+            "V1: VX12": {
+              "reading": "1806 mV",
+              "state": "Normal"
+            },
+            "V1: VX13": {
+              "reading": "2508 mV",
+              "state": "Normal"
+            },
+            "V1: VX14": {
+              "reading": "3298 mV",
+              "state": "Normal"
+            },
+            "V1: VX15": {
+              "reading": "5047 mV",
+              "state": "Normal"
+            },
+            "V1: VX16": {
+              "reading": "899 mV",
+              "state": "Normal"
+            },
+            "V1: VX2": {
+              "reading": "1494 mV",
+              "state": "Normal"
+            },
+            "V1: VX3": {
+              "reading": "1055 mV",
+              "state": "Normal"
+            },
+            "V1: VX4": {
+              "reading": "850 mV",
+              "state": "Normal"
+            },
+            "V1: VX5": {
+              "reading": "1516 mV",
+              "state": "Normal"
+            },
+            "V1: VX6": {
+              "reading": "1302 mV",
+              "state": "Normal"
+            },
+            "V1: VX7": {
+              "reading": "1004 mV",
+              "state": "Normal"
+            },
+            "V1: VX8": {
+              "reading": "1100 mV",
+              "state": "Normal"
+            },
+            "V1: VX9": {
+              "reading": "1204 mV",
+              "state": "Normal"
+            }
+          }
+        }
+      }
+    }
+  },
+  "switch": {
+    "1": {
+      "fantray": {
+        "fantray_airflow_direction": "side-to-side",
+        "fantray_beacon_led": "off",
+        "fantray_status_led": "green",
+        "power_consumed_by_fantray_watts": 540,
+        "status": "good",
+        "system": "GREEN"
+      },
+      "power_supply": {
+        "current_configuration_mode": "Combined",
+        "current_operating_state": "Combined",
+        "currently_active": 2,
+        "currently_available": 2,
+        "slot": {
+          "PS1": {
+            "capacity": "3200 W",
+            "fan_1_state": "good",
+            "fan_2_state": "good",
+            "model_no": "C9400-PWR-3200AC",
+            "status": "active",
+            "type": "ac"
+          },
+          "PS2": {
+            "capacity": "3200 W",
+            "fan_1_state": "good",
+            "fan_2_state": "good",
+            "model_no": "C9400-PWR-3200AC",
+            "status": "active",
+            "type": "ac"
+          }
+        }
+      }
+    },
+    "2": {
+      "fantray": {
+        "fantray_airflow_direction": "side-to-side",
+        "fantray_beacon_led": "off",
+        "fantray_status_led": "green",
+        "power_consumed_by_fantray_watts": 540,
+        "status": "good",
+        "system": "GREEN"
+      },
+      "power_supply": {
+        "current_configuration_mode": "Combined",
+        "current_operating_state": "Combined",
+        "currently_active": 2,
+        "currently_available": 2,
+        "slot": {
+          "PS1": {
+            "capacity": "3200 W",
+            "fan_1_state": "good",
+            "fan_2_state": "good",
+            "model_no": "C9400-PWR-3200AC",
+            "status": "active",
+            "type": "ac"
+          },
+          "PS2": {
+            "capacity": "3200 W",
+            "fan_1_state": "good",
+            "fan_2_state": "good",
+            "model_no": "C9400-PWR-3200AC",
+            "status": "active",
+            "type": "ac"
+          }
+        }
+      }
+    }
+  }
+}
